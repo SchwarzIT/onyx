7ffbe367f5c01cab5087e2849a2d8e3d0547efea
Lars Rickert <lars.rickert@mail.schwarz>
2024-01-05 12:23:54 +0100

    feat(storybook-utils): use decorator for v-models instead of render function

--- packages/storybook-utils/src/actions.ts
+++ packages/storybook-utils/src/actions.ts
@@ -1,7 +1,12 @@
-import { useArgs } from "@storybook/client-api";
+import { useArgs } from "@storybook/preview-api";
 import type { ArgTypes, Meta } from "@storybook/vue3";
 import { deepmerge } from "deepmerge-ts";
-import type { DefineStorybookActionsAndVModelsOptions, ExtractVueEventNames } from ".";
+import { isReactive, reactive, watch } from "vue";
+import type {
+  ArrayElement,
+  DefineStorybookActionsAndVModelsOptions,
+  ExtractVueEventNames,
+} from ".";
 
 /**
  * Utility to define Storybook meta for a given Vue component which will take care of defining argTypes for
@@ -32,18 +37,7 @@ export const defineStorybookActionsAndVModels = <T>(
       ...defineActions(options.events),
       ...{}, // this is needed to fix a type issue
     },
-    decorators: [
-      (story, context) => {
-        // provide the `updateArgs` function so we can use it in the render() function below
-        const [_, updateArgs] = useArgs();
-        return story({ ...context, updateArgs });
-      },
-    ],
-    /**
-     * Define a custom render function will provide event handlers for all v-models
-     * so that the Storybook controls are updated live when the user interacts with the component
-     */
-    render: defineRenderFunctionForVModels(options),
+    decorators: [withVModelDecorator(options.events)],
   } satisfies Meta<T>;
 
   return deepmerge(options, defaultMeta);
@@ -75,66 +69,67 @@ export const defineActions = <T>(events: ExtractVueEventNames<T>[]): ArgTypes =>
 };
 
 /**
- * Defines a custom render function that will provide event handlers for all v-models
+ * Defines a custom decorator that will implement event handlers for all v-models
  * so that the Storybook controls are updated live when the user interacts with the component
+ *
+ * @example
+ * ```ts
+ * import Input from './Input.vue';
+ *
+ * {
+ *   decorators: [withVModelDecorator<typeof Input>(["update:modelValue"])]
+ * }
+ * ```
  */
-const defineRenderFunctionForVModels = <T>(
-  options: Pick<DefineStorybookActionsAndVModelsOptions<T>, "component" | "events">,
-): Meta<T>["render"] => {
-  const componentName = options.component.__name;
-  const vModelEvents = options.events.filter((event) => event.startsWith("update:"));
+export const withVModelDecorator = <T>(
+  events: ExtractVueEventNames<T>[],
+): ArrayElement<Meta<T>["decorators"]> => {
+  return (story, ctx) => {
+    const vModelEvents = events.filter((event) => event.startsWith("update:"));
+    if (!vModelEvents.length) return story();
 
-  // if the component does not have any v-models, we don't need to specify a custom render function
-  if (!vModelEvents.length || !componentName) return;
+    const [args, updateArgs] = useArgs();
 
-  return (args, context) => {
-    /**
-     * Mapped event handlers, e.g. `["update:modelValue"]` will be changed to:
-     *
-     * @example
-     * ```ts
-     * const eventHandler = {
-     *    updateModelValue: (value) => updateArgs({ modelValue: value })
-     * }
-     * ```
-     */
-    const eventHandlers = vModelEvents.reduce<Record<string, () => void>>((handler, event) => {
-      const methodName = vModelEventToMethodName(event);
-      handler[methodName] = (value?: unknown) =>
-        context.updateArgs({ [event.replace("update:", "")]: value });
-      return handler;
-    }, {});
+    // proxy the args so that we can add custom event handlers for all v-models below
+    // the destructuring is needed to fix the Storybook issue that the code preview is broken
+    const proxiedArgs = reactive({ ...args });
 
-    return {
-      components: { [componentName]: options.component },
-      setup: () => ({ args, ...eventHandlers }),
-      /**
-       * Add v-model event handlers to source code
-       *
-       * @example
-       * ```html
-       * <MyComponent v-bind="args" @update:modelValue="updateModelValue" />
-       * ```
-       */
-      template: `<${componentName} v-bind="args" ${vModelEvents
-        .map((event) => `@${event}="${vModelEventToMethodName(event)}"`)
-        .join(" ")} />`,
-    };
+    vModelEvents.forEach((eventName) => {
+      const propName = eventName.replace("update:", "");
+      const argName = `onUpdate:${propName}`;
+      const originalEventHandler = proxiedArgs[argName];
+
+      // emit event to update the value of the property inside the Storybook controls / table.
+      const updateVModel = (newValue?: unknown) => {
+        updateArgs({ [propName]: newValue });
+        proxiedArgs[propName] = newValue;
+      };
+
+      // proxy the original event handler to additionally call our custom `updateVModel` function
+      proxiedArgs[argName] = (...args: unknown[]) => {
+        const newValue = args.at(0);
+        updateVModel(newValue);
+        if (typeof originalEventHandler === "function") originalEventHandler(...args);
+      };
+    });
+
+    // since we are proxying the args, we need to watch the "original" `args` to
+    // reflect the changes when the user updates the control/input for a property inside Storybooks property table
+    if (isReactive(args)) {
+      watch(args, (newArgs) => {
+        for (const key in newArgs) {
+          if (key.startsWith("onUpdate:")) continue;
+          proxiedArgs[key] = newArgs[key];
+        }
+      });
+    }
+
+    ctx.args = proxiedArgs as typeof ctx.args;
+    return story(ctx);
   };
 };
 
 /** Capitalizes the first letter of the given string */
 const capitalizeFirstLetter = (value: string) => {
   return value.charAt(0).toUpperCase() + value.slice(1);
-};
-
-/**
- * Converts a v-model event name to a corresponding method name.
- * @example "update:modelValue" => "updateModelValue"
- */
-const vModelEventToMethodName = (eventName: string) => {
-  const index = eventName.indexOf(":");
-  return `${eventName.substring(0, index)}${eventName[
-    index + 1
-  ].toUpperCase()}${eventName.substring(index + 2)}`;
 };
