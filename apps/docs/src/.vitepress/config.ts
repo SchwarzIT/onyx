c48f8cbcbbd1fb1d6d0807204251a195aca86d57
Lars Rickert <lars.rickert@mail.schwarz>
2024-01-10 14:56:27 +0100

    feat: add package `@sit-onyx/vitepress-theme`

--- apps/docs/src/.vitepress/config.ts
+++ apps/docs/src/.vitepress/config.ts
@@ -52,6 +52,7 @@ export default defineConfig({
           { text: "Figma utilities", link: "/figma-utils" },
           { text: "Headless composables", link: "/headless" },
           { text: "Storybook utilities", link: "/storybook-utils" },
+          { text: "VitePress theme", link: "/vitepress-theme" },
         ],
       },
     ],
---
+++ packages/vitepress-theme/src/index.ts
@@ -0,0 +1,10 @@
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+
+import "./index.scss";
+
+const theme: Theme = {
+  extends: DefaultTheme,
+};
+
+export default theme;
