dbba92c4d45c6022b36f5da43829cfc2fec878c9
Lars Rickert <lars.rickert@mail.schwarz>
2024-01-12 17:12:51 +0100

    revert changes

--- packages/figma-utils/src/utils/generate.spec.ts
+++ packages/figma-utils/src/utils/generate.spec.ts
@@ -19,58 +19,46 @@ describe("generate.ts", () => {
   test("should generate as CSS", () => {
     const fileContent = generateAsCSS(mockData);
 
-    // due to issue between macOS and linux, we can not directly compare the whole
-    // string because this would fail in CI, so we check for each line individually
-    // we must also remove new lines at the start of each line
-    const expectedLines = [
-      "/**",
-      "* Do not edit directly.",
-      '* This file contains the specific variables for the "test-mode-1" theme.',
-      "* Imported from Figma API on Sun, 07 Jan 2024 12:42:00 GMT",
-      "*/",
-      ":root {",
-      "--test-1: #ffffff;",
-      "--test-2: 1rem;",
-      "--test-3: var(--test-2);",
-      "}",
-    ];
-
-    expectedLines.forEach((line) => expect(fileContent.includes(line)).toBe(true));
+    expect(fileContent).toBe(`/**
+ * Do not edit directly.
+ * This file contains the specific variables for the "test-mode-1" theme.
+ * Imported from Figma API on Sun, 07 Jan 2024 12:42:00 GMT
+ */
+:root {
+  --test-1: #ffffff;
+  --test-2: 1rem;
+  --test-3: var(--test-2);
+}
+`);
   });
 
   test("should generate as CSS with custom selector", () => {
     const fileContent = generateAsCSS(mockData, "html");
 
-    const expectedLines = [
-      "/**",
-      "* Do not edit directly.",
-      '* This file contains the specific variables for the "test-mode-1" theme.',
-      "* Imported from Figma API on Sun, 07 Jan 2024 12:42:00 GMT",
-      "*/",
-      "html.test-mode-1 {",
-      "--test-1: #ffffff;",
-      "--test-2: 1rem;",
-      "--test-3: var(--test-2);",
-      "}",
-    ];
-
-    expectedLines.forEach((line) => expect(fileContent.includes(line)).toBe(true));
+    expect(fileContent).toBe(`/**
+ * Do not edit directly.
+ * This file contains the specific variables for the "test-mode-1" theme.
+ * Imported from Figma API on Sun, 07 Jan 2024 12:42:00 GMT
+ */
+html.test-mode-1 {
+  --test-1: #ffffff;
+  --test-2: 1rem;
+  --test-3: var(--test-2);
+}
+`);
   });
 
   test("should generate as SCSS", () => {
     const fileContent = generateAsSCSS(mockData);
 
-    const expectedLines = [
-      "/**",
-      "* Do not edit directly.",
-      '* This file contains the specific variables for the "test-mode-1" theme.',
-      "* Imported from Figma API on Sun, 07 Jan 2024 12:42:00 GMT",
-      "*/",
-      "$test-1: #ffffff;",
-      "$test-2: 1rem;",
-      "$test-3: $test-2;",
-    ];
-
-    expectedLines.forEach((line) => expect(fileContent.includes(line)).toBe(true));
+    expect(fileContent).toBe(`/**
+ * Do not edit directly.
+ * This file contains the specific variables for the "test-mode-1" theme.
+ * Imported from Figma API on Sun, 07 Jan 2024 12:42:00 GMT
+ */
+$test-1: #ffffff;
+$test-2: 1rem;
+$test-3: $test-2;
+`);
   });
 });
