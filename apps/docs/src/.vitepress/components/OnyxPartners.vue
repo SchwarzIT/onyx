cb7786ceaff3050f665a934a4c0333e5924cb3e8
Lars Rickert <lars.rickert@mail.schwarz>
2024-01-15 13:26:06 +0100

    docs: add more partners

--- apps/docs/src/.vitepress/components/OnyxPartners.vue
+++ apps/docs/src/.vitepress/components/OnyxPartners.vue
@@ -8,8 +8,43 @@ const sponsors = [
     items: [
       {
         name: "STACKIT",
-        img: "/runs-on-stackit.svg",
+        img: new URL("../../assets/runs-on-stackit.svg", import.meta.url).href,
         url: "https://www.stackit.de",
+      },
+      {
+        name: "WaWi Nexus",
+        img: new URL("../../assets/wawi-logo.svg", import.meta.url).href,
+        url: "https://nexus.schwarz",
+      },
+      {
+        name: "CoreUI",
+        img: new URL("../../assets/coreui-logo.svg", import.meta.url).href,
+        url: "https://core-ui.schwarz",
+      },
+    ],
+  },
+  {
+    tier: "Tools & Technologies",
+    items: [
+      {
+        name: "Vue.js",
+        img: new URL("../../assets/vue-logo.svg", import.meta.url).href,
+        url: "https://vuejs.org",
+      },
+      {
+        name: "Figma",
+        img: "/icons/figma.svg",
+        url: "https://www.figma.com",
+      },
+      {
+        name: "Storybook",
+        img: new URL("../../assets/storybook-logo.svg", import.meta.url).href,
+        url: "https://storybook.js.org",
+      },
+      {
+        name: "VitePress",
+        img: new URL("../../assets/vitepress-logo.svg", import.meta.url).href,
+        url: "https://vitepress.dev",
       },
     ],
   },
