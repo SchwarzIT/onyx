0644ae7e5b6e668722b20fd755e40a6c8b023085
Lars Rickert <lars.rickert@mail.schwarz>
2024-01-16 14:27:11 +0100

    add eslint rule for import test and expect

---
+++ packages/eslint-plugin/src/rules/import-playwright-a11y.cjs
@@ -0,0 +1,27 @@
+/**
+ * @type {import('eslint').Rule.RuleModule}
+ */
+module.exports = {
+  meta: {
+    type: "problem",
+    docs: {
+      recommended: true,
+      description:
+        "Prevents importing test and except from Playwright when a custom fixture is available",
+    },
+  },
+  create: (context) => ({
+    ImportSpecifier(node) {
+      const hasFixture = ["expect", "test"].includes(node.imported.name);
+      if (!hasFixture) return;
+
+      if (node.parent.source.value === "@playwright/test") {
+        context.report({
+          node,
+          loc: node.loc,
+          message: `Import "${node.imported.name}" from "../../playwright-axe" instead because Onyx uses custom Playwright fixtures for providing a global configuration for accessability testing.`,
+        });
+      }
+    },
+  }),
+};
