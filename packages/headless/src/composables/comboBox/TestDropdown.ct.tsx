6e6d21837a6100d9385c6dd8441d657b27718fc4
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-01-17 15:31:23 +0100

    update test to be reusable

--- packages/headless/src/composables/comboBox/TestDropdown.ct.tsx
+++ packages/headless/src/composables/comboBox/TestDropdown.ct.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable playwright/expect-expect */
 import { expect, test } from "@playwright/experimental-ct-vue";
-import type { Locator } from "@playwright/test";
+import type { Locator, Page } from "@playwright/test";
 import TestDropdown from "./TestDropdown.vue";
 
-const expectToHaveFocus = async (locator: Locator) => {
+const expectToHaveFocus = async (
+  locator: Locator,
+  messageOptions?: Parameters<typeof expect>[1],
+) => {
   const result = await locator.evaluateHandle((element) => element === document.activeElement);
-  await expect(result.jsonValue()).resolves.toBeTruthy();
+  await expect(result.jsonValue(), messageOptions).resolves.toBeTruthy();
 };
 
 /**
@@ -15,41 +19,72 @@ test("combobox", async ({ mount, page }) => {
   const listbox = page.getByRole("listbox");
   const combobox = page.getByRole("combobox");
   const button = page.getByRole("button");
+  const options = page.getByRole("option");
 
-  // Typically, the initial state of a combobox is collapsed.
-  await expect(listbox).toBeHidden();
+  await comboboxTesting(page, listbox, combobox, button, options);
+});
 
-  // In the collapsed state, only the combobox element and a separate, optional popup control button are visible.
-  await expect(combobox).toBeVisible();
-  await expect(button).toBeVisible();
+/**
+ * Based on https://w3c.github.io/aria/#combobox
+ */
+const comboboxTesting = async (
+  page: Page,
+  listbox: Locator,
+  combobox: Locator,
+  button: Locator,
+  options: Locator,
+) => {
+  await expect(listbox, "Typically, the initial state of a combobox is collapsed.").toBeHidden();
+
+  await expect(combobox, "In the collapsed state, the combobox element is visible.").toBeVisible();
+  await expect(
+    button,
+    "In the collapsed state, the optional button element is visible.",
+  ).toBeVisible();
 
   // A combobox is said to be expanded when both the combobox element showing its current value and
   // its associated popup element are visible.
   await button.click();
-  await expect(combobox).toBeVisible();
-  await expect(listbox).toBeVisible();
+  await expect(
+    combobox,
+    "A combobox is said to be expanded when the combobox element shows its current value",
+  ).toHaveValue("");
+  await expect(
+    listbox,
+    "A combobox is said to be expanded when the associated popup is visible",
+  ).toBeVisible();
   await button.click();
 
-  // Authors MUST set aria-expanded to true on an element with role combobox when it is expanded and false when it is collapsed.
-  await expect(combobox).toHaveAttribute("aria-expanded", "false");
+  await expect(
+    combobox,
+    "Authors MUST set aria-expanded to false when it is collapsed.",
+  ).toHaveAttribute("aria-expanded", "false");
   await button.click();
-  await expect(combobox).toHaveAttribute("aria-expanded", "true");
+  await expect(
+    combobox,
+    "Authors MUST set aria-expanded to true when it is expanded.",
+  ).toHaveAttribute("aria-expanded", "true");
   await button.click();
 
   // If the user interface includes an additional icon that allows the visibility of the popup to be controlled via pointer and touch events,
   // authors SHOULD ensure that element has role button, that it is focusable but not included in the page Tab sequence,
   // and that it is not a descendant of the element with role combobox.
   await button.focus();
-  await expectToHaveFocus(button);
-
-  await expect(button).toHaveAttribute("tabindex", "-1");
-  await expect(combobox.getByRole("button")).toHaveCount(0);
+  await expectToHaveFocus(button, "authors SHOULD ensure that the button is focusable");
+  await expect(
+    button,
+    "authors SHOULD ensure that the button is not included in the page Tab sequence",
+  ).toHaveAttribute("tabindex", "-1");
+  await expect(
+    combobox.getByRole("button"),
+    "authors SHOULD ensure that the button is not a descendant of the element with role combobox",
+  ).toHaveCount(0);
 
   // In addition, to be keyboard accessible, authors SHOULD provide keyboard mechanisms for moving focus between the combobox element and elements contained in the popup.
   // For example, one common convention is that Down Arrow moves focus from the input to the first focusable descendant of the popup element.
   // If the popup element supports aria-activedescendant, in lieu of moving focus, such keyboard mechanisms can control the value of aria-activedescendant on the combobox element.
   // When a descendant of the popup element is active, authors MAY set aria-activedescendant on the combobox to a value that refers to the active element within the popup while focus remains on the combobox element.
-  const firstElement = page.getByRole("option").first();
+  const firstElement = options.first();
 
   await combobox.focus();
   await page.keyboard.press("ArrowDown");
@@ -57,6 +92,12 @@ test("combobox", async ({ mount, page }) => {
 
   const firstId = await (await firstElement.elementHandle())!.getAttribute("id");
   expect(typeof firstId).toBe("string");
-  await expect(combobox).toHaveAttribute("aria-activedescendant", firstId as string);
-  await expectToHaveFocus(combobox);
-});
+  await expect(
+    combobox,
+    "When a descendant of the popup element is active, authors MAY set aria-activedescendant on the combobox to a value that refers to the active element within the popup.",
+  ).toHaveAttribute("aria-activedescendant", firstId as string);
+  await expectToHaveFocus(
+    combobox,
+    "When a descendant of the popup element is active, authors MAY ensure that the focus remains on the combobox element",
+  );
+};
