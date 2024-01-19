2a7d78f7ad1b23b5d3b55957a5c2c717641c77eb
boppli <linda.bopp@mail.schwarz>
2024-01-19 12:28:20 +0100

    feat: create pure vitest app

---
+++ apps/alpha-test-app/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { provideI18n } from "sit-onyx";
+import deDE from "sit-onyx/locales/de-DE.json";
+import { useI18n } from "vue-i18n";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const { locale } = useI18n();
+provideI18n({ locale, messages: { "de-DE": deDE } });
+</script>
+
+<template>
+  <div>
+    {{ locale }}
+    <button @click="locale = 'de-DE'">Deutsch</button>
+    <button @click="locale = 'en-US'">English</button>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ apps/alpha-test-app/src/main.ts
@@ -0,0 +1,17 @@
+import "sit-onyx/style.css";
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createI18n } from "vue-i18n";
+import enUS from "./i18n/locales/en-US.json";
+import deDE from "./i18n/locales/de-DE.json";
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "en-US",
+  messages: { "en-US": enUS, "de-DE": deDE },
+});
+
+// await import(`./locales/${locale}.json`);
+
+createApp(App).use(i18n).mount("#app");
