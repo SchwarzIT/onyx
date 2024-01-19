c4b27e3108b030cf3cdc16d424b99fbc8a1cb1eb
boppli <linda.bopp@mail.schwarz>
2024-01-19 15:17:04 +0100

    fix: use typecast to resolve error

--- apps/alpha-test-app/src/App.vue
+++ apps/alpha-test-app/src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import { provideI18n } from "sit-onyx";
+import { OnyxTranslations, provideI18n } from "sit-onyx";
 import deDE from "sit-onyx/locales/de-DE.json";
 import { useI18n } from "vue-i18n";
 import HelloWorld from "./components/HelloWorld.vue";
 
 const { t, locale } = useI18n();
-locale.value = "en-US";
-provideI18n({ locale, messages: { "de-DE": deDE } });
+provideI18n({ locale, messages: { "de-DE": deDE as OnyxTranslations } });
 </script>
 
 <template>
