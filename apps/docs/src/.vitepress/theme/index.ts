ea42360cd50171ef56e4b5e369c76435c76a3c5f
Lars Rickert <lars.rickert@mail.schwarz>
2024-01-19 15:31:38 +0100

    revert unused changes

--- apps/docs/src/.vitepress/theme/index.ts
+++ apps/docs/src/.vitepress/theme/index.ts
@@ -1,17 +1,6 @@
 import OnyxTheme from "@sit-onyx/vitepress-theme";
-import type { Theme } from "vitepress";
-import ColorPalette from "../components/ColorPalette.vue";
 
 // custom styles must be imported after the theme
 import "./theme.scss";
 
-const theme: Theme = {
-  extends: OnyxTheme,
-  enhanceApp: (ctx) => {
-    // globally provide frequently used custom components so
-    // they can be used without importing them individually
-    ctx.app.component("ColorPalette", ColorPalette);
-  },
-};
-
-export default theme;
+export default OnyxTheme;
