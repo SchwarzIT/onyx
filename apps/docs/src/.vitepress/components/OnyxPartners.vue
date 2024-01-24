fe64f3637170464aa2a2979801d72c159e7c6494
Lars Rickert <lars.rickert@mail.schwarz>
2024-01-24 10:12:41 +0100

    docs: add missing images

--- apps/docs/src/.vitepress/components/OnyxPartners.vue
+++ apps/docs/src/.vitepress/components/OnyxPartners.vue
@@ -1,49 +1,29 @@
 <script lang="ts" setup>
 import { VPHomeSponsors } from "vitepress/theme";
+import coreUILogo from "../../assets/coreui-logo.svg";
+import stackitLogo from "../../assets/runs-on-stackit.svg";
+import storybookLogo from "../../assets/storybook-logo.svg";
+import vitepressLogo from "../../assets/vitepress-logo.svg";
+import vueLogo from "../../assets/vue-logo.svg";
+import waWiLogo from "../../assets/wawi-logo.svg";
 
 const partners = [
   {
     tier: "Special Partners",
     size: "big",
     items: [
-      {
-        name: "STACKIT",
-        img: new URL("../../assets/runs-on-stackit.svg", import.meta.url).href,
-        url: "https://www.stackit.de",
-      },
-      {
-        name: "WaWi Nexus",
-        img: new URL("../../assets/wawi-logo.svg", import.meta.url).href,
-      },
-      {
-        name: "CoreUI",
-        img: new URL("../../assets/coreui-logo.svg", import.meta.url).href,
-      },
+      { name: "STACKIT", img: stackitLogo, url: "https://www.stackit.de" },
+      { name: "WaWi Nexus", img: waWiLogo },
+      { name: "CoreUI", img: coreUILogo },
     ],
   },
   {
     tier: "Tools & Technologies",
     items: [
-      {
-        name: "Vue.js",
-        img: new URL("../../assets/vue-logo.svg", import.meta.url).href,
-        url: "https://vuejs.org",
-      },
-      {
-        name: "Figma",
-        img: "/icons/figma.svg",
-        url: "https://www.figma.com",
-      },
-      {
-        name: "Storybook",
-        img: new URL("../../assets/storybook-logo.svg", import.meta.url).href,
-        url: "https://storybook.js.org",
-      },
-      {
-        name: "VitePress",
-        img: new URL("../../assets/vitepress-logo.svg", import.meta.url).href,
-        url: "https://vitepress.dev",
-      },
+      { name: "Vue.js", img: vueLogo, url: "https://vuejs.org" },
+      { name: "Figma", img: "/icons/figma.svg", url: "https://www.figma.com" },
+      { name: "Storybook", img: storybookLogo, url: "https://storybook.js.org" },
+      { name: "VitePress", img: vitepressLogo, url: "https://vitepress.dev" },
     ],
   },
 ];
