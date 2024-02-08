eb35c813e584ec66bf4334c68578472e479cfeea
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-02-08 18:15:09 +0100

    add selected argType

--- packages/sit-onyx/src/components/OnyxRadioButton/OnyxRadioButton.stories.ts
+++ packages/sit-onyx/src/components/OnyxRadioButton/OnyxRadioButton.stories.ts
@@ -14,6 +14,7 @@ const meta: Meta<typeof OnyxRadioButton> = {
       isReadonly: { type: "boolean" },
       isLoading: { type: "boolean" },
       isDisabled: { type: "boolean" },
+      selected: { type: "boolean" },
     },
   }),
 };
