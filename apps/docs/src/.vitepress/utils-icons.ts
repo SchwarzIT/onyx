070ccda5cb31703f0a791ffc4bff4b8062121e57
Linda Bopp <151019977+BoppLi@users.noreply.github.com>
2024-02-09 09:02:40 +0100

    docs(icons): Implement search functionality in icon library (#263)
    Implements a search functionality for our icon library documentation.
    Supports lower case search for icon name, icon alias and category name.

---
+++ apps/docs/src/.vitepress/utils-icons.ts
@@ -0,0 +1,34 @@
+import { ICON_CATEGORIES } from "@sit-onyx/icons";
+import { capitalize } from "vue";
+
+const getIconContextData = (iconName: string, allIconContents: Record<string, string>) => {
+  const parts = iconName.split("-");
+  // bell-disabled => `Bell Disabled`
+  const tooltipName = parts.map((word) => capitalize(word)).join(" ");
+  // bell-disabled => `bellDisabled`
+  const importName = parts
+    .map((word, index) => {
+      if (index === 0) return word;
+      return capitalize(word);
+    })
+    .join("");
+  // svg content for OnyxIcon `icon` prop
+  const content =
+    allIconContents[`../../../node_modules/@sit-onyx/icons/src/assets/${iconName}.svg`];
+  return { tooltipName, importName, content };
+};
+
+// Collects all needed icon context data and provides them as a list.
+export const getEnrichedIconCategoryList = (allIconContents: Record<string, string>) =>
+  Object.entries(ICON_CATEGORIES).map(([category, icons]) => ({
+    name: category,
+    icons: icons.map((icon) => ({
+      ...icon,
+      ...getIconContextData(icon.iconName, allIconContents),
+      metadata: {
+        ...icon.metadata,
+        // make the searchability more reliable
+        aliases: icon.metadata.aliases?.map((alias) => alias.toLowerCase()),
+      },
+    })),
+  }));
