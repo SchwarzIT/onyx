bb0267430f95966161aafd7ac78bdf1db628f378
Lars Rickert <lars.rickert@mail.schwarz>
2024-02-13 13:52:56 +0100

    feat: add basic `OnyxCheckbox` component (#278)
    #220
    
    Add the basic `OnyxCheckbox` component with support for checked and
    indeterminate state.

---
+++ packages/sit-onyx/src/components/OnyxCheckbox/types.ts
@@ -0,0 +1,14 @@
+export type OnyxCheckboxProps = {
+  /**
+   * Whether the checkbox is checked.
+   */
+  modelValue?: boolean;
+  /**
+   * Label to show.
+   */
+  label?: string;
+  /**
+   * If `true`, an indeterminate indicator is shown.
+   */
+  indeterminate?: boolean;
+};
