751bb6f8672a97a32253fa15627ab3279eee681c
boppli <linda.bopp@mail.schwarz>
2024-02-13 11:17:02 +0100

    docs: create tooltip on hover or focus-visible

--- apps/docs/src/.vitepress/components/OnyxIconLibrary.vue
+++ apps/docs/src/.vitepress/components/OnyxIconLibrary.vue
@@ -40,12 +40,12 @@ const filteredCategories = computed(() => {
       <h3 class="category__headline">{{ category.name }}</h3>
 
       <div class="category__icons">
-        <div class="category__icon" v-for="icon in category.icons" :key="icon.iconName">
+        <div class="icon" v-for="icon in category.icons" :key="icon.iconName" tabindex="0">
           <OnyxIcon
             :icon="icon.content"
-            :title="icon.tooltipName"
             :color="icon.metadata.deprecated ? 'secondary' : 'currentColor'"
           />
+          <div class="icon__tooltip">{{ icon.tooltipName }}</div>
         </div>
       </div>
     </section>
@@ -65,19 +65,41 @@ const filteredCategories = computed(() => {
     grid-template-columns: repeat(auto-fit, 3.5rem);
     grid-template-rows: repeat(auto-fit, 3.5rem);
   }
+}
 
-  &__icon {
-    padding: var(--onyx-spacing-md);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    border: 0.0625rem solid transparent;
+.icon {
+  height: 3.5rem;
+  width: 3.5rem;
+  padding: var(--onyx-spacing-md);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: 0.0625rem solid transparent;
+  position: relative;
 
-    &:hover {
-      border-radius: var(--onyx-radius-md);
-      border: 0.0625rem solid var(--onyx-color-base-neutral-300);
-      background: var(--onyx-color-base-background-blank);
+  &__tooltip {
+    position: absolute;
+    top: 4rem;
+    z-index: 1;
+    visibility: hidden;
+
+    padding: var(--onyx-spacing-4xs) var(--onyx-spacing-2xs);
+    border-radius: var(--onyx-radius-xs);
+    background: var(--onyx-color-base-neutral-900);
+    color: var(--onyx-color-icon-neutral-inverted);
+    font-size: 0.8125rem;
+  }
+
+  &:hover,
+  &:focus-visible {
+    border-radius: var(--onyx-radius-md);
+    border: 0.0625rem solid var(--onyx-color-base-neutral-300);
+    background: var(--onyx-color-base-background-blank);
+    outline-style: unset;
+
+    .icon__tooltip {
+      visibility: inherit;
     }
   }
 }
