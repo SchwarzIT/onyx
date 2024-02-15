0172e9f984acb55390eff73911368740076306c1
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-02-15 14:08:35 +0100

    update docs to use plugin

--- packages/sit-onyx/src/utils/plugin.ts
+++ packages/sit-onyx/src/utils/plugin.ts
@@ -5,9 +5,13 @@ export type OnyxPluginOptions = {
   i18n?: ProvideI18nOptions;
 };
 
+/**
+ * Use this plugin to set up onyx.
+ * You can provide and overwrite default translations.
+ */
 export const createOnyx = (options: OnyxPluginOptions): Plugin<[]> => ({
   install: (app) => {
-    provideI18n(options.i18n, app);
+    provideI18n(app, options.i18n);
     const i18n = app.runWithContext(() => injectI18n());
     watchEffect(() => syncGlobalOptionalText(i18n.t.value("optional")));
   },
