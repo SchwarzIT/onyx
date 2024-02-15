8ad01f2c477f19363fb60436ebccc7328f3431b6
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-02-15 08:45:57 +0100

    review: add comment

--- packages/sit-onyx/src/types/utils.ts
+++ packages/sit-onyx/src/types/utils.ts
@@ -3,6 +3,7 @@
  */
 export type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]> } : T;
 
+// TODO: remove. Used as a temporary workaround to ensure duplicated props stay in sync
 export type Equals<A extends never> = A;
 export type TypeEqualityGuard<A, B, C = Required<A>, D = Required<B>> =
   | Exclude<C, D>
