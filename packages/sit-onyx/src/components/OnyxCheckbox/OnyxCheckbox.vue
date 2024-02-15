95b6e75595d5c2a142c366f5052710ec1fcd1a41
Lars Rickert <lars.rickert@mail.schwarz>
2024-02-15 09:47:30 +0100

    feat(OnyxCheckbox): add property `required` (#305)
    #220
    
    Add property `required` to the OnyxCheckbox component.

--- packages/sit-onyx/src/components/OnyxCheckbox/OnyxCheckbox.vue
+++ packages/sit-onyx/src/components/OnyxCheckbox/OnyxCheckbox.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import type { OnyxCheckboxProps } from "./types";
 
 const props = withDefaults(defineProps<OnyxCheckboxProps>(), {
@@ -7,6 +7,7 @@ const props = withDefaults(defineProps<OnyxCheckboxProps>(), {
   label: "",
   indeterminate: false,
   disabled: false,
+  required: false,
 });
 
 const emit = defineEmits<{
@@ -18,6 +19,9 @@ const isChecked = computed({
   get: () => props.modelValue,
   set: (value) => emit("update:modelValue", value),
 });
+
+/** True if the user has interacted with the checkbox once. */
+const isTouched = ref(false);
 </script>
 
 <template>
@@ -26,9 +30,12 @@ const isChecked = computed({
       <input
         v-model="isChecked"
         class="onyx-checkbox__input"
+        :class="{ 'onyx-checkbox__input--touched': isTouched }"
         type="checkbox"
         :indeterminate="props.indeterminate"
         :disabled="props.disabled"
+        :required="props.required"
+        @blur="isTouched = true"
       />
     </div>
 
@@ -37,6 +44,29 @@ const isChecked = computed({
 </template>
 
 <style lang="scss">
+@mixin define-hover-border($state, $color) {
+  .onyx-checkbox__input#{$state} {
+    border-color: var(--onyx-color-base-#{$color}-300);
+  }
+}
+
+@mixin define-focus-ring($state, $color) {
+  .onyx-checkbox__container:has(.onyx-checkbox__input#{$state}) {
+    background-color: var(--onyx-color-base-#{$color}-200);
+  }
+}
+
+@mixin define-checked-background($state, $color) {
+  &#{$state} {
+    border-color: var(--onyx-color-base-#{$color}-500);
+    background-color: var(--onyx-color-base-#{$color}-500);
+
+    &:hover {
+      background-color: var(--onyx-color-base-#{$color}-300);
+    }
+  }
+}
+
 .onyx-checkbox {
   font-family: var(--onyx-font-family);
   color: var(--onyx-color-text-icons-neutral-intense);
@@ -45,14 +75,18 @@ const isChecked = computed({
   cursor: pointer;
 
   &:hover {
-    .onyx-checkbox__input:enabled {
-      border-color: var(--onyx-color-base-primary-300);
+    @include define-hover-border($state: ":enabled", $color: primary);
+
+    &:has(.onyx-checkbox__input--touched) {
+      @include define-hover-border($state: ":invalid", $color: danger);
     }
   }
 
   &:has(&__input:focus-visible) {
-    .onyx-checkbox__container {
-      background-color: var(--onyx-color-base-primary-200);
+    @include define-focus-ring($state: ":enabled", $color: primary);
+
+    &:has(.onyx-checkbox__input--touched) {
+      @include define-focus-ring($state: ":invalid", $color: danger);
     }
   }
 
@@ -85,14 +119,10 @@ const isChecked = computed({
 
     &:checked,
     &:indeterminate {
-      border-color: var(--onyx-color-base-primary-500);
-      background-color: var(--onyx-color-base-primary-500);
+      @include define-checked-background(":enabled", primary);
 
-      &:enabled {
-        &:hover {
-          border-color: var(--onyx-color-base-primary-300);
-          background-color: var(--onyx-color-base-primary-300);
-        }
+      &.onyx-checkbox__input--touched {
+        @include define-checked-background(":invalid", danger);
       }
 
       &:disabled {
@@ -102,6 +132,12 @@ const isChecked = computed({
 
     &:disabled {
       border-color: var(--onyx-color-base-neutral-300);
+    }
+
+    &--touched {
+      &:invalid {
+        border-color: var(--onyx-color-base-danger-500);
+      }
     }
 
     &:checked {
@@ -120,6 +156,16 @@ const isChecked = computed({
     margin: 0;
     font-size: 1rem;
     line-height: 1.5rem;
+  }
+
+  &:has(&__input:required) {
+    .onyx-checkbox__label {
+      &::after {
+        content: "*";
+        color: var(--onyx-color-text-icons-danger-intense);
+        padding-left: var(--onyx-spacing-5xs);
+      }
+    }
   }
 }
 </style>
