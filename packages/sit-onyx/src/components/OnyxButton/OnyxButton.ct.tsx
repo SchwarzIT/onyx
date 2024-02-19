67f6fcda9be9202d5d5fec1258e130088bcd5c19
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-02-19 11:27:56 +0200

    Add test

--- packages/sit-onyx/src/components/OnyxButton/OnyxButton.ct.tsx
+++ packages/sit-onyx/src/components/OnyxButton/OnyxButton.ct.tsx
@@ -1,5 +1,6 @@
 import { expect, test } from "../../playwright-axe";
 import OnyxButton from "./OnyxButton.vue";
+import happyIcon from "@sit-onyx/icons/emoji-happy-2.svg?raw";
 
 test("should render", async ({ mount, makeAxeBuilder }) => {
   // ARRANGE
@@ -23,6 +24,22 @@ test("should display correctly when disabled", async ({ mount, makeAxeBuilder })
   // ASSERT
   await expect(component).toContainText("Button");
   await expect(component).toBeDisabled();
+
+  // ACT
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+  // ASSERT
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+test("should render button with icon", async ({ mount, makeAxeBuilder }) => {
+  // ARRANGE
+  const component = await mount(
+    <OnyxButton label="Button" variation="secondary" icon={happyIcon} />,
+  );
+
+  // ASSERT
+  await expect(component).toContainText("Button");
 
   // ACT
   const accessibilityScanResults = await makeAxeBuilder().analyze();
