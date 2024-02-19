d58ee4ee45979196254da92a4a70903d32a62a76
Lars Rickert <lars.rickert@mail.schwarz>
2024-02-19 17:01:14 +0100

    update storybook

--- packages/sit-onyx/src/index.ts
+++ packages/sit-onyx/src/index.ts
@@ -1,18 +1,18 @@
 import "@/styles/index.scss";
 
+export { default as OnyxButton } from "@/components/OnyxButton/OnyxButton.vue";
+export * from "@/components/OnyxButton/types";
 export * from "@/components/OnyxCheckbox/types";
 export { default as OnyxCheckboxGroup } from "@/components/OnyxCheckboxGroup/OnyxCheckboxGroup.vue";
 export * from "@/components/OnyxCheckboxGroup/types";
 export { default as OnyxHeadline } from "@/components/OnyxHeadline/OnyxHeadline.vue";
-export { default as OnyxButton } from "@/components/OnyxButton/OnyxButton.vue";
 export * from "@/components/OnyxHeadline/types";
-export * from "@/components/OnyxButton/types";
 export { default as OnyxIcon } from "@/components/OnyxIcon/OnyxIcon.vue";
 export * from "@/components/OnyxIcon/types";
 export { default as TestInput } from "@/components/TestInput/TestInput.vue";
+export type { OnyxTranslations, ProvideI18nOptions } from "@/i18n";
 export * from "@/types/colors";
 export * from "@/types/i18n";
 export * from "@/types/utils";
-export { type OnyxTranslations } from "@/i18n";
 
 export { createOnyx } from "@/utils/plugin";
