b73832dfc13bd172b1cabbeb3dda655e737bda98
boppli <linda.bopp@mail.schwarz>
2024-02-19 20:46:45 +0100

    wip: layers demo for temp overlay + global info tile

--- apps/alpha-test-app/src/views/LayersDemoView.vue
+++ apps/alpha-test-app/src/views/LayersDemoView.vue
@@ -1,14 +1,15 @@
 <script lang="ts" setup>
 import chevronRightSmall from "@sit-onyx/icons/chevron-right-small.svg?raw";
-import type { OnyxIcon } from "sit-onyx";
+import chevronLeftSmall from "@sit-onyx/icons/chevron-left-small.svg?raw";
+import { OnyxIcon } from "sit-onyx";
 import { ref } from "vue";
 import LayersDemoOptionsMolecule from "../components/molecules/LayersDemoOptionsMolecule.vue";
 
 const options = ref({
-  showTempOverlay: false, // todo needs different page structure
+  showTempOverlay: false,
   showMobileFlyIn: false,
   showFlyout: false,
-  showNotification: false, // todo
+  showNotification: false,
   showToast: false, // todo
   showTooltip: false, // todo
   showPopover: false,
@@ -21,6 +22,8 @@ const options = ref({
   detailFooter: true,
 });
 
+const tempOverlayOpen = ref(true);
+
 const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor ${index}`);
 </script>
 
@@ -32,15 +35,11 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
       <LayersDemoOptionsMolecule v-model="options" />
     </div>
     <div class="page">
+      <div v-if="options.showNotification" class="demo notification">
+        Global Info Tile / Notification
+      </div>
       Scrollable page content
 
-      <div v-if="options.showTempOverlay" class="backdrop">
-        <span class="demo temp-overlay">
-          >>
-          <!-- todo find out why onyxicon does not work -->
-          <OnyxIcon :icon="chevronRightSmall" />
-        </span>
-      </div>
       <p>
         <label>
           Demo Drop Down:
@@ -52,7 +51,6 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
           </span>
         </label>
       </p>
-      <div v-if="options.showNotification" class="demo notification">info tile / notification</div>
       <div v-if="options.showToast" class="demo toast">toast</div>
       <div v-if="options.detailFooter" class="demo footer">detail footer</div>
       <div v-if="options.showTooltip" class="demo tooltip">tooltip</div>
@@ -76,6 +74,20 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
               <LayersDemoOptionsMolecule v-model="options" />
             </div>
             <div v-if="options.fullFooter" class="demo footer">full footer</div>
+          </div>
+        </div>
+      </Teleport>
+
+      <Teleport v-if="options.showTempOverlay" class="backdrop" to="body">
+        <div v-if="tempOverlayOpen" class="backdrop"></div>
+        <div class="temp-overlay">
+          <button class="demo temp-overlay__hinge" @click="tempOverlayOpen = !tempOverlayOpen">
+            <OnyxIcon v-if="tempOverlayOpen" :icon="chevronRightSmall" />
+            <OnyxIcon v-else :icon="chevronLeftSmall" />
+          </button>
+          <div v-if="tempOverlayOpen" class="demo temp-overlay__content">
+            Temp overlay
+            <LayersDemoOptionsMolecule v-model="options" />
           </div>
         </div>
       </Teleport>
@@ -118,6 +130,7 @@ body {
 .footer {
   position: fixed;
   bottom: 0;
+  z-index: 10;
   width: 100%;
   height: 50px;
 }
@@ -175,5 +188,32 @@ body {
   min-width: 100px;
   right: 0;
   left: 0;
+}
+
+.temp-overlay {
+  position: absolute;
+  z-index: 10;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  display: flex;
+
+  &__content {
+    width: 250px;
+  }
+
+  &__hinge {
+    max-height: 50px;
+    margin: auto 0;
+  }
+}
+
+.notification {
+  position: sticky;
+  top: 0;
+  z-index: 20;
+
+  border: 1px solid #eee;
+  padding: 24px;
 }
 </style>
