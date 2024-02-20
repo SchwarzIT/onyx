91ec4eb05c78f2c87928fc8f217c63b8a9ca833b
boppli <linda.bopp@mail.schwarz>
2024-02-20 10:04:51 +0100

    wip: implement tooltip + add descriptions

--- apps/alpha-test-app/src/views/LayersDemoView.vue
+++ apps/alpha-test-app/src/views/LayersDemoView.vue
@@ -5,19 +5,26 @@ import { OnyxIcon } from "sit-onyx";
 import { ref } from "vue";
 import LayersDemoOptionsMolecule from "../components/molecules/LayersDemoOptionsMolecule.vue";
 
+/** TASK LIST
+ * - scroll bars (see without any footer)
+ * - without long content: footer does not stick to bottom
+ * - try alternative approach without grid?
+ */
+
 const options = ref({
   longPageContent: true,
 
+  showFlyout: false,
+  forceTooltip: false,
+
+  showPopover: false,
   showTempOverlay: false,
   showMobileFlyIn: false,
-  showFlyout: false,
+  fullSizePopup: false, // todo
+  showLoadingPage: false, // todo
+
   showNotification: false,
   showToast: false,
-  showTooltip: false, // todo
-  showPopover: false,
-  showLoadingPage: false, // todo
-  fullSizePopup: false, // todo
-
   // don't combine with detail footer
   fullFooter: false,
   // don't combine with full footer
@@ -37,10 +44,11 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
       'app--detail-footer': options.detailFooter,
     }"
   >
-    <!-- grid top row -->
+    <!----------- GRID top row ----------->
     <div class="demo top-nav">
       Top nav bar |
 
+      <!-- demo flyout -->
       <label>
         Demo Drop Down:
         <span class="flyout-parent">
@@ -50,22 +58,37 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
           </div>
         </span>
       </label>
+
+      |
+      <!-- demo tooltip -->
+      <span class="demo tooltip" :class="{ 'tooltip--forced': options.forceTooltip }">
+        Tooltip parent
+        <div class="tooltip__text">Hello world Hello world Hello world Hello world</div>
+      </span>
     </div>
 
-    <!-- grid left col -->
+    <!----------- GRID left col ----------->
     <div class="demo side-bar">
       Sidebar
       <LayersDemoOptionsMolecule v-model="options" />
+
+      <!-- demo tooltip -->
+      <div class="demo tooltip" :class="{ 'tooltip--forced': options.forceTooltip }">
+        Tooltip parent
+        <div class="tooltip__text">Hello world Hello world Hello world Hello world</div>
+      </div>
     </div>
 
-    <!-- grid right col -->
+    <!----------- GRID right col ----------->
     <div class="page" :class="{ 'page--full-height': !options.fullFooter }">
+      <!-- demo global info tile -->
       <div v-if="options.showNotification" class="demo notification">
         Global Info Tile / Notification
       </div>
 
       Scrollable page content
 
+      <!-- demo flyout -->
       <p>
         <label>
           Demo Drop Down:
@@ -77,41 +100,60 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
           </span>
         </label>
       </p>
-      <div v-if="options.showTooltip" class="demo tooltip">tooltip</div>
+
+      <!-- demo tooltip -->
+      <div class="demo tooltip" :class="{ 'tooltip--forced': options.forceTooltip }">
+        Tooltip parent
+        <div class="tooltip__text">Hello world Hello world Hello world Hello world</div>
+      </div>
+
+      <!-- demo page content -->
       <template v-if="options.longPageContent">
         <p v-for="content in muchContent" :key="content">{{ content }}</p>
       </template>
     </div>
 
-    <!-- grid bottom row -->
+    <!----------- GRID bottom row ----------->
     <div
       v-if="options.detailFooter || options.fullFooter || options.showToast"
       class="footer"
       :class="{ 'footer--detail': options.detailFooter }"
     >
+      <!-- demo toast -->
       <template v-if="options.showToast">
         <div class="demo toast">Toast message 1</div>
         <div v-if="options.showToast" class="demo toast">Toast message 2</div>
       </template>
+
+      <!-- demo bottom-bar -->
       <div v-if="options.detailFooter || options.fullFooter" class="demo bottom-bar">
         detail footer
       </div>
     </div>
 
-    <!-- grid full overlay except top bar -->
+    <!----------- GRID full overlay excluding top bar ----------->
     <div v-if="options.showLoadingPage" class="demo page-overlay">Loading page overlay</div>
   </div>
 
+  <!----------- APP cover overlays ----------->
+  <!-- demo popover / modal -->
   <Teleport v-if="options.showPopover" to="body">
     <div class="backdrop">
       <div class="demo popover">
-        Popover content
+        Popover / Modal content
 
         <LayersDemoOptionsMolecule v-model="options" />
+
+        <!-- demo tooltip -->
+        <div class="demo tooltip" :class="{ 'tooltip--forced': options.forceTooltip }">
+          Tooltip parent
+          <div class="tooltip__text">Hello world Hello world Hello world Hello world</div>
+        </div>
       </div>
     </div>
   </Teleport>
 
+  <!-- demo mobile fly-in -->
   <Teleport v-if="options.showMobileFlyIn" to="body">
     <div class="backdrop">
       <div class="demo mobile-fly-in" :class="{ 'mobile-fly-in--split': options.fullFooter }">
@@ -125,7 +167,8 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
     </div>
   </Teleport>
 
-  <Teleport v-if="options.showTempOverlay" class="backdrop" to="body">
+  <!-- demo temporary overlay from right -->
+  <Teleport v-if="options.showTempOverlay" to="body">
     <div v-if="tempOverlayOpen" class="backdrop"></div>
     <div class="temp-overlay">
       <button class="demo temp-overlay__hinge" @click="tempOverlayOpen = !tempOverlayOpen">
@@ -135,6 +178,12 @@ const muchContent = new Array(100).fill("").map((_, index) => `Lorem ipsum dolor
       <div v-if="tempOverlayOpen" class="demo temp-overlay__content">
         Temp overlay
         <LayersDemoOptionsMolecule v-model="options" />
+
+        <!-- demo tooltip -->
+        <div class="demo tooltip" :class="{ 'tooltip--forced': options.forceTooltip }">
+          Tooltip parent
+          <div class="tooltip__text">Hello world Hello world Hello world Hello world</div>
+        </div>
       </div>
     </div>
   </Teleport>
@@ -165,6 +214,17 @@ body {
 }
 .bottom-bar {
   height: 50px;
+}
+.tooltip {
+  padding: 4px;
+  width: fit-content;
+  &__text {
+    padding: 4px;
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: white;
+    width: 150px;
+  }
 }
 
 // positions
@@ -244,7 +304,7 @@ body {
   left: 0;
   bottom: 0;
   right: 0;
-  z-index: 10;
+  z-index: 20;
   position: absolute;
 }
 
@@ -263,7 +323,7 @@ body {
 
 .temp-overlay {
   position: absolute;
-  z-index: 10;
+  z-index: 20;
   right: 0;
   bottom: 0;
   top: 0;
@@ -283,5 +343,24 @@ body {
   position: sticky;
   top: 0;
   z-index: 20;
+}
+
+.tooltip {
+  position: relative;
+
+  &:hover,
+  &--forced {
+    .tooltip__text {
+      visibility: inherit;
+    }
+  }
+
+  &__text {
+    position: absolute;
+    top: 28px;
+    left: 0;
+    z-index: 5;
+    visibility: hidden;
+  }
 }
 </style>
