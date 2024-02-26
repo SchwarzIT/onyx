321e0ec62b23da5ac8c0fa933b98b93cda1d9fc7
Lars Rickert <lars.rickert@mail.schwarz>
2024-02-26 13:03:28 +0100

    update spinner

--- packages/sit-onyx/src/components/OnyxLoadingIndicator/CircleSpinner.vue
+++ packages/sit-onyx/src/components/OnyxLoadingIndicator/CircleSpinner.vue
@@ -1,5 +1,3 @@
-<script lang="ts" setup></script>
-
 <template>
   <svg class="onyx-circle-spinner" viewBox="0 0 50 50">
     <circle class="onyx-circle-spinner__circle" cx="50%" cy="50%" r="45%" />
@@ -7,13 +5,30 @@
 </template>
 
 <style lang="scss">
+@use "sass:math";
+
+// must be the same as "r" attribute of the <circle> in HTML (without %)
+$radius: 45;
+
+/**
+* Calculates the stroke length for the circle spinner.
+* @param $percentage: Stroke percentage that should be covered.
+*/
+@function calculate-stroke-length($percentage) {
+  $length: $percentage * math.$pi * $radius;
+  @return math.div($length, 100);
+}
+
 .onyx-circle-spinner {
   :where(&) {
     --indicator-size: 24px;
     --stroke-color: currentColor;
   }
 
-  $timing: ease-out;
+  $stroke-gap: 25; // stroke gap in percent, same as in icon loading-circle.svg
+  $timing: cubic-bezier(0.4, 0.35, 0.7, 0.8);
+  $default-stroke-dasharray: calculate-stroke-length(100 - $stroke-gap)
+    calculate-stroke-length($stroke-gap);
 
   width: var(--indicator-size);
   aspect-ratio: 1;
@@ -23,27 +38,36 @@
     fill: transparent;
     stroke-width: 2px;
     stroke-linecap: round;
-    stroke-dasharray: 300%;
     stroke: var(--stroke-color);
-    stroke-dashoffset: 90%;
+    stroke-dasharray: $default-stroke-dasharray;
+
+    // add dashoffset so the gap starts from top, counter-clockwise, just like the loading-circle.svg icon
+    stroke-dashoffset: calculate-stroke-length(25);
+
     animation: onyx-circle-spinner-stroke var(--onyx-duration-lg) $timing infinite;
   }
 
+  /**
+  * Animates the circle by spinning/rotating it.
+  */
   @keyframes onyx-circle-spinner {
     100% {
       transform: rotate(1turn);
     }
   }
 
+  /**
+  * Animates the stroke gap from 25% to 75% to 25%.
+  */
   @keyframes onyx-circle-spinner-stroke {
     0% {
-      stroke-dashoffset: 90%;
+      stroke-dasharray: $default-stroke-dasharray;
     }
-    80% {
-      stroke-dashoffset: 270%;
+    75% {
+      stroke-dasharray: calculate-stroke-length(25) calculate-stroke-length(75);
     }
     100% {
-      stroke-dashoffset: 90%;
+      stroke-dasharray: $default-stroke-dasharray;
     }
   }
 }
