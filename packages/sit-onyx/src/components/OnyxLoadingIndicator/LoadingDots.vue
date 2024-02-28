259c1de98da66d2f3e9a0f85539a638552293095
Lars Rickert <lars.rickert@mail.schwarz>
2024-02-28 13:20:54 +0100

    feat(OnyxLoadingIndicator): animate dots

--- packages/sit-onyx/src/components/OnyxLoadingIndicator/LoadingDots.vue
+++ packages/sit-onyx/src/components/OnyxLoadingIndicator/LoadingDots.vue
@@ -1,12 +1,40 @@
-<script lang="ts" setup>
-import loadingDots from "@sit-onyx/icons/loading-dots.svg?raw";
-import OnyxIcon from "../OnyxIcon/OnyxIcon.vue";
-</script>
-
 <template>
-  <OnyxIcon :icon="loadingDots" />
+  <svg class="onyx-loading-dots" viewBox="0 0 32 32">
+    <circle class="onyx-loading-dots__circle" cx="5" cy="16" r="3" />
+    <circle class="onyx-loading-dots__circle" cx="16" cy="16" r="2" />
+    <circle class="onyx-loading-dots__circle" cx="27" cy="16" r="3" />
+  </svg>
 </template>
 
 <style lang="scss">
-// TODO: animate icon
+.onyx-loading-dots {
+  --duration: var(--onyx-duration-md);
+
+  width: 24px;
+  aspect-ratio: 1;
+  fill: currentColor;
+
+  &__circle {
+    animation: onyx-loading-dots var(--duration) infinite alternate;
+
+    &:nth-child(2) {
+      animation-delay: calc(var(--duration) / 2);
+    }
+
+    &:last-child {
+      animation-delay: var(--duration);
+    }
+  }
+
+  @keyframes onyx-loading-dots {
+    0% {
+      opacity: 1;
+    }
+
+    50%,
+    100% {
+      opacity: 0.2;
+    }
+  }
+}
 </style>
