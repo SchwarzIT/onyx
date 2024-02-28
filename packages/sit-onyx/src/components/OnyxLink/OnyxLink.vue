6324d32399fe31c643cf066349920edbf65c9799
Lars Rickert <lars.rickert@mail.schwarz>
2024-02-28 11:01:49 +0100

    feat: add `OnyxLink` component (#426)
    relates to #237
    
    Add a default `OnyxLink` component. States like hover, active etc. will
    be implemented in a follow-up PR once they are clarified with UX.

---
+++ packages/sit-onyx/src/components/OnyxLink/OnyxLink.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import type { OnyxLinkProps } from "./types";
+
+const props = withDefaults(defineProps<OnyxLinkProps>(), {
+  target: "_self",
+});
+
+const emit = defineEmits<{
+  /**
+   * Emitted when the link is opened (via click or keyboard).
+   */
+  click: [];
+}>();
+
+defineSlots<{
+  /**
+   * Link label.
+   */
+  default(props: Record<string, never>): unknown;
+}>();
+</script>
+
+<template>
+  <a
+    class="onyx-link"
+    :href="props.href"
+    :target="props.target"
+    :rel="props.target === '_blank' ? 'noopener noreferrer' : undefined"
+    @click="emit('click')"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<style lang="scss">
+.onyx-link {
+  text-decoration: underline;
+  color: var(--onyx-color-text-icons-primary-intense);
+
+  // other styles like size, font family etc. should be inherited
+  // so we don't define them here
+}
+</style>
