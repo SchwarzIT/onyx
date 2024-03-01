95edff8625d306caa019475ab74e0192ab7b18be
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-01 18:04:42 +0100

    show dots when animations are disabled

--- packages/sit-onyx/src/components/OnyxLoadingIndicator/LoadingDots.vue
+++ packages/sit-onyx/src/components/OnyxLoadingIndicator/LoadingDots.vue
@@ -9,15 +9,23 @@
 $duration: var(--onyx-duration-lg);
 
 $max-shrink: calc(-1 * var(--dot-size) / 2);
-$default-box-shadow: 0 var(--dot-size) 0 $max-shrink;
+
+/**
+* Defines the box shadow.
+* @param $shrink: Shrink factor. Set to 0 (default) to not shrink the box shadow.
+*/
+@mixin define-box-shadow($shrink: 0) {
+  box-shadow: 0 var(--dot-size) 0 $shrink;
+}
 
 @mixin define-dot() {
+  @include define-box-shadow;
+
   position: absolute;
   background-color: transparent;
   width: var(--dot-size);
   aspect-ratio: 1;
   border-radius: var(--onyx-radius-full);
-  box-shadow: $default-box-shadow;
   animation: onyx-loading-dots $duration infinite linear;
 }
 
@@ -34,16 +42,19 @@ $default-box-shadow: 0 var(--dot-size) 0 $max-shrink;
   position: relative;
 
   &__dot {
-    @include define-dot();
-    top: calc(50% - 1.5 * var(--dot-size));
-    left: calc(50% - 0.5 * var(--dot-size));
+    @include define-dot;
     animation-delay: calc($duration / 6);
+
+    // center horizontally
+    left: calc(50% - 0.5 * var(--dot-size));
+
+    // center vertically + move the dot up by 1 dot size so it does not cover the box shadow
+    top: calc(50% - 1.5 * var(--dot-size));
 
     &::before,
     &::after {
       content: "";
-      display: inline-block;
-      @include define-dot();
+      @include define-dot;
     }
 
     $dot-offset: calc(-1.5 * var(--dot-size));
@@ -58,16 +69,19 @@ $default-box-shadow: 0 var(--dot-size) 0 $max-shrink;
     }
   }
 
+  /**
+  * Animates the box shadow of the dots with a pulsing effect (grow and shrink).
+  */
   @keyframes onyx-loading-dots {
     0% {
-      box-shadow: $default-box-shadow;
+      @include define-box-shadow(0);
     }
     30% {
-      // this will hide the dot (scale it to 0)
-      box-shadow: 0 var(--dot-size) 0 0;
+      // this will show the dot by setting the shrink to 0.
+      @include define-box-shadow($max-shrink);
     }
     60% {
-      box-shadow: $default-box-shadow;
+      @include define-box-shadow($max-shrink);
     }
   }
 }
