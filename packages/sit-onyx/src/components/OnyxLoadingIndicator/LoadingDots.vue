8df1c29bc437a598f91449d5bfd0fdf7317cbe10
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-01 17:48:25 +0100

    update CSS

--- packages/sit-onyx/src/components/OnyxLoadingIndicator/LoadingDots.vue
+++ packages/sit-onyx/src/components/OnyxLoadingIndicator/LoadingDots.vue
@@ -8,16 +8,13 @@
 /** Animation duration. */
 $duration: var(--onyx-duration-lg);
 
-/** Default (non-animated) size of a single dot. */
-$dot-size: calc(0.25 * var(--indicator-size));
-
-$max-shrink: calc(-1 * $dot-size / 2);
-$default-box-shadow: 0 $dot-size 0 $max-shrink;
+$max-shrink: calc(-1 * var(--dot-size) / 2);
+$default-box-shadow: 0 var(--dot-size) 0 $max-shrink;
 
 @mixin define-dot() {
   position: absolute;
   background-color: transparent;
-  width: $dot-size;
+  width: var(--dot-size);
   aspect-ratio: 1;
   border-radius: var(--onyx-radius-full);
   box-shadow: $default-box-shadow;
@@ -29,17 +26,17 @@ $default-box-shadow: 0 $dot-size 0 $max-shrink;
     --indicator-size: 24px;
   }
 
+  /** Default (non-animated) size of a single dot. */
+  --dot-size: calc(0.25 * var(--indicator-size));
+
   width: var(--indicator-size);
   aspect-ratio: 1;
   position: relative;
 
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-
   &__dot {
     @include define-dot();
-    top: calc(0.5 * $dot-size);
+    top: calc(50% - 1.5 * var(--dot-size));
+    left: calc(50% - 0.5 * var(--dot-size));
     animation-delay: calc($duration / 6);
 
     &::before,
@@ -49,7 +46,7 @@ $default-box-shadow: 0 $dot-size 0 $max-shrink;
       @include define-dot();
     }
 
-    $dot-offset: calc(-1.5 * $dot-size);
+    $dot-offset: calc(-1.5 * var(--dot-size));
 
     &::before {
       left: $dot-offset;
@@ -67,7 +64,7 @@ $default-box-shadow: 0 $dot-size 0 $max-shrink;
     }
     30% {
       // this will hide the dot (scale it to 0)
-      box-shadow: 0 $dot-size 0 0;
+      box-shadow: 0 var(--dot-size) 0 0;
     }
     60% {
       box-shadow: $default-box-shadow;
