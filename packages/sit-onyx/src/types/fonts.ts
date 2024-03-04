59cf2de4723f484857d8eb4d8ef07b6e485e8bd0
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-04 16:45:57 +0100

    add docs

--- packages/sit-onyx/src/types/fonts.ts
+++ packages/sit-onyx/src/types/fonts.ts
@@ -1,5 +1,7 @@
 export const TEXT_SIZES = ["small", "default", "large"] as const;
 export type TextSize = (typeof TEXT_SIZES)[number];
 
+// when adding a new truncation type, make sure to also add CSS for it
+// inside styles/fonts.scss
 export const TRUNCATION_TYPES = ["ellipsis", "multiline"] as const;
 export type TruncationType = (typeof TRUNCATION_TYPES)[number];
