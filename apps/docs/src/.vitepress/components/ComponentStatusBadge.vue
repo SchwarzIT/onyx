cafa489d72b0151ce7a7d2d5b9757a8e141325ac
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-04 09:53:12 +0100

    add ComponentStatusBadge

---
+++ apps/docs/src/.vitepress/components/ComponentStatusBadge.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup>
+import check from "@sit-onyx/icons/check.svg?raw";
+import OnyxIcon from "~components/OnyxIcon/OnyxIcon.vue";
+
+export type ComponentStatus = "implemented" | "in-progress" | "planned";
+
+const props = defineProps<{
+  status: ComponentStatus;
+  /** If true, a status label will be shown. */
+  showLabel?: boolean;
+}>();
+
+const icons = {
+  implemented: {
+    icon: check,
+    label: "Implemented",
+  },
+  "in-progress": {
+    icon: check,
+    label: "In progress",
+  },
+  planned: {
+    icon: check,
+    label: "Planned",
+  },
+} satisfies Record<ComponentStatus, { icon: string; label: string }>;
+</script>
+
+<template>
+  <div class="status">
+    <div class="status__icon" :class="[`status__icon--${props.status}`]">
+      <OnyxIcon :icon="icons[props.status].icon" size="16px" />
+    </div>
+
+    <span v-if="props.showLabel">{{ icons[props.status].label }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.status {
+  --icon-size: 1.5rem;
+
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  gap: var(--onyx-spacing-2xs);
+
+  &__icon {
+    width: var(--icon-size);
+    height: var(--icon-size);
+    border-radius: var(--onyx-radius-full);
+    color: var(--onyx-color-text-icons-neutral-intense);
+    background-color: var(--onyx-color-base-neutral-200);
+
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    &--implemented {
+      color: var(--onyx-color-text-icons-success-intense);
+      background-color: var(--onyx-color-base-success-200);
+    }
+
+    &--in-progress {
+      color: var(--onyx-color-text-icons-warning-intense);
+      background-color: var(--onyx-color-base-warning-200);
+    }
+  }
+}
+</style>
