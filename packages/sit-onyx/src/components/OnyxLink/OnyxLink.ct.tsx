a3bc165c43dd2b4a423d85634b1df0e765b9d6f8
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-04 14:16:09 +0100

    feat(OnyxLink): add CSS states and external link icon (#442)
    relates to #237
    
    Add hover, active, focus-visible and visited state to the OnyxLink.
    Also added an icon for external links which can be enabled/disabled via
    the `withExternalIcon` property. By default it will show automatically
    when the link is external (`http://` or `https://`)

---
+++ packages/sit-onyx/src/components/OnyxLink/OnyxLink.ct.tsx
@@ -0,0 +1,26 @@
+import { test } from "../../playwright-axe";
+import { createScreenshotsForAllStates } from "../../utils/playwright";
+import OnyxLink from "./OnyxLink.vue";
+
+const STATES = {
+  state: ["default", "external"],
+  focusState: ["", "hover", "focus-visible"],
+} as const;
+
+test(
+  "State screenshot testing",
+  createScreenshotsForAllStates(STATES, "link", async ({ state, focusState }, mount, page) => {
+    const component = await mount(
+      <OnyxLink
+        href={state === "external" ? "https://onyx.schwarz" : "#"}
+        style="font-family: var(--onyx-font-family);"
+      >
+        Click me
+      </OnyxLink>,
+    );
+
+    if (focusState === "focus-visible") await page.keyboard.press("Tab");
+    if (focusState === "hover") await component.hover();
+    return component;
+  }),
+);
