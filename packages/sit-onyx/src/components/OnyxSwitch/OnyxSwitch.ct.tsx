406c03e6f8d9356c21663e41b7f4d148b29feb2b
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-03-05 16:36:10 +0200

    Apply code suggestions

--- packages/sit-onyx/src/components/OnyxSwitch/OnyxSwitch.ct.tsx
+++ packages/sit-onyx/src/components/OnyxSwitch/OnyxSwitch.ct.tsx
@@ -12,13 +12,8 @@ test("should render unchecked", async ({ mount, makeAxeBuilder }) => {
   // ASSERT
   expect(accessibilityScanResults.violations).toEqual([]);
 
-  const onyxSwitch = component.getByRole("checkbox");
-
-  // ACT
-  await onyxSwitch.hover();
-
   // ASSERT
-  await expect(onyxSwitch).not.toBeChecked();
+  await expect(component).not.toBeChecked();
 });
 
 test("should render checked", async ({ mount, makeAxeBuilder }) => {
@@ -31,24 +26,8 @@ test("should render checked", async ({ mount, makeAxeBuilder }) => {
   // ASSERT
   expect(accessibilityScanResults.violations).toEqual([]);
 
-  const onyxSwitch = component.getByRole("checkbox");
-
-  // ACT
-  await onyxSwitch.hover();
-
   // ASSERT
-  await expect(onyxSwitch).toBeChecked();
-});
-
-test("should render required", async ({ mount, makeAxeBuilder }) => {
-  // ARRANGE
-  await mount(<OnyxSwitch label="Required" required />);
-
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder().analyze();
-
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
+  await expect(component).toBeChecked();
 });
 
 const disabledTestCases = [{ name: "unchecked" }, { name: "checked", modelValue: true }];
@@ -72,50 +51,10 @@ for (const testCase of disabledTestCases) {
 
     const switches = await component.getByRole("checkbox").all();
 
-    // ACT
-    await switches[1].hover();
-    await switches[2].focus();
-
     // ASSERT
     for (const onyxSwitch of switches) {
       await expect(onyxSwitch).toBeDisabled();
     }
-  });
-}
-
-const invalidTestCases = [
-  { name: "unchecked" },
-  { name: "unchecked (disabled)", disabled: true },
-  { name: "indeterminate (disabled)", indeterminate: true, disabled: true },
-];
-for (const testCase of invalidTestCases) {
-  test(`should render invalid (${testCase.name})`, async ({ mount, makeAxeBuilder }) => {
-    // ARRANGE
-    const component = await mount(
-      <div style="display: grid; width: max-content;">
-        <OnyxSwitch label={`Invalid ${testCase.name}`} disabled={testCase.disabled} required />
-        <OnyxSwitch label="Hover" disabled={testCase.disabled} required />
-        <OnyxSwitch label="Focus visible" disabled={testCase.disabled} required />
-      </div>,
-    );
-
-    // ACT
-    const accessibilityScanResults = await makeAxeBuilder().analyze();
-
-    // ASSERT
-    expect(accessibilityScanResults.violations).toEqual([]);
-
-    const switches = await component.getByRole("checkbox").all();
-
-    // invalid only shows if checkbox is touched
-    for (const onyxSwitch of switches) {
-      await onyxSwitch.focus();
-      await onyxSwitch.blur();
-    }
-
-    // ACT
-    await switches[1].hover();
-    await switches[2].focus();
   });
 }
 
@@ -145,12 +84,12 @@ test(
   "State screenshot testing",
   createScreenshotsForAllStates(
     STATES,
-    "checkbox",
+    "switch",
     async ({ select, state, labeled, focusState }, mount, page) => {
       const component = await mount(
         <OnyxSwitch
           modelValue={select === "selected"}
-          label={labeled === "labeled" ? "label" : ""}
+          label={labeled === "labeled" ? "label" : undefined}
           disabled={state === "disabled"}
           required={state === "required"}
         />,
