827a893b0ad87c1bf070137d607c4a8b55a8e61e
Linda Bopp <151019977+BoppLi@users.noreply.github.com>
2024-03-05 17:57:15 +0100

    feat: create app and page layout components (#470)
    - create `OnyxAppLayout` and `OnyxPageLayout`
    - use the layout components in
    `apps/alpha-test-app/src/views/LayoutDemoView.vue`
    - cut out demo components out of `LayoutDemoView` in
    `apps/alpha-test-app/src/components/layout-demo` folder
    - use onyx components for headlines, buttons and setting controls of
    `LayoutDemoView`

---
+++ apps/alpha-test-app/src/components/layout-demo/MobileBottomFlyInDemo.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { OnyxHeadline } from "sit-onyx";
+import { FooterDemo } from ".";
+defineProps<{
+  showFooter?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="mobile-fly-in" :class="{ 'mobile-fly-in--split': showFooter }">
+    <div>
+      <OnyxHeadline is="h2">Mobile fly-in</OnyxHeadline>
+      <slot></slot>
+    </div>
+    <FooterDemo v-if="showFooter" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.mobile-fly-in {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  max-height: fit-content;
+  margin: auto;
+
+  border: var(--onyx-1px-in-rem) solid var(--onyx-color-base-neutral-300);
+  background-color: var(--onyx-color-base-background-blank);
+}
+</style>
