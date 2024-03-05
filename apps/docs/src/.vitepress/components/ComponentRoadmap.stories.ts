1f95e5c7261b3d21e1393e32f64f42c47844e2b7
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-05 11:57:49 +0100

    docs(home): update component roadmap (#462)
    relates to #294
    
    Update the component roadmap on the VitePress home page.

---
+++ apps/docs/src/.vitepress/components/ComponentRoadmap.stories.ts
@@ -0,0 +1,31 @@
+import { defineStorybookActionsAndVModels } from "@sit-onyx/storybook-utils";
+import type { Meta, StoryObj } from "@storybook/vue3";
+import ComponentRoadmap from "./ComponentRoadmap.vue";
+
+const meta: Meta<typeof ComponentRoadmap> = {
+  title: "roadmap/ComponentRoadmap",
+  ...defineStorybookActionsAndVModels({
+    component: ComponentRoadmap,
+    events: [],
+  }),
+};
+
+export default meta;
+type Story = StoryObj<typeof ComponentRoadmap>;
+
+const today = new Date();
+
+export const Default = {
+  args: {
+    components: Array.from({ length: 25 }, (_, index) => {
+      const id = index + 1;
+
+      return {
+        name: `Component ${id}`,
+        status: id % 2 === 0 ? "in-progress" : "implemented",
+        dueDate: id % 2 === 0 ? new Date(today.getFullYear(), today.getMonth() + index) : undefined,
+        href: "#",
+      };
+    }),
+  },
+} satisfies Story;
--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "~components": getFilePath("../../packages/sit-onyx/src/components"),
+      "@": getFilePath("../../packages/sit-onyx/src"),
     },
   },
 });
