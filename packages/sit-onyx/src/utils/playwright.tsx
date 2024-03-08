89bcc1ac0c6b3718c765e455e0a6b9ab19b597ff
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-08 14:28:17 +0100

    fix(OnyxButton): add active state

--- packages/sit-onyx/src/utils/playwright.tsx
+++ packages/sit-onyx/src/utils/playwright.tsx
@@ -123,6 +123,8 @@ export const createScreenshotsForAllStates =
 
       await page.getByRole("document").focus(); // reset focus
       await page.getByRole("document").hover(); // reset mouse
+      await page.mouse.up(); // reset mouse
+      await page.mouse.up();
       const component = await caseBuilder(testCase, wrappedMount, page);
 
       // ASSERT
