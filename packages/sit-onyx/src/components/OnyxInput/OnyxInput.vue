e4e19834cd79e8f5d724c915fdf2c06b3dd4c0f8
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-11 09:20:44 +0100

    feat(OnyxInput): add additional properties (#539)
    relates to #189
    
    Add the following (native HTML) properties to the OnyxInout:
    - autocapitalize
    - autocomplete
    - autofocus
    - name
    - pattern
    
    ## Checklist
    
    - [x] The added / edited code has been documented with
    [JSDoc](https://jsdoc.app/about-getting-started)
    - [x] A changeset is added with `npx changeset add` if your changes
    should be released as npm package (because they affect the library
    usage)

--- packages/sit-onyx/src/components/OnyxInput/OnyxInput.vue
+++ packages/sit-onyx/src/components/OnyxInput/OnyxInput.vue
@@ -5,6 +5,7 @@ import type { OnyxInputProps } from "./types";
 const props = withDefaults(defineProps<OnyxInputProps>(), {
   modelValue: "",
   type: "text",
+  autocapitalize: "sentences",
 });
 
 const emit = defineEmits<{
@@ -38,6 +39,11 @@ const handleChange = (event: Event) => {
   const inputValue = (event.target as HTMLInputElement).value;
   emit("change", inputValue);
 };
+
+const patternSource = computed(() => {
+  if (props.pattern instanceof RegExp) return props.pattern.source;
+  return props.pattern;
+});
 </script>
 
 <template>
@@ -47,15 +53,25 @@ const handleChange = (event: Event) => {
     </span>
 
     <div class="onyx-input__wrapper">
+      <!-- eslint-disable vuejs-accessibility/no-autofocus -
+         We want to provide the flexibility to have the autofocus property.
+         The JSDoc description includes a warning that it should be used carefully.
+      -->
       <input
         v-model="value"
         class="onyx-input__native"
         :placeholder="props.placeholder"
         :type="props.type"
+        :autocapitalize="props.autocapitalize"
+        :autocomplete="props.autocomplete"
+        :autofocus="props.autofocus"
+        :name="props.name"
+        :pattern="patternSource"
         @change="handleChange"
         @focus="emit('focus')"
         @blur="emit('blur')"
       />
+      <!-- eslint-enable vuejs-accessibility/no-autofocus -->
     </div>
   </label>
 </template>
