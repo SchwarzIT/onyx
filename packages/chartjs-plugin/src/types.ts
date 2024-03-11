784a0016a9c7f4b9c11f01e9dd26db59da887693
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-11 16:12:04 +0100

    add eslint ignore comment

--- packages/chartjs-plugin/src/types.ts
+++ packages/chartjs-plugin/src/types.ts
@@ -19,7 +19,7 @@ export const ONYX_COLORS = [
 export type OnyxColor = (typeof ONYX_COLORS)[number];
 
 declare module "chart.js" {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- we don't need the generic here but it is needed because of "All declarations of 'PluginOptionsByType' must have identical type parameters." error
   interface PluginOptionsByType<TType extends ChartType> {
     /**
      * onyx chart options. Can be set to `false` for disabling the plugin.
