b6618756d1c749e45c72c5556697d4fe2daff358
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-12 15:16:35 +0100

    setup screenshot tests

---
+++ packages/chartjs-plugin/playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from "@playwright/experimental-ct-vue";
+import vue from "@vitejs/plugin-vue";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: "./src",
+  testMatch: `**/*.ct.tsx`,
+  snapshotDir: "./playwright/snapshots",
+  // custom snapshotPathTemplate to remove the testFileName folder that we don't want
+  snapshotPathTemplate: "{snapshotDir}/{testFileDir}/{arg}-{projectName}-{platform}{ext}",
+  // we don't want to update snapshots on the local machine of each developer.
+  // if you want to update snapshots for your branch, use the corresponding GitHub action:
+  // https://github.com/SchwarzIT/onyx/actions/workflows/playwright-screenshots.yml
+  ignoreSnapshots: !process.env.CI,
+  updateSnapshots: "none",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI, // fail build on CI if we left test.only in the source code
+  retries: process.env.CI ? 2 : 0, // retry on CI only
+  reporter: [["html", { open: "never" }]],
+  use: {
+    trace: process.env.CI ? "retain-on-failure" : "off",
+    video: process.env.CI ? "retain-on-failure" : "off",
+    ctPort: 3100,
+    ctViteConfig: {
+      plugins: [vue()],
+    },
+  },
+  /* Configure projects for major browsers */
+  projects: [
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    { name: "webkit", use: { ...devices["Desktop Safari"] } },
+  ],
+});
