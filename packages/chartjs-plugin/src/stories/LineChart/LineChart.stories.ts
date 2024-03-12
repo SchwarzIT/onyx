4dbeae848a1223836bb82d176ef014b6f448110f
Lars Rickert <dev@lars-rickert.de>
2024-03-12 19:25:35 +0100

    add more example charts

--- packages/chartjs-plugin/src/stories/LineChart/LineChart.stories.ts
+++ packages/chartjs-plugin/src/stories/LineChart/LineChart.stories.ts
@@ -1,16 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
-import { ONYX_COLORS } from "sit-onyx/types";
 import LineChart from "./LineChart.vue";
 
 const meta: Meta<typeof LineChart> = {
   title: "examples/LineChart",
   component: LineChart,
-  argTypes: {
-    color: {
-      options: ONYX_COLORS,
-      control: { type: "select" },
-    },
-  },
 };
 
 export default meta;
---
+++ packages/chartjs-plugin/src/stories/RadarChart/RadarChart.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import RadarChart from "./RadarChart.vue";
+
+const meta: Meta<typeof RadarChart> = {
+  title: "examples/RadarChart",
+  component: RadarChart,
+};
+
+export default meta;
+type Story = StoryObj<typeof RadarChart>;
+
+export const Default = { args: {} } satisfies Story;
