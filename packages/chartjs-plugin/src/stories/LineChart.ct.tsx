f904526b5bb655d34feff5c501a0c3a3b0fad0fe
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-12 15:42:33 +0100

    update test

--- packages/chartjs-plugin/src/stories/LineChart.ct.tsx
+++ packages/chartjs-plugin/src/stories/LineChart.ct.tsx
@@ -2,37 +2,31 @@ import { expect, test } from "@playwright/experimental-ct-vue";
 import type { Page } from "@playwright/test";
 import LineChart from "./LineChart.vue";
 
-const THEMES = ["light", "dark"] as const;
-
 const switchTheme = async (page: Page, theme: string) => {
   await page.evaluate((theme) => document.documentElement.classList.add(theme), theme);
 };
 
-THEMES.forEach((theme) => {
-  test(`should render in ${theme} theme`, async ({ mount, page }) => {
-    // ARRANGE
-    const component = await mount(LineChart);
+test(`should render in light and dark theme`, async ({ mount, page }) => {
+  // ARRANGE
+  const component = await mount(LineChart);
 
-    // ACT
-    await switchTheme(page, theme);
+  // hover so that the tooltip is shown for the screenshot
+  await component.hover();
 
-    // hover so that the tooltip is shown for the screenshot
-    await component.hover();
+  // ASSERT
+  await expect(component).toHaveScreenshot("default-light.png");
 
-    // ASSERT
-    await expect(component).toHaveScreenshot(`default-${theme}.png`);
-  });
+  // ACT
+  await switchTheme(page, "dark");
+
+  // ASSERT
+  await expect(component).toHaveScreenshot("default-dark.png");
 });
 
-THEMES.forEach((theme) => {
-  test(`should render with custom color (${theme})`, async ({ mount, page }) => {
-    // ARRANGE
-    const component = await mount(LineChart, { props: { color: "info" } });
+test(`should render with custom color`, async ({ mount }) => {
+  // ARRANGE
+  const component = await mount(LineChart, { props: { color: "info" } });
 
-    // ACT
-    await switchTheme(page, theme);
-
-    // ASSERT
-    await expect(component).toHaveScreenshot(`default-${theme}.png`);
-  });
+  // ASSERT
+  await expect(component).toHaveScreenshot("custom-color.png");
 });
