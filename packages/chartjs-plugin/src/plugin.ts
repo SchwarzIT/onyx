3a34d7dd0ba9db74def0956203460182c5c6eacc
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-13 10:34:02 +0100

    move getDatasetColors to utils file

--- packages/chartjs-plugin/src/plugin.ts
+++ packages/chartjs-plugin/src/plugin.ts
@@ -10,7 +10,6 @@ import {
   type ScaleOptionsByType,
   type ScaleTypeRegistry,
 } from "chart.js";
-import type { OnyxColor } from "sit-onyx/types";
 import { getCSSVariableValue, hexToRgb } from "./utils";
 
 /**
@@ -76,35 +75,6 @@ export const registerOnyxPlugin = (chart: typeof Chart) => {
       },
     };
   });
-};
-
-/**
- * Utility for easily styling a Chart.js dataset with a specific onyx color.
- *
- * @example
- * ```ts
- * const chartData: ChartData<"line"> = {
- *   labels: ["A", "B", "C"],
- *   datasets: [
- *     {
- *       label: "Dataset A",
- *       data: [1, 2, 3],
- *       ...getDatasetColors("primary"),
- *     },
- *   ],
- * };
- * ```
- */
-export const getDatasetColors = (
-  color: OnyxColor | `quantitatives-${100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900}`,
-) => {
-  const cssVariable = `--onyx-color-base-${color.startsWith("quantitatives-") ? color : `${color}-500`}`;
-  const baseColor = getCSSVariableValue(cssVariable);
-
-  return {
-    borderColor: baseColor,
-    backgroundColor: `rgba(${hexToRgb(baseColor)}, 0.3)`,
-  };
 };
 
 const plugin: Plugin<ChartType, undefined> = {
