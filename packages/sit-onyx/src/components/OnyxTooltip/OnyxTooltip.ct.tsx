550137869ae7db1e0f9f7baf4d616f1e7efd9113
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-18 18:19:58 +0100

    add tests

---
+++ packages/sit-onyx/src/components/OnyxTooltip/OnyxTooltip.ct.tsx
@@ -0,0 +1,54 @@
+import { executeScreenshotsForAllStates, mockPlaywrightIcon } from "@/utils/playwright";
+import { expect, test } from "../../playwright-axe";
+import OnyxTooltip from "./OnyxTooltip.vue";
+
+test("should pass accessibility checks", async ({ mount, makeAxeBuilder }) => {
+  // ARRANGE
+  await mount(
+    <div style={{ width: "max-content", padding: "3rem 1rem" }}>
+      <OnyxTooltip text="Test tooltip">Test slot content</OnyxTooltip>
+    </div>,
+  );
+
+  // ACT
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+  // ASSERT
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+const STATES = {
+  variant: ["default", "icon"],
+  color: ["neutral", "danger"],
+  text: ["default", "long", "fitParent"],
+} as const;
+
+test.describe("state screenshot tests", () => {
+  executeScreenshotsForAllStates(STATES, "tooltip", async ({ variant, color, text }, mount) => {
+    const component = await mount(
+      <div
+        style={{
+          width: "max-content",
+          padding: text === "long" ? "4rem 6rem" : "3rem 1rem",
+        }}
+      >
+        <OnyxTooltip
+          text={text === "long" ? "Lorem ipsum dolor sit amet ".repeat(5) : "Test tooltip"}
+          color={color}
+          icon={variant === "icon" ? mockPlaywrightIcon : undefined}
+        >
+          <span
+            style={{
+              fontFamily: "var(--onyx-font-family)",
+              color: "var(--onyx-color-text-icons-neutral-intense)",
+            }}
+          >
+            Test slot content
+          </span>
+        </OnyxTooltip>
+      </div>,
+    );
+
+    return component;
+  });
+});
