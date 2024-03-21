5b97189feae02dc5fc9b9ab0fa1cc55563f1ed6a
Lars Rickert <lars.rickert@mail.schwarz>
2024-03-21 17:31:57 +0100

    replace tooltip in demo app

--- apps/alpha-test-app/src/components/layout-demo/TooltipDemo.vue
+++ apps/alpha-test-app/src/components/layout-demo/TooltipDemo.vue
@@ -1,44 +1,17 @@
 <script lang="ts" setup>
-defineProps<{
+import { OnyxTooltip } from "sit-onyx";
+
+const props = defineProps<{
   forceTooltip?: boolean;
 }>();
 </script>
 
 <template>
-  <div class="tooltip-demo">
-    <div class="tooltip" :class="{ 'tooltip--forced': forceTooltip }">
-      Tooltip parent
-      <div class="tooltip__text">Hello world Hello world Hello world Hello world</div>
-    </div>
-  </div>
+  <OnyxTooltip
+    :trigger="props.forceTooltip != undefined ? props.forceTooltip : undefined"
+    text="Hi, I am the tooltip"
+    position="bottom"
+  >
+    Tooltip parent
+  </OnyxTooltip>
 </template>
-
-<style lang="scss" scoped>
-.tooltip-demo {
-  position: relative;
-}
-.tooltip {
-  padding: var(--onyx-spacing-4xs);
-  width: fit-content;
-
-  &__text {
-    padding: var(--onyx-spacing-4xs);
-    border-radius: var(--onyx-radius-sm);
-    background-color: rgba(0, 0, 0, 0.7);
-    color: var(--onyx-color-base-background-blank);
-    width: 9.375rem;
-    position: absolute;
-    top: 1.75rem;
-    left: 0;
-    z-index: var(--onyx-z-index-flyout);
-    visibility: hidden;
-  }
-
-  &:hover,
-  &--forced {
-    .tooltip__text {
-      visibility: inherit;
-    }
-  }
-}
-</style>
