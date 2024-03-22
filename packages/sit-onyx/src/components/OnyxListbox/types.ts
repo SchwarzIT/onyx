416db9ebdd46034367918ee4c1583eb7d1fefca8
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-03-22 13:22:00 +0100

    review add title change to comment

--- packages/sit-onyx/src/components/OnyxListbox/types.ts
+++ packages/sit-onyx/src/components/OnyxListbox/types.ts
@@ -15,7 +15,7 @@ export type OnyxListboxProps<TValue extends SelectionOptionValue = SelectionOpti
    */
   modelValue?: TValue;
   /**
-   * If true, the label will be visually hidden.
+   * If `true`, the label will be visually hidden and the `title` attribute will be set.
    * For accessibility / screen readers, the aria-label will still be set.
    */
   hideLabel?: boolean;
