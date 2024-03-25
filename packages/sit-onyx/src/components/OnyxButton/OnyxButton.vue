cd6d9e12e50183268060021d61a09b1660086ab9
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-03-25 17:47:55 +0100

    base impl

--- packages/sit-onyx/src/components/OnyxButton/OnyxButton.vue
+++ packages/sit-onyx/src/components/OnyxButton/OnyxButton.vue
@@ -39,145 +39,149 @@ const emit = defineEmits<{
 </template>
 
 <style lang="scss">
+@use "../../styles/layers.scss";
+
 $button-height: 2.5rem;
 
 .onyx-button {
-  --onyx-button-background-color: transparent;
-  --onyx-button-background-hover-color: var(--onyx-color-base-primary-100);
-  --onyx-button-border-color: transparent;
-  --onyx-button-text-color: var(--onyx-color-text-icons-primary-intense);
-  --onyx-button-outline-color: var(--onyx-color-base-primary-200);
+  @include layers.component() {
+    --onyx-button-background-color: transparent;
+    --onyx-button-background-hover-color: var(--onyx-color-base-primary-100);
+    --onyx-button-border-color: transparent;
+    --onyx-button-text-color: var(--onyx-color-text-icons-primary-intense);
+    --onyx-button-outline-color: var(--onyx-color-base-primary-200);
 
-  display: inline-flex;
-  height: $button-height;
-  max-width: 100%;
-  box-sizing: border-box;
-  padding: var(--onyx-spacing-2xs) var(--onyx-spacing-sm);
-  justify-content: center;
-  align-items: center;
-  gap: var(--onyx-spacing-4xs);
-  border-radius: var(--onyx-radius-sm);
-  cursor: pointer;
-  font-family: var(--onyx-font-family);
-  background-color: var(--onyx-button-background-color);
-  border: var(--onyx-1px-in-rem) solid var(--onyx-button-border-color);
-  color: var(--onyx-button-text-color);
-
-  &--primary {
-    &:disabled:not(.onyx-button--loading) {
-      --onyx-button-text-color: var(--onyx-color-text-icons-primary-soft);
-    }
-
-    &.onyx-button--default {
-      --onyx-button-background-color: var(--onyx-color-base-primary-500);
-      --onyx-button-background-hover-color: var(--onyx-color-base-primary-400);
-      --onyx-button-text-color: var(--onyx-color-text-icons-neutral-inverted);
-      --onyx-button-border-color: var(--onyx-color-base-primary-500);
-
-      &:disabled:not(.onyx-button--loading) {
-        --onyx-button-background-color: var(--onyx-color-base-primary-200);
-        --onyx-button-border-color: var(--onyx-color-base-primary-200);
-        --onyx-button-text-color: var(--onyx-color-text-icons-neutral-inverted);
-      }
-    }
-
-    &.onyx-button--outline {
-      --onyx-button-border-color: var(--onyx-color-base-primary-500);
-
-      &:disabled:not(.onyx-button--loading) {
-        --onyx-button-border-color: var(--onyx-color-base-primary-200);
-      }
-    }
-  }
-
-  &--secondary {
-    --onyx-button-background-hover-color: var(--onyx-color-base-neutral-200);
-    --onyx-button-outline-color: var(--onyx-color-base-neutral-300);
-    --onyx-button-text-color: var(--onyx-color-text-icons-neutral-intense);
-
-    &:disabled:not(.onyx-button--loading) {
-      --onyx-button-text-color: var(--onyx-color-text-icons-neutral-soft);
-    }
-
-    &.onyx-button--default {
-      --onyx-button-background-color: var(--onyx-color-base-background-blank);
-      --onyx-button-background-hover-color: var(--onyx-color-base-neutral-200);
-      --onyx-button-border-color: var(--onyx-color-base-neutral-400);
-
-      &:disabled:not(.onyx-button--loading) {
-        --onyx-button-background-color: var(--onyx-color-base-background-blank);
-        --onyx-button-border-color: var(--onyx-color-base-neutral-200);
-      }
-    }
-
-    &.onyx-button--outline {
-      --onyx-button-border-color: var(--onyx-color-base-neutral-400);
-
-      &:disabled:not(.onyx-button--loading) {
-        --onyx-button-border-color: var(--onyx-color-base-neutral-200);
-      }
-    }
-  }
-
-  &--danger {
-    --onyx-button-background-hover-color: var(--onyx-color-base-danger-200);
-    --onyx-button-outline-color: var(--onyx-color-base-danger-300);
-    --onyx-button-text-color: var(--onyx-color-text-icons-danger-intense);
-
-    &:disabled:not(.onyx-button--loading) {
-      --onyx-button-text-color: var(--onyx-color-text-icons-danger-medium);
-    }
-
-    &.onyx-button--default {
-      --onyx-button-background-color: var(--onyx-color-base-danger-200);
-      --onyx-button-background-hover-color: var(--onyx-color-base-danger-100);
-      --onyx-button-border-color: var(--onyx-color-base-danger-500);
-
-      &:disabled:not(.onyx-button--loading) {
-        --onyx-button-background-color: var(--onyx-color-base-danger-100);
-        --onyx-button-border-color: var(--onyx-color-base-danger-200);
-      }
-    }
-
-    &.onyx-button--outline {
-      --onyx-button-border-color: var(--onyx-color-base-danger-500);
-
-      &:disabled:not(.onyx-button--loading) {
-        --onyx-button-border-color: var(--onyx-color-base-danger-200);
-      }
-    }
-  }
-
-  &:hover:enabled:not(:active) {
-    --onyx-button-background-color: var(--onyx-button-background-hover-color);
-  }
-
-  &:focus-visible {
-    outline: 0.25rem solid var(--onyx-button-outline-color);
-  }
-
-  &:disabled {
-    cursor: default;
-  }
-
-  &__label {
-    padding: 0 var(--onyx-spacing-4xs);
-    font-size: 1rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 1.5rem;
-  }
-
-  &__loading {
-    width: var(--onyx-spacing-3xl);
+    display: inline-flex;
     height: $button-height;
-  }
-}
+    max-width: 100%;
+    box-sizing: border-box;
+    padding: var(--onyx-spacing-2xs) var(--onyx-spacing-sm);
+    justify-content: center;
+    align-items: center;
+    gap: var(--onyx-spacing-4xs);
+    border-radius: var(--onyx-radius-sm);
+    cursor: pointer;
+    font-family: var(--onyx-font-family);
+    background-color: var(--onyx-button-background-color);
+    border: var(--onyx-1px-in-rem) solid var(--onyx-button-border-color);
+    color: var(--onyx-button-text-color);
 
-.onyx-button-skeleton {
-  width: var(--onyx-spacing-4xl);
-  height: $button-height;
-  display: inline-block;
-  vertical-align: middle;
+    &--primary {
+      &:disabled:not(.onyx-button--loading) {
+        --onyx-button-text-color: var(--onyx-color-text-icons-primary-soft);
+      }
+
+      &.onyx-button--default {
+        --onyx-button-background-color: var(--onyx-color-base-primary-500);
+        --onyx-button-background-hover-color: var(--onyx-color-base-primary-400);
+        --onyx-button-text-color: var(--onyx-color-text-icons-neutral-inverted);
+        --onyx-button-border-color: var(--onyx-color-base-primary-500);
+
+        &:disabled:not(.onyx-button--loading) {
+          --onyx-button-background-color: var(--onyx-color-base-primary-200);
+          --onyx-button-border-color: var(--onyx-color-base-primary-200);
+          --onyx-button-text-color: var(--onyx-color-text-icons-neutral-inverted);
+        }
+      }
+
+      &.onyx-button--outline {
+        --onyx-button-border-color: var(--onyx-color-base-primary-500);
+
+        &:disabled:not(.onyx-button--loading) {
+          --onyx-button-border-color: var(--onyx-color-base-primary-200);
+        }
+      }
+    }
+
+    &--secondary {
+      --onyx-button-background-hover-color: var(--onyx-color-base-neutral-200);
+      --onyx-button-outline-color: var(--onyx-color-base-neutral-300);
+      --onyx-button-text-color: var(--onyx-color-text-icons-neutral-intense);
+
+      &:disabled:not(.onyx-button--loading) {
+        --onyx-button-text-color: var(--onyx-color-text-icons-neutral-soft);
+      }
+
+      &.onyx-button--default {
+        --onyx-button-background-color: var(--onyx-color-base-background-blank);
+        --onyx-button-background-hover-color: var(--onyx-color-base-neutral-200);
+        --onyx-button-border-color: var(--onyx-color-base-neutral-400);
+
+        &:disabled:not(.onyx-button--loading) {
+          --onyx-button-background-color: var(--onyx-color-base-background-blank);
+          --onyx-button-border-color: var(--onyx-color-base-neutral-200);
+        }
+      }
+
+      &.onyx-button--outline {
+        --onyx-button-border-color: var(--onyx-color-base-neutral-400);
+
+        &:disabled:not(.onyx-button--loading) {
+          --onyx-button-border-color: var(--onyx-color-base-neutral-200);
+        }
+      }
+    }
+
+    &--danger {
+      --onyx-button-background-hover-color: var(--onyx-color-base-danger-200);
+      --onyx-button-outline-color: var(--onyx-color-base-danger-300);
+      --onyx-button-text-color: var(--onyx-color-text-icons-danger-intense);
+
+      &:disabled:not(.onyx-button--loading) {
+        --onyx-button-text-color: var(--onyx-color-text-icons-danger-medium);
+      }
+
+      &.onyx-button--default {
+        --onyx-button-background-color: var(--onyx-color-base-danger-200);
+        --onyx-button-background-hover-color: var(--onyx-color-base-danger-100);
+        --onyx-button-border-color: var(--onyx-color-base-danger-500);
+
+        &:disabled:not(.onyx-button--loading) {
+          --onyx-button-background-color: var(--onyx-color-base-danger-100);
+          --onyx-button-border-color: var(--onyx-color-base-danger-200);
+        }
+      }
+
+      &.onyx-button--outline {
+        --onyx-button-border-color: var(--onyx-color-base-danger-500);
+
+        &:disabled:not(.onyx-button--loading) {
+          --onyx-button-border-color: var(--onyx-color-base-danger-200);
+        }
+      }
+    }
+
+    &:hover:enabled:not(:active) {
+      --onyx-button-background-color: var(--onyx-button-background-hover-color);
+    }
+
+    &:focus-visible {
+      outline: 0.25rem solid var(--onyx-button-outline-color);
+    }
+
+    &:disabled {
+      cursor: default;
+    }
+
+    &__label {
+      padding: 0 var(--onyx-spacing-4xs);
+      font-size: 1rem;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 1.5rem;
+    }
+
+    &__loading {
+      width: var(--onyx-spacing-3xl);
+      height: $button-height;
+    }
+  }
+
+  .onyx-button-skeleton {
+    width: var(--onyx-spacing-4xl);
+    height: $button-height;
+    display: inline-block;
+    vertical-align: middle;
+  }
 }
 </style>
--- packages/sit-onyx/src/components/OnyxIconButton/OnyxIconButton.vue
+++ packages/sit-onyx/src/components/OnyxIconButton/OnyxIconButton.vue
@@ -35,84 +35,83 @@ const emit = defineEmits<{
     <OnyxLoadingIndicator v-if="props.loading" type="circle" />
     <OnyxIcon v-else-if="props.icon" :icon="props.icon" />
     <slot v-else></slot>
-    <p>not slot</p>
   </button>
 </template>
 
 <style lang="scss">
-@use "../../styles/base.scss";
+@use "../../styles/layers.scss";
 
 .onyx-icon-button {
-  @include base.reset(&);
-
-  --icon-button-color: var(--onyx-color-text-icons-primary-intense);
-  --icon-button-bg-color: transparent;
-  --icon-button-cursor: pointer;
-
-  display: grid;
-  place-items: center;
-  padding: var(--onyx-spacing-2xs);
-  color: var(--icon-button-color);
-  cursor: var(--icon-button-cursor);
-
-  outline: none;
-  appearance: none;
-  border: none;
-  background: none;
-
-  border-radius: var(--onyx-radius-full);
-  background-color: var(--icon-button-bg-color);
-
-  &:hover,
-  &:focus-visible {
-    --icon-button-bg-color: var(--onyx-color-base-primary-200);
-  }
-
-  &:disabled:not(&--loading) {
-    --icon-button-color: var(--onyx-color-text-icons-primary-soft);
-  }
-
-  &:active {
-    --icon-button-bg-color: var(--onyx-color-base-primary-300);
-  }
-
-  &--secondary {
-    --icon-button-color: var(--onyx-color-text-icons-neutral-medium);
-
-    &:hover,
-    &:focus-visible {
-      --icon-button-bg-color: var(--onyx-color-base-neutral-200);
-    }
-
-    &:disabled:not(&--loading) {
-      --icon-button-color: var(--onyx-color-base-neutral-300);
-    }
-
-    &:active {
-      --icon-button-bg-color: var(--onyx-color-base-neutral-300);
-    }
-  }
-
-  &--danger {
-    --icon-button-color: var(--onyx-color-text-icons-danger-intense);
-
-    &:hover,
-    &:focus-visible {
-      --icon-button-bg-color: var(--onyx-color-base-danger-200);
-    }
-
-    &:disabled:not(&--loading) {
-      --icon-button-color: var(--onyx-color-base-danger-300);
-    }
-
-    &:active {
-      --icon-button-bg-color: var(--onyx-color-base-danger-300);
-    }
-  }
-
-  &:disabled {
+  @include layers.component() {
+    --icon-button-color: var(--onyx-color-text-icons-primary-intense);
     --icon-button-bg-color: transparent;
-    --icon-button-cursor: default;
+    --icon-button-cursor: pointer;
+
+    display: grid;
+    place-items: center;
+    padding: var(--onyx-spacing-2xs);
+    color: var(--icon-button-color);
+    cursor: var(--icon-button-cursor);
+
+    outline: none;
+    appearance: none;
+    border: none;
+    background: none;
+
+    border-radius: var(--onyx-radius-full);
+    background-color: var(--icon-button-bg-color);
+
+    &:hover,
+    &:focus-visible {
+      --icon-button-bg-color: var(--onyx-color-base-primary-200);
+    }
+
+    &:disabled:not(&--loading) {
+      --icon-button-color: var(--onyx-color-text-icons-primary-soft);
+    }
+
+    &:active {
+      --icon-button-bg-color: var(--onyx-color-base-primary-300);
+    }
+
+    &--secondary {
+      --icon-button-color: var(--onyx-color-text-icons-neutral-medium);
+
+      &:hover,
+      &:focus-visible {
+        --icon-button-bg-color: var(--onyx-color-base-neutral-200);
+      }
+
+      &:disabled:not(&--loading) {
+        --icon-button-color: var(--onyx-color-base-neutral-300);
+      }
+
+      &:active {
+        --icon-button-bg-color: var(--onyx-color-base-neutral-300);
+      }
+    }
+
+    &--danger {
+      --icon-button-color: var(--onyx-color-text-icons-danger-intense);
+
+      &:hover,
+      &:focus-visible {
+        --icon-button-bg-color: var(--onyx-color-base-danger-200);
+      }
+
+      &:disabled:not(&--loading) {
+        --icon-button-color: var(--onyx-color-base-danger-300);
+      }
+
+      &:active {
+        --icon-button-bg-color: var(--onyx-color-base-danger-300);
+      }
+    }
+
+    &:disabled {
+      --icon-button-bg-color: transparent;
+      --icon-button-cursor: default;
+    }
   }
 }
 </style>
