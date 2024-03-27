541939a3a342372845a82c216de14752e5494f0b
boppli <linda.bopp@mail.schwarz>
2024-03-27 17:55:25 +0100

    increase screenshot width

--- packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.ct.tsx
+++ packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.ct.tsx
@@ -4,7 +4,7 @@ import OnyxSelect from "./OnyxSelect.vue";
 
 test("should have aria-label if label is hidden", async ({ mount, makeAxeBuilder }) => {
   // ARRANGE
-  const component = await mount(<OnyxSelect style="width: 12rem" label="Test label" hideLabel />);
+  const component = await mount(<OnyxSelect style="width: 16rem" label="Test label" hideLabel />);
 
   // ACT
   const accessibilityScanResults = await makeAxeBuilder().analyze();
@@ -29,7 +29,7 @@ test.describe("basic state screenshot tests", () => {
     async ({ state, labeled }, mount) => {
       const component = await mount(
         <OnyxSelect
-          style="width: 12rem"
+          style="width: 16rem"
           modelValue={undefined}
           label={labeled}
           hideLabel={labeled === "Hidden Label"}
@@ -103,7 +103,7 @@ test.describe("multiselect value display states screenshot tests", () => {
     async ({ selection, multiselectTextMode }, mount) => {
       const component = await mount(
         <OnyxSelect
-          style="width: 12rem"
+          style="width: 16rem"
           modelValue={JSON.parse(selection)}
           label={`Multiselect with ${multiselectTextMode}`}
           multiple={{ textMode: multiselectTextMode }}
