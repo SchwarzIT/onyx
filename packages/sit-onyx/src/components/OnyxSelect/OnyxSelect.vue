68da55e46ea21479153e50ded84b32e07ecd9268
Jonathan <jonathan_leo.carle@mail.schwarz>
2024-04-02 12:21:24 +0200

    Update packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
    Co-authored-by: Lars Rickert <lars.rickert@mail.schwarz>

--- packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
+++ packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
@@ -285,7 +285,7 @@ const { densityClass } = useDensity(props);
       height: 1.25rem;
     }
     &__input {
-      width: 17rem;
+      width: 16rem;
       // TODO: apply height based on density
       height: calc(1lh + 2 * var(--onyx-select-padding-vertical));
     }
