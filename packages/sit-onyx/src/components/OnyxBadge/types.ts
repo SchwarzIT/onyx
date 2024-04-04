51baffdfd9d4ce241f4a2a3ce2a20d42f963ce8e
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-04-04 12:26:44 +0300

    Update OnyxColor type

--- packages/sit-onyx/src/components/OnyxBadge/types.ts
+++ packages/sit-onyx/src/components/OnyxBadge/types.ts
@@ -1,23 +1,14 @@
 import type { DensityProp } from "../../composables/density";
+import type { OnyxColor } from "../../types/colors";
 
 export type OnyxBadgeProps = DensityProp & {
   /**
    * The color of the badge.
    */
-  variation?: BadgeVariation;
+  variation?: OnyxColor;
   /**
    * An icon which will be displayed inside the badge.
    * Note: If the icon property is set, the default slot will not be rendered.
    */
   icon?: string;
 };
-
-export const BADGE_VARIATIONS = [
-  "primary",
-  "secondary",
-  "danger",
-  "warning",
-  "success",
-  "info",
-] as const;
-export type BadgeVariation = (typeof BADGE_VARIATIONS)[number];
