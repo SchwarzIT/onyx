4e2a5bbbc9f7ca3e3bb007789b076279edd49971
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-08 17:49:53 +0200

    feat: add `OnyxEmpty` component (#830)
    Relates to #744
    
    Add empty component.

---
+++ packages/sit-onyx/src/components/OnyxEmpty/OnyxEmpty.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+import circleX from "@sit-onyx/icons/circle-x.svg?raw";
+import OnyxIcon from "../OnyxIcon/OnyxIcon.vue";
+
+defineSlots<{
+  /**
+   * Label / text to display.
+   */
+  default(): unknown;
+  /**
+   * Optional slot to override the default icon.
+   */
+  icon?(): unknown;
+}>();
+</script>
+
+<template>
+  <div class="onyx-empty">
+    <slot name="icon">
+      <OnyxIcon :icon="circleX" size="48px" />
+    </slot>
+
+    <div class="onyx-empty__label onyx-text onyx-truncation-multiline">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.onyx-empty {
+  padding: var(--onyx-spacing-xl);
+  box-sizing: border-box;
+  max-width: max-content;
+  font-family: var(--onyx-font-family);
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: var(--onyx-spacing-md);
+
+  &__label {
+    color: var(--onyx-color-text-icons-neutral-soft);
+    font-weight: 600;
+    white-space: pre-line;
+  }
+
+  .onyx-icon {
+    color: var(--onyx-color-text-icons-primary-medium);
+  }
+}
+</style>
