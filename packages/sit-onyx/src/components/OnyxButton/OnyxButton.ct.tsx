f2ad5783914be0e12b2d9fcda4a921fb766bdcb3
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-12 08:55:14 +0200

    optimize test cases

--- packages/sit-onyx/src/components/OnyxButton/OnyxButton.ct.tsx
+++ packages/sit-onyx/src/components/OnyxButton/OnyxButton.ct.tsx
@@ -10,7 +10,6 @@ import { BUTTON_MODES, BUTTON_VARIATIONS } from "./types";
 
 test.describe("Screenshot tests", () => {
   const screenshotOptions = {
-    columns: DENSITIES,
     rows: ["default", "hover", "active", "focus-visible"] as const,
     beforeScreenshot: async (component, page, column, row) => {
       if (row === "hover") await component.hover();
@@ -20,71 +19,64 @@ test.describe("Screenshot tests", () => {
   } satisfies Partial<MatrixScreenshotTestOptions>;
 
   for (const mode of BUTTON_MODES) {
-    for (const variation of BUTTON_VARIATIONS) {
-      executeMatrixScreenshotTest({
-        ...screenshotOptions,
-        name: `Button (${mode}, ${variation})`,
-        component: (density) => (
-          <OnyxButton label="Button" density={density} mode={mode} variation={variation} />
-        ),
-      });
+    executeMatrixScreenshotTest({
+      ...screenshotOptions,
+      columns: BUTTON_VARIATIONS,
+      name: `Button (${mode})`,
+      component: (column) => <OnyxButton label="Button" mode={mode} variation={column} />,
+    });
 
-      executeMatrixScreenshotTest({
-        ...screenshotOptions,
-        columns: BUTTON_MODES,
-        name: `Button (${mode}, ${variation}, disabled)`,
-        component: (mode) => (
-          <OnyxButton label="Button" mode={mode} variation={variation} disabled />
-        ),
-        beforeScreenshot: async (component, page, column, row) => {
-          await expect(component.getByRole("button", { name: "Button" })).toBeDisabled();
-          await screenshotOptions.beforeScreenshot(component, page, column, row);
-        },
-      });
+    executeMatrixScreenshotTest({
+      ...screenshotOptions,
+      columns: BUTTON_VARIATIONS,
+      name: `Button (${mode}, disabled)`,
+      component: (column) => <OnyxButton label="Button" mode={mode} variation={column} disabled />,
+    });
 
-      executeMatrixScreenshotTest({
-        ...screenshotOptions,
-        name: `Button (${mode}, ${variation}, with icon)`,
-        columns: DENSITIES,
-        rows: BUTTON_MODES,
-        component: (density, mode) => (
-          <OnyxButton
-            label="Button"
-            density={density}
-            mode={mode}
-            variation={variation}
-            icon={mockPlaywrightIcon}
-          />
-        ),
-      });
+    executeMatrixScreenshotTest({
+      ...screenshotOptions,
+      columns: BUTTON_VARIATIONS,
+      name: `Button (${mode}, with icon)`,
+      component: (column) => (
+        <OnyxButton label="Button" mode={mode} variation={column} icon={mockPlaywrightIcon} />
+      ),
+    });
 
-      executeMatrixScreenshotTest({
-        ...screenshotOptions,
-        name: `Button (${mode}, ${variation}, loading)`,
-        component: (density) => (
-          <OnyxButton label="Button" density={density} mode={mode} variation={variation} loading />
-        ),
-      });
-    }
+    executeMatrixScreenshotTest({
+      ...screenshotOptions,
+      columns: BUTTON_VARIATIONS,
+      name: `Button (${mode}, loading)`,
+      component: (column) => <OnyxButton label="Button" mode={mode} variation={column} loading />,
+    });
   }
 
   executeMatrixScreenshotTest({
-    name: "Button (other)",
+    ...screenshotOptions,
     columns: DENSITIES,
-    rows: ["truncation", "skeleton"],
-    component: (density, row) => {
-      const label = row === "truncation" ? "Very long label that should be truncated" : "Button";
-
-      return (
-        <OnyxButton
-          label={label}
-          density={density}
-          skeleton={row === "skeleton"}
-          style={row === "truncation" ? "max-width: 8rem;" : undefined}
-        />
-      );
-    },
+    rows: [...BUTTON_MODES, "skeleton"],
+    name: `Button (densities)`,
+    component: (column, row) => (
+      <OnyxButton
+        label="Button"
+        density={column}
+        mode={row !== "skeleton" ? row : undefined}
+        skeleton={row === "skeleton"}
+      />
+    ),
   });
+});
+
+test("should truncate text", async ({ mount }) => {
+  const label = "Very long label that should be truncated";
+
+  // ARRANGE
+  const component = await mount(<OnyxButton label={label} style="max-width: 8rem;" />);
+
+  // ASSERT
+  await expect(component).toContainText(label);
+
+  // ASSERT
+  await expect(component).toHaveScreenshot("truncation-ellipsis.png");
 });
 
 test("should render button inline aligned with text", async ({ mount }) => {
