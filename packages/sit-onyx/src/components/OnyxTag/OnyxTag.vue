7cf342e98cac422eefef1f6a216332703a40c934
kai <nerdrun.kai@gmail.com>
2024-04-14 16:39:52 +0200

    feat(OnyxTag): added density and onyx-text

--- packages/sit-onyx/src/components/OnyxTag/OnyxTag.vue
+++ packages/sit-onyx/src/components/OnyxTag/OnyxTag.vue
@@ -1,34 +1,53 @@
 <script lang="ts" setup>
+import { useDensity } from "../../composables/density";
 import OnyxIcon from "../OnyxIcon/OnyxIcon.vue";
 import type { OnyxTagProps } from "./types";
 
 const props = withDefaults(defineProps<OnyxTagProps>(), {
   color: "primary",
 });
+const { densityClass } = useDensity(props);
 </script>
 
 <template>
-  <div :class="['onyx-tag', `onyx-tag--${props.type}`]">
+  <div :class="['onyx-tag', `onyx-tag--${props.color}`, densityClass]">
     <OnyxIcon v-if="props.icon" :icon="props.icon" size="16px" />
-    <span class="onyx-tag__label onyx-truncation-ellipsis">{{ props.label }}</span>
+    <span class="onyx-tag__label onyx-text onyx-truncation-ellipsis">{{ props.label }}</span>
   </div>
 </template>
 
 <style lang="scss">
+@use "../../styles/mixins/density.scss";
+
 .onyx-tag {
+  @include density.compact {
+    --onyx-button-height: 2rem;
+    --onyx-button-padding-vertical: var(--onyx-spacing-4xs);
+  }
+
+  @include density.default {
+    --onyx-button-height: 2.5rem;
+    --onyx-button-padding-vertical: var(--onyx-spacing-2xs);
+  }
+
+  @include density.cozy {
+    --onyx-button-height: 3rem;
+    --onyx-button-padding-vertical: var(--onyx-spacing-sm);
+  }
+
   display: inline-flex;
+  height: var(--onyx-button-height);
+  max-width: 100%;
   align-items: center;
   box-sizing: border-box;
   color: var(--onyx-tag-text-color);
   font-family: var(--onyx-font-family);
-  padding: var(--onyx-spacing-2xs) var(--onyx-spacing-sm);
+  padding: var(--onyx-button-padding-vertical) var(--onyx-spacing-sm);
   border-radius: var(--onyx-radius-sm);
   border: var(--onyx-1px-in-rem) solid var(--onyx-tag-text-color);
   background-color: var(--onyx-tag-background-color);
 
-  /// [TODO] ask primary and secondary colors are different between code and figma for OnyxTag to Onyx team members.
-  /// Checked that primary and secondary colors are set the same in variables-onyx.css file
-  $colors: primary, secondary, danger, warning, success, info;
+  $colors: primary, secondary, neutral, danger, warning, success, info;
 
   @each $color in $colors {
     &--#{$color} {
@@ -39,7 +58,6 @@ const props = withDefaults(defineProps<OnyxTagProps>(), {
 
   &__label {
     padding: 0 var(--onyx-spacing-4xs);
-    font-size: 1rem;
   }
 }
 </style>
