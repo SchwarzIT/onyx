b904da6bbf460b7cbcda0ef99a40e4417193f01a
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-15 10:53:26 +0200

    update badge screenshots

--- packages/sit-onyx/src/components/OnyxBadge/OnyxBadge.ct.tsx
+++ packages/sit-onyx/src/components/OnyxBadge/OnyxBadge.ct.tsx
@@ -1,71 +1,49 @@
+import { DENSITIES } from "../../composables/density";
 import { expect, test } from "../../playwright-axe";
-import { executeScreenshotsForAllStates, mockPlaywrightIcon } from "../../utils/playwright";
+import { executeMatrixScreenshotTest } from "../../playwright/screenshots";
 import { ONYX_COLORS } from "../../types/colors";
+import { mockPlaywrightIcon } from "../../utils/playwright";
 import OnyxBadge from "./OnyxBadge.vue";
 
-test("should render", async ({ mount, makeAxeBuilder }) => {
-  // ARRANGE
-  const component = await mount(<OnyxBadge variation="danger">Badge</OnyxBadge>);
+test.describe("Screenshot tests", () => {
+  executeMatrixScreenshotTest({
+    name: `Badge`,
+    columns: DENSITIES,
+    rows: ONYX_COLORS,
+    component: (column, row) => (
+      <OnyxBadge density={column} variation={row}>
+        Badge
+      </OnyxBadge>
+    ),
+    beforeScreenshot: async (component) => {
+      await expect(component).toContainText("Badge");
+    },
+  });
 
-  // ASSERT
-  await expect(component).toContainText("Badge");
-
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder().analyze();
-
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
+  executeMatrixScreenshotTest({
+    name: `Badge (with icon)`,
+    columns: DENSITIES,
+    rows: ONYX_COLORS,
+    component: (column, row) => (
+      <OnyxBadge density={column} variation={row} icon={mockPlaywrightIcon}>
+        Badge
+      </OnyxBadge>
+    ),
+    beforeScreenshot: async (component) => {
+      await expect(component).not.toContainText("Badge");
+    },
+  });
 });
 
 test("should truncate text", async ({ mount }) => {
   const label = "Very long label that should be truncated";
 
   // ARRANGE
-  const component = await mount(<OnyxBadge variation="danger">{label}</OnyxBadge>);
+  const component = await mount(<OnyxBadge style={{ width: "12rem" }}>{label}</OnyxBadge>);
 
   // ASSERT
   await expect(component).toContainText(label);
 
   // ASSERT
   await expect(component).toHaveScreenshot("truncation-ellipsis.png");
-});
-
-test("should render badge with icon", async ({ mount, makeAxeBuilder }) => {
-  // ARRANGE
-  await mount(<OnyxBadge variation="danger" icon={mockPlaywrightIcon} />);
-
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder().analyze();
-
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
-});
-
-const STATES = {
-  variation: ONYX_COLORS,
-  density: ["compact", "default", "cozy"],
-} as const;
-
-test.describe("state screenshot tests", () => {
-  executeScreenshotsForAllStates(STATES, "badge", async ({ variation, density }, mount) => {
-    const component = await mount(
-      <OnyxBadge variation={variation} density={density}>
-        Badge
-      </OnyxBadge>,
-    );
-    return component;
-  });
-});
-
-const ICONSTATE = {
-  density: ["compact", "default", "cozy"],
-} as const;
-
-test.describe("icon screenshot tests", () => {
-  executeScreenshotsForAllStates(ICONSTATE, "badge", async ({ density }, mount) => {
-    const component = await mount(
-      <OnyxBadge icon={mockPlaywrightIcon} variation="primary" density={density}></OnyxBadge>,
-    );
-    return component;
-  });
 });
