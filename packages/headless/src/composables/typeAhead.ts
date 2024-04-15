69ab9262ae85a0a4ebbab57861effb8864f415bd
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-04-15 16:32:47 +0200

    add description

--- packages/headless/src/composables/typeAhead.ts
+++ packages/headless/src/composables/typeAhead.ts
@@ -1,6 +1,16 @@
 import { isPrintableCharacter } from "../utils/keyEvent";
 import { debounce } from "../utils/timer";
 
+/**
+ * Enhances typeahead to combine multiple inputs in quick succession and filter out non-printable characters.
+ *
+ * @example
+ * ```ts
+ * const typeAhead = useTypeAhead((inputString) => onTypeAhead(inputString));
+ * // ...
+ * const onKeydown = (key: KeyboardEvent) => typeAhead(key.event);
+ * ```
+ */
 export const useTypeAhead = (callback: (input: string) => void) => {
   let inputString = "";
   const debouncedReset = debounce(() => (inputString = ""), 500);
