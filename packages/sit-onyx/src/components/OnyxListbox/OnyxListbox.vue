cc7e712b34b9b3fad9cc9be019ec27f74a5acd4c
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-15 08:58:11 +0200

    feat(OnyxListbox): add loading and empty state (#845)
    Relates to #743
    
    Add loading and empty state to the OnyxListbox.

--- packages/sit-onyx/src/components/OnyxListbox/OnyxListbox.vue
+++ packages/sit-onyx/src/components/OnyxListbox/OnyxListbox.vue
@@ -1,18 +1,46 @@
 <script lang="ts" setup generic="TValue extends SelectionOptionValue = SelectionOptionValue">
 import { createListbox } from "@sit-onyx/headless";
-import { computed, ref, watch } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
+import { useScrollEnd } from "../../composables/scrollEnd";
+import { injectI18n } from "../../i18n";
+import OnyxEmpty from "../OnyxEmpty/OnyxEmpty.vue";
 import OnyxListboxOption from "../OnyxListboxOption/OnyxListboxOption.vue";
-import type { OnyxListboxProps } from "./types";
+import OnyxLoadingIndicator from "../OnyxLoadingIndicator/OnyxLoadingIndicator.vue";
 import type { SelectionOptionValue } from "../OnyxRadioButton/types";
+import type { OnyxListboxProps } from "./types";
 
-const props = defineProps<OnyxListboxProps<TValue>>();
+const props = withDefaults(defineProps<OnyxListboxProps<TValue>>(), {
+  loading: false,
+});
 
 const emit = defineEmits<{
   /**
    * Emitted when the current value changes.
    */
   "update:modelValue": [value: typeof props.modelValue];
+  /**
+   * Emitted if lazy loading is triggered / the users scrolls to the end of the options.
+   * See property `lazyLoading` for enabling the lazy loading.
+   */
+  lazyLoad: [];
 }>();
+
+const slots = defineSlots<{
+  /**
+   * Optional slot to customize the empty state when no options exist.
+   * It is recommended to use the `<OnyxEmpty>` component here.
+   *
+   * If unset, a default translated message will be displayed for the current locale.
+   */
+  empty?(props: { defaultMessage: string }): unknown;
+  /**
+   * Optional slot that is displayed below all options that can be used
+   * to e.g. show a button to load more options instead of lazy loading on scroll.
+   */
+  optionsEnd?(): unknown;
+}>();
+
+const { t } = injectI18n();
 
 /**
  * Currently (visually) active option.
@@ -59,11 +87,31 @@ const {
     activeOption.value = firstMatch.id;
   },
 });
+
+const { vScrollEnd, isScrollEnd } = useScrollEnd({
+  enabled: computed(() => props.lazyLoading?.enabled ?? false),
+  loading: computed(() => props.loading),
+  offset: computed(() => props.lazyLoading?.scrollOffset),
+});
+
+watchEffect(() => {
+  if (isScrollEnd.value) emit("lazyLoad");
+});
+
+const isEmpty = computed(() => props.options.length === 0);
 </script>
 
 <template>
-  <div class="onyx-listbox">
-    <ul v-bind="listbox" class="onyx-listbox__options">
+  <div class="onyx-listbox" :aria-busy="props.loading">
+    <div v-if="props.loading" class="onyx-listbox__slot onyx-listbox__slot--loading">
+      <OnyxLoadingIndicator class="onyx-listbox__loading" />
+    </div>
+
+    <slot v-else-if="isEmpty" name="empty" :default-message="t('empty')">
+      <OnyxEmpty>{{ t("empty") }}</OnyxEmpty>
+    </slot>
+
+    <ul v-else v-scroll-end v-bind="listbox" class="onyx-listbox__options">
       <OnyxListboxOption
         v-for="option in props.options"
         :key="option.id.toString()"
@@ -79,6 +127,14 @@ const {
       >
         {{ option.label }}
       </OnyxListboxOption>
+
+      <li v-if="props.lazyLoading?.loading" class="onyx-listbox__slot">
+        <OnyxLoadingIndicator class="onyx-listbox__loading" />
+      </li>
+
+      <li v-if="slots.optionsEnd" class="onyx-listbox__slot">
+        <slot name="optionsEnd"></slot>
+      </li>
     </ul>
 
     <span v-if="props.message" class="onyx-listbox__message onyx-text--small">
@@ -94,30 +150,29 @@ const {
   @include layers.component() {
     --max-options: 8;
     --option-height: calc(1.5rem + 2 * var(--onyx-spacing-2xs));
+    $wrapper-padding: var(--onyx-spacing-2xs);
 
     border-radius: var(--onyx-radius-md);
     background-color: var(--onyx-color-base-background-blank);
-    padding: var(--onyx-spacing-2xs) 0;
+    padding: $wrapper-padding 0;
     box-shadow: var(--onyx-shadow-medium-bottom);
+    box-sizing: border-box;
     width: max-content;
     min-width: var(--onyx-spacing-4xl);
     max-width: 20rem;
     font-family: var(--onyx-font-family);
 
-    &__label {
-      color: var(--onyx-color-text-icons-neutral-soft);
-      display: inline-block;
-      width: 100%;
-      text-align: right;
-      padding: var(--onyx-spacing-2xs) var(--onyx-spacing-sm) 0;
-    }
-
     &__message {
       color: var(--onyx-color-text-icons-neutral-soft);
       display: inline-block;
       width: 100%;
+      box-sizing: border-box;
       text-align: right;
-      padding: var(--onyx-spacing-2xs) var(--onyx-spacing-sm) 0;
+      padding: $wrapper-padding var(--onyx-spacing-sm) 0;
+    }
+
+    .onyx-listbox-option {
+      height: var(--option-height);
     }
 
     &__options {
@@ -128,12 +183,24 @@ const {
       padding: 0;
     }
 
-    .onyx-listbox-option {
-      height: var(--option-height);
-    }
-
     &:has(&__options:focus-visible) {
       outline: 0.25rem solid var(--onyx-color-base-primary-200);
+    }
+
+    &__slot {
+      padding: 0 $wrapper-padding;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      &--loading {
+        min-height: calc(5 * var(--option-height));
+      }
+    }
+
+    &__loading {
+      color: var(--onyx-color-text-icons-primary-intense);
     }
   }
 }
