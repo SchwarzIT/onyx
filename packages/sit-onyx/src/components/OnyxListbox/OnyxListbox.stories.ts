f464b426f1279eb7c40a37ec40a470e7785a4f0d
Maja Zarkova <60856270+MajaZarkova@users.noreply.github.com>
2024-04-16 16:14:05 +0300

    feat(OnyxListbox): Implement grouping for OnyxListbox (#868)
    <!-- Is your PR related to an issue? Then please link it via the
    "Relates to #" below. Else, remove it. -->
    
    Relates to #734 
    
    Implement grouped options for OnyxListbox
    
    ## Checklist
    
    - [x] The added / edited code has been documented with
    [JSDoc](https://jsdoc.app/about-getting-started)
    - [x] All changes are documented in the documentation app (folder
    `apps/docs`)
    - [x] A changeset is added with `npx changeset add` if your changes
    should be released as npm package (because they affect the library
    usage)
    
    ---------
    
    Co-authored-by: Lars Rickert <lars.rickert@mail.schwarz>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/sit-onyx/src/components/OnyxListbox/OnyxListbox.stories.ts
+++ packages/sit-onyx/src/components/OnyxListbox/OnyxListbox.stories.ts
@@ -53,11 +53,64 @@ const meta: Meta<typeof OnyxListbox> = {
   ],
 };
 
+const groupedAnimals = [
+  {
+    id: "cat",
+    label: "Cat",
+    group: "Land",
+  },
+  {
+    id: "dog",
+    label: "Dog",
+    group: "Land",
+  },
+  {
+    id: "tiger",
+    label: "Tager",
+    group: "Land",
+  },
+  {
+    id: "reindeer",
+    label: "Reindeer",
+    group: "Land",
+  },
+  {
+    id: "racoon",
+    label: "Racoon",
+    group: "Land",
+  },
+  {
+    id: "dolphin",
+    label: "Dolphin",
+    group: "Water",
+  },
+  {
+    id: "flounder",
+    label: "Flounder",
+    group: "Water",
+  },
+  {
+    id: "eel",
+    label: "Eel",
+    group: "Water",
+  },
+  {
+    id: "falcon",
+    label: "Falcon",
+    group: "Air",
+  },
+  {
+    id: "owl",
+    label: "Owl",
+    group: "Air",
+  },
+];
+
 export default meta;
 type Story = StoryObj<typeof OnyxListbox>;
 
 /**
- * This examples shows a default single select listbox.
+ * This example shows a default single select listbox.
  */
 export const Default = {
   args: {
@@ -79,12 +132,15 @@ export const Default = {
       "Melon",
       "Raspberry",
       "Strawberry",
-    ].map((option) => ({ id: option.toLowerCase(), label: option })),
+    ].map((option) => ({
+      id: option.toLowerCase(),
+      label: option,
+    })),
   },
 } satisfies Story;
 
 /**
- * This examples shows a listbox with a message / help text at the bottom.
+ * This example shows a listbox with a message / help text at the bottom.
  */
 export const WithMessage = {
   args: {
@@ -94,7 +150,17 @@ export const WithMessage = {
 } satisfies Story;
 
 /**
- * This examples shows an empty listbox with default translated message.
+ * This example shows a listbox with grouped options.
+ */
+export const GroupedOptions = {
+  args: {
+    label: "Grouped listbox",
+    options: groupedAnimals,
+  },
+} satisfies Story;
+
+/**
+ * This example shows an empty listbox with default translated message.
  * You can use the `empty` slot to customize the content.
  */
 export const Empty = {
@@ -105,7 +171,7 @@ export const Empty = {
 } satisfies Story;
 
 /**
- * This examples shows a loading listbox.
+ * This example shows a loading listbox.
  */
 export const Loading = {
   args: {
@@ -115,7 +181,7 @@ export const Loading = {
 } satisfies Story;
 
 /**
- * This examples shows a loading listbox with lazy loading. The `lazyLoad` event will be emitted if the user scrolls
+ * This example shows a loading listbox with lazy loading. The `lazyLoad` event will be emitted if the user scrolls
  * to the end of the options.
  */
 export const LazyLoading = {
@@ -128,7 +194,7 @@ export const LazyLoading = {
 } satisfies Story;
 
 /**
- * This examples shows a loading listbox with button loading.
+ * This example shows a loading listbox with button loading.
  */
 export const ButtonLoading = {
   args: {
