3439e9f1d3066d273bfff16dfed9dc0d1e3c3075
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-18 18:07:37 +0200

    add screenshot tests

--- packages/sit-onyx/src/components/OnyxTable/OnyxTable.ct.tsx
+++ packages/sit-onyx/src/components/OnyxTable/OnyxTable.ct.tsx
@@ -1,36 +1,62 @@
 import { expect, test } from "../../playwright/a11y";
 import OnyxTable from "./OnyxTable.vue";
 
-test("should render", async ({ mount, makeAxeBuilder }) => {
-  // ARRANGE
-  await mount(
-    <OnyxTable>
-      <thead>
-        <tr>
-          <th>Fruit</th> <th>Price (€/kg)</th> <th>Inventory (kg)</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Strawberry</td> <td>4.50</td> <td>200</td>
-        </tr>
-        <tr>
-          <td>Apple</td> <td>1.99</td> <td>3000</td>
-        </tr>
-        <tr>
-          <td>Banana</td> <td>3.75</td> <td>18000</td>
-        </tr>
-      </tbody>
-    </OnyxTable>,
+for (const state of ["default", "striped", "grid", "striped-grid"]) {
+  const tableBody = (
+    <tbody>
+      <tr>
+        <td>Strawberry</td> <td>4.50</td> <td>200</td>
+      </tr>
+      <tr>
+        <td>Apple</td> <td>1.99</td> <td>3000</td>
+      </tr>
+      <tr>
+        <td>Banana</td> <td>3.75</td> <td>18000</td>
+      </tr>
+    </tbody>
   );
 
-  // ASSERT
-  // TODO: Enable the screenshot as soon as we have styles.
-  // await expect(component).toHaveScreenshot("default.png");
+  test(`should render ${state}`, async ({ mount, makeAxeBuilder }) => {
+    // ARRANGE
+    const component = await mount(
+      <OnyxTable striped={state.includes("striped")} grid={state.includes("grid")}>
+        <thead>
+          <tr>
+            <th>Fruit</th> <th>Price (€/kg)</th> <th>Inventory (kg)</th>
+          </tr>
+        </thead>
+        {tableBody}
+      </OnyxTable>,
+    );
 
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder().analyze();
+    // ASSERT
+    await expect(component).toHaveScreenshot(`${state}.png`);
 
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
-});
+    // ACT
+    const accessibilityScanResults = await makeAxeBuilder()
+      // TODO: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
+      .disableRules(["color-contrast"])
+      .analyze();
+
+    // ASSERT
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+
+  test(`should render ${state} without header`, async ({ mount, makeAxeBuilder }) => {
+    // ARRANGE
+    const component = await mount(
+      <OnyxTable striped={state.includes("striped")} grid={state.includes("grid")}>
+        {tableBody}
+      </OnyxTable>,
+    );
+
+    // ASSERT
+    await expect(component).toHaveScreenshot(`${state}-without-header.png`);
+
+    // ACT
+    const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+    // ASSERT
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+}
