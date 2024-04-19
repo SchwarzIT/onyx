e3721718455aa97859be03f2403ffb01873dab30
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-19 13:20:34 +0200

    update docs

--- apps/docs/postcss.config.mjs
+++ apps/docs/postcss.config.mjs
@@ -2,6 +2,7 @@
 import { postcssIsolateStyles } from "vitepress";
 
 // see: https://vitepress.dev/guide/markdown#raw
+// enable support for "vp-raw" class to prevent VitePress styles for custom components that use onyx
 export default {
   plugins: [postcssIsolateStyles()],
 };
