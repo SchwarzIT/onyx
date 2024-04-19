49923518a2c59f4b9edd86e3c97ceb937dc56982
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-19 10:56:23 +0200

    use matrix screenshots

--- packages/sit-onyx/src/components/OnyxTable/OnyxTable.ct.tsx
+++ packages/sit-onyx/src/components/OnyxTable/OnyxTable.ct.tsx
@@ -1,62 +1,58 @@
-import { expect, test } from "../../playwright/a11y";
+import { DENSITIES } from "../../composables/density";
+import { test } from "../../playwright/a11y";
+import { executeMatrixScreenshotTest } from "../../playwright/screenshots";
 import OnyxTable from "./OnyxTable.vue";
 
-for (const state of ["default", "striped", "grid", "striped-grid"]) {
-  const tableBody = (
-    <tbody>
-      <tr>
-        <td>Strawberry</td> <td>4.50</td> <td>200</td>
-      </tr>
-      <tr>
-        <td>Apple</td> <td>1.99</td> <td>3000</td>
-      </tr>
-      <tr>
-        <td>Banana</td> <td>3.75</td> <td>18000</td>
-      </tr>
-    </tbody>
-  );
+const tableHead = (
+  <thead>
+    <tr>
+      <th>Fruit</th>
+      <th>Price (€/kg)</th>
+      <th>Inventory (kg)</th>
+    </tr>
+  </thead>
+);
 
-  test(`should render ${state}`, async ({ mount, makeAxeBuilder }) => {
-    // ARRANGE
-    const component = await mount(
-      <OnyxTable striped={state.includes("striped")} grid={state.includes("grid")}>
-        <thead>
-          <tr>
-            <th>Fruit</th> <th>Price (€/kg)</th> <th>Inventory (kg)</th>
-          </tr>
-        </thead>
+const tableBody = (
+  <tbody>
+    <tr>
+      <td>Strawberry</td> <td>4.50</td> <td>200</td>
+    </tr>
+    <tr>
+      <td>Apple</td> <td>1.99</td> <td>3000</td>
+    </tr>
+    <tr>
+      <td>Banana</td> <td>3.75</td> <td>18000</td>
+    </tr>
+  </tbody>
+);
+
+test.describe("Screenshot tests", () => {
+  executeMatrixScreenshotTest({
+    name: "Table",
+    columns: ["with-header", "without-header"],
+    rows: ["default", "striped", "grid", "striped-grid"],
+    // TODO: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
+    disabledAccessibilityRules: ["color-contrast"],
+    component: (column, row) => (
+      <OnyxTable striped={row.includes("striped")} grid={row.includes("grid")}>
+        {column === "with-header" ? tableHead : undefined}
         {tableBody}
-      </OnyxTable>,
-    );
-
-    // ASSERT
-    await expect(component).toHaveScreenshot(`${state}.png`);
-
-    // ACT
-    const accessibilityScanResults = await makeAxeBuilder()
-      // TODO: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
-      .disableRules(["color-contrast"])
-      .analyze();
-
-    // ASSERT
-    expect(accessibilityScanResults.violations).toEqual([]);
+      </OnyxTable>
+    ),
   });
 
-  test(`should render ${state} without header`, async ({ mount, makeAxeBuilder }) => {
-    // ARRANGE
-    const component = await mount(
-      <OnyxTable striped={state.includes("striped")} grid={state.includes("grid")}>
+  executeMatrixScreenshotTest({
+    name: "Table (densities)",
+    columns: DENSITIES,
+    rows: ["default"],
+    // TODO: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
+    disabledAccessibilityRules: ["color-contrast"],
+    component: (column) => (
+      <OnyxTable density={column}>
+        {tableHead}
         {tableBody}
-      </OnyxTable>,
-    );
-
-    // ASSERT
-    await expect(component).toHaveScreenshot(`${state}-without-header.png`);
-
-    // ACT
-    const accessibilityScanResults = await makeAxeBuilder().analyze();
-
-    // ASSERT
-    expect(accessibilityScanResults.violations).toEqual([]);
+      </OnyxTable>
+    ),
   });
-}
+});
