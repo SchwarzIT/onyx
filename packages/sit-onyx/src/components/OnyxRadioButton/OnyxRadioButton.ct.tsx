61c49647fee985d5900fdd9632968f9c67219416
Lars Rickert <lars.rickert@mail.schwarz>
2024-04-19 09:18:42 +0200

    test: run accessibility tests for every screenshot (#942)
    Relates to #855
    
    Run the Playwright accessibility tests for every screenshot / component
    variation in our matrix screenshot utility.
    For some components I disabled the contrast checking for now since they
    are failing until #410 is fixed by UX.
    
    The workflow to update screenshots seems to take ~1m 30s longer (~7m 15s
    instead of 5m 45s) which is acceptable I guess based on the fact that we
    are performing ~400 accessibility tests.

--- packages/sit-onyx/src/components/OnyxRadioButton/OnyxRadioButton.ct.tsx
+++ packages/sit-onyx/src/components/OnyxRadioButton/OnyxRadioButton.ct.tsx
@@ -107,14 +107,3 @@ test.describe("Screenshot tests", () => {
     ),
   });
 });
-
-test("should pass accessibility tests", async ({ mount, makeAxeBuilder }) => {
-  // ARRANGE
-  await mount(<OnyxRadioButton id="test-id" label="Test label" name="test-name" />);
-
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder().analyze();
-
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
-});
