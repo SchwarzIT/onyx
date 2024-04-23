0d06e04f1c971beee4f7d45067338dd456c555e1
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-04-23 09:40:37 +0300

    Try to fix screenshot tests

--- packages/sit-onyx/src/components/OnyxNavItem/OnyxNavItem.ct.tsx
+++ packages/sit-onyx/src/components/OnyxNavItem/OnyxNavItem.ct.tsx
@@ -28,7 +28,7 @@ test.describe("Screenshot tests with children", () => {
 
   executeMatrixScreenshotTest({
     name: "NavItem with children",
-    columns: ["default", "active", "focus-visible"],
+    columns: ["hover", "active", "focus-visible"],
     rows: ["hover"],
     disabledAccessibilityRules: ["aria-required-parent", "color-contrast"],
     component: (column) => (
