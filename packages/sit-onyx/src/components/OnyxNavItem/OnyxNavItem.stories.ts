afe16cff23fb786fc79be92761c826477c778f9c
Maja Zarkova <60856270+MajaZarkova@users.noreply.github.com>
2024-04-24 17:49:43 +0300

    feat(OnyxNavItem): Implement basic OnyxNavItem (#992)
    Relates to #874 
    
    Implement basic OnyxNavItem behavior.

---
+++ packages/sit-onyx/src/components/OnyxNavItem/OnyxNavItem.stories.ts
@@ -0,0 +1,70 @@
+import { defineStorybookActionsAndVModels } from "@sit-onyx/storybook-utils";
+import type { Meta, StoryObj } from "@storybook/vue3";
+import { h } from "vue";
+import OnyxNavItem from "./OnyxNavItem.vue";
+import OnyxBadge from "../OnyxBadge/OnyxBadge.vue";
+
+/**
+ * The nav item is used internally to build the main navigation bar component and is not intended to be used individually.
+ */
+const meta: Meta<typeof OnyxNavItem> = {
+  title: "components/NavItem",
+  ...defineStorybookActionsAndVModels({
+    component: OnyxNavItem,
+    events: ["navigate"],
+    argTypes: {
+      default: {
+        control: { disable: true },
+      },
+    },
+  }),
+};
+
+export default meta;
+type Story = StoryObj<typeof OnyxNavItem>;
+
+/**
+ * This example shows a default nav item.
+ */
+export const Default = {
+  args: {
+    label: "Item",
+    href: "#",
+  },
+} satisfies Story;
+
+/**
+ * This example shows an active nav item.
+ */
+export const Active = {
+  args: {
+    ...Default.args,
+    active: true,
+  },
+} satisfies Story;
+
+/**
+ * This example shows the nav item with nested options.
+ */
+export const WithOptions = {
+  args: {
+    ...Default.args,
+    options: [
+      { label: "Nested Item 1", href: "#", active: true },
+      { label: "Nested Item 2", href: "#2" },
+      { label: "Nested Item 3", href: "#3" },
+      { label: "Nested Item 4", href: "#4" },
+      { label: "Nested Item 5", href: "#5" },
+    ],
+  },
+} satisfies Story;
+
+/**
+ * This example shows the nav item with additional content (a dot badge in this case).
+ */
+export const WithAdditionalContent = {
+  args: {
+    ...Default.args,
+    default: () => h(OnyxBadge, { dot: true }),
+  },
+} satisfies Story;
