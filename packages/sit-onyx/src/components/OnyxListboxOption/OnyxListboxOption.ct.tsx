1378b36b729ccaf4d2cab3a6e3ab8103333b2bfb
boppli <linda.bopp@mail.schwarz>
2024-04-24 08:39:48 +0200

    update test

--- packages/sit-onyx/src/components/OnyxListboxOption/OnyxListboxOption.ct.tsx
+++ packages/sit-onyx/src/components/OnyxListboxOption/OnyxListboxOption.ct.tsx
@@ -2,12 +2,12 @@ import { test } from "../../playwright/a11y";
 import { executeMatrixScreenshotTest } from "../../playwright/screenshots";
 import OnyxListboxOption from "./OnyxListboxOption.vue";
 
-test.describe("Screenshot tests", () => {
+test.describe("Single select screenshot tests", () => {
   for (const state of ["default", "disabled"] as const) {
     executeMatrixScreenshotTest({
       name: `Listbox option (${state})`,
-      columns: ["default", "selected", "indeterminate"],
-      rows: ["default", "hover", "focus-visible", "multiple"],
+      columns: ["default", "selected"],
+      rows: ["default", "hover", "focus-visible"],
       // TODO: color-contrast: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
       // aria-required-parent is ignored here because this component is only a single option which is internally always
       // used together with a parent so we disable the failing rule here
@@ -21,8 +21,42 @@ test.describe("Screenshot tests", () => {
       component: (column, row) => (
         <OnyxListboxOption
           aria-label="Label"
-          aria-selected={column === "selected" && row !== "multiple"}
-          aria-checked={column === "selected" && row === "multiple"}
+          aria-selected={column === "selected"}
+          active={row === "focus-visible"}
+          aria-disabled={state === "disabled"}
+          multiple={["multiple", "indeterminate"].includes(row)}
+          {...{ role: "option" }}
+        >
+          Test label
+        </OnyxListboxOption>
+      ),
+      beforeScreenshot: async (component, page, column, row) => {
+        if (row === "hover") await component.hover();
+      },
+    });
+  }
+});
+
+test.describe("Multiselect Screenshot tests", () => {
+  for (const state of ["default", "disabled"] as const) {
+    executeMatrixScreenshotTest({
+      name: `Multiselect Listbox option (${state})`,
+      columns: ["default", "checked", "indeterminate"],
+      rows: ["default", "hover", "focus-visible"],
+      // TODO: color-contrast: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
+      // aria-required-parent is ignored here because this component is only a single option which is internally always
+      // used together with a parent so we disable the failing rule here
+      disabledAccessibilityRules: [
+        "aria-required-parent",
+        "color-contrast",
+        // TODO: as part of https://github.com/SchwarzIT/onyx/issues/732,
+        // the following disabled rule must be removed / fixed.
+        "nested-interactive",
+      ],
+      component: (column, row) => (
+        <OnyxListboxOption
+          aria-label="Label"
+          aria-checked={column === "checked"}
           active={row === "focus-visible"}
           aria-disabled={state === "disabled"}
           multiple={["multiple", "indeterminate"].includes(row)}
