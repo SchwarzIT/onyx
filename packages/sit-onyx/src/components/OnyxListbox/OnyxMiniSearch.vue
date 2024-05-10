3e7638cab48f8f33c5f0119711ee2c51a214c572
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-05-10 14:51:44 +0200

    add v-model support for OnyxMiniSearch

--- packages/sit-onyx/src/components/OnyxListbox/OnyxMiniSearch.vue
+++ packages/sit-onyx/src/components/OnyxListbox/OnyxMiniSearch.vue
@@ -1,14 +1,41 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import OnyxIcon from "../OnyxIcon/OnyxIcon.vue";
 import xSmall from "@sit-onyx/icons/x-small.svg?raw";
 
 defineOptions({ inheritAttrs: false });
+
+const props = defineProps<{ modelValue: string }>();
+
+const emit = defineEmits<{
+  "update:modelValue": [input: string];
+}>();
+
+/**
+ * Current value (with getter and setter) that can be used as "v-model" for the native input.
+ */
+const value = computed({
+  get: () => props.modelValue,
+  set: (value) => emit("update:modelValue", value),
+});
 </script>
 <template>
   <div class="onyx-mini-search">
     <!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label -->
-    <input v-bind="$attrs" class="onyx-mini-search__input" placeholder="Search" type="text" />
-    <button class="onyx-mini-search__clear">
+    <input
+      v-bind="$attrs"
+      v-model="value"
+      class="onyx-mini-search__input"
+      placeholder="Search"
+      type="text"
+    />
+    <!-- We use `@mousedown.prevent` here to not lose the input focus when the button is clicked  -->
+    <button
+      class="onyx-mini-search__clear"
+      aria-hidden="true"
+      tabindex="-1"
+      @mousedown.prevent="value = ''"
+    >
       <OnyxIcon :icon="xSmall" />
     </button>
   </div>
