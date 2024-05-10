35d4647949a4998bf92fb4b073165b176665a01e
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-05-10 13:30:55 +0300

    Export duplicated code in a constant

--- packages/sit-onyx/src/components/OnyxListboxOption/OnyxListboxOption.ct.tsx
+++ packages/sit-onyx/src/components/OnyxListboxOption/OnyxListboxOption.ct.tsx
@@ -3,6 +3,17 @@ import { DENSITIES } from "../../composables/density";
 import { executeMatrixScreenshotTest } from "../../playwright/screenshots";
 import OnyxListboxOption from "./OnyxListboxOption.vue";
 
+const disabledRules = [
+  // aria-required-parent is ignored here because this component is only a single option which is internally always
+  // used together with a parent so we disable the failing rule here
+  "aria-required-parent",
+  // TODO: color-contrast: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
+  "color-contrast",
+  // TODO: as part of https://github.com/SchwarzIT/onyx/issues/1026,
+  // the following disabled rule should be removed.
+  "nested-interactive",
+];
+
 test.describe("Single select screenshot tests", () => {
   for (const state of ["default", "disabled", "danger"] as const) {
     executeMatrixScreenshotTest({
@@ -38,16 +49,7 @@ test.describe("Multiselect Screenshot tests", () => {
       name: `Multiselect Listbox option (${state})`,
       columns: ["default", "checked", "indeterminate"],
       rows: ["default", "hover", "focus-visible"],
-      // aria-required-parent is ignored here because this component is only a single option which is internally always
-      // used together with a parent so we disable the failing rule here
-      disabledAccessibilityRules: [
-        "aria-required-parent",
-        // TODO: color-contrast: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
-        "color-contrast",
-        // TODO: as part of https://github.com/SchwarzIT/onyx/issues/1026,
-        // the following disabled rule should be removed.
-        "nested-interactive",
-      ],
+      disabledAccessibilityRules: disabledRules,
       component: (column, row) => (
         <OnyxListboxOption
           aria-label="Label"
@@ -73,16 +75,7 @@ test.describe("Density Screenshot tests", () => {
     name: "Listbox option (densities)",
     columns: DENSITIES,
     rows: ["default", "multiselect"],
-    // aria-required-parent is ignored here because this component is only a single option which is internally always
-    // used together with a parent so we disable the failing rule here
-    disabledAccessibilityRules: [
-      "aria-required-parent",
-      // TODO: color-contrast: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
-      "color-contrast",
-      // TODO: as part of https://github.com/SchwarzIT/onyx/issues/1026,
-      // the following disabled rule should be removed.
-      "nested-interactive",
-    ],
+    disabledAccessibilityRules: disabledRules,
     component: (column, row) => (
       <OnyxListboxOption
         aria-label="Label"
