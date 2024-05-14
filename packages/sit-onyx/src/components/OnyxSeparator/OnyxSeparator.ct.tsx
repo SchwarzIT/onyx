76333c1b54d86e29a64d897878a7ee6183884c02
Lars Rickert <lars.rickert@mail.schwarz>
2024-05-14 13:33:25 +0200

    separator: test for aria roles

--- packages/sit-onyx/src/components/OnyxSeparator/OnyxSeparator.ct.tsx
+++ packages/sit-onyx/src/components/OnyxSeparator/OnyxSeparator.ct.tsx
@@ -1,4 +1,4 @@
-import { test } from "../../playwright/a11y";
+import { expect, test } from "../../playwright/a11y";
 import { executeMatrixScreenshotTest } from "../../playwright/screenshots";
 import OnyxSeparator from "./OnyxSeparator.vue";
 
@@ -8,5 +8,11 @@ test.describe("Screenshot tests", () => {
     columns: ["default"],
     rows: ["default"],
     component: () => <OnyxSeparator />,
+    beforeScreenshot: async (component) => {
+      await expect(component.getByRole("separator")).toHaveAttribute(
+        "aria-orientation",
+        "vertical",
+      );
+    },
   });
 });
