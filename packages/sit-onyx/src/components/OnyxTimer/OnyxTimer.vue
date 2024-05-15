d9692f37ce97fd925a87323f62e1e1cf601a22f4
Oliver Müller <oliver.mueller2@mail.schwarz>
2024-05-15 16:16:00 +0200

    feat: format time left

--- packages/sit-onyx/src/components/OnyxTimer/OnyxTimer.vue
+++ packages/sit-onyx/src/components/OnyxTimer/OnyxTimer.vue
@@ -2,6 +2,7 @@
 import { computed, onBeforeUnmount, onMounted, toRef, watch } from "vue";
 import type { OnyxTimerProps } from "./types";
 import { useTimer } from "../../composables/useTimer";
+import { injectI18n } from "../../i18n";
 
 const props = withDefaults(defineProps<OnyxTimerProps>(), {
   endTime: new Date().toISOString(),
@@ -11,18 +12,31 @@ const emit = defineEmits<{
   timerEnded: [];
 }>();
 
+const { t } = injectI18n();
+
 const { startTimer, endTimer, timeLeft, isEnded } = useTimer({
   endTime: toRef(props, "endTime"),
   isPaused: toRef(props, "isPaused"),
 });
 
-// TODO: support hours, use i18n for labels
+// format the time left in hours, minutes and seconds, could be moved to utils
 const formattedTime = computed(() => {
+  const hours = Math.floor(timeLeft.value / 60000 / 60);
   const minutes = Math.floor(timeLeft.value / 60000);
-  const seconds = Math.round((timeLeft.value % 60000) / 1000);
-  return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  const seconds = Math.floor((timeLeft.value % 60000) / 1000);
+  let time = "";
+  let label = t.value("time.seconds", { n: seconds });
+  if (minutes > 0) {
+    label = t.value("time.minutes", { n: minutes });
+  }
+  if (hours > 0) {
+    label = t.value("time.hours", { n: hours });
+    time = `${hours.toString().padStart(2, "0")}:`;
+  }
+  return `${time}${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")} ${label}`;
 });
 
+//TODO: duration
 const formattedDuration = computed(() => {
   return "tbd";
 });
