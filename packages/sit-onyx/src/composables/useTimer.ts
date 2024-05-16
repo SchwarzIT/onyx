439f23778da2a888389119d3177a3d271f32c71b
Oliver Müller <oliver.mueller2@mail.schwarz>
2024-05-16 14:21:48 +0200

    docs(timer): add

--- packages/sit-onyx/src/composables/useTimer.ts
+++ packages/sit-onyx/src/composables/useTimer.ts
@@ -1,11 +1,16 @@
 import { computed, ref, watch, type Ref } from "vue";
 
 type TimerOptions = {
-  endTime: Ref<string>; // expect ISO Date string
+  endTime: Ref<string> /** expect ISO string @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString} */;
   isPaused?: Ref<boolean>;
   startImmediately?: boolean;
 };
 
+/**
+ * Timer composable
+ * @param options @see TimerOptions
+ * @returns composable to start and stop a timer, get the time left and track if the timer has ended.
+ */
 export const useTimer = (options: TimerOptions) => {
   const intervalId = ref<ReturnType<typeof setInterval>>();
   const timeLeft = ref(0);
