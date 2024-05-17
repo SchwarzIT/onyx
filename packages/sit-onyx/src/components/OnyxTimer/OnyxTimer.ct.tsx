d044dd342519c7429981c2a108142b8081fb2da6
Oliver Müller <oliver.mueller2@mail.schwarz>
2024-05-17 11:53:00 +0200

    refactor(timer-spec): use fixed now

--- packages/sit-onyx/src/components/OnyxTimer/OnyxTimer.ct.tsx
+++ packages/sit-onyx/src/components/OnyxTimer/OnyxTimer.ct.tsx
@@ -2,11 +2,8 @@ import { executeMatrixScreenshotTest } from "src/playwright/screenshots";
 import { expect, test } from "../../playwright/a11y";
 import OnyxTimer from "./OnyxTimer.vue";
 
-const endTime = new Date();
-endTime.setTime(Date.now() + 30 * 1000);
-
-const endTimeEvent = new Date();
-endTimeEvent.setTime(Date.now() + 2000);
+const endTime = new Date("June 21 2026 13:00:30");
+const endTimeEvent = new Date("June 21 2026 13:00:02");
 
 const defaultProps = {
   endTime: endTime.toISOString(),
@@ -14,14 +11,21 @@ const defaultProps = {
 };
 
 test.describe("Timer", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.evaluate(() => {
+      const fakeNow = new Date("June 21 2026 13:00:00").valueOf();
+      const __DateNowOffset = fakeNow - Date.now();
+      const __DateNow = Date.now;
+      Date.now = () => __DateNow() + __DateNowOffset;
+    });
+  });
   test("should render timer", async ({ mount }) => {
     // ARRANGE
     const component = await mount(OnyxTimer, {
       props: defaultProps,
     });
 
-    // can't rely on exact seconds since browser rendering is delayed
-    await expect(component).toContainText(/00:\d{2} seconds/);
+    await expect(component).toContainText("00:29 seconds");
   });
 
   test.describe("Screenshot tests", () => {
@@ -43,14 +47,14 @@ test.describe("Timer", () => {
   test("should render timer with no time left", async ({ mount }) => {
     // ARRANGE
     const component = await mount(OnyxTimer, {
-      props: { ...defaultProps, endTime: new Date().toISOString() },
+      props: { ...defaultProps, endTime: new Date("June 21 2026 13:00:00").toISOString() },
     });
 
     // ASSERT
     await expect(component).toContainText(/00:00 seconds/);
   });
 
-  test("emits event when timer is finished", async ({ mount, page }) => {
+  test("emits event when timer is finished and renders 2 seconds", async ({ mount, page }) => {
     let timerEnded = false;
     // ARRANGE
     const component = await mount(
