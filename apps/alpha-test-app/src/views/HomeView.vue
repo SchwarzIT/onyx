01d31c11fc0d372b229e9856268200c5da0ab44f
Lars Rickert <lars.rickert@mail.schwarz>
2024-05-21 12:25:53 +0200

    add examples to alpha test app and storybook

--- apps/alpha-test-app/src/views/HomeView.vue
+++ apps/alpha-test-app/src/views/HomeView.vue
@@ -21,6 +21,7 @@ import {
   OnyxSwitch,
   OnyxTable,
   OnyxTag,
+  OnyxTimer,
   OnyxTooltip,
   type ListboxOption,
   type SelectOption,
@@ -50,6 +51,7 @@ const COMPONENTS = [
   "OnyxSwitch",
   "OnyxTable",
   "OnyxTag",
+  "OnyxTimer",
   "OnyxTooltip",
 ] as const;
 
@@ -118,6 +120,9 @@ const groupedListboxOptions: ListboxOption[] = [
 
 const multiSelectState = ref(selectOptions.slice(0, 5));
 const singleSelectState = ref(selectOptions[0]);
+
+const timerEndDate = new Date();
+timerEndDate.setHours(timerEndDate.getHours() + 2);
 </script>
 
 <template>
@@ -281,6 +286,8 @@ const singleSelectState = ref(selectOptions[0]);
         </OnyxTable>
 
         <OnyxTag v-if="show('OnyxTag')" label="Example tag" :icon="emojiHappy2" />
+
+        <OnyxTimer v-if="show('OnyxTimer')" label="Timer" :end-time="timerEndDate" />
 
         <OnyxTooltip v-if="show('OnyxTooltip')" text="Example tooltip text">
           Hover me to show tooltip
