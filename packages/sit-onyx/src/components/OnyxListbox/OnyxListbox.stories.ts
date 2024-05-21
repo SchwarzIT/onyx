9db27456cca12a25abe72323b28296c41c8f6307
Lars Rickert <lars.rickert@mail.schwarz>
2024-05-21 09:33:46 +0200

    remove TODO

--- packages/sit-onyx/src/components/OnyxListbox/OnyxListbox.stories.ts
+++ packages/sit-onyx/src/components/OnyxListbox/OnyxListbox.stories.ts
@@ -196,7 +196,6 @@ export const ButtonLoading = {
   args: {
     ...Default.args,
   },
-  // TODO: check generated source code for this
   render: (args) => ({
     setup: () => {
       return { args, ...useLazyLoading(args.options) };
