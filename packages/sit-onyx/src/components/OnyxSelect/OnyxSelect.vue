dc2125ec4d0b751e77454f8461d259bd849dcec1
Lars Rickert <lars.rickert@mail.schwarz>
2024-06-04 12:55:06 +0200

    fix(OnyxSelect): hide check all when searching (#1226)
    Relates to
    https://github.com/SchwarzIT/onyx/issues/565#issuecomment-2144776218
    
    - hide check all when searching
    - update docs

--- packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
+++ packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
@@ -64,7 +64,7 @@ const selectRef = ref<HTMLElement>();
 const activeValue = ref<TValue>();
 
 /**
- * Current value but always as array (even if not multiselect) so its easier
+ * Current value but always as array (even if not multiselect) so it is easier
  * to work with it in a unified way.
  */
 const arrayValue = computed(() => {
@@ -296,7 +296,7 @@ const selectInputProps = computed(() => {
               </li>
 
               <!-- select-all option for "multiple" -->
-              <template v-if="props.multiple && props.withCheckAll">
+              <template v-if="props.multiple && props.withCheckAll && !props.searchTerm">
                 <OnyxSelectOption
                   v-bind="
                     headlessOption({
