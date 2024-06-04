4712c0f18c381ba2d90a5d52d4f3a0f33aabaee7
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-06-04 13:22:19 +0300

    Remove test for info tooltip

--- packages/sit-onyx/src/components/OnyxInfoTooltip/OnyxInfoTooltip.ct.tsx
+++ packages/sit-onyx/src/components/OnyxInfoTooltip/OnyxInfoTooltip.ct.tsx
@@ -3,47 +3,6 @@ import type { Locator } from "@playwright/test";
 import { executeMatrixScreenshotTest } from "../../playwright/screenshots";
 import OnyxInfoTooltip from "./OnyxInfoTooltip.vue";
 
-test("should trigger with hover", async ({ mount, page }) => {
-  // ARRANGE
-  let component = await mount(OnyxInfoTooltip, {
-    props: {
-      text: "Test tooltip",
-    },
-  });
-
-  let tooltip = component.getByRole("tooltip");
-
-  // ASSERT
-  await expect(tooltip).toBeHidden();
-
-  // ACT
-  await component.hover();
-
-  // ASSERT
-  await expect(tooltip).toBeHidden(); // should use debounce to show tooltip only after a short delay
-  await expect(tooltip).toBeVisible();
-
-  await page.mouse.move(0, 0);
-  await expect(tooltip).toBeVisible(); // should use debounce to hide tooltip only after a short delay
-  await expect(tooltip).toBeHidden();
-
-  // ACT
-  await component.unmount();
-  component = await mount(<OnyxInfoTooltip text="Test tooltip" />);
-  tooltip = component.getByRole("tooltip");
-
-  await page.keyboard.press("Tab");
-
-  // ASSERT
-  await expect(tooltip).toBeVisible();
-
-  // ACT
-  await page.keyboard.press("Escape");
-
-  // ASSERT
-  await expect(tooltip).toBeHidden();
-});
-
 test.describe("Screenshot tests", () => {
   const isTooltipVisible = async (tooltip: Locator) => {
     await expect(tooltip).toBeVisible();
