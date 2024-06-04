e7dc53eec59519c6f282da4e048840ca922e8bfb
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-06-04 14:22:31 +0300

    Reverse last commit -m

--- packages/sit-onyx/src/components/OnyxInput/OnyxInput.vue
+++ packages/sit-onyx/src/components/OnyxInput/OnyxInput.vue
@@ -84,15 +84,13 @@ const { t } = injectI18n();
         :class="[!props.required ? requiredMarkerClass : undefined]"
       >
         <div class="onyx-input__header">
-          <span>
-            <span class="onyx-truncation-ellipsis">
-              {{ props.label }}
-            </span>
-            <span
-              v-if="props.required"
-              :class="[props.required ? requiredMarkerClass : undefined]"
-            ></span>
+          <span class="onyx-truncation-ellipsis">
+            {{ props.label }}
           </span>
+          <span
+            v-if="props.required"
+            :class="[props.required ? requiredMarkerClass : undefined]"
+          ></span>
           <OnyxInfoTooltip v-if="props.labelTooltip" :text="props.labelTooltip" />
           <span v-if="!props.required" class="onyx-input__optional">{{ t("optional") }}</span>
         </div>
