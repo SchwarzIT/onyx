e3aacff0e287e841bd5298ae92b05ad093cdb71d
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-06-05 15:59:05 +0300

    Replace NonNullable with Required

--- packages/sit-onyx/src/components/OnyxInfoTooltip/types.ts
+++ packages/sit-onyx/src/components/OnyxInfoTooltip/types.ts
@@ -1,4 +1,4 @@
 import type { OnyxTooltipProps } from "../OnyxTooltip/types";
 
-export type OnyxInfoTooltipProps = NonNullable<Pick<OnyxTooltipProps, "text">> &
+export type OnyxInfoTooltipProps = Required<Pick<OnyxTooltipProps, "text">> &
   Pick<OnyxTooltipProps, "position">;
