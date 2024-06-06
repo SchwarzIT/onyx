4fd1cce6e7a9bb1e9ba855dbfcb2d5f483ee8fca
Lars Rickert <lars.rickert@mail.schwarz>
2024-06-06 16:10:13 +0200

    fix import

--- packages/sit-onyx/src/components/OnyxMobileNavButton/OnyxMobileNavButton.stories.ts
+++ packages/sit-onyx/src/components/OnyxMobileNavButton/OnyxMobileNavButton.stories.ts
@@ -2,7 +2,7 @@ import menu from "@sit-onyx/icons/menu.svg?raw";
 import { defineStorybookActionsAndVModels } from "@sit-onyx/storybook-utils";
 import type { Meta, StoryObj } from "@storybook/vue3";
 import { defineIconSelectArgType } from "../../utils/storybook";
-import OnyxMobileNavButton from "./MobileNavButton.vue";
+import OnyxMobileNavButton from "./OnyxMobileNavButton.vue";
 
 /**
  * Nav button that is mainly used inside the nav bar on mobile, e.g. for the burger and context menu buttons.
