62cb2cad16ebcf66246e70ced6409632da866a43
Lars Rickert <lars.rickert@mail.schwarz>
2024-06-12 13:34:12 +0200

    feat: support `global.css` file (#1280)
    Relates to #1266
    
    Support `global.css` file that can be optionally imported by a project
    to apply global application styles.
    Will be imported by default for Nuxt projects.

---
+++ .changeset/healthy-actors-yawn.md
@@ -0,0 +1,7 @@
+---
+"sit-onyx": minor
+---
+
+feat: support `global.css` file
+
+See [Getting Started guide](https://onyx.schwarz/development/#installation) for further information.
--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "sit-onyx/global.css";
 import "sit-onyx/style.css";
-import "./styles/index.scss";
 
 // set default select app for the Vue devtools to the inner user app, not the parent application/playground
 (window as Window & { VUE_DEVTOOLS_CONFIG?: unknown }).VUE_DEVTOOLS_CONFIG = {
