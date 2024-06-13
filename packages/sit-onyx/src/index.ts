23357d4da7e59a8ae5af10f416c45114b8ee0601
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-06-13 13:59:46 +0300

    Remove multiple exports

--- packages/sit-onyx/src/index.ts
+++ packages/sit-onyx/src/index.ts
@@ -104,8 +104,6 @@ export * from "./components/OnyxUserMenu/types";
 export { default as OnyxVisuallyHidden } from "./components/OnyxVisuallyHidden/OnyxVisuallyHidden.vue";
 export * from "./components/OnyxVisuallyHidden/types";
 
-export { default as OnyxFlyoutMenu } from "./components/OnyxFlyoutMenu/OnyxFlyoutMenu.vue";
-
 export * from "./composables/density";
 export * from "./composables/scrollEnd";
 export type { OnyxTranslations, ProvideI18nOptions } from "./i18n";
