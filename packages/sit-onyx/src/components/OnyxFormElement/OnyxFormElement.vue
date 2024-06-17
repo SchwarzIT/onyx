eb0d0fdc997085902c7ca98efc62dbf7840715ad
boppli <linda.bopp@mail.schwarz>
2024-06-17 11:52:34 +0200

    separate styles

--- packages/sit-onyx/src/components/OnyxFormElement/OnyxFormElement.vue
+++ packages/sit-onyx/src/components/OnyxFormElement/OnyxFormElement.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import { useRequired } from "../../composables/required";
-import type { OnyxFormElementProps } from "./types";
-import OnyxInfoTooltip from "../OnyxInfoTooltip/OnyxInfoTooltip.vue";
 import { injectI18n } from "../../i18n";
+import OnyxInfoTooltip from "../OnyxInfoTooltip/OnyxInfoTooltip.vue";
+import type { OnyxFormElementProps } from "./types";
 
 const props = withDefaults(defineProps<OnyxFormElementProps>(), {
   required: false,
@@ -79,7 +79,6 @@ defineSlots<{
 
 <style lang="scss">
 @use "../../styles/mixins/layers.scss";
-@use "../../styles/mixins/input.scss";
 
 .onyx-use-optional:not(:has(.onyx-required-marker)) {
   .onyx-form-element__optional {
@@ -89,11 +88,31 @@ defineSlots<{
 
 .onyx-form-element {
   @include layers.component() {
-    @include input.define-shared-styles(
-      $base-selector: ".onyx-form-element",
-      // todo move frame related stuff out of the mixin, remove mixin here.
-      $vertical-padding: var(--onyx-spacing-2xs)
-    );
+    &__label {
+      display: flex;
+      margin-bottom: var(--onyx-spacing-5xs);
+      color: var(--onyx-color-text-icons-neutral-medium);
+
+      // optional marker should be displayed at the very end of the label
+      &.onyx-optional-marker {
+        justify-content: space-between;
+
+        &::after {
+          display: none;
+        }
+      }
+    }
+
+    &__optional {
+      display: none;
+      font-family: var(--onyx-font-family);
+      font-weight: 400;
+      font-style: normal;
+      color: var(--onyx-color-text-icons-neutral-soft);
+      padding-left: var(--onyx-spacing-2xs);
+      hyphens: none;
+      margin-left: auto;
+    }
 
     &__header {
       display: flex;
@@ -107,6 +126,19 @@ defineSlots<{
         height: 1rem;
         align-self: center;
       }
+    }
+
+    &__footer {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      color: var(--onyx-color-text-icons-neutral-soft);
+    }
+
+    &__counter {
+      text-align: right;
+      flex-grow: 1;
+      margin-left: var(--onyx-spacing-2xs);
     }
   }
 }
