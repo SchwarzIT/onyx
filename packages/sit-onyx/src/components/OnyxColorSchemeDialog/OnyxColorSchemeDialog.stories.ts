b7fc7f0c6d3fc714bf650d2431f4a314b8c7f5b6
Lars Rickert <lars.rickert@mail.schwarz>
2024-06-17 12:00:27 +0200

    update docs

--- packages/sit-onyx/src/components/OnyxColorSchemeDialog/OnyxColorSchemeDialog.stories.ts
+++ packages/sit-onyx/src/components/OnyxColorSchemeDialog/OnyxColorSchemeDialog.stories.ts
@@ -5,24 +5,7 @@ import OnyxColorSchemeDialog from "./OnyxColorSchemeDialog.vue";
 /**
  * Pre-built dialog where the user can select which color scheme (light/dark mode or auto) to use for the application.
  *
- * ## Usage
- *
- * The color scheme dialog can easily be used together with [`@vueuse/core`](https://vueuse.org/core/useColorMode):
- *
- * ```html
- * <script lang="ts" setup>
- * import { useColorMode } from "@vueuse/core";
- * import { ref } from "vue";
- * import { OnyxColorSchemeDialog } from "sit-onyx";
- *
- * const { store: colorScheme } = useColorMode();
- * const isOpen = ref(true);
- * </script>
- *
- * <template>
- *    <OnyxColorSchemeDialog v-model="colorScheme" :open="isOpen" @close="isOpen = false" />
- * </template>
- * ```
+ * For recommended usage, please visit our [theming docs](https://onyx.schwarz/development/theming.html).
  */
 const meta: Meta<typeof OnyxColorSchemeDialog> = {
   title: "support/ColorSchemeDialog",
