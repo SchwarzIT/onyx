83de46829eda0788c1cf248912a269e8296841bf
Lars Rickert <lars.rickert@mail.schwarz>
2024-06-17 17:23:50 +0200

    update example

--- packages/sit-onyx/src/components/OnyxNavItem/OnyxNavItem.stories.ts
+++ packages/sit-onyx/src/components/OnyxNavItem/OnyxNavItem.stories.ts
@@ -1,8 +1,7 @@
 import { defineStorybookActionsAndVModels } from "@sit-onyx/storybook-utils";
 import type { Meta, StoryObj } from "@storybook/vue3";
-import { computed, h, provide } from "vue";
+import { h } from "vue";
 import OnyxBadge from "../OnyxBadge/OnyxBadge.vue";
-import { mobileNavBarInjectionKey } from "../OnyxNavBar/types";
 import OnyxNavItem from "./OnyxNavItem.vue";
 
 /**
@@ -78,24 +77,4 @@ export const WithExternalLink = {
     label: "onyx",
     href: "https://onyx.schwarz/",
   },
-} satisfies Story;
-
-/**
- * This example shows the nav item in mobile mode (will be provided by the OnyxNavBar).
- */
-export const Mobile = {
-  args: {
-    ...WithOptions.args,
-  },
-  render: (args) => ({
-    components: { OnyxNavItem },
-    setup() {
-      provide(
-        mobileNavBarInjectionKey,
-        computed(() => true),
-      );
-      return { args };
-    },
-    template: '<OnyxNavItem v-bind="args" style="width: 24rem;" />',
-  }),
 } satisfies Story;
