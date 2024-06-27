4a01a72243b00b1bf5146c2580d740fb6a8556ea
Lars Rickert <lars.rickert@mail.schwarz>
2024-06-27 16:54:13 +0200

    update screenshots

--- packages/sit-onyx/src/components/OnyxToastProvider/OnyxToastProvider.ct.tsx
+++ packages/sit-onyx/src/components/OnyxToastProvider/OnyxToastProvider.ct.tsx
@@ -1,26 +1,31 @@
 import { expect, test } from "../../playwright/a11y";
+import { ONYX_BREAKPOINTS } from "../../types";
 import PlaywrightTestWrapper from "./PlaywrightTestWrapper.vue";
 
-test("should render", async ({ mount, makeAxeBuilder, page }) => {
-  // ARRANGE
-  const component = await mount(<PlaywrightTestWrapper />);
+Object.entries(ONYX_BREAKPOINTS).forEach(([breakpoint, width]) => {
+  test(`should render (${breakpoint})`, async ({ mount, makeAxeBuilder, page }) => {
+    await page.setViewportSize({ width, height: 512 });
 
-  // ASSERT
-  await expect(page).toHaveScreenshot("default.png");
+    // ARRANGE
+    const component = await mount(<PlaywrightTestWrapper />);
 
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder()
-    // TODO: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
-    .disableRules(["color-contrast"])
-    .analyze();
+    // ASSERT
+    await expect(page).toHaveScreenshot(`breakpoint-${breakpoint}.png`);
 
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
-  await expect(component).toContainText("Example toast 1");
+    // ACT
+    const accessibilityScanResults = await makeAxeBuilder()
+      // TODO: remove when contrast issues are fixed in https://github.com/SchwarzIT/onyx/issues/410
+      .disableRules(["color-contrast"])
+      .analyze();
 
-  // ACT
-  await component.getByLabel("Close").nth(4).click();
+    // ASSERT
+    expect(accessibilityScanResults.violations).toEqual([]);
+    await expect(component).toContainText("Example toast 1");
 
-  // ASSERT
-  await expect(component).not.toContainText("Example toast 1");
+    // ACT
+    await component.getByLabel("Close").nth(4).click();
+
+    // ASSERT
+    await expect(component).not.toContainText("Example toast 1");
+  });
 });
