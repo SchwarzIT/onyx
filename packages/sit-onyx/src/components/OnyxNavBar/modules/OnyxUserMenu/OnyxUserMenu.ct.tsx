a89118eaf28b2db3cb7649dbac953190f2483941
Lars Rickert <lars.rickert@mail.schwarz>
2024-07-03 13:56:47 +0200

    fix user menu screenshots

--- packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxUserMenu/OnyxUserMenu.ct.tsx
+++ packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxUserMenu/OnyxUserMenu.ct.tsx
@@ -44,7 +44,7 @@ test.describe("Screenshot tests", () => {
         await component.evaluate((element) => {
           element.style.height = `${element.scrollHeight}px`;
           element.style.width = `${element.scrollWidth}px`;
-          element.style.paddingLeft = "48px";
+          element.style.paddingLeft = "64px";
         });
       }
     },
