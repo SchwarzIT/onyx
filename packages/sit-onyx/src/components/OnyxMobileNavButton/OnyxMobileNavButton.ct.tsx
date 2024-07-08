103b24b0e62018e5319cea6625945857a32c113f
boppli <linda.bopp@mail.schwarz>
2024-07-08 18:32:47 +0200

    playwright test

--- packages/sit-onyx/src/components/OnyxMobileNavButton/OnyxMobileNavButton.ct.tsx
+++ packages/sit-onyx/src/components/OnyxMobileNavButton/OnyxMobileNavButton.ct.tsx
@@ -1,18 +1,54 @@
-import { test } from "../../playwright/a11y";
+import { expect, test } from "../../playwright/a11y";
 import { executeMatrixScreenshotTest, mockPlaywrightIcon } from "../../playwright/screenshots";
 import OnyxMobileNavButton from "./OnyxMobileNavButton.vue";
+
+test("should render without errors", async ({ mount, makeAxeBuilder }) => {
+  await mount(
+    <OnyxMobileNavButton
+      label="Label"
+      icon={mockPlaywrightIcon}
+      open
+      // playwright sets 8px on the body which interfers with the positioning of the flyout
+      style="--top-position: 4rem"
+    >
+      Lorem ipsum dolor sit amet consectetur. Non in felis erat velit consectetur. Sed integer non
+      hac viverra nibh vehicula risus ultrices. Molestie cras lobortis vitae gravida et ut. Turpis
+      nisl pharetra amet ante eu sagittis sit elementum ut.
+    </OnyxMobileNavButton>,
+  );
+
+  // accessibility tests
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
+  expect(accessibilityScanResults.violations, "should pass accessibility checks").toEqual([]);
+});
 
 test.describe("Screenshot tests", () => {
   executeMatrixScreenshotTest({
     name: "Mobile nav button",
     columns: ["default", "open"],
     rows: ["default", "hover", "active", "focus-visible"],
+    disablePadding: true,
     component: (column) => (
-      <OnyxMobileNavButton label="Label" icon={mockPlaywrightIcon} open={column === "open"}>
+      <OnyxMobileNavButton
+        label="Label"
+        icon={mockPlaywrightIcon}
+        open={column === "open"}
+        // playwright sets 8px on the body which interfers with the positioning of the flyout
+        style="--top-position: 4rem"
+      >
         Flyout slot content...
       </OnyxMobileNavButton>
     ),
     beforeScreenshot: async (component, page, column, row) => {
+      if (column === "open") {
+        // TODO: how can the viewport be set for each screenshot but not for the screenshot matrix?
+        await page.setViewportSize({ width: 200, height: 300 });
+        await component.evaluate((element) => {
+          element.style.height = `${300}px`;
+          element.style.width = `${200}px`;
+        });
+      }
+
       if (row === "hover") await component.hover();
       if (row === "focus-visible") await page.keyboard.press("Tab");
       if (row === "active") {
