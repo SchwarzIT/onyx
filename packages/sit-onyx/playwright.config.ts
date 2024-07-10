078eb9de48b09519ce23f774d8bd7a3ac12f8ed7
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-07-10 15:52:54 +0200

    revert playwright projects

--- packages/sit-onyx/playwright.config.ts
+++ packages/sit-onyx/playwright.config.ts
@@ -40,5 +40,9 @@ export default defineConfig({
     },
   },
   /* Configure projects for major browsers */
-  projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"] } }],
+  projects: [
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    { name: "webkit", use: { ...devices["Desktop Safari"] } },
+  ],
 });
