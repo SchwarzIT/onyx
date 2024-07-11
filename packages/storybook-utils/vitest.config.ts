5f9b81af5fdc75daed747b3b8d27e4e77ba815d7
Lars Rickert <lars.rickert@mail.schwarz>
2024-07-11 10:22:39 +0200

    fix unit tests

--- packages/storybook-utils/vitest.config.ts
+++ packages/storybook-utils/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     include: ["src/**/*.spec.ts"],
+    setupFiles: ["vitest.setup.ts"],
     coverage: {
       include: ["src"],
       provider: "v8",
