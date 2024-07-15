63de9ad3653d0760724d845cc6d946f61a6ee008
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-07-15 09:09:54 +0200

    fix merge issue

--- packages/headless/src/utils/vitest.ts
+++ packages/headless/src/utils/vitest.ts
@@ -22,41 +22,6 @@ export const mockVueLifecycle = () => {
   }));
 
   vi.mock("vue", async (original) => ({
-    ...((await original()) as object),
-    onBeforeMount: (cb: Callback) => cb(),
-    onMounted: (cb: Callback) => cb(),
-    onBeforeUnmount: (cb: Callback) => (callbacks.onBeforeUnmountedCb = cb),
-    onUnmounted: (cb: Callback) => (callbacks.onUnmountedCb = cb),
-  }));
-
-  return async () => {
-    await callbacks.onBeforeUnmountedCb?.();
-    await callbacks.onUnmountedCb?.();
-  };
-};
-
-type Callback = () => Awaitable<void>;
-
-/**
- * Mocks the following vue lifecycle functions:
- * - onBeforeMount
- * - onMounted
- * - onBeforeUnmount
- * - onUnmounted
- *
- * `onBeforeMount` and `onMounted` callbacks are executed immediately.
- * `onBeforeUnmount` and `onUnmounted` are executed when the returned callback is run.
- * @returns a callback to trigger the run of `onBeforeUnmount` and `onUnmounted`
- */
-export const mockVueLifecycle = () => {
-  const { callbacks } = vi.hoisted(() => ({
-    callbacks: {
-      onBeforeUnmountedCb: null as Callback | null,
-      onUnmountedCb: null as Callback | null,
-    },
-  }));
-
-  vi.mock("vue", async (original) => ({
     ...((await original()) as typeof import("vue")),
     onBeforeMount: vi.fn((cb: Callback) => cb()),
     onMounted: vi.fn((cb: Callback) => cb()),
