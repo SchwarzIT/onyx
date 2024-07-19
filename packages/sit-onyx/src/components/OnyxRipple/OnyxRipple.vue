516b31204585af695cfa37041fc4f4da4c43c24c
Oliver Müller <oliver.mueller2@mail.schwarz>
2024-07-19 17:00:24 +0200

    feat: add ripple component

---
+++ packages/sit-onyx/src/components/OnyxRipple/OnyxRipple.vue
@@ -0,0 +1,85 @@
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import { RippleConfig, useRippleRenderer } from "../../composables/useRippleRenderer";
+
+/**
+    - check prefers motion setting
+ */
+
+const rippleTrigger = ref<HTMLElement>();
+
+const config = computed<RippleConfig>(() => ({
+  terminateOnPointerUp: false,
+  color: "var(--onyx-ripple-color)",
+  trigger: rippleTrigger,
+  duration: "300ms",
+  durationLeave: "100ms",
+}));
+
+const { ripples, hideRipple, events } = useRippleRenderer(config);
+</script>
+
+<template>
+  <span ref="rippleTrigger" class="onyx-ripple" v-on="events">
+    <transition-group name="ripple" @after-enter="hideRipple">
+      <span
+        v-for="[key, r] in ripples"
+        :key="key"
+        class="onyx-ripple__element"
+        :style="{
+          left: r.x + 'px',
+          top: r.y + 'px',
+          width: r.radius + 'px',
+          height: r.radius + 'px',
+          backgroundColor: r.backgroundColor,
+        }"
+        :data-rippleid="key"
+      ></span>
+    </transition-group>
+  </span>
+</template>
+
+<style lang="scss">
+@use "../../styles/mixins/layers.scss";
+@use "../../styles/mixins/density.scss";
+
+.onyx-ripple {
+  --onyx-ripple-color: var(--onyx-color-base-primary-800);
+  display: block;
+  position: absolute;
+  inset: 0;
+  overflow: hidden;
+  border-radius: inherit;
+
+  &__element {
+    display: block;
+    position: absolute;
+    border-radius: 50%;
+    pointer-events: none;
+    transition:
+      opacity,
+      transform 0ms cubic-bezier(0, 0, 0.2, 1);
+  }
+}
+
+.ripple-enter-active {
+  transition-property: all;
+  transition-timing-function: ease;
+  transition-duration: v-bind("config.duration");
+}
+
+.ripple-leave-active {
+  transition-property: all;
+  transition-timing-function: ease;
+  transition-duration: v-bind("config.durationLeave");
+}
+
+.ripple-enter-from {
+  opacity: 0;
+  scale: 0.5;
+}
+
+.ripple-leave-to {
+  opacity: 0;
+}
+</style>
