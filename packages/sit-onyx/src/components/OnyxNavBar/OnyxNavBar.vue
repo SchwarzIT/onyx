SchwarzIT/onyx

commit 86874c4137c330c2ce6d2a1434a69c0de5582167

chore: fix sass deprecation warnings (#1566)
sass is changing how nested rules are handled to align with native css
specs: https://sass-lang.com/documentation/breaking-changes/mixed-decls/

packages/sit-onyx/src/components/OnyxNavBar/OnyxNavBar.vue
@@ -194,6 +194,10 @@ $gap: var(--onyx-spacing-md);
     position: relative;
     container-type: size;
 
+    // sync with grid
+    max-width: var(--onyx-grid-max-width);
+    margin-inline: var(--onyx-grid-margin-inline);
+
     // implement bottom border with ::after so it does not add to the height
     &::after {
       content: " ";
@@ -217,10 +221,6 @@ $gap: var(--onyx-spacing-md);
         grid-template-columns: max-content max-content 1fr auto;
         grid-template-areas: "app back nav context";
       }
-
-      // sync with grid
-      max-width: var(--onyx-grid-max-width);
-      margin-inline: var(--onyx-grid-margin-inline);
     }
 
     &__back {

packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
@@ -411,8 +411,6 @@ const selectInputProps = computed(() => {
     $wrapper-padding: var(--onyx-spacing-2xs);
     $outline-size: 0.25rem;
 
-    @include list.styles();
-
     position: absolute;
     left: 0;
     top: calc(100% + $outline-size);
@@ -422,6 +420,8 @@ const selectInputProps = computed(() => {
     transition:
       visibility var(--onyx-duration-sm),
       opacity var(--onyx-duration-sm);
+
+    @include list.styles();
 
     &--open {
       visibility: visible;

packages/sit-onyx/src/components/OnyxSelectOption/OnyxSelectOption.vue
@@ -54,9 +54,10 @@ defineSlots<{
 
   @include layers.component() {
     &__checkbox {
-      @include checkbox.styles();
       // prevent the checkbox to get squished by a long label
       flex-shrink: 0;
+
+      @include checkbox.styles();
     }
   }
 }

packages/sit-onyx/src/components/OnyxTable/OnyxTable.vue
@@ -201,13 +201,13 @@ $border: var(--onyx-1px-in-rem) solid var(--onyx-color-base-neutral-300);
   }
 
   @include layers.component() {
-    @include define-borders();
-
     font-family: var(--onyx-font-family);
     color: var(--onyx-color-text-icons-neutral-intense);
     text-align: left;
     contain: paint;
     width: 100%;
+
+    @include define-borders();
 
     &__empty {
       &-content {

commit 7b8ad3d15170af082a0bbca37784a5bf7854e9c4

fix(OnyxNavBar): make bottom border full width (#1589)
Fix nav bar bottom bar to be full width when grid width is limited.
Regression caused by #1566

packages/sit-onyx/src/components/OnyxNavBar/OnyxNavBar.vue
@@ -199,10 +199,6 @@ $gap: var(--onyx-spacing-md);
     position: relative;
     container-type: size;
 
-    // sync with grid
-    max-width: var(--onyx-grid-max-width);
-    margin-inline: var(--onyx-grid-margin-inline);
-
     // implement bottom border with ::after so it does not add to the height
     &::after {
       content: " ";
@@ -221,6 +217,10 @@ $gap: var(--onyx-spacing-md);
       gap: $gap;
       height: 100%;
       padding-inline: var(--onyx-grid-margin);
+
+      // sync with grid
+      max-width: var(--onyx-grid-max-width);
+      margin-inline: var(--onyx-grid-margin-inline);
 
       &:has(.onyx-nav-bar__back) {
         grid-template-columns: max-content max-content 1fr auto;