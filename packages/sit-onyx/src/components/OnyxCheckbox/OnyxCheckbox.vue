52895949a78f04c897981d8d47dd57eb9618707c
boppli <linda.bopp@mail.schwarz>
2024-07-22 13:25:35 +0200

    feat(OnyxCheckbox): support error message tooltip feat(OnyxCheckboxGroup): allow customErrors on options

--- packages/sit-onyx/src/components/OnyxCheckbox/OnyxCheckbox.vue
+++ packages/sit-onyx/src/components/OnyxCheckbox/OnyxCheckbox.vue
@@ -2,7 +2,7 @@
 import { computed } from "vue";
 import { useDensity } from "../../composables/density";
 import { useRequired } from "../../composables/required";
-import { useCustomValidity } from "../../composables/useCustomValidity";
+import { getCustomErrorText, useCustomValidity } from "../../composables/useCustomValidity";
 import { OnyxLoadingIndicator } from "../../index";
 import type { SelectOptionValue } from "../../types";
 import OnyxSkeleton from "../OnyxSkeleton/OnyxSkeleton.vue";
@@ -35,7 +35,14 @@ const isChecked = computed({
 const { requiredMarkerClass, requiredTypeClass } = useRequired(props);
 const { densityClass } = useDensity(props);
 
-const { vCustomValidity } = useCustomValidity({ props, emit });
+const { vCustomValidity, errorMessages } = useCustomValidity({ props, emit });
+
+const checkboxTitle = computed(() => {
+  const title = [];
+  if (props.hideLabel) title.push(props.label);
+  if (errorMessages.value) title.push(getCustomErrorText(errorMessages.value));
+  return title.length ? title.join("\n") : undefined;
+});
 </script>
 
 <template>
@@ -44,7 +51,12 @@ const { vCustomValidity } = useCustomValidity({ props, emit });
     <OnyxSkeleton v-if="!props.hideLabel" class="onyx-checkbox-skeleton__label" />
   </div>
 
-  <label v-else class="onyx-checkbox" :class="[requiredTypeClass, densityClass]">
+  <label
+    v-else
+    class="onyx-checkbox"
+    :class="[requiredTypeClass, densityClass]"
+    :title="checkboxTitle"
+  >
     <div class="onyx-checkbox__container">
       <OnyxLoadingIndicator v-if="props.loading" class="onyx-checkbox__loading" type="circle" />
       <input
@@ -52,7 +64,6 @@ const { vCustomValidity } = useCustomValidity({ props, emit });
         v-model="isChecked"
         v-custom-validity
         :aria-label="props.hideLabel ? props.label : undefined"
-        :title="props.hideLabel ? props.label : undefined"
         class="onyx-checkbox__input"
         type="checkbox"
         :indeterminate="props.indeterminate"
