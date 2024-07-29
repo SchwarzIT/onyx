7ea91941d1cff7f08d958c5e3029c79c8511da1f
Jonathan <jonathan_leo.carle@mail.schwarz>
2024-07-29 10:33:40 +0200

    feat: improve headless typings (#1644)
    Closes #1643
    
    - Replaced incorrect usage of `HtmlHTMLAttributes` with
    `HTMLAttributes`: `HTMLAttributes` is the correct supertype, while
    `HtmlHTMLAttributes` actually refers to attributes of an `html` element.
    - implemented `createElRef`, which creates a special writeable computed
    that references a DOM Element and unwraps components.
    - `createElRef` returns a `HeadlessElRef`, which is a special subtype of
    `WritableComputedRef`. It uses a type differentiator, so no other `Ref`
    types can be assigned to it.
    - updated `createBuilder` `HeadlessElement` typings to only accept
    `HeadlessElRef` for `ref` keys. So `createElRef` must be used to create
    these.

--- packages/headless/src/utils/builder.ts
+++ packages/headless/src/utils/builder.ts
@@ -1,19 +1,37 @@
-import type { ComputedRef, HtmlHTMLAttributes, Ref, VNodeRef } from "vue";
+import {
+  computed,
+  shallowRef,
+  type ComponentPublicInstance,
+  type HTMLAttributes,
+  type MaybeRef,
+  type Ref,
+  type WritableComputedOptions,
+  type WritableComputedRef,
+} from "vue";
 import type { IfDefined } from "./types";
 
-export type ElementAttributes = HtmlHTMLAttributes & { ref?: VNodeRef };
+/**
+ * `v-bind`able attributes as they are provided by the headless composables.
+ * `ref` is restricted to be a `HeadlessElRef` which only can by created through `createElRef`.
+ */
+export type VBindAttributes<
+  A extends HTMLAttributes = HTMLAttributes,
+  E extends Element = Element,
+> = A & {
+  ref?: VueTemplateRef<E>;
+};
 
-export type IteratedHeadlessElementFunc<T extends Record<string, unknown>> = (
-  opts: T,
-) => ElementAttributes;
+export type IteratedHeadlessElementFunc<
+  A extends HTMLAttributes,
+  T extends Record<string, unknown>,
+> = (opts: T) => VBindAttributes<A>;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type HeadlessElementAttributes = ElementAttributes | IteratedHeadlessElementFunc<any>;
+export type HeadlessElementAttributes<A extends HTMLAttributes> =
+  | VBindAttributes<A>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  | IteratedHeadlessElementFunc<A, any>;
 
-export type HeadlessElements = Record<
-  string,
-  HeadlessElementAttributes | ComputedRef<HeadlessElementAttributes>
->;
+export type HeadlessElements = Record<string, MaybeRef<HeadlessElementAttributes<HTMLAttributes>>>;
 
 export type HeadlessState = Record<string, Ref>;
 
@@ -28,6 +46,39 @@ export type HeadlessComposable<
 
 /**
  * We use this identity function to ensure the correct typings of the headless composables
+ * @example
+ * ```ts
+ * export const createTooltip = createBuilder(({ initialVisible }: CreateTooltipOptions) => {
+ *   const tooltipId = createId("tooltip");
+ *   const isVisible = ref(initialVisible);
+ *
+ *   const hoverEvents = {
+ *     onMouseover: () => (isVisible.value = true),
+ *     onMouseout: () => (isVisible.value = false),
+ *     onFocusin: () => (isVisible.value = true),
+ *     onFocusout: () => (isVisible.value = false),
+ *   };
+ *
+ *   return {
+ *     elements: {
+ *       trigger: {
+ *         "aria-describedby": tooltipId,
+ *         ...hoverEvents,
+ *       },
+ *       tooltip: {
+ *         role: "tooltip",
+ *         id: tooltipId,
+ *         tabindex: "-1",
+ *         ...hoverEvents,
+ *       },
+ *     },
+ *     state: {
+ *       isVisible,
+ *     },
+ *   };
+ * });
+ *
+ * ```
  */
 export const createBuilder = <
   Args extends unknown[] = unknown[],
@@ -37,3 +88,47 @@ export const createBuilder = <
 >(
   builder: (...args: Args) => HeadlessComposable<Elements, State, Internals>,
 ) => builder;
+
+type VueTemplateRefElement<E extends Element> = E | (ComponentPublicInstance & { $el: E }) | null;
+type VueTemplateRef<E extends Element> = Ref<VueTemplateRefElement<E>>;
+
+declare const HeadlessElRefSymbol: unique symbol;
+type HeadlessElRef<E extends Element> = WritableComputedRef<E> & {
+  /**
+   * type differentiator
+   * ensures that only `createElRef` can be used for headless element ref bindings
+   */
+  [HeadlessElRefSymbol]: true;
+};
+
+/**
+ * Creates a special writeable computed that references a DOM Element.
+ * Vue Component references will be unwrapped.
+ * @example
+ * ```ts
+ * createBuilder() => {
+ *  const buttonRef = createElRef<HtmlButtonElement>();
+ *  return {
+ *    elements: {
+ *      button: {
+ *        ref: buttonRef,
+ *      },
+ *    }
+ *  };
+ * });
+ * ```
+ */
+export function createElRef<E extends Element>(): HeadlessElRef<E>;
+export function createElRef<
+  E extends Element,
+  V extends VueTemplateRefElement<E> = VueTemplateRefElement<E>,
+>() {
+  const elementRef = shallowRef<E>();
+
+  return computed({
+    set: (element: V) => {
+      elementRef.value = element != null && "$el" in element ? element.$el : (element as E);
+    },
+    get: () => elementRef.value,
+  } as WritableComputedOptions<E>);
+}
