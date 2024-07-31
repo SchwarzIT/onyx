a38897ae434909e9aa6018f56e1e086fca0ae090
Oliver Müller <oliver.mueller2@mail.schwarz>
2024-07-31 16:11:01 +0200

    refactor

--- packages/sit-onyx/src/components/OnyxRipple/OnyxRipple.ct.tsx
+++ packages/sit-onyx/src/components/OnyxRipple/OnyxRipple.ct.tsx
@@ -3,9 +3,7 @@ import OnyxRipple from "./OnyxRipple.vue";
 
 test("should trigger some ripples", async ({ mount }) => {
   // ARRANGE
-  const component = await mount(
-    <OnyxRipple enter-duration="2000ms" style="width: 500px; height: 500px;" />,
-  );
+  const component = await mount(<OnyxRipple style="width: 500px; height: 500px;" />);
 
   await component.click({ clickCount: 2, position: { x: 0, y: 50 } });
   await component.click({ clickCount: 1, position: { x: 250, y: 0 } });
