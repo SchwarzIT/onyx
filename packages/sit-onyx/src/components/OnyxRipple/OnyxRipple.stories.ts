c208b7cd05197c3f668710fd6bb76cdcf0aeb09b
Oliver Müller <oliver.mueller2@mail.schwarz>
2024-08-01 14:48:44 +0200

    docs: show button in ripple story

--- packages/sit-onyx/src/components/OnyxRipple/OnyxRipple.stories.ts
+++ packages/sit-onyx/src/components/OnyxRipple/OnyxRipple.stories.ts
@@ -1,37 +1,30 @@
 import { defineStorybookActionsAndVModels } from "@sit-onyx/storybook-utils";
 import type { Meta, StoryObj } from "@storybook/vue3";
+import { Primary as ButtonPrimaryArgs } from "../OnyxButton/OnyxButton.stories";
+import OnyxButton from "../OnyxButton/OnyxButton.vue";
 import OnyxRipple from "./OnyxRipple.vue";
 
 /**
  * OnyxRipple provides visual feedback when a user interacts with the component. Supporting both touch and mouse events.
- * A standalone OnyxRipple does not show any ripples, since the events have to be handled on the parent component. See OnyxButton how to handle that.
+ *
+ * A standalone OnyxRipple does not show any ripples, since the events have to be handled on the parent component. See `OnyxButton` how to do that.
  */
 const meta: Meta<typeof OnyxRipple> = {
   title: "Support/Ripple",
   ...defineStorybookActionsAndVModels({
-    component: OnyxRipple,
-    events: [],
+    component: OnyxButton,
+    events: ["click"],
+    parameters: undefined,
   }),
 };
 
 export default meta;
-type Story = StoryObj<typeof OnyxRipple>;
+
+type Story = StoryObj<typeof OnyxButton>;
 
 /**
- * Default properties like color and durations can be overridden. Click anywhere to create a ripple.
+ * Example usage of OnyxRipple inside OnyxButton
  */
 export const Default = {
-  args: {},
-  decorators: [
-    (story) => ({
-      components: { story },
-      template: `
-        <div style="position: relative; overflow: hidden; padding: 2rem; max-width: 100px;">
-          <span style="z-index: 1; position: relative; pointer-events: none;">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </span>
-          <story />
-        </div>`,
-    }),
-  ],
+  ...ButtonPrimaryArgs,
 } satisfies Story;
