7c58eb71b72b22a89c003198601905f43e2075fc
Lars Rickert <lars.rickert@mail.schwarz>
2024-08-12 08:45:42 +0200

    fix active screenshots

--- packages/sit-onyx/src/components/examples/GridPlayground/GridElement/GridElement.ct.tsx
+++ packages/sit-onyx/src/components/examples/GridPlayground/GridElement/GridElement.ct.tsx
@@ -20,7 +20,11 @@ test.describe("screenshot tests", () => {
       beforeScreenshot: async (component, page, column, row) => {
         if (row === "hover") await component.hover();
         if (row === "focus-visible") await page.keyboard.press("Tab");
-        if (row === "active") await page.mouse.down();
+        if (row === "active") {
+          const box = (await component.boundingBox())!;
+          await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
+          await page.mouse.down();
+        }
       },
     });
   }
