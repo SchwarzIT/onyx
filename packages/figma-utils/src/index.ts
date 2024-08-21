f5c6e671541e0fb29f24f76235f76f937ea98215
Lars Rickert <lars.rickert@mail.schwarz>
2024-08-21 11:19:33 +0200

    add missing exports

--- packages/figma-utils/src/index.ts
+++ packages/figma-utils/src/index.ts
@@ -1,3 +1,6 @@
+export * from "./icons/generate.js";
+export * from "./icons/optimize.js";
+export * from "./icons/parse.js";
 export * from "./types/figma.js";
 export * from "./utils/fetch.js";
 export * from "./variables/generate.js";
