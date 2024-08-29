d4fe7d1fb995e3f01972a0d042c94d387c570c11
Lars Rickert <lars.rickert@mail.schwarz>
2024-08-29 10:31:58 +0200

    fix(button): make background transparent for `mode="plain"` (#1806)
    Relates to #1764
    
    We accidently added a background color to the `plain` mode of the button
    in #1609.
    This PR fixes the issue. I needed to invert the ripple effect logic for
    this to work since we can not animate transparent.

---
+++ .changeset/ten-hairs-bake.md
@@ -0,0 +1,5 @@
+---
+"sit-onyx": patch
+---
+
+fix(button): make background transparent for `mode="plain"`
--- packages/sit-onyx/src/utils/math.ts
+++
@@ -1,5 +0,0 @@
-export function distanceToFurthestCorner(x: number, y: number, rect: DOMRect) {
-  const dx = Math.max(Math.abs(x - rect.left), Math.abs(x - rect.right));
-  const dy = Math.max(Math.abs(y - rect.top), Math.abs(y - rect.bottom));
-  return Math.hypot(dx, dy);
-}
