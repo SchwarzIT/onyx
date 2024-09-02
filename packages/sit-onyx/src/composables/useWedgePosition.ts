a955742d7afeff853785d84825ed630dca85d2ac
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-09-02 14:40:17 +0300

    Fix float behavior

--- packages/sit-onyx/src/composables/useWedgePosition.ts
+++ packages/sit-onyx/src/composables/useWedgePosition.ts
@@ -6,69 +6,44 @@ export const useWedgePosition = (
   element: MaybeRef<Element | undefined>,
   tooltipElement: MaybeRef<Element | undefined>,
 ) => {
-  // const minMargin = 16;
+  const minMargin = 16;
   const wedgePosition = ref<WedgePosition>("center");
 
   const updateWedgePosition = () => {
-    const el = unref(element);
+    const wrapperEl = unref(element);
     const tooltipEl = unref(tooltipElement);
-    if (!el || !tooltipEl) {
+
+    if (!wrapperEl || !tooltipEl) {
       wedgePosition.value = "center";
       return;
     }
 
-    const overflowParentRect = findParentWithHiddenOverflow(el)?.getBoundingClientRect();
-    const elementRect = el.getBoundingClientRect();
+    const overflowParentRect = findParentWithHiddenOverflow(wrapperEl)?.getBoundingClientRect();
+    const wrapperRect = wrapperEl.getBoundingClientRect();
     const tooltipElementRect = tooltipEl.getBoundingClientRect();
-    // elementwidth
 
-    if (tooltipElementRect.width < elementRect.width) {
+    if (tooltipElementRect.width < wrapperRect.width) {
       wedgePosition.value = "center";
       return;
     }
-    //TODO: tooltipElement.width is not working correctly
-    //const minSpace = (tooltipElementRect.width - elementRect.width + minMargin * 2) / 2;
-    // const minSpaceLeft =
-    //   tooltipElementRect.left - (tooltipElementRect.width - elementRect.width + minMargin * 2) / 2;
-    // const minSpaceRight =
-    //   tooltipElementRect.right + (tooltipElementRect.width - elementRect.width + minMargin * 2) / 2;
-    // const minSpace = 10;
-    // console.log("minSpaceLeft", minSpaceLeft);
-    // console.log("minSpaceRight", minSpaceRight);
+
+    const minSpace = (tooltipElementRect.width - wrapperRect.width + minMargin * 2) / 2;
 
     const parentLeft = overflowParentRect?.left ?? window.visualViewport?.pageLeft ?? 0;
-    const parenRight =
+    const parentRight =
       overflowParentRect?.right ??
       (window.visualViewport?.width ?? window.innerWidth) +
         (window.visualViewport?.offsetLeft ?? 0) ??
       0;
-    // console.log("parentLeft", parentLeft);
-    // console.log("parenRight", parenRight);
-    // const centerX = tooltipElementRect.x + tooltipElementRect.width * 0.5;
 
-    const freeSpaceLeft = tooltipElementRect.left - parentLeft;
-    const freeSpaceRight = parenRight - tooltipElementRect.right;
+    const freeSpaceLeft = wrapperRect.left - parentLeft;
+    const freeSpaceRight = parentRight - wrapperRect.right;
 
-    //console.log("freeSpaceLeft", freeSpaceLeft);
-    // console.log("freeSpaceRight", freeSpaceRight);
-    //console.log("minSpace", minSpace);
-    // console.log("center", freeSpaceLeft > minSpace && freeSpaceRight > minSpace);
-    // const enoughSpaceLeft = centerX - elementRect.width * 0.5 >= 0;
-    // const enoughSpaceRight =
-    //   centerX + elementRect.width * 0.5 < document.documentElement.clientWidth;
-
-    // console.log("enoughSpaceLeft", enoughSpaceLeft);
-    // console.log("enoughSpaceRight", enoughSpaceRight);
-
-    //const hostBoundingRect = this.parentTooltip.getBoundingClientRect();
-    //const tooltipBoundingRect = this.hostElement.getBoundingClientRect();
-    //const centerX = hostBoundingRect.x + hostBoundingRect.width * 0.5;
-    // const enoughSpaceLeft = centerX - tooltipBoundingRect.width * 0.5 >= 0;
-    //const enoughSpaceRight =centerX + tooltipBoundingRect.width * 0.5 < document.documentElement.clientWidth;
+    const enoughSpaceLeft = freeSpaceLeft >= minSpace;
+    const enoughSpaceRight = freeSpaceRight >= minSpace;
 
     wedgePosition.value =
-      // (freeSpaceLeft > minSpace && freeSpaceRight > minSpace) ||
-      freeSpaceLeft === freeSpaceRight
+      enoughSpaceLeft === enoughSpaceRight
         ? "center"
         : freeSpaceLeft > freeSpaceRight
           ? "right"
@@ -82,7 +57,7 @@ export const useWedgePosition = (
     if (!element) return undefined;
 
     const style = getComputedStyle(element);
-    if (style.overflow === "hidden") {
+    if (style.overflow === "hidden" || style.overflow === "hidden auto") {
       // if the element has hidden overflow, the flyout would be cut off by this element so we need to use
       // this element as parent to calculate the open direction instead of the body.
       return element;
