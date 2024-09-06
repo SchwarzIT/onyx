5209762d3ed7646672c819ebf7e49bc4f6fee7c4
Jonathan <jonathan_leo.carle@mail.schwarz>
2024-09-06 13:45:28 +0200

    docs: fix charts not rendering because of version mismatch (#1823)
    Closes #1822
    
    - fix charts not rendering because of version mismatch
    - **Cause:** This was caused by mismatching versions of the `chart.js`
    package in our `docs` and the `@sit-onyx/chartjs-plugin` packages.
    - **Fix:** We now make use of the [PNPM catalog
    feature](https://pnpm.io/catalogs) to enforce usage of the same package
    version. Added screenshot tests to ensure that we catch these issues in
    the future.
    
    - fix warning logged by vitepress about `gobalThis.document` not being
    available
    - **Cause:** Vitepress does server-side rendering, where the document is
    not available. But we try to set a CSS variable.
    - **Fix:** We use the optional chaining operator to ensure that we set
    the CSS variable only when `gobalThis.document` is defined.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: JoCa96 <18483883+JoCa96@users.noreply.github.com>

---
+++ apps/docs/tests/pages.spec.ts
@@ -0,0 +1,53 @@
+import { expect, test } from "@playwright/test";
+import { join } from "node:path";
+import { DefaultTheme } from "../node_modules/vitepress/types/default-theme";
+import { CONFIG } from "../src/.vitepress/config";
+
+test("has title", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page).toHaveTitle(/onyx/);
+});
+
+const BLACKLIST_PATHS = [/changelog/i, /icons/i];
+
+/**
+ * maps links listed in the vitepress config to their respective absolute url
+ */
+const mapToLinks = (
+  navItem: DefaultTheme.NavItem | DefaultTheme.SidebarItem,
+  baseParm?: string,
+): string[] => {
+  const base = baseParm ?? (navItem as DefaultTheme.SidebarItem).base ?? "";
+  const links: string[] = [];
+  if ("link" in navItem && navItem.link && navItem.link.startsWith("/")) {
+    links.push(`${base}${navItem.link}`);
+  }
+  if ("items" in navItem && navItem.items) {
+    navItem.items.forEach((item) => links.push(...mapToLinks(item, navItem["base"])));
+  }
+  return links;
+};
+
+const navItems = CONFIG.themeConfig.nav;
+const sidebarItems = Object.values(CONFIG.themeConfig.sidebar).flat(1);
+
+const items = [...navItems, ...sidebarItems].map((item) => mapToLinks(item)).flat(1);
+const uniqueItems = new Set(items);
+const pathsTotTest = Array.from(uniqueItems).filter(
+  (p) => !BLACKLIST_PATHS.some((bl) => p.match(bl)),
+);
+
+pathsTotTest.forEach((path) => {
+  test(`screenshot content of ${path}`, async ({ page }) => {
+    const name = path
+      .replace(/^\//, "")
+      .replace(/\.html$/, "")
+      .replace(/[_/ ]/g, "_");
+    await page.goto(path);
+    const main = page.getByRole("main");
+    await expect(main).toHaveScreenshot(`${name}.png`, {
+      stylePath: join(import.meta.dirname, "hide-non-main.css"),
+    });
+  });
+});
