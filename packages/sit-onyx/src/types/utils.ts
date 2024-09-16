4278e9024432ad217df390d9f40559aaa4d68982
Maja Zarkova <maja.zarkova@mail.schwarz>
2024-09-16 15:08:36 +0300

    Replace AnyKey with native PropertyKey

--- packages/sit-onyx/src/types/utils.ts
+++ packages/sit-onyx/src/types/utils.ts
@@ -6,12 +6,6 @@ import type { HTMLAttributes } from "vue";
 export type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]> } : T;
 
 /**
- * Any key that can be used to index and object.
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type AnyKey = keyof any;
-
-/**
  * Adds all native HTML attributes to the given type.
  */
 export type WithHTMLAttributes<
