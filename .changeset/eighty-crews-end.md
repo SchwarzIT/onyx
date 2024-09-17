32b2108bbaae0bd22738a29b744c7f0500469722
Jonathan <jonathan_leo.carle@mail.schwarz>
2024-09-17 17:29:53 +0200

    feat(@sit-onyx/eslint-plugin): add "no-shadow-native-events" from "eslint-plugin-vue" until its released (#1877)
    Duplicate "no-shadow-native-events" from "eslint-plugin-vue" until it is
    released:
    - PR with `eslint-plugin-vue`:
    https://github.com/vuejs/eslint-plugin-vue/pull/2558
      - Tests can also be found in that PR
    - Official Rule proposal:
    https://github.com/vuejs/eslint-plugin-vue/issues/2557
    
    Relates to #1603

---
+++ .changeset/eighty-crews-end.md
@@ -0,0 +1,5 @@
+---
+"@sit-onyx/eslint-plugin": minor
+---
+
+feat(no-shadow-native-events): duplicated implementation here until it is [released offically](https://github.com/vuejs/eslint-plugin-vue/issues/2557)
--- packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxNavButton/MobileComponentTestWrapper.vue
+++ packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxNavButton/MobileComponentTestWrapper.vue
@@ -6,13 +6,6 @@ import type { OnyxNavButtonProps } from "./types";
 
 const props = defineProps<OnyxNavButtonProps>();
 
-const emit = defineEmits<{
-  /**
-   * An optional slot to override the label content.
-   */
-  click: [href: string];
-}>();
-
 const slots = defineSlots<{
   /**
    * An optional slot to show additional content behind the label (e.g. a `OnyxBadge`).
@@ -36,7 +29,6 @@ provide(
     :label="props.label"
     :href="props.href"
     :active="props.active"
-    @navigate="emit('click', $event)"
   >
     <slot></slot>
 
