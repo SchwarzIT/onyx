92ddec3a0247115615af5d8a93a1ec383708e57e
Jonathan <jonathan_leo.carle@mail.schwarz>
2024-10-01 10:17:32 +0200

    chore: switch to faster "sass-embedded" and activate fatal deprecations (#1899)
    - we now use
    [`sass-embedded`](https://www.npmjs.com/package/sass-embedded) which is
    faster
    - we make all sass deprecations `fatal`, so that they error instead of
    just creating a warning

--- packages/sit-onyx/vite.config.ts
+++ packages/sit-onyx/vite.config.ts
@@ -1,9 +1,12 @@
 /// <reference types="vitest" />
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
+import { Deprecation, deprecations, Options } from "sass";
 import { defineConfig } from "vite";
 import packageJson from "./package.json";
 import { vuePluginOptions } from "./playwright.config";
+
+type ViteScssOptions = Options<"async"> & { api: "modern-compiler" };
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -27,6 +30,17 @@ export default defineConfig({
     coverage: {
       include: ["src"],
       exclude: ["src/**/*.stories.ts"],
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        // error for all warnings
+        fatalDeprecations: (Object.values(deprecations) as Deprecation[]).filter(
+          ({ status }) => status !== "future",
+        ),
+      } satisfies ViteScssOptions,
     },
   },
 });
