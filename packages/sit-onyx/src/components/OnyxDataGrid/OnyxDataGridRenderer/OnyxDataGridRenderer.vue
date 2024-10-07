8a42e50fcf54aa9f64d2932aa341d40610203415
Lars Rickert <lars.rickert@mail.schwarz>
2024-10-07 09:37:45 +0200

    add comment

--- packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGridRenderer/OnyxDataGridRenderer.vue
+++ packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGridRenderer/OnyxDataGridRenderer.vue
@@ -4,6 +4,8 @@ import type { DataGridEntry, DataGridMetadata } from "../types";
 import type { OnyxDataGridRendererProps } from "./types";
 
 const props = withDefaults(defineProps<OnyxDataGridRendererProps<TEntry, TMetadata>>(), {
+  // usually we default all boolean props to false (see https://onyx.schwarz/principles/technical-vision.html#component-interface).
+  // However, here for striped and withVerticalBorders this makes sense from UX perspective
   striped: true,
   withVerticalBorders: true,
 });
