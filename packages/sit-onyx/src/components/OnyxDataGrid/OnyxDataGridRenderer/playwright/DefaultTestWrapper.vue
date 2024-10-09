cfcc2ef820ab7bcae5df31413dac8cad7f4323d6
Lars Rickert <lars.rickert@mail.schwarz>
2024-10-09 09:11:08 +0200

    test(OnyxDataGridRenderer): add screenshot tests

---
+++ packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGridRenderer/playwright/DefaultTestWrapper.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { Default } from "../OnyxDataGridRenderer.stories";
+import OnyxDataGridRenderer from "../OnyxDataGridRenderer.vue";
+import type { OnyxDataGridRendererProps } from "../types";
+
+const props = defineProps<Pick<OnyxDataGridRendererProps, "density">>();
+</script>
+
+<template>
+  <OnyxDataGridRenderer v-bind="{ ...props, ...Default.args }" />
+</template>
