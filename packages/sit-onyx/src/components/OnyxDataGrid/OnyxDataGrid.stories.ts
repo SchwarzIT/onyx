c71de53386c9f21a39757c8a45baca1be4e70a64
Jonathan <jonathan_leo.carle@mail.schwarz>
2024-10-29 08:54:00 +0100

    Update packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.stories.ts
    Co-authored-by: Lars Rickert <lars.rickert@mail.schwarz>

--- packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.stories.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.stories.ts
@@ -14,8 +14,8 @@ export const Default = {
     columns: ["name", "age", "birthday"],
     data: [
       { id: 1, name: "Alice", age: 30, birthday: new Date("1990-01-01") },
-      { id: 2, name: "Bob", age: 25, birthday: new Date("1995-06-15") },
-      { id: 3, name: "Charlie", age: 35, birthday: new Date("1998-02-11") },
+      { id: 2, name: "Charlie", age: 35, birthday: new Date("1998-02-11") },
+      { id: 3, name: "Bob", age: 25, birthday: new Date("1995-06-15") },
     ],
   },
 } satisfies Story;
