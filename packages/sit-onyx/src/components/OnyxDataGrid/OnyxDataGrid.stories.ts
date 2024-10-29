b6382e4b22be98c6df029229ac016a819bd5a3ba
Jonathan <jonathan_leo.carle@mail.schwarz>
2024-10-29 10:23:19 +0100

    Update packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.stories.ts
    Co-authored-by: Lars Rickert <lars.rickert@mail.schwarz>

--- packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.stories.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import OnyxDataGrid from "./OnyxDataGrid.vue";
 
 const meta: Meta<typeof OnyxDataGrid> = {
-  title: "DataGrid/DataGrid",
+  title: "Data/DataGrid",
   component: OnyxDataGrid,
 };
 
