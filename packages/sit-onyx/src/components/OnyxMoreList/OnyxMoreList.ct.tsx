9347fd1ce27728cc47d35e397734cf39e6dc2d1f
Lars Rickert <lars.rickert@mail.schwarz>
2024-10-30 11:09:52 +0100

    update tests

--- packages/sit-onyx/src/components/OnyxMoreList/OnyxMoreList.ct.tsx
+++ packages/sit-onyx/src/components/OnyxMoreList/OnyxMoreList.ct.tsx
@@ -5,15 +5,43 @@ test("should render", async ({ mount, makeAxeBuilder }) => {
   // ARRANGE
   const component = await mount(TestWrapperCt, {
     props: {
-      count: 3.5,
+      count: 2.5,
     },
   });
+
+  const expectVisible = (label: string) => {
+    return expect(component.getByLabel(label, { exact: true })).toBeInViewport({
+      ratio: 1,
+    });
+  };
+
+  const expectHidden = (label: string) => {
+    return expect(component.getByLabel(label, { exact: true })).toBeHidden();
+  };
 
   // ACT
   const accessibilityScanResults = await makeAxeBuilder().analyze();
 
   // ASSERT
   expect(accessibilityScanResults.violations).toEqual([]);
-  await expect(component.getByLabel("Element 5")).toBeInViewport({ ratio: 1 });
-  await expect(component.getByLabel("Element 6")).not.toBeInViewport({ ratio: 1 });
+  await expectVisible("Element 1");
+  await expectVisible("Element 2");
+  await expectHidden("Element 3");
+
+  // ACT
+  await component.update({ props: { count: 1 } });
+
+  // ASSERT
+  await expectVisible("Element 1");
+  await expectHidden("Element 2");
+
+  // ACT
+  await component.update({ props: { count: 4.25 } });
+
+  // ASSERT
+  await expectVisible("Element 1");
+  await expectVisible("Element 2");
+  await expectVisible("Element 3");
+  await expectVisible("Element 4");
+  await expectHidden("Element 5");
 });
