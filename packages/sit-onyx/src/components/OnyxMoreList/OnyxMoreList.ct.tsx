9fb1d63ce533830c6b17ca22a7ee8b4b71ee499e
Lars Rickert <lars.rickert@mail.schwarz>
2024-10-30 12:16:49 +0100

    test event emits

--- packages/sit-onyx/src/components/OnyxMoreList/OnyxMoreList.ct.tsx
+++ packages/sit-onyx/src/components/OnyxMoreList/OnyxMoreList.ct.tsx
@@ -1,12 +1,20 @@
 import { expect, test } from "../../playwright/a11y";
 import TestWrapperCt from "./TestWrapper.ct.vue";
+import type { MoreListSlotBindings } from "./types";
 
 test("should render", async ({ mount, makeAxeBuilder }) => {
+  const events: MoreListSlotBindings[] = [];
+
+  const eventHandlers = {
+    onVisibilityChange: (data: MoreListSlotBindings) => events.push(data),
+  };
+
   // ARRANGE
   const component = await mount(TestWrapperCt, {
     props: {
       count: 2.5,
     },
+    on: eventHandlers,
   });
 
   const expectVisible = (label: string) => {
@@ -27,16 +35,18 @@ test("should render", async ({ mount, makeAxeBuilder }) => {
   await expectVisible("Element 1");
   await expectVisible("Element 2");
   await expectHidden("Element 3");
+  expect(events.at(-1)).toStrictEqual({ visibleElements: 2, hiddenElements: 22 });
 
   // ACT
-  await component.update({ props: { count: 1 } });
+  await component.update({ props: { count: 1 }, on: eventHandlers });
 
   // ASSERT
   await expectVisible("Element 1");
   await expectHidden("Element 2");
+  expect(events.at(-1)).toStrictEqual({ visibleElements: 1, hiddenElements: 23 });
 
   // ACT
-  await component.update({ props: { count: 4.25 } });
+  await component.update({ props: { count: 4.25 }, on: eventHandlers });
 
   // ASSERT
   await expectVisible("Element 1");
@@ -44,4 +54,5 @@ test("should render", async ({ mount, makeAxeBuilder }) => {
   await expectVisible("Element 3");
   await expectVisible("Element 4");
   await expectHidden("Element 5");
+  expect(events.at(-1)).toStrictEqual({ visibleElements: 4, hiddenElements: 20 });
 });
