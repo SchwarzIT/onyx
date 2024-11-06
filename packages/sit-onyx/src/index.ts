25c3d212b76cc65a2a43037c5aad946c31c7c67a
Lars Rickert <lars.rickert@mail.schwarz>
2024-11-06 16:32:37 +0100

    feat: implement basic datepicker

--- packages/sit-onyx/src/index.ts
+++ packages/sit-onyx/src/index.ts
@@ -30,6 +30,9 @@ export * from "./components/OnyxDataGrid/types";
 
 export * as DataGridFeatures from "./components/OnyxDataGrid/features/all";
 
+export { default as OnyxDatepicker } from "./components/OnyxDatepicker/OnyxDatepicker.vue";
+export * from "./components/OnyxDatepicker/types";
+
 export { default as OnyxDialog } from "./components/OnyxDialog/OnyxDialog.vue";
 export * from "./components/OnyxDialog/types";
 
