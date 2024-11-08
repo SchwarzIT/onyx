8f229e5cb24f1a6814b337799c792a6691eabff0
Lars Rickert <lars.rickert@mail.schwarz>
2024-11-08 09:28:36 +0100

    add min and max property

--- packages/sit-onyx/src/components/OnyxDatepicker/types.ts
+++ packages/sit-onyx/src/components/OnyxDatepicker/types.ts
@@ -1,5 +1,8 @@
 import type { OnyxInputProps } from "../OnyxInput/types";
 
+/** Data types that are parsable as date via `new Date()`. */
+export type DateValue = ConstructorParameters<typeof Date>[0];
+
 // TODO: check autocomplete
 export type OnyxDatepickerProps = Omit<
   OnyxInputProps,
@@ -8,9 +11,17 @@ export type OnyxDatepickerProps = Omit<
   /**
    * Current date value. Supports all data types that are parsable by `new Date()`.
    */
-  modelValue?: ConstructorParameters<typeof Date>[0];
+  modelValue?: DateValue;
   /**
    * Whether the user should be able to select only date or date + time.
    */
   type?: "date" | "datetime";
+  /**
+   * Minimum selectable date (inclusive).
+   */
+  min?: DateValue;
+  /**
+   * Maximum selectable date (inclusive).
+   */
+  max?: DateValue;
 };
