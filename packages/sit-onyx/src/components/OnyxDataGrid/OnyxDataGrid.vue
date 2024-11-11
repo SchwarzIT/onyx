451c7e49b4c3cea64788cff829ff4974bd2f30e2
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-11-11 10:25:07 +0100

    clean up feature watcher

--- packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.vue
+++ packages/sit-onyx/src/components/OnyxDataGrid/OnyxDataGrid.vue
@@ -3,7 +3,7 @@
   lang="ts"
   generic="TEntry extends DataGridEntry, TFeatures extends DataGridFeature<TEntry, symbol>[] | []"
 >
-import { ref, toRefs, watch, type Ref, type WatchHandle } from "vue";
+import { computed, ref, toRefs, watch, type Ref, type WatchHandle } from "vue";
 import {
   OnyxDataGridRenderer,
   type DataGridEntry,
@@ -18,26 +18,23 @@ const props = withDefaults(defineProps<OnyxDataGridProps<TEntry, TFeatures>>(), 
   features: () => [] as TFeatures,
 });
 
-const featureBuilder = ref<ReturnType<typeof useDataGridFeatures<TEntry, TFeatures>>>();
-
-// eslint-disable-next-line vue/no-setup-props-reactivity-loss
-const { watchSources, createRendererRows, createRendererColumns } = useDataGridFeatures<
-  TEntry,
-  TFeatures
->(props.features);
-
 // Using Ref types to avoid `UnwrapRef` issues
 const renderColumns: Ref<DataGridRendererColumn<TEntry, object>[]> = ref([]);
 const renderRows: Ref<DataGridRendererRow<TEntry, DataGridMetadata>[]> = ref([]);
 
 const { columns, data, features } = toRefs(props);
 
+const featureBuilder = computed<ReturnType<typeof useDataGridFeatures<TEntry, TFeatures>>>(() =>
+  useDataGridFeatures(features.value),
+);
+
 /**
  * Function to be able to reset the watcher in case of the features being updated.
  */
 let featureBuilderWatchHandle: WatchHandle | undefined;
-const createFeatureBuilderWatcher = () =>
-  watch(
+const createFeatureBuilderWatcher = () => {
+  const { createRendererColumns, createRendererRows, watchSources } = featureBuilder.value;
+  return watch(
     [columns, data, ...watchSources],
     () => {
       renderColumns.value = createRendererColumns(columns.value);
@@ -45,12 +42,11 @@ const createFeatureBuilderWatcher = () =>
     },
     { immediate: true, deep: true },
   );
+};
 
 watch(
-  features,
-  (newFeatures) => {
-    featureBuilder.value = useDataGridFeatures(newFeatures);
-
+  featureBuilder,
+  () => {
     featureBuilderWatchHandle?.();
     featureBuilderWatchHandle = createFeatureBuilderWatcher();
   },
