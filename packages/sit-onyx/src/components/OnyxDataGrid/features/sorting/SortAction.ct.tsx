080c352c1ae548fce842d5293778eb63de04d3fd
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-11-11 18:47:27 +0100

    add more tests

---
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/SortAction.ct.tsx
@@ -0,0 +1,12 @@
+import { test } from "../../../../playwright/a11y";
+import { executeMatrixScreenshotTest } from "../../../../playwright/screenshots";
+import SortAction from "./SortAction.vue";
+
+test.describe("Screenshot tests", () => {
+  executeMatrixScreenshotTest({
+    rows: ["none", "asc", "desc"] as const,
+    columns: ["default"],
+    name: "Sort Action",
+    component: (_, row) => <SortAction columnLabel="Column-Label" sortDirection={row} />,
+  });
+});
