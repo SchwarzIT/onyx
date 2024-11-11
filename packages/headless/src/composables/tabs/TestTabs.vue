c2176c0d7dc374a34d73969cb77dd525da1978f9
Lars Rickert <lars.rickert@mail.schwarz>
2024-11-11 08:58:30 +0100

    feat(tabs): support keyboard navigation

--- packages/headless/src/composables/tabs/TestTabs.vue
+++ packages/headless/src/composables/tabs/TestTabs.vue
@@ -18,9 +18,11 @@ const {
     <div v-bind="tablist">
       <button v-bind="tab({ value: 'tab-1' })" type="button">Tab 1</button>
       <button v-bind="tab({ value: 'tab-2' })" type="button">Tab 2</button>
+      <button v-bind="tab({ value: 'tab-3' })" type="button">Tab 3</button>
     </div>
 
     <div v-if="selectedTab === 'tab-1'" v-bind="tabpanel({ value: 'tab-1' })">Tab content 1</div>
     <div v-if="selectedTab === 'tab-2'" v-bind="tabpanel({ value: 'tab-2' })">Tab content 2</div>
+    <div v-if="selectedTab === 'tab-3'" v-bind="tabpanel({ value: 'tab-2' })">Tab content 3</div>
   </div>
 </template>
