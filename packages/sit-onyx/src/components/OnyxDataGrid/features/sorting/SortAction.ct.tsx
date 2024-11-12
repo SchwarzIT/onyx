1b2b327adcf86af018cb6f8fa4640ec9721e2837
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-11-12 11:04:11 +0100

    review: remove as const

--- packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/SortAction.ct.tsx
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/SortAction.ct.tsx
@@ -4,7 +4,7 @@ import SortAction from "./SortAction.vue";
 
 test.describe("Screenshot tests", () => {
   executeMatrixScreenshotTest({
-    rows: ["none", "asc", "desc"] as const,
+    rows: ["none", "asc", "desc"],
     columns: ["default"],
     name: "Sort Action",
     component: (_, row) => <SortAction columnLabel="Column-Label" sortDirection={row} />,
