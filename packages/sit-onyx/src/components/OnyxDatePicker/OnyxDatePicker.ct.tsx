229ba4efc4f9f32cd394d55ef00c0f013a7050ea
Lars Rickert <lars.rickert@mail.schwarz>
2024-11-25 16:22:12 +0100

    update tests

--- packages/sit-onyx/src/components/OnyxDatePicker/OnyxDatePicker.ct.tsx
+++ packages/sit-onyx/src/components/OnyxDatePicker/OnyxDatePicker.ct.tsx
@@ -1,7 +1,6 @@
 import { DENSITIES } from "../../composables/density";
 import { expect, test } from "../../playwright/a11y";
 import { executeMatrixScreenshotTest } from "../../playwright/screenshots";
-import { createFormElementUtils } from "../OnyxFormElement/OnyxFormElement.ct-utils";
 import OnyxDatePicker from "./OnyxDatePicker.vue";
 
 test.describe("Screenshot tests", () => {
@@ -74,89 +73,4 @@ test("should emit events", async ({ mount, makeAxeBuilder }) => {
   expect(events).toMatchObject({
     updateModelValue: ["2024-11-25T00:00:00.000Z"],
   });
-});
-
-test("should have aria-label if label is hidden", async ({ mount, makeAxeBuilder }) => {
-  // ARRANGE
-  const component = await mount(
-    <OnyxDatePicker label="Test label" style="width: 12rem;" hideLabel />,
-  );
-
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder().analyze();
-
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
-  await expect(component).not.toContainText("Test label");
-  await expect(component.getByLabel("Test label")).toBeAttached();
-});
-
-test("should show error message after interaction", async ({ mount, makeAxeBuilder }) => {
-  // ARRANGE
-  const component = await mount(<OnyxDatePicker label="Demo" style="width: 12rem;" required />);
-  const formElementUtils = createFormElementUtils(component);
-  const input = component.getByLabel("Demo");
-  const errorPreview = component.getByText("Required");
-  const fullError = formElementUtils
-    .getTooltipPopover("message")
-    .getByText("Please fill in this field.");
-
-  // ASSERT: initially no error shows
-  await expect(errorPreview).toBeHidden();
-  await expect(fullError).toBeHidden();
-
-  // ACT: interact with the input
-  await input.click();
-  await input.fill("2024-11-25");
-  await input.blur();
-
-  await input.click();
-  await input.press("Delete");
-  await input.blur();
-
-  // ASSERT: after interaction, the error preview shows
-  await expect(errorPreview).toBeVisible();
-  await expect(formElementUtils.getTooltipTrigger("message")).toBeVisible();
-  await expect(fullError).toBeHidden();
-
-  // ACT
-  await formElementUtils.triggerTooltipVisible("message");
-  // ASSERT: the full error message shows
-  await expect(fullError).toBeVisible();
-
-  // ACT
-  const accessibilityScanResults = await makeAxeBuilder().analyze();
-  // ASSERT
-  expect(accessibilityScanResults.violations).toEqual([]);
-});
-
-test("should show correct message", async ({ mount }) => {
-  const message = { shortMessage: "Test short message" };
-  const successMessage = { shortMessage: "Test success short message" };
-  const component = await mount(
-    <OnyxDatePicker label="Label" required success={successMessage} message={message} />,
-  );
-
-  const messageElement = component.getByText("Test short message");
-  const successMessageElement = component.getByText("Test success short message");
-  const errorMessageElement = component.getByText("Required");
-  const input = component.getByLabel("Label");
-
-  // ASSERT
-  await expect(messageElement).toBeHidden();
-  await expect(successMessageElement).toBeVisible();
-
-  //ACT
-  await input.click();
-  await input.fill("2024-11-25");
-  await input.blur();
-
-  await input.click();
-  await input.press("Delete");
-  await input.blur();
-
-  // ASSERT
-  await expect(messageElement).toBeHidden();
-  await expect(successMessageElement).toBeHidden();
-  await expect(errorMessageElement).toBeVisible();
 });
