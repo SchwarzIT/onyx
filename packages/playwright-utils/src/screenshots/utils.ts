90339ee752afe3647345fd36454eff3a999649bb
Lars Rickert <lars.rickert@mail.schwarz>
2024-12-09 09:12:28 +0100

    escape all non-word characters

--- packages/playwright-utils/src/screenshots/utils.ts
+++ packages/playwright-utils/src/screenshots/utils.ts
@@ -2,5 +2,5 @@
  * Escapes the given string so its save to be used as CSS grid-area, e.g. by replacing whitespaces.
  */
 export const escapeGridAreaName = (name: string) => {
-  return name.replace(/ /g, "-");
+  return name.replace(/\W/g, "-");
 };
