8eeda61ba9e72d3ba2e168e6ce68b562f1d4f871
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-12-16 17:30:02 +0100

    open up i18n t option type

--- packages/sit-onyx/src/i18n/index.ts
+++ packages/sit-onyx/src/i18n/index.ts
@@ -1,12 +1,4 @@
-import {
-  computed,
-  inject,
-  unref,
-  type App,
-  type ComputedRef,
-  type InjectionKey,
-  type MaybeRef,
-} from "vue";
+import { computed, inject, toRef, type App, type InjectionKey, type MaybeRef } from "vue";
 import type { FlattenedKeysOf, TranslationValue } from "../types/i18n";
 import type { DeepPartial } from "../types/utils";
 import enUS from "./locales/en-US.json";
@@ -68,7 +60,7 @@ export type ProvideI18nOptions = {
    *
    * Note: If a custom `t` function is used, passed messages will not be used.
    */
-  t?: ComputedRef<TranslationFunction>;
+  t?: MaybeRef<TranslationFunction>;
 };
 
 export type TranslationFunction = (
@@ -87,11 +79,13 @@ const createI18n = (options: ProvideI18nOptions = {}) => {
    * Current locale.
    * @default "en-US"
    */
-  const locale = computed(() => unref(options?.locale) ?? "en-US");
+  const locale = toRef(options?.locale ?? "en-US");
+  const userT = options.t ? toRef(options.t) : undefined;
 
-  // If the user provided a custom `t` function it should be used instead of the default one
+  // If the user provided a custom `t` function, it is used instead of the default.
+  // Then we also skip the loading and applying of messages.
   if (options.t) {
-    return { t: options.t, locale };
+    return { t: userT, locale };
   }
 
   const messages = computed(() => {
