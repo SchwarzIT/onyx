84533bb859bd68d798f52f1ad7b6f9d0c68b18aa
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2024-12-16 16:15:57 +0100

    structure base config

--- packages/shared/src/playwright.config.base.ts
+++ packages/shared/src/playwright.config.base.ts
@@ -12,9 +12,16 @@ export const vuePluginOptions: Options = {
 };
 
 /**
- * See https://playwright.dev/docs/test-configuration.
+ * Basic, shared playwright configuration
+ *
+ * See https://playwright.dev/docs/test-configuration
  */
 export const PLAYWRIGHT_BASE_CONFIG = {
+  /**
+   * SCREENSHOTS
+   *
+   * See: https://playwright.dev/docs/screenshots
+   */
   snapshotDir: "./playwright/snapshots",
   // custom snapshotPathTemplate to remove the testFileName folder that we don't want
   snapshotPathTemplate: "{snapshotDir}/{testFileDir}/{arg}-{projectName}-{platform}{ext}",
@@ -23,17 +30,36 @@ export const PLAYWRIGHT_BASE_CONFIG = {
   // https://github.com/SchwarzIT/onyx/actions/workflows/playwright-screenshots.yml
   ignoreSnapshots: !process.env.CI,
   updateSnapshots: process.env.PW_UPDATE_SNAPSHOTS ? "all" : "none",
-  timeout: 20 * 1000,
+
+  /**
+   * SHARDING
+   *
+   * See: https://playwright.dev/docs/test-sharding
+   */
   fullyParallel: true,
+  // when (in the pipeline) the sharding environment variables are set, sharding is enabled
   shard:
-    process.env.PW_SHARD && process.env.PW_TOTAL_SHARDS
+    process.env.CI && process.env.PW_SHARD && process.env.PW_TOTAL_SHARDS
       ? {
           current: +process.env.PW_SHARD,
           total: +process.env.PW_TOTAL_SHARDS,
         }
       : null,
+
+  /**
+   * FAILURE HANDLING
+   *
+   * See: https://playwright.dev/docs/test-retries
+   */
+  timeout: 20 * 1000,
   forbidOnly: !!process.env.CI, // fail build on CI if we left test.only in the source code
   retries: process.env.CI ? 1 : 0, // retry on CI only
+
+  /**
+   * REPORTERS
+   *
+   * See: https://playwright.dev/docs/test-reporters
+   */
   /* In the CI pipeline it generates dot (for the stdout) and blob reports, locally only a html report is generated */
   reporter: process.env.CI ? [["dot"], ["blob"]] : [["html", { open: "never" }]],
   use: {
@@ -44,7 +70,12 @@ export const PLAYWRIGHT_BASE_CONFIG = {
       plugins: [vue(vuePluginOptions)],
     },
   },
-  /* Configure projects for major browsers */
+
+  /**
+   * BROWSERS
+   *
+   * See: https://playwright.dev/docs/test-projects
+   */
   projects: [
     { name: "edge", use: { ...devices["Desktop Edge"], channel: "msedge" } },
     { name: "firefox", use: { ...devices["Desktop Firefox"] } },
