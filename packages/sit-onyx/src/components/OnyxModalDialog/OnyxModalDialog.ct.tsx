8c1cc57da60f61753ad68f2a1589ec98e687b84f
Lars Rickert <lars.rickert@mail.schwarz>
2024-12-17 13:05:29 +0100

    limit screenshot size

--- packages/sit-onyx/src/components/OnyxModalDialog/OnyxModalDialog.ct.tsx
+++ packages/sit-onyx/src/components/OnyxModalDialog/OnyxModalDialog.ct.tsx
@@ -1,7 +1,10 @@
+import { ONYX_BREAKPOINTS } from "@sit-onyx/shared/breakpoints";
 import { expect, test } from "../../playwright/a11y";
 import TestWrapperCt from "./TestWrapper.ct.vue";
 
-test("should behave correctly", async ({ mount, makeAxeBuilder }) => {
+test("should behave correctly", async ({ mount, makeAxeBuilder, page }) => {
+  await page.setViewportSize({ width: ONYX_BREAKPOINTS.xs + 1, height: 256 });
+
   let closeEventCount = 0;
 
   // ARRANGE
@@ -9,7 +12,7 @@ test("should behave correctly", async ({ mount, makeAxeBuilder }) => {
   const closeButton = component.getByRole("button", { name: "Close dialog" });
 
   // ASSERT
-  await expect(component).toHaveScreenshot("default.png");
+  await expect(page).toHaveScreenshot("default.png");
 
   // ACT
   const accessibilityScanResults = await makeAxeBuilder().analyze();
