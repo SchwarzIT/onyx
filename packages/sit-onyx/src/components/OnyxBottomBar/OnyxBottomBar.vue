8fe1ab69f617a1e67be8ce13a0280e92108c5a97
Maja Zarkova <60856270+MajaZarkova@users.noreply.github.com>
2024-12-20 13:51:38 +0200

    feat(OnyxBottomBar): Implement OnyxBottomBar (#2382)
    <!-- Is your PR related to an issue? Then please link it via the
    "Relates to #" below. Else, remove it. -->
    
    Relates to #2290 
    
    Implement OnyxBottomBar component
    
    ## Checklist
    
    - [x] The added / edited code has been documented with
    [JSDoc](https://jsdoc.app/about-getting-started)
    - [ ] All changes are documented in the documentation app (folder
    `apps/docs`)
    - [ ] If a new component is added, at least one [Playwright screenshot
    test](https://github.com/SchwarzIT/onyx/actions/workflows/playwright-screenshots.yml)
    is added
    - [x] A changeset is added with `npx changeset add` if your changes
    should be released as npm package (because they affect the library
    usage)

---
+++ packages/sit-onyx/src/components/OnyxBottomBar/OnyxBottomBar.vue
@@ -0,0 +1,71 @@
+<script lang="ts" setup>
+import { useDensity } from "../../composables/density";
+import type { OnyxBottomBarProps } from "./types";
+
+const props = withDefaults(defineProps<OnyxBottomBarProps>(), {
+  hideBorder: false,
+});
+
+defineSlots<{
+  /**
+   * Bottom bar content. This slot is left aligned and should be used by buttons and icon buttons.
+   */
+  left?(): unknown;
+  /**
+   * Bottom bar content. This slot is right aligned and should be used by buttons and icon buttons.
+   */
+  right?(): unknown;
+}>();
+
+const { densityClass } = useDensity(props);
+</script>
+
+<template>
+  <div
+    :class="[
+      'onyx-component',
+      'onyx-bottom-bar',
+      densityClass,
+      props.hideBorder ? '' : 'onyx-bottom-bar--border',
+    ]"
+  >
+    <div class="onyx-bottom-bar__left">
+      <slot name="left"></slot>
+    </div>
+    <div class="onyx-bottom-bar__right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use "../../styles/mixins/layers.scss";
+
+.onyx-bottom-bar {
+  @include layers.component() {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    margin-inline: var(--onyx-grid-margin-inline);
+
+    background-color: var(--onyx-color-base-background-blank);
+    display: flex;
+    justify-content: space-between;
+    gap: var(--onyx-density-xs);
+    padding: var(--onyx-spacing-2xs) var(--onyx-grid-margin);
+    overflow-x: auto;
+
+    &--border {
+      border-top: var(--onyx-1px-in-rem) solid var(--onyx-color-component-border-neutral);
+    }
+
+    &__left,
+    &__right {
+      position: relative;
+      display: flex;
+      max-width: var(--onyx-grid-max-width, max-content);
+      gap: var(--onyx-density-xs);
+    }
+  }
+}
+</style>
