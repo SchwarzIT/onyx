690a81fb48aacefc67edd0745aab28dabde5ed1a
Maja Zarkova <maja.zarkova@mail.schwarz>
2025-01-09 10:33:52 +0200

    Improve storybook example

--- packages/sit-onyx/src/components/OnyxBottomBar/OnyxBottomBar.stories.ts
+++ packages/sit-onyx/src/components/OnyxBottomBar/OnyxBottomBar.stories.ts
@@ -1,6 +1,9 @@
+import chevronLeftSmall from "@sit-onyx/icons/chevron-left-small.svg?raw";
+import chevronRightSmall from "@sit-onyx/icons/chevron-right-small.svg?raw";
 import type { Meta, StoryObj } from "@storybook/vue3";
 import { h } from "vue";
 import OnyxButton from "../OnyxButton/OnyxButton.vue";
+import OnyxIconButton from "../OnyxIconButton/OnyxIconButton.vue";
 import OnyxBottomBar from "./OnyxBottomBar.vue";
 
 /**
@@ -48,7 +51,10 @@ export const Default = {
  */
 export const WithLeftSlot = {
   args: {
-    left: () => [h(OnyxButton, { label: "Button" })],
+    left: () => [
+      h(OnyxIconButton, { label: "Prev", icon: chevronLeftSmall, color: "neutral" }),
+      h(OnyxIconButton, { label: "Next", icon: chevronRightSmall, color: "neutral" }),
+    ],
   },
 } satisfies Story;
 
@@ -61,6 +67,9 @@ export const WithLeftAndRightSlot = {
       h(OnyxButton, { label: "Button", mode: "plain", color: "neutral" }),
       h(OnyxButton, { label: "Button" }),
     ],
-    left: () => [h(OnyxButton, { label: "Approve" })],
+    left: () => [
+      h(OnyxIconButton, { label: "Prev", icon: chevronLeftSmall, color: "neutral" }),
+      h(OnyxIconButton, { label: "Next", icon: chevronRightSmall, color: "neutral" }),
+    ],
   },
 } satisfies Story;
