ad695a3dc164dbdb39e92bf7e3427b8c5ad3bbdb
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-01-14 14:54:29 +0100

    resolve comment

--- packages/sit-onyx/src/composables/useAutoFocus.ts
+++ packages/sit-onyx/src/composables/useAutoFocus.ts
@@ -1,16 +1,12 @@
-import { nextTick, onMounted, type Ref } from "vue";
+import { onMounted, type Ref } from "vue";
 
 export const useAutofocus = (
   inputRef: Ref<HTMLElement | null>,
   props: { autofocus: boolean; loading?: boolean },
 ) => {
   onMounted(() => {
-    if (props.autofocus && !(props.loading ?? false)) {
-      nextTick(() => {
-        if (document.activeElement !== inputRef.value) {
-          inputRef.value?.focus();
-        }
-      });
+    if (props.autofocus && !(props.loading ?? false) && document.activeElement !== inputRef.value) {
+      inputRef.value?.focus();
     }
   });
 };
