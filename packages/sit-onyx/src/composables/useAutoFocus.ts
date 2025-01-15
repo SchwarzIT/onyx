f1a3784825be60fa4d0b985ec5822f15ac37bba6
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-01-15 08:26:12 +0100

    changes

--- packages/sit-onyx/src/composables/useAutoFocus.ts
+++ packages/sit-onyx/src/composables/useAutoFocus.ts
@@ -1,12 +1,23 @@
-import { onMounted, type Ref } from "vue";
+import { onMounted, watch, type Ref } from "vue";
 
 export const useAutofocus = (
   inputRef: Ref<HTMLElement | null>,
   props: { autofocus: boolean; loading?: boolean },
 ) => {
+  if (!props.autofocus) {
+    return;
+  }
+
   onMounted(() => {
-    if (props.autofocus && !(props.loading ?? false) && document.activeElement !== inputRef.value) {
+    if (!props.loading) {
       inputRef.value?.focus();
+      return;
     }
+
+    watch(
+      () => !!props.loading,
+      () => inputRef.value?.focus(),
+      { once: true },
+    );
   });
 };
