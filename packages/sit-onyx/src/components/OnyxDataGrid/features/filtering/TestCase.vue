2874c51890c3e467b181479466c7b4ba9eb9d8ef
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-01-17 09:17:21 +0100

    first Ideas

---
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/filtering/TestCase.vue
@@ -0,0 +1,24 @@
+<!-- <script setup lang="ts">
+import { ref, watch } from "vue";
+import type { DataGridEntry, OnyxDataGridProps } from "../../../..";
+import { DataGridFeatures, OnyxDataGrid } from "../../../..";
+
+const { columns, data } = defineProps<Pick<OnyxDataGridProps, "columns" | "data">>();
+
+const emit = defineEmits<{
+  filterChange: [filterState: DataGridFeatures.FilterState<DataGridEntry>];
+}>();
+
+const filterState = ref<DataGridFeatures.SortState<DataGridEntry>>({
+  column: undefined,
+  direction: "none",
+});
+watch(sortState, () => emit("sortChange", sortState.value), { deep: true });
+
+const withSorting = DataGridFeatures.useSorting({ sortState });
+const features = [withSorting];
+</script>
+
+<template>
+  <OnyxDataGrid :columns :data :features />
+</template> -->
