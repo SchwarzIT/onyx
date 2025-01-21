6876178790453e04bf4c166268df3e6a383a829d
Lars Rickert <lars.rickert@mail.schwarz>
2025-01-21 08:20:26 +0100

    implement basic Nuxt layer

---
+++ packages/nuxt-layer/.playground/nuxt.config.ts
@@ -0,0 +1,4 @@
+export default defineNuxtConfig({
+  compatibilityDate: "2025-01-20",
+  extends: [".."],
+});
---
+++ packages/nuxt-layer/components/content/ProseH2.vue
@@ -0,0 +1,3 @@
+<template>
+  <OnyxHeadline is="h2"> <slot /> </OnyxHeadline>
+</template>
