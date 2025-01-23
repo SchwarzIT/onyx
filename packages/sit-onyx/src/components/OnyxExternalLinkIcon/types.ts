96334854ef8bd16f8b8743a1238c514d1e720999
Lars Rickert <lars.rickert@mail.schwarz>
2025-01-23 19:28:50 +0100

    wip

--- packages/sit-onyx/src/components/OnyxExternalLinkIcon/types.ts
+++ packages/sit-onyx/src/components/OnyxExternalLinkIcon/types.ts
@@ -2,7 +2,7 @@ export type OnyxExternalLinkIcon = {
   /**
    * The URL/link to point to.
    */
-  href?: string;
+  href: string;
   /**
    * Whether to show the external link icon.
    * If set to `auto`, it will be shown when the `href` leads to another website
---
+++ packages/sit-onyx/src/components/OnyxRouterLink/OnyxRouterLink.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { useLink } from "../../composables/useLink";
+import type { OnyxRouterLinkProps } from "./types";
+
+const props = withDefaults(defineProps<OnyxRouterLinkProps>(), {
+  target: "_self",
+});
+
+defineSlots<{
+  default(): unknown;
+}>();
+
+const { navigate } = useLink();
+</script>
+
+<template>
+  <a
+    class="onyx-component onyx-router-link"
+    :href="props.href"
+    :target="props.target"
+    :rel="props.target === '_blank' ? 'noreferrer' : undefined"
+    @click.prevent="navigate(props.href, props.target)"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<style lang="scss">
+@use "../../styles/mixins/layers.scss";
+
+.onyx-router-link {
+  @include layers.component() {
+    all: inherit;
+    cursor: pointer;
+  }
+}
+</style>
