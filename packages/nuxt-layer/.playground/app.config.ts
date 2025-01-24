c35e9a8342031480bfdeb52aa29bb546ec90c9a5
Lars Rickert <lars.rickert@mail.schwarz>
2025-01-24 15:14:45 +0100

    update nav bar

--- packages/nuxt-layer/.playground/app.config.ts
+++ packages/nuxt-layer/.playground/app.config.ts
@@ -1,3 +1,15 @@
 export default defineAppConfig({
-  onyxDocs: {},
+  onyxDocs: {
+    app: {
+      name: "App name",
+      logo: "https://storybook.onyx.schwarz/onyx-logo.svg",
+    },
+    nav: {
+      items: [
+        { label: "Home", href: "/" },
+        { label: "Page 1", href: "/page-1" },
+        { label: "Page 2", href: "/page-2" },
+      ],
+    },
+  },
 });
