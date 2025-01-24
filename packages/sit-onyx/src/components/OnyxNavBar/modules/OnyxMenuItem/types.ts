d795e3a960d51ab7401aea8b7fdc0310a57ce66b
Lars Rickert <lars.rickert@mail.schwarz>
2025-01-24 12:31:35 +0100

    update docs

--- packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxMenuItem/types.ts
+++ packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxMenuItem/types.ts
@@ -1,19 +1,18 @@
 import type { OnyxColor } from "../../../../types";
 import type { OnyxLinkProps } from "../../../OnyxLink/types";
 
-export type OnyxMenuItemProps = Pick<OnyxLinkProps, "target"> &
-  Partial<Pick<OnyxLinkProps, "href">> & {
-    /**
-     * Whether the menu item is currently active.
-     * This is the case if the given link is currently opened.
-     */
-    active?: boolean;
-    /**
-     * Whether the menu item is disabled.
-     */
-    disabled?: boolean;
-    /**
-     * Main color of the item content.
-     */
-    color?: Extract<OnyxColor, "primary" | "danger">;
-  };
+export type OnyxMenuItemProps = Partial<Pick<OnyxLinkProps, "href" | "target">> & {
+  /**
+   * Whether the menu item is currently active.
+   * This is the case if the given link is currently opened.
+   */
+  active?: boolean;
+  /**
+   * Whether the menu item is disabled.
+   */
+  disabled?: boolean;
+  /**
+   * Main color of the item content.
+   */
+  color?: Extract<OnyxColor, "primary" | "danger">;
+};
