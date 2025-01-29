5d7c3a70a9931ceffb3342483ccda95204018df9
Lars Rickert <lars.rickert@mail.schwarz>
2025-01-29 08:32:12 +0100

    add more examples

--- packages/nuxt-layer/content.config.ts
+++ packages/nuxt-layer/content.config.ts
@@ -1,10 +1,21 @@
-import { defineCollection, defineContentConfig } from "@nuxt/content";
+import { defineCollection, defineContentConfig, z } from "@nuxt/content";
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: "page",
       source: "**/*.md",
+      schema: z.object({
+        layout: z.enum(["default", "test"]).optional(),
+      }),
+    }),
+    blog: defineCollection({
+      type: "page",
+      source: "blog/**/*.md",
+      schema: z.object({
+        date: z.string(),
+        image: z.string(),
+      }),
     }),
   },
 });
