84ff8ddbaba8d5a12b640f03b93e02e7ba8f4a5f
Lars Rickert <lars.rickert@mail.schwarz>
2025-02-04 11:24:47 +0100

    link router docs

--- packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxMenuItem/types.ts
+++ packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxMenuItem/types.ts
@@ -5,7 +5,7 @@ export type OnyxMenuItemProps = WithLinkProp & {
   /**
    * Whether the item is currently active.
    * This is the case if the page of the given link is currently open.
-   * If "auto" and a router is provided, the active state will be determined automatically based on the current route.
+   * If "auto" and a [router](https://onyx.schwarz/development/router.html) is provided, the active state will be determined automatically based on the current route.
    */
   active?: boolean | "auto";
   /**
