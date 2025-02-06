b27af05a1bb104ec34589ba22c164c5a5f712e1f
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-02-06 15:23:43 +0100

    fix linting issue

--- packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
+++ packages/sit-onyx/src/components/OnyxSelect/OnyxSelect.vue
@@ -102,7 +102,7 @@ const searchTerm = defineModel<string>("searchTerm", { default: "" });
  */
 const open = defineModel<boolean>("open", { default: false });
 
-const select = useTemplateRef<HTMLElement>("selectRef");
+const select = useTemplateRef<HTMLElement>("selectRef") as Ref<HTMLElement | undefined>;
 const { openDirection, updateOpenDirection } = useOpenDirection(select);
 
 /**
