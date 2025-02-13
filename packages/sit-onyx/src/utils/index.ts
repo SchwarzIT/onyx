b85136ecfba44e5b06af62c45dd8f8509f179095
Lars Rickert <lars.rickert@mail.schwarz>
2025-02-13 16:41:51 +0100

    refactor: use `URL.canParse()`

--- packages/sit-onyx/src/utils/index.ts
+++ packages/sit-onyx/src/utils/index.ts
@@ -2,5 +2,7 @@
  * Checks whether the given link is internal / a relative link of the current application.
  */
 export const isInternalLink = (href: string) => {
-  return href === "" || href.startsWith("/") || href.startsWith("#") || /^\.+\//.test(href);
+  // if link can be parsed, it is a full URL with some protocol and domain and no relative link
+  // see: https://developer.mozilla.org/en-US/docs/Web/API/URL/canParse_static
+  return !URL.canParse(href);
 };
