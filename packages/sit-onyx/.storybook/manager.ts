18547fb559c5465d6f2f5473e32d4aa066b2d357
Lars Rickert <lars.rickert@mail.schwarz>
2025-02-17 11:08:03 +0100

    docs: show `new` badges for new components and features

---
+++ packages/sit-onyx/.storybook/manager.ts
@@ -0,0 +1,35 @@
+import type { Badge, TagBadgeParameters } from "storybook-addon-tag-badges";
+import { addons } from "storybook/internal/manager-api";
+
+addons.setConfig({
+  tagBadges: [
+    {
+      tags: {
+        prefix: "new:*",
+      },
+      display: {
+        toolbar: false,
+      },
+      badge: ({ getTagSuffix, tag }) => {
+        const type: string = getTagSuffix(tag);
+
+        const TAG_TYPES: Record<string, Omit<Badge, "text">> = {
+          component: {
+            bgColor: "#7392aa",
+            fgColor: "#fff",
+          },
+          feature: {
+            bgColor: "#e3eaf0",
+            borderColor: "#7392aa",
+            fgColor: "#697985",
+          },
+        };
+
+        return {
+          text: "New",
+          ...TAG_TYPES[type],
+        };
+      },
+    },
+  ] satisfies TagBadgeParameters,
+});
