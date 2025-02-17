7ae464d906092a162351590f30ad7b5809f62a73
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-02-17 18:14:30 +0100

    implement column categorization for datagrid

---
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/index.spec.ts
@@ -0,0 +1,99 @@
+import { describe, expect, test } from "vitest";
+import type { TableColumnGroup } from "../../../components/OnyxTable/types";
+import type { DataGridEntry } from "../types";
+import {
+  createTableColumnGroups,
+  type ColumnGroupConfig,
+  type NormalizedColumnConfig,
+} from "./index";
+
+describe("createTableColumnGroups", () => {
+  test.each([
+    { title: "no columns", columns: [], columnGroups: { a: { label: "a" } }, expected: [] },
+    {
+      title: "no columnGroups",
+      columns: [{ key: "a" }, { key: "b" }],
+      columnGroups: undefined,
+      expected: undefined,
+    },
+    {
+      title: "neither columnGroups nor columns",
+      columns: undefined,
+      columnGroups: undefined,
+      expected: undefined,
+    },
+    {
+      title: "empty columnGroups and columns",
+      columns: [],
+      columnGroups: {},
+      expected: [],
+    },
+    {
+      title: "basic example",
+      columns: [
+        { key: "a", columnGroupKey: "1st" },
+        { key: "b", columnGroupKey: "1st" },
+      ],
+      columnGroups: { "1st": { label: "First" } },
+      expected: [
+        {
+          key: "1st",
+          span: 2,
+          header: "First",
+        },
+      ],
+    },
+    {
+      title: "mixed",
+      columns: [
+        { key: "a", columnGroupKey: "1st" },
+        { key: "b", columnGroupKey: "1st" },
+        { key: "c", columnGroupKey: "2nd" },
+        { key: "d" },
+        { key: "e", columnGroupKey: "3rd" },
+        { key: "f", columnGroupKey: "1st" },
+        { key: "g", columnGroupKey: "1st" },
+        { key: "h" },
+      ],
+      columnGroups: { "1st": { label: "First" }, "2nd": { label: "Second" } },
+      expected: [
+        {
+          key: "1st",
+          span: 2,
+          header: "First",
+        },
+        {
+          key: "2nd",
+          span: 1,
+          header: "Second",
+        },
+        {
+          key: "",
+          span: 1,
+          header: undefined,
+        },
+        {
+          key: "3rd",
+          span: 1,
+          header: undefined,
+        },
+        {
+          key: "1st",
+          span: 2,
+          header: "First",
+        },
+        {
+          key: "",
+          span: 1,
+          header: undefined,
+        },
+      ],
+    },
+  ])("should create correct columnGroups for $title", ({ columns, columnGroups, expected }) => {
+    const result = createTableColumnGroups(
+      columns as NormalizedColumnConfig<DataGridEntry>[],
+      columnGroups as ColumnGroupConfig | undefined,
+    );
+    expect(result).toMatchObject(expected as TableColumnGroup[]);
+  });
+});
