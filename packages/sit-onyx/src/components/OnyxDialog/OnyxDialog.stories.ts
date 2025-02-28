c557f369ed6cfc273776c5d834b2e62974f0b3e3
Lars Rickert <lars.rickert@mail.schwarz>
2025-02-28 14:54:07 +0100

    update dialog example

--- packages/sit-onyx/src/components/OnyxDialog/OnyxDialog.stories.ts
+++ packages/sit-onyx/src/components/OnyxDialog/OnyxDialog.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
-import { ref, watchEffect } from "vue";
-import OnyxButton from "../OnyxButton/OnyxButton.vue";
+import { createAdvancedStoryExample } from "../../utils/storybook";
 import OnyxDialog from "./OnyxDialog.vue";
 
 /**
@@ -36,26 +35,4 @@ export const Default = {
  * This example shows a modal dialog which will block the primary page content and show a backdrop.
  * **Hint**: In this example, you can close the dialog by pressing "Escape".
  */
-export const Modal: Story = {
-  args: {
-    label: "Example modal dialog",
-    default: "Dialog content...",
-    modal: true,
-  },
-  decorators: [
-    (story, ctx) => ({
-      components: { story, OnyxButton },
-      setup: () => {
-        const isOpen = ref(false);
-        watchEffect(() => {
-          ctx.args.open = isOpen.value;
-        });
-        return { isOpen };
-      },
-      template: `<div>
-        <OnyxButton label="Show modal" @click="isOpen = true" />
-        <story :open="isOpen" @close="isOpen = false;" />
-      </div>`,
-    }),
-  ],
-};
+export const Modal = await createAdvancedStoryExample("OnyxDialog", "ModalExample");
