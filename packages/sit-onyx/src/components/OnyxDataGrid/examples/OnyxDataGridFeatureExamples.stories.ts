49222b40e70d07840f5e863370e1ede7b3205c6e
Lars Rickert <lars.rickert@mail.schwarz>
2025-02-28 14:39:20 +0100

    refactor data grid examples

---
+++ packages/sit-onyx/src/components/OnyxDataGrid/examples/OnyxDataGridFeatureExamples.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import { createAdvancedStoryExample } from "../../../utils/storybook";
+import OnyxDataGrid from "../../OnyxDataGrid/OnyxDataGrid.vue";
+
+const meta: Meta<typeof OnyxDataGrid> = {
+  title: "Data/DataGrid/Features",
+  component: OnyxDataGrid,
+  tags: ["!autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Sorting: Story = {
+  tags: ["new:feature"],
+  ...(await createAdvancedStoryExample("OnyxDataGrid", "SortingExample")),
+};
+
+export const Filtering: Story = {
+  tags: ["new:feature"],
+  ...(await createAdvancedStoryExample("OnyxDataGrid", "FilteringExample")),
+};
+
+export const Selection: Story = await createAdvancedStoryExample(
+  "OnyxDataGrid",
+  "SelectionExample",
+);
