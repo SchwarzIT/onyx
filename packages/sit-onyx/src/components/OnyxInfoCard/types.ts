c60b3bf330f5c5a78426f68a24a4f49e2bb5867d
d-dimitrovv <dimitar.dimitrov@mail.schwarz>
2025-03-06 16:54:25 +0200

    feat: implement OnyxInfoCard component (#2854)
    Relates to #2788, #2737
    
    Implement a new OnyxInfoCard component.
    
    ---------
    
    Co-authored-by: Lars Rickert <lars.rickert@mail.schwarz>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: larsrickert <67898185+larsrickert@users.noreply.github.com>

---
+++ packages/sit-onyx/src/components/OnyxInfoCard/types.ts
@@ -0,0 +1,22 @@
+import type { DensityProp } from "../../composables/density";
+import type { OnyxColor } from "../../types";
+
+export type OnyxInfoCardProps = DensityProp & {
+  /**
+   * Card headline.
+   */
+  headline?: string;
+  /**
+   * Toast color.
+   */
+  color?: OnyxColor;
+  /**
+   * Icon to display. If unset, a default icon will be displayed.
+   * Can be set to `false` to hide the icon.
+   */
+  icon?: string | false;
+  /**
+   * Whether the info card can be closed. Will show an "x" icon.
+   */
+  closable?: boolean;
+};
