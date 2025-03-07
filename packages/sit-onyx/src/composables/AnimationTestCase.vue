9ca7eec1bac0414efc2ac7d429183d5697c3b31f
Jonathan <jonathan_leo.carle@mail.schwarz>
2025-03-07 08:50:42 +0100

    fix(OnyxSkeleton): fix animation de-synced in some cases (#2911)
    Relates to #771

---
+++ packages/sit-onyx/src/composables/AnimationTestCase.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import OnyxSkeleton from "../components/OnyxSkeleton/OnyxSkeleton.vue";
+
+const shown = ref(false);
+
+// we actively delay the second skeleton after the first one is mounted to ensure that they have different animation start times
+onMounted(() => setTimeout(() => (shown.value = true), 50));
+</script>
+
+<template>
+  <OnyxSkeleton data-testid="permanent" />
+  <OnyxSkeleton v-if="shown" data-testid="delayed" />
+</template>
+
+<style>
+.onyx-skeleton {
+  height: 1rem;
+  width: 4rem;
+}
+</style>
