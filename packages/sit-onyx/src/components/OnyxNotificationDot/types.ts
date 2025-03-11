18bd03ab987740d8e556398921e4cf2db3dca1a4
d-dimitrovv <dimitar.dimitrov@mail.schwarz>
2025-03-11 09:39:40 +0200

    feat: implement `OnyxNotificationDot` component (#2635)
    Relates to #2737, #456
    
    Implement new OnyxNotificationDot component to support showing a dot in
    the top right corner of any component
    
    ---------
    
    Co-authored-by: Lars Rickert <lars.rickert@mail.schwarz>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: larsrickert <67898185+larsrickert@users.noreply.github.com>

---
+++ packages/sit-onyx/src/components/OnyxNotificationDot/types.ts
@@ -0,0 +1,12 @@
+import type { OnyxBadgeProps } from "../OnyxBadge/types";
+
+export type OnyxNotificationDotProps = {
+  /**
+   * If `true`, the dot will be hidden.
+   */
+  hidden?: boolean;
+  /**
+   * Dot color.
+   */
+  color?: OnyxBadgeProps["color"];
+};
