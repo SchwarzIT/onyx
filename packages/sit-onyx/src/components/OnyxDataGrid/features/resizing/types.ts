47ec7ff2b577236eb5a333e0ff2948e907f891db
Maja Zarkova <maja.zarkova@mail.schwarz>
2025-03-12 13:24:10 +0200

    Remove unused type

--- packages/sit-onyx/src/components/OnyxDataGrid/features/resizing/types.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/resizing/types.ts
@@ -1,19 +1,4 @@
 import type { DataGridEntry } from "../../types";
-
-/**
- * Configuration for filtering on a per-column basis.
- * If at least one column has a filtering configuration, filtering must be explicitly enabled for all columns.
- */
-export type ResizeColumnOptions<TEntry extends DataGridEntry> = {
-  [TKey in keyof TEntry]?: {
-    /**
-     * Determines if resizing is enabled for this column.
-     * If set to false, the column will not be resizable.
-     */
-    enabled: boolean;
-  };
-};
-
 /**
  * The configuration options for the resizing feature in the OnyxDataGrid component.
  * Includes settings for the individual columns and general resizing behavior.
