c1ebd87ba5b81db2d4853c1ccddb8234b5fbdfe3
Lars Rickert <lars.rickert@mail.schwarz>
2025-03-17 08:51:14 +0100

    update JSDoc

--- packages/sit-onyx/src/components/OnyxDataGrid/features/hideColumns/types.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/hideColumns/types.ts
@@ -2,15 +2,20 @@ import type { MaybeRef } from "vue";
 
 export type HideColumnsOptions = {
   /**
-   * Defines which columns can be hidden
-   * If omitted, all columns are hidable
+   * Defines which columns can be hidden. If omitted, all columns are hidable.
    */
   columns?: MaybeRef<HideColumn[]>;
 };
 
 export type HideColumn = {
-  /** The name of the column */
+  /**
+   * The name of the column.
+   */
   name: string;
-  /** Whether the column is hidden (default: false) */
+  /**
+   * Whether the column is hidden.
+   *
+   * @default false
+   */
   hidden?: boolean;
 };
