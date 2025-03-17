da169bbfda9576f781c60eb96bea2b94e338d28b
d-dimitrovv <dimitar.dimitrov@mail.schwarz>
2025-03-17 12:50:41 +0200

    feat(OnyxAccordion): add new property `type` (#2633)
    Relates to #2737
    
    Implement new `type` property for the OnyxAccordion to support
    nested-large and nested-small visual type that can be used e.g. inside
    the OnyxDrawer, sidebar, notification center etc.
    
    ---------
    
    Co-authored-by: Lars Rickert <lars.rickert@mail.schwarz>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: larsrickert <67898185+larsrickert@users.noreply.github.com>

---
+++ packages/sit-onyx/src/components/OnyxAccordion/examples/NestedLargeExample.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { OnyxAccordion, OnyxAccordionItem, OnyxDrawer } from "../../..";
+</script>
+
+<template>
+  <OnyxDrawer label="Example headline" open>
+    <template #description>
+      The "nested-large" accordion type is intended to be used inside parent components like e.g.
+      the OnyxDrawer.
+    </template>
+
+    <OnyxAccordion type="nested-large">
+      <OnyxAccordionItem value="item-1">
+        <template #header>Accordion 1</template>
+        Content...
+      </OnyxAccordionItem>
+
+      <OnyxAccordionItem value="item-2">
+        <template #header>Accordion 2</template>
+        Content...
+      </OnyxAccordionItem>
+
+      <OnyxAccordionItem value="item-3">
+        <template #header>Accordion 3</template>
+        Content...
+      </OnyxAccordionItem>
+    </OnyxAccordion>
+  </OnyxDrawer>
+</template>
