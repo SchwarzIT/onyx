5c7f33409c28ddb28b8eb1224a9497da4ca42757
Lars Rickert <lars.rickert@mail.schwarz>
2025-03-18 08:54:59 +0100

    refactor notifications provider

--- packages/sit-onyx/src/components/OnyxNotifications/OnyxNotifications.vue
+++ packages/sit-onyx/src/components/OnyxNotifications/OnyxNotifications.vue
@@ -1,26 +1,60 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-import OnyxNotificationDot from "../OnyxNotificationDot/OnyxNotificationDot.vue";
+import OnyxNotificationMessage from "../OnyxNotificationMessage/OnyxNotificationMessage.vue";
 import { useNotification } from "./useNotification";
 
 const notificationProvider = useNotification();
-const currentNotification = ref(notificationProvider.notificationsQueue.value[0]);
-
-const removeNotification = () => {
-  notificationProvider.remove(currentNotification.value.id);
-  if (notificationProvider.notificationsQueue.value.length !== 0) {
-    currentNotification.value = notificationProvider.notificationsQueue.value[0];
-  }
-};
-
-const handleManualClose = () => {
-  removeNotification();
-};
 </script>
 
 <!-- eslint-disable vue/no-root-v-if -->
 <template>
-  <div v-if="notificationProvider.notificationsQueue.value.length" class="onyx-component">
-    <OnyxNotificationDot v-bind="currentNotification" @close="handleManualClose" />
-  </div>
+  <dialog
+    v-if="notificationProvider.notifications.value.length"
+    class="onyx-component onyx-notifications"
+    role="presentation"
+    aria-live="polite"
+    open
+  >
+    <OnyxNotificationMessage
+      v-for="{ id, ...notification } in notificationProvider.notifications.value"
+      :key="id"
+      v-bind="notification"
+    />
+  </dialog>
 </template>
+
+<style lang="scss">
+@use "../../styles/mixins/layers.scss";
+
+.onyx-notifications {
+  @include layers.component() {
+    $margin-bottom: var(--onyx-spacing-xl);
+
+    padding: 0;
+    border: none;
+    background: none;
+    z-index: var(--onyx-z-index-notification);
+
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    overflow: auto;
+
+    width: max-content;
+    max-width: 100%;
+    max-height: calc(100% - $margin-bottom);
+
+    // we are using margin on the individual toasts instead of gap/margin on the parent
+    // so the box shadows of the toasts are not cut off
+    // .onyx-toast-message {
+    //   $margin-inline: var(--onyx-grid-margin);
+    //   margin: var(--onyx-spacing-2xs) $margin-inline;
+    //   max-width: calc(100% - 2 * $margin-inline);
+
+    //   &:last-child {
+    //     margin-bottom: $margin-bottom;
+    //   }
+    // }
+  }
+}
+</style>
---
+++ packages/sit-onyx/src/components/OnyxNotifications/examples/DefaultExample.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { OnyxButton, OnyxNotifications, useNotification } from "../../..";
+
+const notification = useNotification();
+
+const showExampleNotifications = () => {
+  notification.show({ headline: "Example notification 1", description: "Example description 1" });
+  notification.show({ headline: "Example notification 2", description: "Example description 2" });
+  notification.show({ headline: "Example notification 3", description: "Example description 3" });
+};
+</script>
+
+<template>
+  <div class="example">
+    <OnyxButton label="Show example notifications" @click="showExampleNotifications" />
+    <OnyxNotifications />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.example {
+  height: 24rem;
+}
+</style>
