81dd4cddb1e3c2d1c69c983344d1418bf4ed8a2c
Lars Rickert <lars.rickert@mail.schwarz>
2025-03-18 15:03:40 +0100

    add tests

---
+++ packages/sit-onyx/src/components/OnyxNotifications/PlaywrightTestWrapper.ct.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { provide } from "vue";
+import PlaywrightTest from "./PlaywrightTest.ct.vue";
+import {
+  createNotificationsProvider,
+  NOTIFICATIONS_PROVIDER_INJECTION_KEY,
+} from "./useNotification";
+
+provide(NOTIFICATIONS_PROVIDER_INJECTION_KEY, createNotificationsProvider());
+</script>
+
+<template>
+  <PlaywrightTest class="onyx-component" />
+</template>
