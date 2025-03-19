87a4fcbd1875f0cc085e67961a789e60956bd349
Lars Rickert <lars.rickert@mail.schwarz>
2025-03-19 15:38:13 +0100

    move breadcrumb item to support components

--- packages/sit-onyx/src/components/OnyxBreadcrumbItem/OnyxBreadcrumbItem.stories.ts
+++ packages/sit-onyx/src/components/OnyxBreadcrumbItem/OnyxBreadcrumbItem.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import OnyxBreadcrumbItem from "./OnyxBreadcrumbItem.vue";
 
 const meta: Meta<typeof OnyxBreadcrumbItem> = {
-  title: "Navigation/BreadcrumbItem",
+  title: "Support/BreadcrumbItem",
   component: OnyxBreadcrumbItem,
   tags: ["new:component"],
 };
