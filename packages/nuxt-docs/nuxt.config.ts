2f0eb533bf3e31c64dd6f4de43567d22f965954e
Lars Rickert <lars.rickert@mail.schwarz>
2025-03-20 14:14:19 +0100

    update prose components

--- packages/nuxt-docs/nuxt.config.ts
+++ packages/nuxt-docs/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2025-01-20",
-  modules: ["@sit-onyx/nuxt", "@nuxt/content", "@nuxtjs/color-mode"],
+  modules: ["@sit-onyx/nuxt", "@nuxt/content", "@nuxtjs/color-mode", "@nuxt/image"],
   css: ["@fontsource-variable/source-code-pro", "@fontsource-variable/source-sans-3"],
   colorMode: {
     classSuffix: "",
