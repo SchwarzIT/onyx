89e26ef89ba3ede336ca65862e15024b4877fb82
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-03-25 16:46:12 +0100

    looks alright

--- packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxNavButton/NavButtonLayout.vue
+++ packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxNavButton/NavButtonLayout.vue
@@ -2,11 +2,16 @@
 // this layout component is only used internally for the nav button component
 // to easily switch between mobile and desktop layout
 import arrowSmallLeft from "@sit-onyx/icons/arrow-small-left.svg?raw";
+import { computed } from "vue";
+import { useLink } from "../../../../composables/useLink";
 import { useVModel, type Nullable } from "../../../../composables/useVModel";
 import { injectI18n } from "../../../../i18n";
 import OnyxButton from "../../../OnyxButton/OnyxButton.vue";
 import OnyxFlyoutMenu from "../OnyxFlyoutMenu/OnyxFlyoutMenu.vue";
+import OnyxNavItemMobile from "../OnyxNavItemMobile/OnyxNavItemMobile.vue";
 import OnyxNavSeparator from "../OnyxNavSeparator/OnyxNavSeparator.vue";
+import OnyxNavItemDesktop from "./NavItemDesktop.vue";
+import NavListItemDesktop from "./NavListItemDesktop.vue";
 import type { OnyxNavButtonProps } from "./types";
 
 const props = withDefaults(
@@ -16,15 +21,20 @@ const props = withDefaults(
        * If the mobile layout should be used.
        */
       isMobile: boolean;
+      /**
+       * If the layout is the outer most item.
+       */
+      isTopLevel: boolean;
     }
   >(),
-  { mobileChildrenOpen: undefined },
+  { open: undefined },
 );
+
 const emit = defineEmits<{
   /**
    * Emitted when the state of mobile children visibility changes.
    */
-  "update:mobileChildrenOpen": [value: Nullable<boolean>];
+  "update:open": [value: Nullable<boolean>];
 }>();
 
 const slots = defineSlots<{
@@ -41,53 +51,104 @@ const slots = defineSlots<{
 }>();
 
 const { t } = injectI18n();
-const mobileChildrenOpen = useVModel({
+
+const open = useVModel({
   props,
   emit,
-  key: "mobileChildrenOpen",
+  key: "open",
   initialValue: false,
+});
+
+const hasChildren = computed(() => !!slots.options);
+
+const { isActive } = useLink();
+const active = computed(() => {
+  if (props.active !== "auto") return props.active;
+  return isActive.value(props.link);
 });
 </script>
 
 <template>
-  <li class="onyx-component" role="presentation">
-    <template v-if="props.isMobile">
-      <OnyxButton
-        v-if="props.isMobile && mobileChildrenOpen"
-        :label="t('back')"
-        mode="plain"
-        color="neutral"
-        :icon="arrowSmallLeft"
-        @click="mobileChildrenOpen = false"
-      />
+  <li
+    v-if="props.isMobile"
+    :class="{
+      'onyx-component': true,
+      'onyx-nav-button-layout': true,
+      'onyx-nav-button-layout--mobile': props.isMobile,
+      'onyx-nav-button-layout--open': open,
+    }"
+    role="presentation"
+  >
+    <template v-if="open">
+      <div class="onyx-nav-button-layout__controls" role="presentation">
+        <OnyxButton
+          :label="t('back')"
+          mode="plain"
+          color="neutral"
+          :icon="arrowSmallLeft"
+          @click="open = false"
+        />
 
-      <slot
-        v-if="!mobileChildrenOpen || props.link"
-        name="button"
-        :trigger="{ trigger: {} }"
-      ></slot>
-      <OnyxNavSeparator v-if="mobileChildrenOpen && props.link" orientation="horizontal" />
-
-      <ul
-        v-show="mobileChildrenOpen"
-        role="menu"
-        class="onyx-nav-button__mobile-children"
-        :class="{ 'onyx-nav-button__mobile-children--open': mobileChildrenOpen }"
-      >
+        <OnyxNavItemMobile v-bind="props" :has-children="false" :active="active" />
+        <OnyxNavSeparator orientation="horizontal" />
+      </div>
+      <ul role="menu" class="onyx-nav-button-layout__mobile-children">
         <slot name="options"></slot>
       </ul>
     </template>
-
-    <template v-else>
-      <OnyxFlyoutMenu :label="t('navItemOptionsLabel', { label: props.label })">
+    <OnyxNavItemMobile
+      v-else
+      v-bind="props"
+      :has-children="hasChildren"
+      :active="active"
+      @click="hasChildren && (open = true)"
+    />
+  </li>
+  <template v-else>
+    <li v-if="props.isTopLevel" class="onyx-component onyx-nav-button-layout">
+      <OnyxFlyoutMenu v-if="hasChildren" :label="t('navItemOptionsLabel', { label: props.label })">
         <template #button="{ trigger }">
-          <slot name="button" :trigger="trigger"></slot>
+          <OnyxNavItemDesktop v-bind="{ ...props, ...trigger, active }" />
         </template>
 
-        <template v-if="slots.options" #options>
+        <template #options>
           <slot name="options"></slot>
         </template>
       </OnyxFlyoutMenu>
-    </template>
-  </li>
+      <OnyxNavItemDesktop v-else v-bind="props" :active="active" />
+    </li>
+    <NavListItemDesktop v-else v-bind="props" :active />
+  </template>
 </template>
+
+<style lang="scss">
+.onyx-nav-button-layout {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: var(--onyx-spacing-xs);
+
+  &__mobile-children {
+    padding: 0;
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    gap: var(--onyx-spacing-2xs);
+  }
+
+  &__controls {
+    display: contents;
+  }
+
+  &:has(~ .onyx-nav-button-layout--open),
+  &--open ~ .onyx-nav-button-layout {
+    display: none;
+  }
+
+  &--open:has(.onyx-nav-button-layout--open) > &__controls {
+    display: none;
+  }
+}
+</style>
