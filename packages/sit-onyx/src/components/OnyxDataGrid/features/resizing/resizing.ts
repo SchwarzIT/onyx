e40c5e6dca67062aa9871cb24bbd91d2566e0076
Maja Zarkova <60856270+MajaZarkova@users.noreply.github.com>
2025-04-01 15:09:03 +0300

    fix(OnyxDataGrid): Apply mob session feedback (#3026)
    <!-- Is your PR related to an issue? Then please link it via the
    "Relates to #" below. Else, remove it. -->
    
    Relates to #2972 
    
    - The column is not growing both sides when resizing it (reproducible
    with second column)
    - The last column is not resizible
    - Increased the clickarea for the resizing stroke
    - The cursor stays in resezing tool during dragging the column border
    
    ## Checklist
    
    <!-- If bullet points below do not apply to your changes (e.g. no
    documentation needed), just remove it. -->
    
    - [x] The added / edited code has been documented with
    [JSDoc](https://jsdoc.app/about-getting-started)
    - [x] If a new component is added, at least one [Playwright screenshot
    test](https://github.com/SchwarzIT/onyx/actions/workflows/playwright-screenshots.yml)
    or functional test is added
    - [x] I have performed a self review of my code ("Files changed" tab in
    the pull request)
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/sit-onyx/src/components/OnyxDataGrid/features/resizing/resizing.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/resizing/resizing.ts
@@ -1,4 +1,4 @@
-import { h, ref, type Slot, type TdHTMLAttributes } from "vue";
+import { h, ref, type Slot, type TdHTMLAttributes, type ThHTMLAttributes } from "vue";
 import { createFeature, useIsFeatureEnabled, type InternalColumnConfig } from "..";
 import { mergeVueProps } from "../../../../utils/attrs";
 import type { DataGridEntry } from "../../types";
@@ -8,50 +8,85 @@ import type { ResizingOptions } from "./types";
 export const RESIZING_FEATURE = Symbol("Resizing");
 export const useResizing = createFeature(
   <TEntry extends DataGridEntry>(options?: ResizingOptions<TEntry>) => {
-    let column: Readonly<InternalColumnConfig<TEntry>>;
+    let resizingCol: Readonly<InternalColumnConfig<TEntry>>;
     const min = 70;
     const headerBeingResized = ref<Record<PropertyKey, HTMLElement>>({});
+    const rowsBeingResized = ref<HTMLElement[]>([]); //ref<HTMLElement>(HTMLElement[]);
     const { isEnabled } = useIsFeatureEnabled(options);
+    const columnWidths = ref<Record<PropertyKey, number>>({});
 
     const onMouseMove = (ev: { clientX: number }) => {
       // Calculate the desired width
       const horizontalScrollOffset = document.documentElement.scrollLeft;
       const width =
-        horizontalScrollOffset + ev.clientX - headerBeingResized.value[column.key].offsetLeft;
+        horizontalScrollOffset + ev.clientX - headerBeingResized.value[resizingCol.key].offsetLeft;
 
-      headerBeingResized.value[column.key].style.width = Math.max(min, width) + "px";
+      columnWidths.value[resizingCol.key as PropertyKey] = width;
+
+      // Changing the width directly is needed to avoid re-rendering the table too often.
+      headerBeingResized.value[resizingCol.key].style.width = Math.max(min, width) + "px";
     };
 
     // Clean up event listeners, classes, etc.
     const onMouseUp = () => {
       window.removeEventListener("mousemove", onMouseMove);
       window.removeEventListener("mouseup", onMouseUp);
-      headerBeingResized.value[column.key].classList.remove("header--being-resized");
-      headerBeingResized.value = {};
+      headerBeingResized.value[resizingCol.key].classList.remove("header--being-resized");
+      rowsBeingResized.value.forEach((row) =>
+        row.classList.remove("onyx-data-grid--resize-border"),
+      );
     };
 
     const initResize = (ev: Event, cols: Readonly<InternalColumnConfig<TEntry>>) => {
       const target = ev.target as HTMLElement;
-      column = cols;
+      const tableEl = target.closest("table");
+      resizingCol = cols;
 
-      headerBeingResized.value[column.key] = target.closest("th")!;
+      Object.entries(headerBeingResized.value).forEach(([key, headerRef]) => {
+        const width = headerRef.getBoundingClientRect().width;
+        headerRef.style.width = width + "px";
+        columnWidths.value[key] = width;
+      });
+
+      headerBeingResized.value[resizingCol.key] = target.closest("th")!;
       window.addEventListener("mousemove", onMouseMove);
       window.addEventListener("mouseup", onMouseUp);
-      headerBeingResized.value[column.key].classList.add("header--being-resized");
+      headerBeingResized.value[resizingCol.key].classList.add("header--being-resized");
+      rowsBeingResized.value.forEach((row) => row.classList.add("onyx-data-grid--resize-border"));
+      tableEl!.style.tableLayout = "fixed";
     };
 
     const modifyColumns = (cols: Readonly<InternalColumnConfig<TEntry>[]>) => {
       return cols.map((column) => {
         if (!isEnabled.value(column.key)) return column;
 
-        const tdAttributes: TdHTMLAttributes | undefined = {
-          class: Object.keys(headerBeingResized.value).includes(column.key as string)
-            ? "onyx-data-grid--resize-border"
-            : "",
+        const style = {
+          width: Math.max(min, columnWidths.value[column.key]) + "px",
         };
+
+        const tdAttributes: TdHTMLAttributes = {
+          ref: (el: HTMLElement) =>
+            resizingCol?.key !== null &&
+            resizingCol?.key === column.key &&
+            rowsBeingResized.value.push(el),
+          class:
+            resizingCol?.key !== null && resizingCol?.key === column.key
+              ? "onyx-data-grid--resize-border"
+              : "",
+        } as TdHTMLAttributes;
+
+        const thAttributes = {
+          ref: (el: HTMLElement) => (headerBeingResized.value[column.key] = el),
+          class:
+            resizingCol?.key !== null && resizingCol?.key === column.key
+              ? "header--being-resized"
+              : "",
+          style,
+        } as ThHTMLAttributes;
 
         return {
           ...column,
+          thAttributes: mergeVueProps(thAttributes, column.thAttributes),
           tdAttributes: mergeVueProps(tdAttributes, column.tdAttributes),
         };
       });
@@ -62,22 +97,28 @@ export const useResizing = createFeature(
       slots: Readonly<{ [name: string]: Slot | undefined }>,
       cols: Readonly<InternalColumnConfig<TEntry>>,
     ) => {
-      const wrapper = !isEnabled.value(cols.key)
-        ? [slots.default?.()]
-        : [
-            h("div", {
-              ...props,
-              class: "onyx-data-grid--resize-handle",
-              onmousedown: (ev: MouseEvent) => initResize(ev, cols),
-            }),
-            slots.default?.(),
-          ];
+      const lastColumn = Object.entries(headerBeingResized.value)[
+        Object.entries(headerBeingResized.value).length - 1
+      ];
+      const isLastColumn = lastColumn !== undefined && lastColumn[0] === cols.key;
+
+      const wrapper =
+        !isEnabled.value(cols.key) || isLastColumn
+          ? [slots.default?.()]
+          : [
+              h("div", {
+                ...props,
+                class: "onyx-data-grid--resize-handle",
+                onmousedown: (ev: MouseEvent) => initResize(ev, cols),
+              }),
+              slots.default?.(),
+            ];
       return wrapper;
     };
 
     return {
       name: RESIZING_FEATURE,
-      watch: [headerBeingResized],
+      watch: [headerBeingResized, columnWidths],
       modifyColumns: {
         func: modifyColumns,
       },
