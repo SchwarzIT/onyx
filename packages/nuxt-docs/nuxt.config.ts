b89ca45880bc2e05efd2c79e602be6abbaf4289b
Lars Rickert <lars.rickert@mail.schwarz>
2025-04-08 12:24:09 +0200

    fix(@sit-onyx/nuxt-docs): prevent missing test module on startup

--- packages/nuxt-docs/nuxt.config.ts
+++ packages/nuxt-docs/nuxt.config.ts
@@ -3,13 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2025-01-20",
   typescript: { typeCheck: "build" },
-  modules: [
-    "@sit-onyx/nuxt",
-    "@nuxt/content",
-    "@nuxtjs/color-mode",
-    "@nuxt/image",
-    "@nuxt/test-utils/module",
-  ],
+  modules: ["@sit-onyx/nuxt", "@nuxt/content", "@nuxtjs/color-mode", "@nuxt/image"],
   css: ["@fontsource-variable/source-code-pro", "@fontsource-variable/source-sans-3"],
   colorMode: {
     classSuffix: "",
