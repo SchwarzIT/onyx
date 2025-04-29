9122f0c7e001832c68e49066422de8002eb46e3f
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-04-29 22:39:24 +0200

    add tests

---
+++ packages/sit-onyx/src/components/OnyxFlyout/OnyxFlyout.ct.tsx
@@ -0,0 +1,48 @@
+import { expect, test } from "../../playwright/a11y";
+import OnyxFlyoutTestCase from "./OnyxFlyoutTestCase.vue";
+
+test.describe("OnyxFlyout", () => {
+  test("should open and close on trigger click", async ({ mount }) => {
+    const component = await mount(OnyxFlyoutTestCase, {
+      props: { label: "Flyout for testing" },
+    });
+
+    const flyout = component.getByText("Flyout Content");
+    const button = component.getByRole("button", { name: "Open" });
+
+    // ASSERT
+    await expect(flyout).toBeHidden();
+
+    // ACT
+    button.click();
+
+    // ASSERT
+    await expect(flyout).toBeVisible();
+
+    // ACT
+    button.click();
+
+    // ASSERT
+    await expect(flyout).toBeHidden();
+  });
+
+  test("should show when expanded is set to true and hide when false", async ({ mount }) => {
+    const component = await mount(OnyxFlyoutTestCase, {
+      props: {
+        label: "Flyout for testing",
+      },
+    });
+
+    const flyout = component.getByText("Flyout Content");
+    const changeButton = component.getByRole("button", { name: "Change Expanded" });
+
+    // ASSERT
+    await expect(flyout).toBeHidden();
+
+    // ACT
+    await changeButton.click();
+
+    // ASSERT
+    await expect(flyout).toBeVisible();
+  });
+});
---
+++ packages/sit-onyx/src/components/OnyxFlyout/OnyxFlyoutTestCase.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import OnyxFlyout from "./OnyxFlyout.vue";
+import type { OnyxFlyoutProps } from "./types";
+
+const props = defineProps<OnyxFlyoutProps>();
+const isExpanded = ref(false);
+</script>
+
+<template>
+  <button
+    class="onyx-component"
+    type="button"
+    label="changeExpandedButton"
+    @click="isExpanded = !isExpanded"
+  >
+    Change Expanded
+  </button>
+  <OnyxFlyout v-bind="props" :expanded="isExpanded">
+    <template #default="{ trigger }">
+      <button v-bind="trigger" label="button" type="button">Open</button>
+    </template>
+    <template #content>
+      <div label="content">Flyout Content</div>
+    </template>
+  </OnyxFlyout>
+</template>
