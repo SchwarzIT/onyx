072a76d28b0146477ab48eff5bed47c89b157b7f
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-05 10:54:11 +0200

    feat: implement basic OnyxSidebar

---
+++ packages/sit-onyx/src/components/OnyxSidebar/examples/DefaultExample.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { OnyxAppLayout, OnyxButton, OnyxHeadline, OnyxPageLayout, OnyxSidebar } from "../../..";
+</script>
+
+<template>
+  <OnyxAppLayout>
+    <OnyxPageLayout>
+      <template #sidebar>
+        <OnyxSidebar>
+          <template #header> Header content </template>
+
+          <div class="sidebar__content">Body content</div>
+
+          <template #footer>
+            <OnyxButton color="neutral" label="Button" />
+            <OnyxButton label="Button" />
+          </template>
+        </OnyxSidebar>
+      </template>
+
+      <!-- page content -->
+      <div class="onyx-grid-container">
+        <OnyxHeadline is="h1">Page content</OnyxHeadline>
+      </div>
+    </OnyxPageLayout>
+  </OnyxAppLayout>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  &__content {
+    padding: var(--onyx-sidebar-padding);
+  }
+}
+</style>
