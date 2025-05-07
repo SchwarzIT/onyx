cefb73efd41d132358066468f22d7b81f686d8b7
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-07 08:04:38 +0200

    update demo app

--- apps/demo-app/src/components/layout-demo/SidebarDemo.vue
+++ apps/demo-app/src/components/layout-demo/SidebarDemo.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import xSmall from "@sit-onyx/icons/x-small.svg?raw";
-import { OnyxButton, OnyxHeadline } from "sit-onyx";
+import { OnyxButton, OnyxHeadline, OnyxSidebar } from "sit-onyx";
 
 const isOpen = defineModel<boolean>();
 defineProps<{ isClosable?: boolean }>();
@@ -14,20 +14,18 @@ defineSlots<{
 </script>
 
 <template>
-  <div class="sidebar">
-    <section>
-      <OnyxHeadline is="h2">Sidebar</OnyxHeadline>
-      <slot></slot>
-    </section>
+  <OnyxSidebar label="Sidebar demo">
+    <div class="sidebar__content">
+      <section>
+        <OnyxHeadline is="h2">Sidebar</OnyxHeadline>
+        <slot></slot>
+      </section>
+    </div>
 
-    <OnyxButton
-      v-if="isClosable"
-      class="sidebar__close"
-      :icon="xSmall"
-      label="Close"
-      @click="isOpen = false"
-    />
-  </div>
+    <template v-if="isClosable" #footer>
+      <OnyxButton :icon="xSmall" label="Close" @click="isOpen = false" />
+    </template>
+  </OnyxSidebar>
 </template>
 
 <style lang="scss" scoped>
@@ -41,8 +39,11 @@ defineSlots<{
   border-right: var(--onyx-1px-in-rem) solid var(--onyx-color-component-border-neutral);
   background-color: var(--onyx-color-base-background-blank);
 
-  &__close {
-    justify-self: flex-end;
+  &__content {
+    padding: var(--onyx-sidebar-padding);
+    display: flex;
+    flex-direction: column;
+    gap: var(--onyx-density-md);
   }
 }
 </style>
