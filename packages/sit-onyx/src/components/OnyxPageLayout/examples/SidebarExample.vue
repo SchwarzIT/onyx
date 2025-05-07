1efdc7b2bf5325d7fc32155dd7e62891666681a8
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-07 07:58:05 +0200

    update examples

--- packages/sit-onyx/src/components/OnyxPageLayout/examples/SidebarExample.vue
+++ packages/sit-onyx/src/components/OnyxPageLayout/examples/SidebarExample.vue
@@ -1,28 +1,23 @@
 <script setup lang="ts">
-import { OnyxHeadline, OnyxLink, OnyxPageLayout, OnyxSidebar } from "../../..";
+import { OnyxAppLayout, OnyxHeadline, OnyxLink, OnyxPageLayout, OnyxSidebar } from "../../..";
 </script>
 
 <template>
-  <OnyxPageLayout class="page">
-    <template #sidebar>
-      <OnyxSidebar label="Example sidebar">
-        <template #header>
-          For sidebar examples, see:
-          <OnyxLink href="/?path=/docs/navigation-sidebar--docs" target="_parent">
-            OnyxSidebar
-          </OnyxLink>
-        </template>
-      </OnyxSidebar>
-    </template>
+  <OnyxAppLayout>
+    <OnyxPageLayout>
+      <template #sidebar>
+        <OnyxSidebar label="Example sidebar">
+          <template #header>
+            For sidebar examples, see:
+            <OnyxLink href="/?path=/docs/navigation-sidebar--docs" target="_parent">
+              OnyxSidebar
+            </OnyxLink>
+          </template>
+        </OnyxSidebar>
+      </template>
 
-    <!-- page content -->
-    <OnyxHeadline is="h1">Page content</OnyxHeadline>
-  </OnyxPageLayout>
+      <!-- page content -->
+      <OnyxHeadline is="h1">Page content</OnyxHeadline>
+    </OnyxPageLayout>
+  </OnyxAppLayout>
 </template>
-
-<style lang="scss" scoped>
-.page {
-  // only needed for this example. Can be removed if used together with the OnyxAppLayout
-  height: 100vh;
-}
-</style>
