b0d286aee3935e6fda3a60c6341cdf93ff8d01c0
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-07 07:58:05 +0200

    update examples

--- packages/sit-onyx/src/components/OnyxPageLayout/examples/SidebarAndFooterExample.vue
+++ packages/sit-onyx/src/components/OnyxPageLayout/examples/SidebarAndFooterExample.vue
@@ -1,36 +1,33 @@
 <script setup lang="ts">
-import { OnyxHeadline, OnyxLink, OnyxPageLayout, OnyxSidebar } from "../../..";
+import { OnyxAppLayout, OnyxHeadline, OnyxLink, OnyxPageLayout, OnyxSidebar } from "../../..";
 </script>
 
 <template>
-  <OnyxPageLayout class="page">
-    <template #sidebar>
-      <OnyxSidebar label="Example sidebar">
-        <template #header>
-          For sidebar examples, see:
-          <OnyxLink href="/?path=/docs/navigation-sidebar--docs" target="_parent">
-            OnyxSidebar
-          </OnyxLink>
-        </template>
-      </OnyxSidebar>
-    </template>
+  <OnyxAppLayout>
+    <OnyxPageLayout>
+      <template #sidebar>
+        <OnyxSidebar label="Example sidebar">
+          <template #header>
+            For sidebar examples, see:
+            <OnyxLink href="/?path=/docs/navigation-sidebar--docs" target="_parent">
+              OnyxSidebar
+            </OnyxLink>
+          </template>
+        </OnyxSidebar>
+      </template>
 
-    <!-- page content -->
-    <OnyxHeadline is="h1">Page content</OnyxHeadline>
+      <!-- page content -->
+      <OnyxHeadline is="h1">Page content</OnyxHeadline>
 
-    <template #footer>
-      <!-- we are using the "onyx-grid-container" class here to sync the footer with the global application max-width, alignment and horizontal padding -->
-      <footer class="footer onyx-grid-container">Footer content</footer>
-    </template>
-  </OnyxPageLayout>
+      <template #footer>
+        <!-- we are using the "onyx-grid-container" class here to sync the footer with the global application max-width, alignment and horizontal padding -->
+        <footer class="footer onyx-grid-container">Footer content</footer>
+      </template>
+    </OnyxPageLayout>
+  </OnyxAppLayout>
 </template>
 
 <style lang="scss" scoped>
-.page {
-  // only needed for this example. Can be removed if used together with the OnyxAppLayout
-  height: 100vh;
-}
-
 .footer {
   border-top: var(--onyx-1px-in-rem) solid var(--onyx-color-component-border-neutral);
   background-color: var(--onyx-color-base-background-blank);
