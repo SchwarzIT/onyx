45fcbdc492c3165d4662511664ed9d479e3c47e7
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-12 10:51:02 +0200

    fix(OnyxFlyoutMenu): make `alignment` property optional

--- packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxFlyoutMenu/types.ts
+++ packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxFlyoutMenu/types.ts
@@ -1,16 +1,16 @@
 import type { OnyxFlyoutProps } from "src/components/OnyxFlyout/types";
 import type { Nullable } from "../../../../composables/useVModel";
 
-export type OnyxFlyoutMenuProps = Pick<OnyxFlyoutProps, "alignment"> & {
+export type OnyxFlyoutMenuProps = Partial<Pick<OnyxFlyoutProps, "alignment">> & {
+  /**
+   * Aria label for the flyout.
+   */
+  label: string;
   /**
    * If the flyout is expanded on click or hover.
    * The default value is 'hover' which will expand the flyout on hover.
    */
   trigger?: "hover" | "click";
-  /**
-   * Aria label for the flyout.
-   */
-  label: string;
   /**
    * Indicates whether the element is expanded or collapsed.
    */
