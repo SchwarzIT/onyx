eee58ce82c6af19914b26dccbab1aca2b654b360
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-12 13:01:22 +0200

    update type

--- packages/sit-onyx/src/playwright/index.ts
+++ packages/sit-onyx/src/playwright/index.ts
@@ -1,5 +1,25 @@
 import type { Locator, Page } from "@playwright/test";
 
+export type DragResizeHandleOptions = {
+  /**
+   * Playwright page.
+   */
+  page: Page;
+  /**
+   * Absolute x-coordinate position to drag the handle to.
+   */
+  to: number;
+  /**
+   * If `true`, the mouse wil not be lifted up after dragging.
+   */
+  preventUp?: boolean;
+  /**
+   * Optional locator to use to find the resize handle button.
+   * If unset, the page will be used to locate the handle.
+   */
+  component?: Locator;
+};
+
 /**
  * Drags the resize handle / mouse to a given position.
  */
@@ -8,12 +28,7 @@ export const dragResizeHandle = async ({
   to,
   preventUp,
   component,
-}: {
-  page: Page;
-  to: number;
-  preventUp?: boolean;
-  component?: Locator;
-}) => {
+}: DragResizeHandleOptions) => {
   const button = (component ?? page).getByRole("button", { name: "Drag to change width" });
   await button.hover();
 
