490b4f0537d06d0d9297496688488420fa0c1a4c
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-13 08:11:59 +0200

    fix flaky button tests

---
+++ flaky-playwright.sh
@@ -0,0 +1,69 @@
+pnpm test:playwright src/components/OnyxAccordion -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxAccordionItem -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxAlertDialog -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxAppLayout -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxAvatar -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxAvatarStack -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxBadge -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxBottomBar -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxBreadcrumb -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxBreadcrumbItem -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxButton -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxCard -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxCheckbox -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxCheckboxGroup -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxDataGrid -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxDatePicker -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxDialog -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxDrawer -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxEmpty -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxErrorTooltip -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxExternalLinkIcon -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxFilterTag -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxFlyout -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxForm -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxFormElement -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxHeadline -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxIcon -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxIconButton -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxImage -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxInfoCard -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxInfoTooltip -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxInput -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxLink -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxListItem -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxLoadingIndicator -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxMiniSearch -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxMobileNavButton -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxModalDialog -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxMoreList -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxNavAppArea -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxNavBar -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxNotificationCard -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxNotificationDot -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxNotificationMessage -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxNotifications -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxPageLayout -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxPagination -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxRadioButton -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxRadioGroup -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxRipple -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxRouterLink -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSelect -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSelectDialog -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSelectInput -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSelectOption -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSidebar -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSkeleton -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxStepper -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSwitch -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxSystemButton -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxTab -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxTable -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxTabs -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxTag -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxTextarea -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxToast -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxToastMessage -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxTooltip -x --repeat-each 100 --project edge
+pnpm test:playwright src/components/OnyxVisuallyHidden -x --repeat-each 100 --project edge
--- packages/sit-onyx/src/components/OnyxRipple/OnyxRipple.ct.tsx
+++ packages/sit-onyx/src/components/OnyxRipple/OnyxRipple.ct.tsx
@@ -5,9 +5,9 @@ test("should trigger some ripples", async ({ mount }) => {
   // ARRANGE
   const component = await mount(<RippleTestWrapper />);
 
-  await component.click({ clickCount: 1, position: { x: 0, y: 50 } });
-  await component.click({ clickCount: 1, position: { x: 100, y: 100 } });
-  await component.click({ clickCount: 1, position: { x: 250, y: 0 } });
+  await component.click({ position: { x: 0, y: 50 } });
+  await component.click({ position: { x: 100, y: 100 } });
+  await component.click({ position: { x: 250, y: 0 } });
 
   // ASSERT
   await expect(component.locator(".onyx-ripple__element")).toHaveCount(3);
