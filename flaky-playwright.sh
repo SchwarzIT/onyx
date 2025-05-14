ca5789c5887b0a29be09c882edd2d2a622080e93
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-14 06:48:06 +0200

    update list

--- flaky-playwright.sh
+++ flaky-playwright.sh
@@ -29,31 +29,31 @@ pnpm test:playwright src/components/OnyxAppLayout -x --repeat-each 100 --project
 ✅ pnpm test:playwright src/components/OnyxImage -x --repeat-each 100 --project edge
 ✅ pnpm test:playwright src/components/OnyxInfoCard -x --repeat-each 100 --project edge
 ✅ pnpm test:playwright src/components/OnyxInfoTooltip -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxInput -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxLink -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxListItem -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxLoadingIndicator -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxMiniSearch -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxMobileNavButton -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxModalDialog -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxMoreList -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxNavAppArea -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxNavBar -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxNotificationCard -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxNotificationDot -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxNotificationMessage -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxNotifications -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxPageLayout -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxPagination -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxRadioButton -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxRadioGroup -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxRipple -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxRouterLink -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxSelect/ -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxSelectDialog -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxSelectInput -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxSelectOption -x --repeat-each 100 --project edge
-pnpm test:playwright src/components/OnyxSidebar -x --repeat-each 100 --project edge
+❌ pnpm test:playwright src/components/OnyxInput -x --repeat-each 100 --project edge
+❌ pnpm test:playwright src/components/OnyxLink -x --repeat-each 100 --project edge
+# pnpm test:playwright src/components/OnyxListItem -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxLoadingIndicator -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxMiniSearch -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxMobileNavButton -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxModalDialog -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxMoreList -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxNavAppArea -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxNavBar -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxNotificationCard -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxNotificationDot -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxNotificationMessage -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxNotifications -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxPageLayout -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxPagination -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxRadioButton -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxRadioGroup -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxRipple -x --repeat-each 100 --project edge
+❌ pnpm test:playwright src/components/OnyxRouterLink -x --repeat-each 100 --project edge
+❌ pnpm test:playwright src/components/OnyxSelect/ -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxSelectDialog -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxSelectInput -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxSelectOption -x --repeat-each 100 --project edge
+✅ pnpm test:playwright src/components/OnyxSidebar -x --repeat-each 100 --project edge
 pnpm test:playwright src/components/OnyxSkeleton -x --repeat-each 100 --project edge
 pnpm test:playwright src/components/OnyxStepper -x --repeat-each 100 --project edge
 pnpm test:playwright src/components/OnyxSwitch -x --repeat-each 100 --project edge
