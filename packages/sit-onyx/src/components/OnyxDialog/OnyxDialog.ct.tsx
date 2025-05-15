1bb49b5f33a2d45b80b5439d23a90143b37a176d
Christian Bußhoff <christian.busshoff@mail.schwarz>
2025-05-15 16:46:00 +0200

    Fixed OnyxTooltip showing outside the dialog. (#3315)
    removed translate from OnyxDialog to not interfere with the anchor of
    the tooltip.
    #3311
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: ChristianBusshoff <178766498+ChristianBusshoff@users.noreply.github.com>

--- packages/sit-onyx/src/components/OnyxDialog/OnyxDialog.ct.tsx
+++ packages/sit-onyx/src/components/OnyxDialog/OnyxDialog.ct.tsx
@@ -1,7 +1,7 @@
 import { ONYX_BREAKPOINTS } from "@sit-onyx/shared/breakpoints";
 import { expect, test } from "../../playwright/a11y";
+import TestWrapper from "../OnyxTooltip/TestWrapper.ct.vue";
 import OnyxDialog from "./OnyxDialog.vue";
-
 test.beforeEach(async ({ page }) => {
   await page.setViewportSize({ width: 256, height: 128 });
 });
@@ -86,3 +86,17 @@ for (const alignment of ["left", "right"] as const) {
     expect(accessibilityScanResults.violations).toEqual([]);
   });
 }
+
+test("tooltip inside dialog", async ({ mount, makeAxeBuilder, page }) => {
+  await mount(
+    <OnyxDialog label="Label" open modal>
+      <TestWrapper text="Test tooltip" open="hover" />
+    </OnyxDialog>,
+  );
+
+  // accessibility tests
+  const accessibilityScanResults = await makeAxeBuilder().analyze();
+  expect(accessibilityScanResults.violations, "should pass accessibility checks").toEqual([]);
+
+  await expect(page).toHaveScreenshot("with-tooltip.png");
+});
