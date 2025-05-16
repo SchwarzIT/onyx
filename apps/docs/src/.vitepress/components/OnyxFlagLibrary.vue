532b020a0bb9a8d77770b251bbd8291f80611c03
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-16 12:44:06 +0200

    unify asset libraries

--- apps/docs/src/.vitepress/components/OnyxFlagLibrary.vue
+++ apps/docs/src/.vitepress/components/OnyxFlagLibrary.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { FLAG_METADATA, groupFlagsByContinent } from "@sit-onyx/flags";
-import { OnyxHeadline, OnyxInput } from "sit-onyx";
-import { computed, ref } from "vue";
+import type { Asset, AssetLibraryGroup } from "./AssetLibrary.vue";
+import AssetLibrary from "./AssetLibrary.vue";
 import AssetLibraryItem from "./AssetLibraryItem.vue";
 
 const ALL_FLAGS = import.meta.glob("../../../node_modules/@sit-onyx/flags/src/assets/*.svg", {
@@ -10,29 +10,17 @@ const ALL_FLAGS = import.meta.glob("../../../node_modules/@sit-onyx/flags/src/as
   eager: true,
 }) as Record<string, string>;
 
-const continents = Object.entries(groupFlagsByContinent(FLAG_METADATA)).map(
-  ([continent, flags]) => ({ name: continent, flags }),
+const assetGroups = Object.entries(groupFlagsByContinent(FLAG_METADATA)).map<AssetLibraryGroup>(
+  ([continent, flags]) => {
+    return {
+      name: continent,
+      assets: flags.map<Asset>((flag) => ({
+        id: flag.code,
+        name: flag.metadata.internationalName,
+      })),
+    };
+  },
 );
-
-const search = ref("");
-
-const filteredContinents = computed(() => {
-  const lowerCaseSearch = search.value.toLowerCase();
-  return continents
-    .map((continent) => {
-      if (continent.name.toLowerCase().includes(lowerCaseSearch)) return continent;
-
-      return {
-        ...continent,
-        flags: continent.flags.filter(
-          (flag) =>
-            flag.code.toLowerCase().includes(lowerCaseSearch) ||
-            flag.metadata.internationalName.toLowerCase().includes(lowerCaseSearch),
-        ),
-      };
-    })
-    .filter((continent) => continent.flags.length);
-});
 
 const getSvgContent = (code: string) => {
   return ALL_FLAGS[`../../../node_modules/@sit-onyx/flags/src/assets/${code}.svg`];
@@ -40,52 +28,14 @@ const getSvgContent = (code: string) => {
 </script>
 
 <template>
-  <div>
-    <OnyxInput
-      v-model="search"
-      label="Search country code or name"
-      placeholder="Country code or name..."
-      class="search"
-      type="search"
-      hide-label
-    />
-
-    <section v-for="continent in filteredContinents" :key="continent.name" class="continent">
-      <OnyxHeadline is="h3" class="continent__headline" :hash="continent.name">
-        {{ continent.name }}
-      </OnyxHeadline>
-
-      <div class="continent__icons">
-        <AssetLibraryItem
-          v-for="flag in continent.flags"
-          :key="flag.code"
-          :tooltip-text="flag.metadata.internationalName"
-          :content="getSvgContent(flag.code)"
-          :clipboard-value="`import ${flag.code} from &quot;@sit-onyx/flags/${flag.code}.svg?raw&quot;`"
-          :success-message="`Import for flag &quot;${flag.code}&quot; (${flag.metadata.internationalName}) has been copied to your clipboard.`"
-        />
-      </div>
-    </section>
-  </div>
+  <AssetLibrary :groups="assetGroups" search-placeholder="Country code or name...">
+    <template #item="{ asset: flag }">
+      <AssetLibraryItem
+        :tooltip-text="flag.name"
+        :content="getSvgContent(flag.id)"
+        :clipboard-value="`import ${flag.id} from &quot;@sit-onyx/flags/${flag.id}.svg?raw&quot;`"
+        :success-message="`Import for flag &quot;${flag.id}&quot; (${flag.name}) has been copied to your clipboard.`"
+      />
+    </template>
+  </AssetLibrary>
 </template>
-
-<style lang="scss" scoped>
-.search {
-  width: 18rem;
-}
-
-.continent {
-  margin-bottom: var(--onyx-spacing-xl);
-
-  &__headline {
-    margin-top: var(--onyx-spacing-xl);
-    margin-bottom: var(--onyx-spacing-md);
-  }
-
-  &__icons {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, 3.5rem);
-    grid-template-rows: repeat(auto-fit, 3.5rem);
-  }
-}
-</style>
--- apps/docs/src/.vitepress/components/OnyxIconLibrary.vue
+++ apps/docs/src/.vitepress/components/OnyxIconLibrary.vue
@@ -5,8 +5,8 @@ import {
   groupIconsByCategory,
   ICON_METADATA,
 } from "@sit-onyx/icons";
-import { OnyxHeadline, OnyxInput } from "sit-onyx";
-import { computed, ref } from "vue";
+import type { Asset, AssetLibraryGroup } from "./AssetLibrary.vue";
+import AssetLibrary from "./AssetLibrary.vue";
 import AssetLibraryItem from "./AssetLibraryItem.vue";
 
 const ALL_ICONS = import.meta.glob("../../../node_modules/@sit-onyx/icons/src/assets/*.svg", {
@@ -15,94 +15,36 @@ const ALL_ICONS = import.meta.glob("../../../node_modules/@sit-onyx/icons/src/as
   eager: true,
 }) as Record<string, string>;
 
-const categories = Object.entries(groupIconsByCategory(ICON_METADATA)).map(([category, icons]) => ({
-  name: category,
-  icons,
-}));
-
-const search = ref("");
-
-const filteredCategories = computed(() => {
-  const lowerCaseSearch = search.value.toLowerCase();
-  return categories
-    .map((category) => {
-      if (category.name.toLowerCase().includes(lowerCaseSearch)) return category;
-
-      return {
-        ...category,
-        icons: category.icons.filter(
-          (icon) =>
-            icon.iconName.toLowerCase().includes(lowerCaseSearch) ||
-            icon.metadata.aliases?.some(
-              (alias) =>
-                alias.includes(lowerCaseSearch) ||
-                alias.replace(/-/g, " ").includes(lowerCaseSearch),
-            ),
-        ),
-      };
-    })
-    .filter((category) => category.icons.length);
-});
+const assetGroups = Object.entries(groupIconsByCategory(ICON_METADATA)).map<AssetLibraryGroup>(
+  ([category, icons]) => {
+    return {
+      name: category,
+      assets: icons.map<Asset>((icon) => ({
+        id: icon.iconName,
+        aliases: icon.metadata.aliases,
+        name: icon.iconName
+          .split("-")
+          .map((word) => capitalize(word))
+          .join(" "),
+      })),
+    };
+  },
+);
 
 const getSvgContent = (iconName: string) => {
   return ALL_ICONS[`../../../node_modules/@sit-onyx/icons/src/assets/${iconName}.svg`];
 };
-
-const getFormattedIconName = (iconName: string) => {
-  return iconName
-    .split("-")
-    .map((word) => capitalize(word))
-    .join(" ");
-};
 </script>
 
 <template>
-  <div>
-    <OnyxInput
-      v-model="search"
-      label="Search icon or category name"
-      placeholder="Icon or category name..."
-      class="search"
-      type="search"
-      hide-label
-    />
-
-    <section v-for="category in filteredCategories" :key="category.name" class="category">
-      <OnyxHeadline is="h3" class="category__headline" :hash="category.name">
-        {{ category.name }}
-      </OnyxHeadline>
-
-      <div class="category__icons">
-        <AssetLibraryItem
-          v-for="icon in category.icons"
-          :key="icon.iconName"
-          :tooltip-text="getFormattedIconName(icon.iconName)"
-          :content="getSvgContent(icon.iconName)"
-          :clipboard-value="`import ${getIconImportName(icon.iconName)} from &quot;@sit-onyx/icons/${icon.iconName}.svg?raw&quot;`"
-          :success-message="`Import for icon &quot;${getIconImportName(icon.iconName)}&quot; has been copied to your clipboard.`"
-        />
-      </div>
-    </section>
-  </div>
+  <AssetLibrary :groups="assetGroups" search-placeholder="Icon or category name...">
+    <template #item="{ asset: icon }">
+      <AssetLibraryItem
+        :tooltip-text="icon.name"
+        :content="getSvgContent(icon.id)"
+        :clipboard-value="`import ${getIconImportName(icon.id)} from &quot;@sit-onyx/icons/${icon.id}.svg?raw&quot;`"
+        :success-message="`Import for icon &quot;${getIconImportName(icon.id)}&quot; has been copied to your clipboard.`"
+      />
+    </template>
+  </AssetLibrary>
 </template>
-
-<style lang="scss" scoped>
-.search {
-  width: 18rem;
-}
-
-.category {
-  margin-bottom: var(--onyx-spacing-xl);
-
-  &__headline {
-    margin-top: var(--onyx-spacing-xl);
-    margin-bottom: var(--onyx-spacing-md);
-  }
-
-  &__icons {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, 3.5rem);
-    grid-template-rows: repeat(auto-fit, 3.5rem);
-  }
-}
-</style>
