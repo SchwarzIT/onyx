96a1ce2df1a7eb244dc70f7e1ed21481ad8fb418
Lars Rickert <lars.rickert@mail.schwarz>
2025-05-21 10:12:29 +0200

    support drag and drop

--- packages/sit-onyx/src/components/illustrations/OnyxFileUploadSVG/OnyxFileUploadSVG.stories.ts
+++ packages/sit-onyx/src/components/illustrations/OnyxFileUploadSVG/OnyxFileUploadSVG.stories.ts
@@ -12,6 +12,12 @@ type Story = StoryObj<typeof OnyxFileUploadSVG>;
 
 export const Default = { args: {} } satisfies Story;
 
+export const Active = {
+  args: {
+    active: true,
+  },
+} satisfies Story;
+
 export const Disabled = {
   args: {
     disabled: true,
--- packages/sit-onyx/src/components/illustrations/OnyxFileUploadSVG/types.ts
+++ packages/sit-onyx/src/components/illustrations/OnyxFileUploadSVG/types.ts
@@ -3,4 +3,8 @@ export type OnyxFileUploadSVGProps = {
    * Whether the illustration should be displayed in disabled styles.
    */
   disabled?: boolean;
+  /**
+   * Whether the illustration should be displayed in active styles.
+   */
+  active?: boolean;
 };
