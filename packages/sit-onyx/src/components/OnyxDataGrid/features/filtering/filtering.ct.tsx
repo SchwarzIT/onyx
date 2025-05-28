dd9ba795ac359ca06b5be37f424e82d4a182ec13
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-05-28 08:50:22 +0200

    fix

--- packages/sit-onyx/src/components/OnyxDataGrid/features/filtering/filtering.ct.tsx
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/filtering/filtering.ct.tsx
@@ -15,7 +15,7 @@ const getTestData = () => [
 ];
 
 const expectRowCount = (dataGrid: Locator, count: number) => {
-  return expect(dataGrid.getByRole("row")).toHaveCount(count);
+  return expect(dataGrid.getByRole("row")).toHaveCount(count + 1);
 };
 test("should filter by single column", async ({ mount }) => {
   // ARRANGE
