a84ede4795e33089d0c041440e16b8c8c3799853
Jonathan <jonathan_leo.carle@mail.schwarz>
2025-05-30 09:08:50 +0200

    fix(OnyxDataGrid): fix `useHideColumns` not using column label in reveal dropdown (#3422)
    Closes: #3425
    
    - implemented generalized `prepareMapping` and `applyMapping` feature
    utils, which
      - allow the definitions of one or multiple mappings
      - can run in a defined order
    
    - we now use this to now save the column config before removing the
    hidden coluns, so that we can access the labels
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: JoCa96 <18483883+JoCa96@users.noreply.github.com>

--- packages/sit-onyx/src/components/OnyxDataGrid/features/hideColumns/hideColumns.ct.tsx
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/hideColumns/hideColumns.ct.tsx
@@ -3,9 +3,9 @@ import { expect, test } from "../../../../playwright/a11y";
 import TestCase from "./TestCase.vue";
 
 const getTestData = () => [
-  { id: 1, a: "1", b: "a", c: "aa" },
-  { id: 2, a: "2", b: "B", c: "bb" },
-  { id: 3, a: "3", b: "C", c: "cc" },
+  { id: 1, a: "1", b: "a", c: "aa", d: "dd" },
+  { id: 2, a: "2", b: "B", c: "bb", d: "dd" },
+  { id: 3, a: "3", b: "C", c: "cc", d: "dd" },
 ];
 
 const expectColumnCount = async (dataGrid: Locator, count: number) => {
@@ -18,7 +18,7 @@ test("should hide and show columns", async ({ mount }) => {
   const component = await mount(
     <TestCase
       data={data}
-      columns={["a", "b", "c"]}
+      columns={["a", "b", "c", { key: "d", label: "Labelled Column" }]}
       hideColumnsOptions={{
         columns: {
           b: { hidden: true },
@@ -29,25 +29,31 @@ test("should hide and show columns", async ({ mount }) => {
   );
 
   const revealButton = component.getByRole("button", { name: "Show hidden columns" });
-  const revealA = component.getByRole("menuitem", { name: "a" });
-  const revealB = component.getByRole("menuitem", { name: "b" });
-  const moreActionA = component
-    .getByRole("columnheader", { name: "a Toggle column actions" })
-    .getByLabel("Toggle column actions");
+  const reveal = async (name: string) =>
+    component.getByRole("menuitem", { name, exact: true }).click();
+
+  const openMoreActions = async (name: string) =>
+    component
+      .getByRole("columnheader", { name: `${name} Toggle column actions`, exact: true })
+      .getByLabel("Toggle column actions")
+      .click();
 
   const menuItem = component.getByRole("menuitem", { name: "Hide column" });
 
   await test.step("Initial state: verify visible columns and reveal button", async () => {
     // ACT
     await expect(revealButton).toBeVisible();
-    await expectColumnCount(component, 3); // 2 Visible + Reveal column
+    await expectColumnCount(component, 4); // 3 Visible + Reveal column
     await expect(component).toHaveScreenshot("data-grid-hide-columns-initial.png");
   });
 
   await test.step("Hide multiple columns", async () => {
     // ARRANGE
-    await moreActionA.click();
+    await openMoreActions("a");
     await expect(component).toHaveScreenshot("data-grid-hide-columns-hide-button.png");
+    await menuItem.click();
+
+    await openMoreActions("Labelled Column");
     await menuItem.click();
 
     // ACT
@@ -59,12 +65,14 @@ test("should hide and show columns", async ({ mount }) => {
     // ARRANGE
     await revealButton.click();
     await expect(component).toHaveScreenshot("data-grid-hide-columns-reveal-button.png");
-    await revealA.click();
+    await reveal("a");
     await revealButton.click();
-    await revealB.click();
+    await reveal("b");
+    await revealButton.click();
+    await reveal("Labelled Column");
 
     // ACT
-    await expectColumnCount(component, 3); // 3 Visible
+    await expectColumnCount(component, 4); // 4 Visible
     await expect(revealButton).toBeHidden();
     await expect(component).toHaveScreenshot(
       "data-grid-hide-columns-revealed-columns-should-be-last.png",
