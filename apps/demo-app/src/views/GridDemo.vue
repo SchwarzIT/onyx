b22e2d7b4d7338602d67f29aa50d7be63eaa1599
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-06-27 11:26:29 +0200

    add sidebar grid

--- apps/demo-app/src/views/GridDemo.vue
+++ apps/demo-app/src/views/GridDemo.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import placeholder from "@sit-onyx/icons/placeholder.svg?raw";
 import {
   OnyxBottomBar,
   OnyxButton,
@@ -6,6 +7,8 @@ import {
   OnyxImage,
   OnyxInput,
   OnyxPageLayout,
+  OnyxSidebar,
+  OnyxSidebarItem,
   OnyxSwitch,
 } from "sit-onyx";
 import { useGridStore } from "../stores/grid-store";
@@ -23,6 +26,41 @@ const gridStore = useGridStore();
       :height="256"
       loading="eager"
     />
+    <template #sidebar>
+      <OnyxSidebar label="Example sidebar" resizable>
+        <template #header> Header content </template>
+
+        <div class="sidebar__content">
+          <div class="onyx-sidebar-grid-container">
+            <div class="onyx-sidebar-grid">
+              <OnyxSidebarItem v-for="i in 6" :key="i" :link="`#link-${i}`">
+                <OnyxIcon :icon="placeholder" />
+                Item {{ i }}
+              </OnyxSidebarItem>
+            </div>
+            <div class="onyx-sidebar-grid second-sidebar-grid">
+              <p
+                class="onyx-sidebar-grid-md-span-2 onyx-sidebar-grid-lg-span-4 onyx-sidebar-grid-xl-span-6"
+              >
+                Half width
+              </p>
+              <p
+                class="onyx-sidebar-grid-md-span-2 onyx-sidebar-grid-lg-span-4 onyx-sidebar-grid-xl-span-6"
+              >
+                Half width
+              </p>
+
+              <p class="onyx-sidebar-grid-span-full">Full width</p>
+            </div>
+          </div>
+        </div>
+
+        <template #footer>
+          <OnyxButton color="neutral" label="Button" />
+          <OnyxButton label="Button" />
+        </template>
+      </OnyxSidebar>
+    </template>
 
     <div class="onyx-grid-container">
       <OnyxHeadline is="h1">Grid demo page</OnyxHeadline>
@@ -71,5 +109,8 @@ const gridStore = useGridStore();
   &__card {
     background-color: var(--onyx-color-base-neutral-200);
   }
+}
+.second-sidebar-grid {
+  margin-top: var(--onyx-sidebar-grid-gutter);
 }
 </style>
