d8600a0c7babb4b81964bfc177ce3ff73aa1c5ae
Lars Rickert <lars.rickert@mail.schwarz>
2025-06-30 10:33:13 +0200

    refactor useOutsideClick

--- packages/headless/src/composables/helpers/useOutsideClick.ts
+++ packages/headless/src/composables/helpers/useOutsideClick.ts
@@ -3,7 +3,7 @@ import { toValue, type MaybeRefOrGetter, type Ref } from "vue";
 import type { Nullable } from "../../utils/types";
 import { useGlobalEventListener } from "./useGlobalListener";
 
-export type UseOutsideClickOptions = {
+export type UseOutsideClickOptions<TCheckOnTab extends boolean | undefined = undefined> = {
   /**
    * HTML element of the component where clicks should be ignored
    */
@@ -11,11 +11,13 @@ export type UseOutsideClickOptions = {
   /**
    * Callback when an outside click occurred.
    */
-  onOutsideClick: (event: MouseEvent | KeyboardEvent) => void;
+  onOutsideClick: (
+    event: TCheckOnTab extends true ? MouseEvent | KeyboardEvent : MouseEvent,
+  ) => void;
   /**
    * Whether the outside focus should also be checked when pressing the Tab key.
    */
-  checkOnTab?: boolean;
+  checkOnTab?: TCheckOnTab;
   /**
    * If `true`, event listeners will be removed and no outside clicks will be captured.
    */
@@ -26,38 +28,40 @@ export type UseOutsideClickOptions = {
  * Composable for listening to click events that occur outside of a component.
  * Useful to e.g. close flyouts or tooltips.
  */
-export const useOutsideClick = ({
+export const useOutsideClick = <TCheckOnTab extends boolean | undefined>({
   inside,
   onOutsideClick,
   disabled,
   checkOnTab,
-}: UseOutsideClickOptions) => {
+}: UseOutsideClickOptions<TCheckOnTab>) => {
+  const isOutsideClick = (target: EventTarget | null) => {
+    if (!target) return true;
+    const raw = toValue(inside);
+    const elements = Array.isArray(raw) ? raw : [raw];
+    return !elements.some((element) => element?.contains(target as Node | null));
+  };
+
   /**
    * Document click handle that closes then tooltip when clicked outside.
    * Should only be called when trigger is "click".
    */
-  const listener = (event: MouseEvent | KeyboardEvent, target = event.target) => {
-    const raw = toValue(inside);
-    const elements = Array.isArray(raw) ? raw : [raw];
-    const isOutsideClick = !elements.some((element) => element?.contains(target as Node | null));
-    if (isOutsideClick) onOutsideClick(event);
+  const clickListener = (event: MouseEvent) => {
+    if (isOutsideClick(event.target)) onOutsideClick(event);
   };
 
-  useGlobalEventListener({ type: "click", listener, disabled });
+  useGlobalEventListener({ type: "click", listener: clickListener, disabled });
 
   if (checkOnTab) {
-    const keydownListener = (event: KeyboardEvent) => {
+    const keydownListener = async (event: KeyboardEvent) => {
       if (event.key !== "Tab") return;
 
-      setTimeout(() => {
-        const focussedElement = document.activeElement;
+      // using setTimeout here to guarantee that side effects that might change the document.activeElement have run before checking
+      // the activeElement
+      await new Promise((resolve) => setTimeout(resolve));
 
-        if (focussedElement) {
-          listener(event, document.activeElement);
-        } else {
-          onOutsideClick(event);
-        }
-      });
+      if (isOutsideClick(document.activeElement)) {
+        onOutsideClick(event as Parameters<typeof onOutsideClick>[0]);
+      }
     };
 
     useGlobalEventListener({ type: "keydown", listener: keydownListener, disabled });
