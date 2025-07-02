f2e4964f1446a828ec7783dfa6897b9dbeed96b7
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-07-02 13:45:08 +0200

    remove esm import test

--- apps/test/test/exports.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from "vitest";
-
-describe("esm export", () => {
-  test("should work with import", async () => {
-    const sitOnyx = await import("sit-onyx");
-    expect(sitOnyx).toBeDefined();
-    expect(sitOnyx.createOnyx).toBeTypeOf("function");
-  });
-});
