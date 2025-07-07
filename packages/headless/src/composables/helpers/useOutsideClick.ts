3aa414ef37d0cd5bc8b3b8100d8807b702eb069d
Lars Rickert <lars.rickert@mail.schwarz>
2025-07-07 14:48:16 +0200

    fix fab click

--- packages/headless/src/composables/helpers/useOutsideClick.ts
+++ packages/headless/src/composables/helpers/useOutsideClick.ts
@@ -36,6 +36,11 @@ export const useOutsideClick = <TCheckOnTab extends boolean | undefined>({
 }: UseOutsideClickOptions<TCheckOnTab>) => {
   const isOutsideClick = (target: EventTarget | null) => {
     if (!target) return true;
+    if (target instanceof Node && !target.isConnected) {
+      // if the node is not connected to the document (because it was e.g. removed from it)
+      // we skip the outside click here. THis is needed to support use cases where e.g. a button icon is toggled when clicking
+      return false;
+    }
     const raw = toValue(inside);
     const elements = Array.isArray(raw) ? raw : [raw];
     return !elements.some((element) => element?.contains(target as Node | null));
