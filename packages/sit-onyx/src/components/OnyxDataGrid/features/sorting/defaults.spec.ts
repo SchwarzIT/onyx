ca6cfbd13508f5755affffba1d089df8580301b9
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-07-15 14:30:04 +0200

    add special sort for time

--- packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/defaults.spec.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/defaults.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { DEFAULT_COMPARES } from "./defaults.js";
+import { DEFAULT_COMPARES, TIME_COMPARE } from "./defaults.js";
 
 const OLDER_DATE = new Date(2021, 11, 31);
 const NEWER_DATE = new Date(2022, 0, 1);
@@ -20,7 +20,7 @@ test.for([
   expect(result).toBe(expected);
 });
 
-test.for(["date", "datetime-local", "time", "timestamp"] as const)(
+test.for(["datetime-local", "timestamp", "date"] as const)(
   "should compare dates correctly for type $0",
   (type) => {
     const collator = new Intl.Collator("de-DE");
@@ -29,3 +29,14 @@ test.for(["date", "datetime-local", "time", "timestamp"] as const)(
     expect(DEFAULT_COMPARES[type](OLDER_DATE, new Date(OLDER_DATE), collator)).toBe(0);
   },
 );
+
+test.for([
+  { a: new Date(2020, 0, 1, 12, 34, 56, 78), b: new Date(2020, 0, 1, 12, 34, 56, 78), expected: 0 },
+  { a: new Date(2019, 0, 1, 12, 34, 56, 78), b: new Date(2020, 0, 1, 12, 34, 56, 78), expected: 0 },
+  { a: new Date(2019, 0, 1, 12, 34, 56, 78), b: new Date(2020, 0, 1, 12, 34, 56, 0), expected: 1 },
+  { a: new Date(2019, 0, 1, 12, 34, 56, 0), b: new Date(2020, 0, 1, 12, 34, 56, 78), expected: -1 },
+])("should compare times correctly for $a and $b", ({ a, b, expected }) => {
+  const collator = new Intl.Collator("de-DE");
+  expect(Math.sign(TIME_COMPARE(a, b, collator))).toBeCloseTo(expected);
+  expect(Math.sign(TIME_COMPARE(b, a, collator) * -1)).toBeCloseTo(expected);
+});
--- packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/defaults.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/defaults.ts
@@ -7,12 +7,33 @@ export const STRING_COMPARE = (a: unknown, b: unknown, collator: Intl.Collator) 
 
 export const NUMBER_COMPARE = (a: unknown, b: unknown) => Number(a) - Number(b);
 
+const toComparableTime = (date: Date) =>
+  new Date(
+    Date.UTC(
+      0,
+      0,
+      1,
+      date.getUTCHours(),
+      date.getUTCMinutes(),
+      date.getUTCSeconds(),
+      date.getUTCMilliseconds(),
+    ),
+  );
+export const TIME_COMPARE = (a: unknown, b: unknown, collator: Intl.Collator) => {
+  if (!(a instanceof Date && b instanceof Date)) {
+    return STRING_COMPARE(a, b, collator);
+  }
+  const dateA = toComparableTime(a);
+  const dateB = toComparableTime(b);
+  return NUMBER_COMPARE(dateA, dateB);
+};
+
 export const DEFAULT_COMPARES: Record<PropertyKey, Compare<unknown>> = Object.freeze({
   string: STRING_COMPARE,
   number: NUMBER_COMPARE,
   date: NUMBER_COMPARE,
   "datetime-local": NUMBER_COMPARE,
-  time: NUMBER_COMPARE,
+  time: TIME_COMPARE,
   timestamp: NUMBER_COMPARE,
   skeleton: () => 0,
 }) satisfies Record<DefaultSupportedTypes, Compare<DataGridEntry>>;
