42c5e76c658136758d4138ad4305bf7c4eefb5a8
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-07-21 17:51:10 +0200

    added ct test

--- packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/sorting.ct.tsx
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/sorting/sorting.ct.tsx
@@ -71,3 +71,70 @@ test("should render correctly", async ({ mount, makeAxeBuilder }) => {
   const accessibilityScanResults = await makeAxeBuilder().analyze();
   expect(accessibilityScanResults.violations, "should pass accessibility checks").toEqual([]);
 });
+
+test("should sort the default types correctly", async ({ mount }) => {
+  // ARRANGE
+  const data = [
+    {
+      id: 1,
+      string: "abcdefgh",
+      number: 12345,
+      date: new Date("2022-01-01"),
+      "datetime-local": new Date("2022-01-01"),
+      time: new Date("2022-01-01 12:34"),
+      timestamp: new Date(123456789),
+    },
+    {
+      id: 2,
+      string: "zxy",
+      number: 987654,
+      date: new Date("2024-01-01"),
+      "datetime-local": new Date("2024-01-01"),
+      time: new Date("2022-01-01 16:34"),
+      timestamp: new Date(987654321),
+    },
+  ];
+
+  const columnsConfig = [
+    { key: "id", type: "number" },
+    { key: "string", type: "string" },
+    { key: "number", type: "number" },
+    { key: "date", type: "date" },
+    { key: "datetime-local", type: "datetime-local" },
+    { key: "time", type: "time" },
+    { key: "timestamp", type: "timestamp" },
+  ];
+
+  const component = await mount(<TestCase data={data} columns={columnsConfig} />);
+
+  const getFirstColumn = () => component.locator("tbody tr td:first-of-type").all();
+
+  // ASSERT
+  const columns = component.locator("th");
+  await expect(columns).toHaveCount(7);
+
+  let rows = await getFirstColumn();
+  expect(rows).toHaveLength(data.length);
+
+  for (const columnConfig of columnsConfig) {
+    // ACT
+    await component
+      .getByLabel(`Sort the table ascending by the ${columnConfig.key} column.`)
+      .click();
+
+    // ASSERT
+    rows = await getFirstColumn();
+    expect(rows).toHaveLength(data.length);
+    await expectOrderedText(rows, ["1", "2"]);
+
+    // ACT
+    await component
+      .getByLabel(`Sort the table descending by the ${columnConfig.key} column.`)
+      .click();
+
+    // ASSERT
+    rows = await getFirstColumn();
+    expect(rows).toHaveLength(data.length);
+    await expectOrderedText(rows, ["2", "1"]);
+  }
+});
