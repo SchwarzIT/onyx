75def09ffb09066fd524739c8802166d55960e9d
Lars Rickert <lars.rickert@mail.schwarz>
2025-07-23 17:00:03 +0200

    add file type icon tests

--- packages/sit-onyx/src/components/OnyxFileTypeIcon/types.ts
+++ packages/sit-onyx/src/components/OnyxFileTypeIcon/types.ts
@@ -7,3 +7,29 @@ export type OnyxFileTypeIconProps = Omit<OnyxIconProps, "icon"> & {
    */
   type: MediaType;
 };
+
+/**
+ * A list of media files that have a dedicated icon for the OnyxFileTypeIcon component.
+ */
+export const SUPPORTED_FILE_TYPE_ICON_MEDIA_TYPES = [
+  "application/octet-stream",
+  "application/pdf",
+  "text/csv",
+  "audio/mp3",
+  "video/mp4",
+  "image/png",
+  "application/rtf",
+  "application/vnd.ms-powerpoint",
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+  "application/vnd.ms-excel",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  "application/msword",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  "text/html",
+  "application/zip",
+  "application/vnd.rar",
+  "application/x-7z-compressed",
+  "application/x-tar",
+  "application/gzip",
+  "application/x-bzip2",
+] satisfies MediaType[];
