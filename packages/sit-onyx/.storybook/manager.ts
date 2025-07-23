c98cff8fcb9f628424d9ec1c117de407a10f4a9e
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-07-23 10:18:04 +0200

    add comment

--- packages/sit-onyx/.storybook/manager.ts
+++ packages/sit-onyx/.storybook/manager.ts
@@ -3,7 +3,8 @@ import { addons } from "storybook/internal/manager-api";
 
 addons.setConfig({
   toolbar: {
-    // Hides background select from docs pages.
+    // Per onyx storybook configuration, there is only a single, default (dynamic) theme.
+    // Therefore the background select is redundant.
     "storybook/background": { hidden: true },
   },
   tagBadges: [
