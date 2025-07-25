24129c2aece9da054ac10376339653df0efaee49
Lars Rickert <lars.rickert@mail.schwarz>
2025-07-25 10:48:25 +0200

    update peerDependency

--- packages/sit-onyx/src/components/OnyxFileCard/OnyxFileCard.stories.ts
+++ packages/sit-onyx/src/components/OnyxFileCard/OnyxFileCard.stories.ts
@@ -1,6 +1,6 @@
 import moreVerticalSmall from "@sit-onyx/icons/more-vertical-small.svg?raw";
 import trash from "@sit-onyx/icons/trash.svg?raw";
-import type { Meta, StoryObj } from "@storybook/vue3";
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
 import { h } from "vue";
 import OnyxIconButton from "../OnyxIconButton/OnyxIconButton.vue";
 import OnyxFlyoutMenu from "../OnyxNavBar/modules/OnyxFlyoutMenu/OnyxFlyoutMenu.vue";
