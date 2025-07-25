7a7a2916536715131ccb0c2fd5453bb75c3a51aa
Lars Rickert <lars.rickert@mail.schwarz>
2025-07-25 08:12:57 +0200

    apply PR feedback

--- packages/sit-onyx/src/components/OnyxFileCard/types.ts
+++ packages/sit-onyx/src/components/OnyxFileCard/types.ts
@@ -1,25 +1,22 @@
 import type { DensityProp } from "../../composables/density.js";
 import type { BinaryPrefixedSize } from "../../utils/numbers.js";
-import type { MediaType } from "../OnyxFileUpload/types.js";
+import type { OnyxFileTypeIconProps } from "../OnyxFileTypeIcon/types.js";
 import type { SharedLinkProps } from "../OnyxRouterLink/types.js";
 
-export type OnyxFileCardProps = DensityProp & {
-  /**
-   * Name of the file.
-   */
-  filename: string;
-  /**
-   * Media type of the file, e.g. "application/pdf".
-   */
-  type: MediaType;
-  /**
-   * File size.
-   * Can be a number in bytes (e.g. 1024 \* 1024 \* 42) or a [binary prefixed size](https://en.wikipedia.org/wiki/Binary_prefix) (e.g. 42MiB).
-   * For the user, the size will be displayed in decimal instead of binary notation (e.g. 42MB instead of 42MiB) since users are mostly non-technical and such visualization is therefore simpler to understand.
-   */
-  size: number | BinaryPrefixedSize;
-  /**
-   * Link to the file. If set, the filename will be rendered as link.
-   */
-  link?: string | SharedLinkProps;
-};
+export type OnyxFileCardProps = DensityProp &
+  Pick<OnyxFileTypeIconProps, "type"> & {
+    /**
+     * Name of the file.
+     */
+    filename: string;
+    /**
+     * File size.
+     * Can be a number in bytes (e.g. 1024 \* 1024 \* 42) or a [binary prefixed size](https://en.wikipedia.org/wiki/Binary_prefix) (e.g. 42MiB).
+     * For the user, the size will be displayed in decimal instead of binary notation (e.g. 42MB instead of 42MiB) since users are mostly non-technical and such visualization is therefore simpler to understand.
+     */
+    size: number | BinaryPrefixedSize;
+    /**
+     * Link to the file. If set, the filename will be rendered as link.
+     */
+    link?: string | SharedLinkProps;
+  };
