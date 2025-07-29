f6d663ac99e6e19ad6a01a9b43d68b2497c002e0
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-07-29 13:03:59 +0200

    add tests

---
+++ packages/sit-onyx/src/components/OnyxGlobalFAB/useGlobalFAB.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "vitest";
+import { createGlobalFABProvider } from "./useGlobalFAB.js";
+
+describe("useToast", () => {
+  test("should create toast provider", () => {
+    const provider = createGlobalFABProvider();
+
+    expect(provider.items.value).toStrictEqual([]);
+
+    provider.add({ id: 1, label: "Test 1" });
+    provider.add({ id: 2, label: "Test 2" });
+
+    expect(provider.items.value).toStrictEqual([
+      {
+        id: 1,
+        label: "Test 1",
+      },
+      {
+        id: 2,
+        label: "Test 2",
+      },
+    ]);
+
+    provider.remove(2);
+
+    expect(provider.items.value).toStrictEqual([
+      {
+        id: 1,
+        label: "Test 1",
+      },
+    ]);
+  });
+});
--- packages/sit-onyx/src/components/OnyxPageLayout/OnyxPageLayout.ct.tsx
+++ packages/sit-onyx/src/components/OnyxPageLayout/OnyxPageLayout.ct.tsx
@@ -1,6 +1,5 @@
 import { ONYX_BREAKPOINTS } from "@sit-onyx/shared/breakpoints";
 import { expect, test } from "../../playwright/a11y.js";
-import OnyxSidebar from "../OnyxSidebar/OnyxSidebar.vue";
 import OnyxPageLayout from "./OnyxPageLayout.vue";
 
 const SIDEBAR_ELEMENT = (
@@ -108,94 +107,6 @@ test("should render with left and right sidebar", async ({ mount, makeAxeBuilder
 
   // ASSERT
   expect(accessibilityScanResults.violations).toEqual([]);
-});
-
-test("should render the sidebar fab button on small screens", async ({ mount, page }) => {
-  // ARRANGE
-  const component = await mount(
-    <OnyxPageLayout>
-      Page content
-      <template v-slot:sidebar>
-        <OnyxSidebar label="Test Sidebar" />
-      </template>
-    </OnyxPageLayout>,
-  );
-
-  const fabButton = component.getByRole("button", { name: "Open Sidebar" });
-  const sidebarDrawer = component.getByRole("dialog", { name: "Test Sidebar" });
-
-  //ACT
-  await page.setViewportSize({ width: ONYX_BREAKPOINTS.md, height: 800 });
-
-  // ASSERT
-  await expect(fabButton).toBeHidden();
-
-  //ACT
-  await page.setViewportSize({ width: ONYX_BREAKPOINTS.sm, height: 800 });
-
-  // ASSERT
-  await expect(fabButton).toBeVisible();
-  await expect(sidebarDrawer).toBeHidden();
-  await expect(component).toHaveScreenshot("sidebar-fab-button.png");
-
-  //ACT
-  await fabButton.click();
-
-  // ASSERT
-  await expect(sidebarDrawer).toBeVisible();
-
-  //ASSERT
-});
-
-test("should render the sidebar fab button on small screens (multiple options)", async ({
-  mount,
-  page,
-}) => {
-  // ARRANGE
-  const component = await mount(
-    <OnyxPageLayout>
-      Page content
-      <template v-slot:sidebar>
-        <OnyxSidebar label="Test Sidebar" />
-      </template>
-      <template v-slot:sidebarRight>
-        <OnyxSidebar label="Test2 Sidebar" />
-      </template>
-    </OnyxPageLayout>,
-  );
-
-  const fabButton = component.getByRole("button", { name: "Open Sidebar" });
-  const fabItem = component.getByRole("menuitem", { name: "Test Sidebar" });
-  const fabItem2 = component.getByRole("menuitem", { name: "Test2 Sidebar" });
-
-  const sidebarDrawer = component.getByRole("dialog", { name: "Test Sidebar" });
-
-  //ACT
-  await page.setViewportSize({ width: ONYX_BREAKPOINTS.md, height: 800 });
-
-  // ASSERT
-  await expect(fabButton).toBeHidden();
-
-  //ACT
-  await page.setViewportSize({ width: ONYX_BREAKPOINTS.sm, height: 800 });
-
-  // ASSERT
-  await expect(fabButton).toBeVisible();
-  await expect(fabItem).toBeHidden();
-  await expect(sidebarDrawer).toBeHidden();
-
-  //ACT
-  await fabButton.click();
-  //ASSERT
-  await expect(fabItem).toBeVisible();
-  await expect(fabItem2).toBeVisible();
-  await expect(component).toHaveScreenshot("sidebar-fab-button-multiple-options.png");
-
-  //ACT
-  await fabItem.click();
-
-  // ASSERT
-  await expect(sidebarDrawer).toBeVisible();
 });
 
 test("should render with footer", async ({ mount, makeAxeBuilder }) => {
