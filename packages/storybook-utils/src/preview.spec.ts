65b2bddb5e41545412d4ec19d645b1a4944c9874
Lars Rickert <lars.rickert@mail.schwarz>
2025-07-31 14:15:31 +0200

    feat(storybook-utils): replace flags with imports in code snippets

--- packages/storybook-utils/src/preview.spec.ts
+++ packages/storybook-utils/src/preview.spec.ts
@@ -1,3 +1,4 @@
+import { flagDE } from "@sit-onyx/flags";
 import { iconBellRing, iconCalendar, iconPlaceholder } from "@sit-onyx/icons";
 import { describe, expect, test } from "vitest";
 import { replaceAll, sourceCodeTransformer } from "./preview.js";
@@ -6,7 +7,7 @@ describe("preview.ts", () => {
   test("should transform source code and add icon/onyx imports", async () => {
     // ACT
     const sourceCode = await sourceCodeTransformer(`<template>
-<OnyxTest icon='${iconPlaceholder}' test='${iconBellRing}' :obj="{foo:'${replaceAll(iconCalendar, '"', "\\'")}'}" />
+<OnyxTest icon='${iconPlaceholder}' test='${iconBellRing}' :obj="{foo:'${replaceAll(iconCalendar, '"', "\\'")}'}" flag='${flagDE}' />
 <OnyxOtherComponent />
 <OnyxComp>Test</OnyxComp>
 </template>`);
@@ -15,6 +16,7 @@ describe("preview.ts", () => {
     expect(sourceCode).toBe(`<script lang="ts" setup>
 import { OnyxComp, OnyxOtherComponent, OnyxTest } from "sit-onyx";
 import { iconBellRing, iconCalendar, iconPlaceholder } from "@sit-onyx/icons";
+import { flagDE } from "@sit-onyx/flags";
 </script>
 
 <template>
@@ -22,6 +24,7 @@ import { iconBellRing, iconCalendar, iconPlaceholder } from "@sit-onyx/icons";
     :icon="iconPlaceholder"
     :test="iconBellRing"
     :obj="{foo:iconCalendar}"
+    :flag="flagDE"
   />
   <OnyxOtherComponent />
   <OnyxComp>Test</OnyxComp>
