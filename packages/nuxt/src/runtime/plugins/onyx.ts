931bf8ac14e9d943358237843847a72c33d52688
Lars Rickert <lars.rickert@mail.schwarz>
2025-07-31 08:06:40 +0200

    update plugin

--- packages/nuxt/src/runtime/plugins/onyx.ts
+++ packages/nuxt/src/runtime/plugins/onyx.ts
@@ -1,7 +1,7 @@
 import { computed, type ComputedRef } from "#imports";
 import type { LocaleObject } from "@nuxtjs/i18n";
 import { defineNuxtPlugin, useNuxtApp, useRouter } from "nuxt/app";
-import { createOnyx, type TranslationFunction } from "sit-onyx";
+import { createOnyx, type ProvideI18nOptions, type TranslationFunction } from "sit-onyx";
 import type { Composer } from "vue-i18n";
 
 type I18n = Composer & { localeProperties: ComputedRef<LocaleObject> };
@@ -13,19 +13,24 @@ export default defineNuxtPlugin({
     const i18n = useNuxtApp().$i18n as I18n | undefined;
     const router = useRouter();
 
+    const getI18nOptions = (): ProvideI18nOptions | undefined => {
+      if (!i18n) return;
+
+      const t = computed((): TranslationFunction => {
+        return (key, placeholders) => {
+          return i18n.t(`onyx.${key}`, placeholders?.n ?? 1, { named: placeholders });
+        };
+      });
+
+      const locale = computed(() => i18n.localeProperties.value.language ?? "en-US");
+
+      return { t, locale };
+    };
+
     app.vueApp.use(
       createOnyx({
         router,
-        i18n: i18n
-          ? {
-              locale: computed(() => i18n.localeProperties.value.language ?? "en-US"),
-              t: computed((): TranslationFunction => {
-                return (key, placeholders) => {
-                  return i18n.t(`onyx.${key}`, placeholders?.n ?? 1, { named: placeholders });
-                };
-              }),
-            }
-          : undefined,
+        i18n: getI18nOptions(),
       }),
     );
   },
