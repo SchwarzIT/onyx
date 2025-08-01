ddbe696a114f3e47add6fee574e7cde1a191bcd5
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-08-01 18:26:40 +0200

    move to playwright-utils package

--- packages/sit-onyx/src/types/utils.ts
+++ packages/sit-onyx/src/types/utils.ts
@@ -94,25 +94,3 @@ export type PrimitiveType = null | number | string | boolean | symbol;
  * A type that can also be null or undefined.
  */
 export type Nullable<T = never> = T | undefined | null;
-
-/**
- * Picks the emits from an components property type.
- * e.g.:
- * ```ts
- * import MyInput from "./MyInput.vue";
- * import type { ComponentExposed } from "vue-component-type-helpers";
- *
- * type MyInputPublicProps = ComponentExposed<typeof MyInput>["$props"];
- * type MyInputVModelEmits = VModelEmits<MyInputPublicProps>; // => { 'onUpdate:modelValue': (newValue: string) => void }
- *
- * ```
- */
-export type PickEmitsFromProps<T> = {
-  // component emits start with `on`
-  -readonly [key in keyof T as key extends `on${string}`
-    ? // exclude onVnode emits
-      key extends `onVnode${string}`
-      ? never
-      : key
-    : never]: NonNullable<T[key]>;
-};
