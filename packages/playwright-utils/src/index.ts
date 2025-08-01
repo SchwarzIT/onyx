8ca844b77a4ecd48647a363842dcc7f23e541467
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-08-01 18:33:01 +0200

    renamed to event to emit

--- packages/playwright-utils/src/index.ts
+++ packages/playwright-utils/src/index.ts
@@ -1,4 +1,4 @@
-export * from "./events/index.js";
-export * from "./events/types.js";
+export * from "./emits/index.js";
+export * from "./emits/types.js";
 export * from "./screenshots/index.js";
 export * from "./screenshots/types.js";
