2feb3f723465db9c3f7ba5ac2ec7b0fc045a2c07
Lars Rickert <lars.rickert@mail.schwarz>
2025-08-06 13:40:32 +0200

    feat(nuxt-docs): add language switch to default nav bar

--- packages/nuxt-docs/app/components/NavBar.vue
+++ packages/nuxt-docs/app/components/NavBar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { iconCircleContrast } from "@sit-onyx/icons";
-import { extractLinkProps, type ColorSchemeValue } from "sit-onyx";
+import { iconCircleContrast, iconTranslate } from "@sit-onyx/icons";
+import { extractLinkProps, type ColorSchemeValue, type SelectDialogOption } from "sit-onyx";
 
 const { onyxDocs } = useAppConfig();
 const router = useRouter();
@@ -16,10 +16,32 @@ const colorScheme = computed({
     colorMode.preference = newValue === "auto" ? "system" : newValue;
   },
 });
+
+const { locale, setLocale, locales } = useI18n();
+const localePath = useLocalePath();
+const isLanguageDialogOpen = ref(false);
+
+const languageOptions = computed(() => {
+  return locales.value.map((locale) => {
+    return {
+      label: locale.name ?? locale.code,
+      value: locale.code,
+    } satisfies SelectDialogOption;
+  });
+});
+
+const currentLocaleLabel = computed(() => {
+  // using "!" here is safe since splitting a string will always return at least one string in the returned array
+  return locale.value.split("-")[0]!.split("_")[0]!.toUpperCase();
+});
 </script>
 
 <template>
-  <OnyxNavBar v-bind="onyxDocs.nav" @navigate-back="router.back">
+  <OnyxNavBar
+    :app-area="{ link: localePath('/') }"
+    v-bind="onyxDocs.nav"
+    @navigate-back="router.back"
+  >
     <NavItem
       v-for="item in onyxDocs.nav?.items"
       :key="extractLinkProps(item.link ?? '').href"
@@ -27,6 +49,27 @@ const colorScheme = computed({
     />
 
     <template #contextArea>
+      <template v-if="locales.length > 1">
+        <OnyxButton
+          :label="currentLocaleLabel"
+          :icon="iconTranslate"
+          color="neutral"
+          mode="plain"
+          @click="isLanguageDialogOpen = true"
+        />
+
+        <OnyxSelectDialog
+          :open="isLanguageDialogOpen"
+          :label="$t('onyx.languageSelect.headline')"
+          :model-value="locale"
+          :options="languageOptions"
+          @close="isLanguageDialogOpen = false"
+          @update:model-value="setLocale($event)"
+        >
+          <template #description> {{ $t("onyx.languageSelect.subtitle") }} </template>
+        </OnyxSelectDialog>
+      </template>
+
       <OnyxIconButton
         label="Toggle color scheme"
         :icon="iconCircleContrast"
