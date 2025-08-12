2748962a14b39e942d42b3d7fa90ff7e7ad35b8f
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-08-12 22:48:05 +0200

    add examples

---
+++ packages/sit-onyx/src/components/OnyxFileUpload/examples/FileUploadModalExample.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+import { iconTrash } from "@sit-onyx/icons";
+import { ref } from "vue";
+import { injectI18n } from "../../../i18n.js";
+import OnyxFileCard from "../../OnyxFileCard/OnyxFileCard.vue";
+import OnyxIconButton from "../../OnyxIconButton/OnyxIconButton.vue";
+import OnyxModal from "../../OnyxModal/OnyxModal.vue";
+import OnyxSystemButton from "../../OnyxSystemButton/OnyxSystemButton.vue";
+import OnyxFileUpload from "../OnyxFileUpload.vue";
+
+const { t } = injectI18n();
+
+const allFiles = ref<File[]>([]);
+const isOpen = ref(false);
+
+const removeFile = (fileToRemove: File) => {
+  allFiles.value = allFiles.value.filter((file) => file !== fileToRemove);
+};
+</script>
+
+<template>
+  <OnyxFileUpload v-model="allFiles" :multiple="true"> </OnyxFileUpload>
+  <OnyxModal v-model:open="isOpen" label="Files">
+    <template #default>
+      <div v-if="allFiles.length" class="file-list">
+        <OnyxFileCard
+          v-for="file in allFiles"
+          :key="file.name"
+          :filename="file.name"
+          :size="file.size"
+        >
+          <template #actions>
+            <OnyxIconButton
+              color="danger"
+              :icon="iconTrash"
+              :label="t('fileUpload.removeFile')"
+              @click="
+                () => {
+                  removeFile(file);
+                }
+              "
+            />
+          </template>
+        </OnyxFileCard>
+      </div>
+      <div v-else class="file-list--empty">
+        <p>No File Selected</p>
+      </div>
+    </template>
+  </OnyxModal>
+  <OnyxSystemButton class="open-modal-button" label="Show Files" @click="isOpen = true" />
+</template>
+
+<style lang="scss">
+.onyx-basic-dialog__content {
+  min-width: 20rem;
+  min-height: 10rem;
+}
+.open-modal-button {
+  margin: var(--onyx-density-md) 0;
+}
+.file-list {
+  padding: var(--onyx-density-sm) var(--onyx-modal-padding-inline);
+  display: flex;
+  flex-direction: column;
+  gap: var(--onyx-density-sm);
+  &--empty {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
