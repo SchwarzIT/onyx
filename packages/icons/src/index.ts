bc024b7b6dc4ba7050ced03cf9a54306f8eb26f4
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-08-18 13:48:50 +0200

    use type export for virtual plugin

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,2 +1,2 @@
 // this virtual import is handled by the @sit-onyx/vite-plugin-svg and will export all SVG icons as JavaScript exports
-export * from "virtual:vite-plugin-svg";
+export type * from "virtual:vite-plugin-svg";
