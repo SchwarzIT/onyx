4834b5e32646feebb8cc134cc005e91650fe6877
Christian Bußhoff <christian.busshoff@mail.schwarz>
2025-08-26 23:04:53 +0200

    feat(OnyxSegmentedControl): implement onyx segmented control (#3989)
    Relates to #2765
    
    - added Segmented Control
    - added Segmented Control Element
    - added tests
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: ChristianBusshoff <178766498+ChristianBusshoff@users.noreply.github.com>

---
+++ packages/sit-onyx/src/components/OnyxSegmentedControl/OnyxSegmentedControl.ct.tsx
@@ -0,0 +1,87 @@
+import { DENSITIES } from "../../composables/density.js";
+import { expect, test } from "../../playwright/a11y.js";
+import { executeMatrixScreenshotTest, mockPlaywrightIcon } from "../../playwright/screenshots.jsx";
+import OnyxSegmentedControl from "./OnyxSegmentedControl.vue";
+
+test.describe("Screenshot tests", () => {
+  executeMatrixScreenshotTest({
+    name: "Segmented Control",
+    columns: DENSITIES,
+    rows: ["default", "icon", "labelAndIcon", "hover", "focus", "selected", "disabled"],
+    component: (column, row) => {
+      const modelValue = row === "selected" ? "option-1" : null;
+      return (
+        <div style={{ width: "30rem" }}>
+          <OnyxSegmentedControl
+            modelValue={modelValue}
+            options={[
+              {
+                value: "option-1",
+                label: "Option 1",
+                hideLabel: row === "icon",
+                icon: row === "icon" || row === "labelAndIcon" ? mockPlaywrightIcon : undefined,
+              },
+              {
+                value: "option-2",
+                label: "Option 2",
+                hideLabel: row === "icon",
+                icon: row === "icon" || row === "labelAndIcon" ? mockPlaywrightIcon : undefined,
+                disabled: row === "disabled",
+              },
+            ]}
+            density={column}
+          />
+        </div>
+      );
+    },
+    hooks: {
+      beforeEach: async (component, page, column, row) => {
+        const firstControl = component.getByText("Option 1");
+        const radio = component.getByRole("radio", { name: "Option 1" });
+
+        if (row === "hover") await firstControl.hover();
+        if (row === "focus") await radio.focus();
+      },
+    },
+  });
+});
+
+test("should behave correctly", async ({ mount }) => {
+  // ARRANGE
+  let modelValue = null;
+  const component = await mount(
+    <OnyxSegmentedControl
+      modelValue={modelValue}
+      onUpdate:modelValue={(value) => (modelValue = value)}
+      options={[
+        {
+          value: "option-1",
+          label: "Option 1",
+        },
+        {
+          value: "option-2",
+          label: "Option 2",
+        },
+        {
+          value: "option-3",
+          label: "Option 3",
+          disabled: true,
+        },
+      ]}
+    />,
+  );
+  const firstControl = component.getByText("Option 1");
+  const radio = component.getByRole("radio", { name: "Option 1" });
+
+  // ACT
+
+  expect(modelValue).toBe(null);
+  await expect(radio).not.toBeChecked();
+
+  // ASSERT
+  await firstControl.click();
+
+  // ACT
+  await expect(radio).toBeChecked();
+  expect(modelValue).toBe("option-1");
+});
---
+++ packages/sit-onyx/src/components/OnyxSegmentedControl/OnyxSegmentedControl.stories.ts
@@ -0,0 +1,95 @@
+import { iconPlaceholder } from "@sit-onyx/icons";
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
+import OnyxSegmentedControl from "./OnyxSegmentedControl.vue";
+
+/**
+ * SegmentedControl is an interactive element that allow users to make a single selection from a set of mutually exclusive options.
+ * Users can choose only one option at the time.
+ */
+const meta: Meta<typeof OnyxSegmentedControl> = {
+  title: "Navigation/SegmentedControl",
+  tags: ["new:component"],
+  component: OnyxSegmentedControl,
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof OnyxSegmentedControl>;
+
+export const Default = {
+  args: {
+    options: [
+      {
+        value: "option-1",
+        label: "Option 1",
+      },
+      {
+        value: "option-2",
+        label: "Option 2",
+      },
+      {
+        value: "option-3",
+        label: "Option 3",
+      },
+    ],
+  },
+} satisfies Story;
+export const Icon = {
+  args: {
+    options: [
+      {
+        value: "option-1",
+        label: "Option 1",
+        icon: iconPlaceholder,
+        hideLabel: true,
+      },
+      {
+        value: "option-2",
+        icon: iconPlaceholder,
+        label: "Option 2",
+        hideLabel: true,
+      },
+    ],
+  },
+} satisfies Story;
+export const TextAndIcon = {
+  args: {
+    options: [
+      {
+        value: "option-1",
+        label: "Option 1",
+        icon: iconPlaceholder,
+      },
+      {
+        value: "option-2",
+        label: "Option 2",
+        icon: iconPlaceholder,
+      },
+    ],
+  },
+} satisfies Story;
+export const Disabled = {
+  args: {
+    options: [
+      {
+        value: "option-1",
+        label: "Option 1",
+      },
+      {
+        value: "option-2",
+        label: "Option 2",
+      },
+      {
+        value: "option-3",
+        label: "Option 3",
+        disabled: true,
+      },
+    ],
+  },
+} satisfies Story;
+
+export const Skeleton = {
+  args: {
+    skeleton: true,
+  },
+} satisfies Story;
