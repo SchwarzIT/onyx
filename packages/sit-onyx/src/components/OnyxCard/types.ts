b65dd4ba9ed19c612ff53c3670bd2817e6cc0a9d
Lars Rickert <lars.rickert@mail.schwarz>
2025-08-27 14:25:57 +0200

    feat(OnyxCard): support `link` property

--- packages/sit-onyx/src/components/OnyxCard/types.ts
+++ packages/sit-onyx/src/components/OnyxCard/types.ts
@@ -1,9 +1,11 @@
 import type { DensityProp } from "../../composables/density.js";
+import type { WithLinkProp } from "../OnyxRouterLink/types.js";
 
-export type OnyxCardProps = DensityProp & {
-  /**
-   * Whether the whole card should be clickable (will render it as `<button>`).
-   * **Important**: If clickable, the card must not contain interactive elements like other buttons etc.
-   */
-  clickable?: boolean;
-};
+export type OnyxCardProps = DensityProp &
+  WithLinkProp & {
+    /**
+     * Whether the whole card should be clickable (will render it as `<button>`).
+     * **Important**: If clickable, the card must not contain interactive elements like other buttons etc.
+     */
+    clickable?: boolean;
+  };
