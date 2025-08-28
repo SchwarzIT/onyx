da03ec4f86866cac81de7c35d3dd8ec89337e376
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-08-28 11:10:14 +0200

    fix filename

--- packages/sit-onyx/src/utils/file.spec.ts
+++ packages/sit-onyx/src/utils/file.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 import type { FileType } from "../components/OnyxFileUpload/types.js";
 import { validateFileType } from "./file.js";
 
-const pngFile = { name: "example.jpg", type: "image/jpeg" };
+const jpegFile = { name: "example.jpg", type: "image/jpeg" };
 
 describe("validateFileType", () => {
   describe("valid cases", () => {
@@ -10,37 +10,37 @@ describe("validateFileType", () => {
       {
         title: "should be valid for no accept list",
         accept: undefined,
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be valid for no empty accept list",
         accept: [],
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be valid for wildcard accept",
         accept: ["*/*" as FileType],
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be valid for single media type",
         accept: ["image/jpeg"],
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be valid for media type range",
         accept: [".docx", "image/*"],
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be valid for single file extension",
         accept: [".jpg"],
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be valid for multiple file extension",
         accept: [".png", ".jpg", ".jpeg", "image/*"],
-        file: pngFile,
+        file: jpegFile,
       },
     ] satisfies {
       title: string;
@@ -56,17 +56,17 @@ describe("validateFileType", () => {
       {
         title: "should be invalid for non-matching media type",
         accept: ["image/png"],
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be invalid for non-matching media type range",
         accept: ["audio/*"],
-        file: pngFile,
+        file: jpegFile,
       },
       {
         title: "should be invalid for non-matching file extension",
         accept: [".png"],
-        file: pngFile,
+        file: jpegFile,
       },
     ] satisfies {
       title: string;
