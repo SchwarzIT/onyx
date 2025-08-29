0a4dababe0a6b6826e3212883c21b0de8288aac4
Lars Rickert <lars.rickert@mail.schwarz>
2025-08-29 11:02:28 +0200

    feat: add sidebar

--- apps/demo-app/app/pages/forms.vue
+++ apps/demo-app/app/pages/forms.vue
@@ -1,7 +1,33 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { iconFile, iconUserGroup, iconUserId } from "@sit-onyx/icons";
+
+definePageMeta({ layout: false });
+
+const { t } = useI18n();
+
+const sidebarItems = computed<SidebarItem[]>(() => [
+  {
+    label: t("personalData"),
+    icon: iconUserId,
+    link: "/forms",
+  },
+  {
+    label: t("role", 2),
+    icon: iconUserGroup,
+    link: "/forms/roles",
+  },
+  {
+    label: t("document", 2),
+    icon: iconFile,
+    link: "/forms/files",
+  },
+]);
+</script>
 
 <template>
-  <OnyxHeadline is="h1">{{ $t("forms") }}</OnyxHeadline>
+  <NuxtLayout name="sidebar" :sidebar-items>
+    <OnyxHeadline is="h1">{{ $t("forms") }}</OnyxHeadline>
+  </NuxtLayout>
 </template>
 
 <style lang="scss" scoped></style>
