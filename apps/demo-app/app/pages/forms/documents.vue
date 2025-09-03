a33e7a4bb87708bc6fbbb8c33a4d88706df06bf1
Lars Rickert <lars.rickert@mail.schwarz>
2025-09-03 12:16:29 +0200

    feat(demo-app): implement user document upload

--- apps/demo-app/app/pages/forms/documents.vue
+++ apps/demo-app/app/pages/forms/documents.vue
@@ -1,7 +1,58 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useToast } from "sit-onyx";
+
+const toast = useToast();
+const { t } = useI18n();
+
+const selectedFiles = ref<File[]>([]);
+const uploadedFiles = ref<File[]>([]);
+
+const removeFile = (file: File) => {
+  selectedFiles.value = selectedFiles.value.filter((f) => f !== file);
+};
+
+const handleFileUploaded = (file: File) => {
+  removeFile(file);
+  uploadedFiles.value.push(file);
+
+  toast.show({
+    headline: t("documents.toasts.upload.success"),
+    description: t("noDemoDataUpdated"),
+    color: "success",
+  });
+};
+</script>
 
 <template>
-  <OnyxHeadline is="h1">{{ $t("document", 2) }}</OnyxHeadline>
-</template>
+  <div class="page">
+    <OnyxHeadline is="h1">{{ $t("documents.document", 2) }}</OnyxHeadline>
 
-<style lang="scss" scoped></style>
+    <div class="onyx-grid">
+      <OnyxInfoCard
+        class="onyx-grid-span-4"
+        :headline="$t('documents.disclaimer.headline')"
+        color="warning"
+      >
+        {{ $t("documents.disclaimer.description") }}
+      </OnyxInfoCard>
+    </div>
+
+    <div class="onyx-grid">
+      <OnyxFileUpload
+        v-model="selectedFiles"
+        class="onyx-grid-span-4"
+        max-size="12MiB"
+        list-type="maxHeight"
+        multiple
+      >
+        <template #default="{ file, props }">
+          <UploadFileCard
+            v-bind="props"
+            @done="handleFileUploaded(file)"
+            @remove="removeFile(file)"
+          />
+        </template>
+      </OnyxFileUpload>
+    </div>
+  </div>
+</template>
