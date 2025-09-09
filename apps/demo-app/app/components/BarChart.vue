795c2d99be99eb40e1b7d7d9d2913f89eda3678a
Lars Rickert <lars.rickert@mail.schwarz>
2025-09-09 19:32:07 +0200

    feat(demo-app): update charts

---
+++ apps/demo-app/app/components/BarChart.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { getDatasetColors } from "@sit-onyx/chartjs-plugin";
+import type { ChartData, ChartOptions } from "chart.js";
+import { Bar } from "vue-chartjs";
+
+const { t } = useI18n();
+
+const data = computed<ChartData<"bar">>(() => {
+  return {
+    labels: ["Mo", "Tu", "We", "Th", "Fr", "Sa"],
+    datasets: [
+      {
+        label: t("charts.productLines.a"),
+        data: [40, 0, 20, 0, 35, 0],
+        borderWidth: 1,
+        ...getDatasetColors("info"),
+      },
+      {
+        label: "Metric B (Green)",
+        data: [-15, 45, -10, 0, 0, 25],
+        borderWidth: 1,
+        ...getDatasetColors("success"),
+      },
+      {
+        label: "Metric C (Pink)",
+        data: [0, 0, 0, 50, 20, 0],
+        borderWidth: 1,
+        ...getDatasetColors("danger"),
+      },
+      {
+        label: "Metric D (Orange)",
+        data: [15, -25, 10, -35, -10, -15],
+        borderWidth: 1,
+        ...getDatasetColors("warning"),
+      },
+    ],
+  };
+});
+
+const options: ChartOptions<"bar"> = { maintainAspectRatio: false };
+</script>
+
+<template>
+  <div class="chart">
+    <ClientOnly>
+      <Bar :data :options />
+    </ClientOnly>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chart {
+  height: 50%;
+  flex-grow: 1;
+}
+</style>
---
+++ apps/demo-app/app/components/PieChart.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import type { ChartData, ChartOptions } from "chart.js";
+import { Pie } from "vue-chartjs";
+
+const { t } = useI18n();
+
+const data = computed<ChartData<"pie">>(() => {
+  return {
+    labels: [
+      t("charts.customerSegments.enterprise"),
+      t("charts.customerSegments.mid-sized"),
+      t("charts.customerSegments.small"),
+      t("charts.customerSegments.private"),
+    ],
+    datasets: [{ data: [55, 25, 15, 5] }],
+  };
+});
+
+const options: ChartOptions<"pie"> = { maintainAspectRatio: false };
+</script>
+
+<template>
+  <div class="chart">
+    <ClientOnly>
+      <Pie :data :options />
+    </ClientOnly>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chart {
+  height: 50%;
+  flex-grow: 1;
+}
+</style>
