c04dabe0e55b878147ab7b232de92a6641568b11
Lars Rickert <lars.rickert@mail.schwarz>
2025-09-10 08:17:52 +0200

    feat(demo-app): make charts look more fancy (#4100)
    Relates to #3983 
    
    - Adjust the chart colors and data so they look fancier ✨ 
    - fix the grid spans of the upper page area to be more responsive
    
    ## Checklist
    
    - [x] The added / edited code has been documented with
    [JSDoc](https://jsdoc.app/about-getting-started)
    - [x] I have performed a self review of my code ("Files changed" tab in
    the pull request)

---
+++ apps/demo-app/app/components/BarChart.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import { getDatasetColors } from "@sit-onyx/chartjs-plugin";
+import type { ChartData } from "chart.js";
+import { Bar } from "vue-chartjs";
+
+const { t } = useI18n();
+
+const data = computed<ChartData<"bar">>(() => {
+  return {
+    labels: ["Mo", "Tu", "We", "Th", "Fr", "Sa"],
+    datasets: [
+      {
+        label: t("charts.productLines.a"),
+        data: [40, 0, 20, 0, 35, 0],
+        borderWidth: 1,
+        ...getDatasetColors("info"),
+      },
+      {
+        label: t("charts.productLines.b"),
+        data: [-15, 45, -10, 0, 0, 25],
+        borderWidth: 1,
+        ...getDatasetColors("primary"),
+      },
+      {
+        label: t("charts.productLines.c"),
+        data: [0, 0, 0, 50, 20, 0],
+        borderWidth: 1,
+        ...getDatasetColors("warning"),
+      },
+      {
+        label: t("charts.productLines.d"),
+        data: [15, -25, 10, -35, -10, -15],
+        borderWidth: 1,
+        ...getDatasetColors("quantitatives-700"),
+      },
+    ],
+  };
+});
+</script>
+
+<template>
+  <div class="chart">
+    <ClientOnly>
+      <Bar :data :options="commonChartOptions" />
+    </ClientOnly>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chart {
+  height: 50%;
+  flex-grow: 1;
+}
+</style>
---
+++ apps/demo-app/app/components/LineChart.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { getDatasetColors } from "@sit-onyx/chartjs-plugin";
+import type { ChartData } from "chart.js";
+import { Line } from "vue-chartjs";
+
+const { t } = useI18n();
+
+const data = computed<ChartData<"line">>(() => {
+  return {
+    labels: ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun"],
+    datasets: [
+      {
+        label: t("charts.metrics.revenue"),
+        data: [5, 15, 22, 40, 15, 30],
+        fill: true,
+        tension: 0.4,
+        ...getDatasetColors("quantitatives-700"),
+      },
+      {
+        label: t("charts.metrics.netProfit"),
+        data: [25, 10, 13, 25, 30, 15],
+        fill: true,
+        tension: 0.4,
+        ...getDatasetColors("primary"),
+      },
+      {
+        label: t("charts.metrics.investments"),
+        data: [20, 48, 25, 26, 5, 42],
+        fill: true,
+        tension: 0.4,
+        ...getDatasetColors("quantitatives-1100"),
+      },
+    ],
+  };
+});
+</script>
+
+<template>
+  <div class="chart">
+    <ClientOnly>
+      <Line :data :options="commonChartOptions" />
+    </ClientOnly>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chart {
+  height: 50%;
+  flex-grow: 1;
+}
+</style>
