4ae02faa9046cd91cf6acd6cbf82a220bf75c01a
Lars Rickert <lars.rickert@mail.schwarz>
2025-09-10 09:57:00 +0200

    add nav item children

--- apps/demo-app/app/components/NavBar.vue
+++ apps/demo-app/app/components/NavBar.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import NavBar from "#layers/blueprint/app/components/NavBar.vue";
+import { iconFile, iconUserGroup, iconUserId } from "@sit-onyx/icons";
 import type { OnyxNavBarSlots } from "sit-onyx";
 import logoUrl from "~/assets/images/onyx-logo.svg";
 
@@ -12,7 +13,26 @@ const localePath = useLocalePath();
   <NavBar app-name="onyx demo" :logo-url>
     <OnyxNavItem :label="$t('overview')" :link="localePath('/')" />
     <OnyxNavItem :label="$t('dataGrid.pageName')" :link="localePath('/data-grid')" />
-    <OnyxNavItem :label="$t('forms')" :link="localePath('/forms')" />
+
+    <OnyxNavItem :label="$t('forms')" :link="localePath('/forms')">
+      <template #children>
+        <OnyxNavItem :label="$t('personalData')" :link="localePath('/forms')">
+          <OnyxIcon :icon="iconUserId" />
+          {{ $t("personalData") }}
+        </OnyxNavItem>
+
+        <OnyxNavItem :label="$t('roles.role', 2)" :link="localePath('/forms/roles')">
+          <OnyxIcon :icon="iconUserGroup" />
+          {{ $t("roles.role", 2) }}
+        </OnyxNavItem>
+
+        <OnyxNavItem :label="$t('documents.document', 2)" :link="localePath('/forms/documents')">
+          <OnyxIcon :icon="iconFile" />
+          {{ $t("documents.document", 2) }}
+        </OnyxNavItem>
+      </template>
+    </OnyxNavItem>
+
     <OnyxNavItem :label="$t('charts.pageName')" :link="localePath('/charts')" />
 
     <template #contextArea>
