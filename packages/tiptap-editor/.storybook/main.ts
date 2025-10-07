48f47a699d9d66d462ae37850de6ebbf02d20e6d
Lars Rickert <lars.rickert@mail.schwarz>
2025-10-07 17:38:38 +0200

    setup initial package

---
+++ packages/tiptap-editor/.storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.ts"],
+  addons: ["@storybook/addon-docs", "@vueless/storybook-dark-mode"],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {
+      docgen: {
+        plugin: "vue-component-meta",
+        tsconfig: "tsconfig.app.json",
+      },
+    },
+  },
+  features: {
+    interactions: false,
+  },
+  core: {
+    disableTelemetry: true,
+  },
+};
+
+export default config;
