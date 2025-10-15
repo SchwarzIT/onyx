1aa2368fc686e67bb766c36cb734a44aaccb37ec
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-10-15 13:38:15 +0200

    improve useForwardProps type

--- packages/sit-onyx/src/utils/props.ts
+++ packages/sit-onyx/src/utils/props.ts
@@ -1,14 +1,42 @@
-import { computed, type Component, type ConcreteComponent } from "vue";
+import { computed, type ConcreteComponent } from "vue";
 import type { ComponentProps } from "vue-component-type-helpers";
 import type { Data, MaybePick } from "../types/utils.js";
 
+/**
+ * The computed value is an object, that only contains properties that are defined by the target component.
+ * Is useful to forward only a matching subset of properties from a parent component to a wrapped child component.
+ *
+ * This is necessary when the parent defines props, which are not defined in the child component.
+ * Otherwise, the child component will set the extraneous props as attributes, which bloats the DOM and can lead to unexpected side-effects.
+ *
+ * @example
+ *
+ * ```vue
+ * <script setup lang="ts">
+ * import { useForwardProps } from "sit-onyx";
+ * import MyChildComponent from "./MyChildComponent.vue";
+ *
+ * const props = defineProps<ParentProps & ChildProps>();
+ * const childProps = useForwardProps(props, MyChildComponent);
+ * </script>
+ * <template>
+ *   <!-- childProps only includes props that exist on MyChildComponent -->
+ *   <MyChildComponent v-bind="childProps" />
+ * </template>
+ * ```
+ *
+ * @param props The reactive props object of the parent component.
+ * @param target Component for which the properties are to be forwarded.
+ * @returns computed value with properties that are also defined the target component.
+ */
 export const useForwardProps = <
   T extends Data,
-  C extends Component,
-  R = MaybePick<T, keyof ComponentProps<C>>,
+  TComponent,
+  TProps = ComponentProps<TComponent>,
+  R = MaybePick<T, keyof TProps>,
 >(
   props: T,
-  target: C,
+  target: TComponent,
 ) => {
   const keys = new Set(Object.keys((target as ConcreteComponent).props));
   return computed(
