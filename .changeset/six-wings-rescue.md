139a47ffc547e57e45e7a384321d35ea05d5ad47
Jonathan <jonathan_leo.carle@mail.schwarz>
2025-10-15 15:17:15 +0200

    feat(OnyxFlyoutMenu): Added `OnyxSystemButton` as default slot content for the `button` slot (#4249)
    Relates to #4247
    
    - The `OnyxFlyoutMenu` tends to be used quite often together with the
    `OnyxSystemButton` and the `iconMoreVertical`. Therefore we can reduce
    code duplication by providing this pairing as the default slot content.
    - Replaced `notificationCard.*` translations with `flyoutMenu.*`
    translation

---
+++ .changeset/six-wings-rescue.md
@@ -0,0 +1,5 @@
+---
+"sit-onyx": minor
+---
+
+feat(OnyxFlyoutMenu): Added `OnyxSystemButton` as default slot content for the `button` slot
--- packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxFlyoutMenu/OnyxFlyoutMenu.stories.ts
+++ packages/sit-onyx/src/components/OnyxNavBar/modules/OnyxFlyoutMenu/OnyxFlyoutMenu.stories.ts
@@ -1,8 +1,9 @@
-import { iconGlobe, iconPlaceholder } from "@sit-onyx/icons";
+import { iconDisc, iconEdit, iconGlobe, iconPlaceholder, iconTrash } from "@sit-onyx/icons";
 import { withNativeEventLogging } from "@sit-onyx/storybook-utils";
 import type { Meta, StoryObj } from "@storybook/vue3-vite";
 import { h } from "vue";
 import OnyxButton from "../../../OnyxButton/OnyxButton.vue";
+import OnyxIcon from "../../../OnyxIcon/OnyxIcon.vue";
 import OnyxMenuItem from "../OnyxMenuItem/OnyxMenuItem.vue";
 import OnyxFlyoutMenu from "./OnyxFlyoutMenu.vue";
 
@@ -10,6 +11,20 @@ import OnyxFlyoutMenu from "./OnyxFlyoutMenu.vue";
  * The flyout menu is used to build up navigation menus such as the [OnyxNavItem](/docs/navigation-navbar-modules-navitem--docs) or [OnyxUserMenu](/docs/navigation-navbar-modules-usermenu--docs).
  *
  * You can also use it to e.g. build custom nav modules like action buttons for your application. The flyout menu will then take care of basic styles and keyboard navigation.
+ *
+ * The default `OnyxSystemButton` of the `button` slot can be replaced with a custom button.
+ * If you do, you **must** bind the `trigger` [slot prop](https://vuejs.org/guide/components/slots.html#scoped-slots) to the button element.
+ * E.g.:
+ * ```html
+ * <OnyxFlyoutMenu>
+ *    <!-- Passing slot props from the button slot to the custom button -->
+ *    <template #button="{ trigger }">
+ *      <MyCustomButton
+ *        v-bind="trigger"
+ *      />
+ *    </template>
+ * </OnyxFlyoutMenu>
+ * ```
  */
 const meta: Meta<typeof OnyxFlyoutMenu> = {
   title: "Basic/FlyoutMenu",
@@ -18,7 +33,7 @@ const meta: Meta<typeof OnyxFlyoutMenu> = {
     (story) => ({
       components: { story },
       template: `
-        <div style="height: 28rem;">
+        <div style="height: 10rem;">
           <story />
         </div>`,
     }),
@@ -39,6 +54,17 @@ type Story = StoryObj<typeof OnyxFlyoutMenu>;
  * This example shows a basic OnyxFlyoutMenu
  */
 export const Default = {
+  args: {
+    label: "Choose an action",
+    options: () => [
+      h(OnyxMenuItem, () => [h(OnyxIcon, { icon: iconEdit }), "Edit"]),
+      h(OnyxMenuItem, () => [h(OnyxIcon, { icon: iconDisc }), "Save"]),
+      h(OnyxMenuItem, () => [h(OnyxIcon, { icon: iconTrash }), "Discard"]),
+    ],
+  },
+} satisfies Story;
+
+export const CustomButton = {
   args: {
     label: "Choose application language",
     button: ({ trigger }) => [
