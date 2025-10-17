6fcbcdc5b9cc352e9a321ce7f3705eb677de6e43
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-10-17 14:32:08 +0200

    add linting issues

--- packages/sit-onyx/src/components/OnyxBasicPopover/OnyxBasicPopover.vue
+++ packages/sit-onyx/src/components/OnyxBasicPopover/OnyxBasicPopover.vue
@@ -204,7 +204,7 @@ const popoverStyles = computed(() => {
 <template>
   <div
     ref="popoverWrapper"
-    class="onyx-component onyx-basic-popover"
+    class="onyx-basic-popover"
     :style="`anchor-name: ${anchorName}`"
   >
     <slot :trigger="trigger"></slot>
