59c8fecb72b6e7f49563c7423b8626401e018a23
Lars Rickert <lars.rickert@mail.schwarz>
2025-10-20 08:34:20 +0200

    fix unique hash errors for nuxt-docs tests

--- packages/nuxt-docs/playground/playwright.config.ts
+++ packages/nuxt-docs/playground/playwright.config.ts
@@ -1,14 +1,19 @@
+import { ModuleOptions as NuxtContentOptions } from "@nuxt/content";
 import type { ConfigOptions } from "@nuxt/test-utils/playwright";
 import { defineConfig, devices } from "@playwright/test";
 import { PLAYWRIGHT_BASE_CONFIG } from "@sit-onyx/shared/playwright.config.base";
 import { fileURLToPath } from "node:url";
+
+type NuxtPlaywrightConfigOptions = ConfigOptions & {
+  nuxt: { nuxtConfig?: { content?: Partial<NuxtContentOptions> } };
+};
 
 /**
  * Global Playwright configuration.
  *
  * @see https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig<ConfigOptions>({
+export default defineConfig<NuxtPlaywrightConfigOptions>({
   ...PLAYWRIGHT_BASE_CONFIG,
   testDir: "./tests/playwright",
   timeout: 60 * 1000,
@@ -16,6 +21,17 @@ export default defineConfig<ConfigOptions>({
     ...PLAYWRIGHT_BASE_CONFIG.use,
     nuxt: {
       rootDir: fileURLToPath(new URL(".", import.meta.url)),
+      nuxtConfig: {
+        content: {
+          // prevent flaky tests / general test issues due to parallel execution
+          // by using a separate/unique database file for each test so the tests do not run / modify the same database
+          _localDatabase: {
+            type: "sqlite",
+            // using performance here instead of Date.now() because its more accurate. Date.now() still caused errors on local machines that are very fast
+            filename: `.data/playwright/playwright-${performance.now() + performance.timeOrigin}.sqlite`,
+          },
+        },
+      },
     },
   },
   projects: [{ name: "edge", use: { ...devices["Desktop Edge"], channel: "msedge" } }],
