7e78232a73d147176806fd76c76fb7aaaa28c01a
Jonathan <jonathan_leo.carle@mail.schwarz>
2025-10-21 18:30:23 +0200

    chore: implement and enable no-v-bind-props rule (#4313)
    Relates to #3966
    
    Implemented eslint rule to check for unguarded forwarding of props.
    The rule is now implemented without any typed linting required.

---
+++ packages/eslint-plugin/src/rules/no-v-bind-props.cjs
@@ -0,0 +1,86 @@
+"use strict";
+
+const utils = require("eslint-plugin-vue/lib/utils");
+
+/** @type {import('eslint').Rule.RuleModule} */
+module.exports = {
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "Finds code occurrences, where props are directly passed to a child component. This can be problematic, as excessive props are rendered as attributes and may cause unexpected behavior.",
+    },
+    fixable: null,
+    schema: [
+      {
+        type: "object",
+        properties: {
+          ignores: {
+            type: "array",
+            items: { type: "string" },
+            uniqueItems: true,
+            additionalItems: false,
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
+    messages: {
+      boundProps: `All props are bound directly using v-bind="{{ name }}", this is usually a mistake! Props which are not defined by the target will be rendered as attributes and have unintended side-effects!`,
+    },
+  },
+  /** @param {import('eslint').Rule.RuleContext} context */
+  create(context) {
+    const ignores = context.options?.[0]?.ignores;
+    if (ignores && new RegExp(ignores).test(context.filename)) {
+      return {};
+    }
+
+    /**
+     * Used to track the names of the defined props objects
+     * @type {string[]}
+     */
+    const propsIdentifiers = ["$props"];
+
+    return utils.compositingVisitors(
+      utils.defineScriptSetupVisitor(context, {
+        /** @param {import("vue-eslint-parser/ast/nodes").Node} node */
+        onDefinePropsEnter(node) {
+          while (node.parent != null && node.type !== "VariableDeclaration") {
+            node = node.parent;
+          }
+
+          if (node == null || node.type !== "VariableDeclaration") {
+            return;
+          }
+
+          node.declarations.forEach((d) => propsIdentifiers.push(d.id.name));
+        },
+      }),
+      utils.defineTemplateBodyVisitor(context, {
+        /** @param {import("vue-eslint-parser/ast/nodes").VAttribute} node */
+        "VAttribute[directive=true][key.name.name='bind'][key.argument=null]"(node) {
+          const name = node.value?.expression?.name;
+          if (!name) {
+            return;
+          }
+
+          // Find the parent node which is the vue element
+          let parentNode = node;
+          while (parentNode && parentNode.type !== "VElement") {
+            parentNode = parentNode.parent;
+          }
+
+          if (utils.isCustomComponent(parentNode) && propsIdentifiers.includes(name)) {
+            context.report({
+              loc: node.value.loc,
+              node,
+              messageId: "boundProps",
+              data: { name },
+            });
+          }
+        },
+      }),
+    );
+  },
+};
