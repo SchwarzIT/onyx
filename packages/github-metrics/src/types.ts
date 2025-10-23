3143768a6015888d91880808a1e46932fbcf7884
Lars Rickert <lars.rickert@mail.schwarz>
2025-10-23 14:23:37 +0200

    calculate mean only for given sprint

--- packages/github-metrics/src/types.ts
+++ packages/github-metrics/src/types.ts
@@ -9,9 +9,25 @@ export type BasicProjectsQueryOptions = {
    * ID of the GitHub project.
    */
   projectId: number;
+  /**
+   * Name of the iteration field that represents your iteration/sprint.
+   * If unset, the first available iteration field will be used.
+   *
+   * This is only relevant for metrics that are iteration-based.
+   */
+  iterationFieldName?: string;
 };
 
 export type PageInfo = {
   hasNextPage: boolean;
   endCursor?: string;
 };
+
+export type Iteration = {
+  id: string;
+  title: string;
+  startDate: DateString;
+  duration: number;
+};
+
+export type DateString = `${string}-${string}-${string}`;
