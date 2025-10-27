004dbcf5ec676a8dbadb6797a33c84b00c17297b
Lars Rickert <lars.rickert@mail.schwarz>
2025-10-27 11:42:15 +0100

    feat: implement initial `@sit-onyx/github-metrics` package (#4321)
    Relates to #4103
    
    Implement initial new `@sit-onyx/github-metrics` npm package that
    provides utilities for collection (NEO) metrics for GitHub projects. Its
    implemented generically to support arbitrary projects (not only onyx)
    and has no runtime dependencies (uses only Node.js).
    
    For now the package only supports the `getMeanStorySize()` metric. More
    metrics will be added in follow up PRs.
    
    ## Checklist
    
    - [x] The added / edited code has been documented with
    [JSDoc](https://jsdoc.app/about-getting-started)
    - [x] A changeset is added with `npx changeset add` if your changes
    should be released as npm package (because they affect the library
    usage)
    - [x] I have performed a self review of my code ("Files changed" tab in
    the pull request)
    - [x] All relevant changes are documented in the documentation app
    (folder `apps/docs`) if needed
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ packages/github-metrics/src/metrics/meanStorySize.ts
@@ -0,0 +1,40 @@
+import type { Client } from "../types.js";
+import { findIterationByDate } from "../utils/github.js";
+
+export type GetMeanStorySizeOptions = {
+  client: Client;
+  /**
+   * Only items that are assigned to this iteration will be considered.
+   * Any date can be passed, the matching iteration will be determined automatically.
+   *
+   * @default `new Date()`
+   */
+  iteration?: Date;
+};
+
+/**
+ * Calculates the mean / average story size of an item in the given iteration.
+ * Items without an assigned size will be ignored.
+ */
+export async function getMeanStorySize(options: GetMeanStorySizeOptions) {
+  // fetch iterations and items in parallel to speed up the process
+  const [allIterations, allItems] = await Promise.all([
+    options.client.getAllIterations(),
+    options.client.getAllItems(),
+  ]);
+
+  const currentIteration = findIterationByDate(allIterations, options.iteration ?? new Date());
+  const items = allItems.filter((item) => item.iteration === currentIteration.title);
+
+  const storySizes = items.map((item) => item.effort).filter((size) => size != undefined);
+  const totalCount = storySizes.length;
+
+  const totalSize = storySizes.reduce((total, size) => total + size, 0);
+
+  return {
+    // prevent division by zero
+    mean: totalCount > 0 ? totalSize / totalCount : 0,
+    items: totalCount,
+    iteration: currentIteration.title,
+  };
+}
---
+++ packages/github-metrics/src/types.ts
@@ -0,0 +1,80 @@
+import type { createClient } from "./utils/client.js";
+
+export type CreateClientOptions = {
+  /**
+   * Name of the GitHub organization
+   *
+   * @example "SchwarzIT"
+   */
+  organization: string;
+  /**
+   * ID of the GitHub project.
+   */
+  projectId: number;
+  /**
+   * GitHub token to use for authentication.
+   * It is recommended to use an environment variable for this and NOT include it in any public code or commit.
+   *
+   * To get a GitHub token, go to: https://github.com/settings/tokens and create a new token with scope "read:project".
+   *
+   * @example `authToken: process.env.GITHUB_TOKEN`
+   */
+  authToken: string;
+  /**
+   * Maps your custom project field names to functional names used by this client.
+   */
+  fields: ClientProjectFields;
+  /**
+   * Fetch function to use for making HTTP requests.
+   *
+   * @default `global.fetch``
+   * @private Only intended for internal usage / testing
+   */
+  fetch?: typeof global.fetch;
+};
+
+export type ClientProjectFields = {
+  /**
+   * Name of the numeric field that represents your Story effort/size/estimation.
+   *
+   * @example "Effort"
+   */
+  effort: string;
+  /**
+   * Name of the iteration field that represents your iteration/sprint.
+   *
+   * @example "Sprint"
+   */
+  iteration: string;
+};
+
+export type RunQueryOptions = {
+  query: string;
+  variables?: Record<string, unknown>;
+};
+
+export type Client = ReturnType<typeof createClient>;
+
+export type GraphQLPageInfo = {
+  hasNextPage: boolean;
+  endCursor?: string;
+};
+
+export type Iteration = {
+  title: string;
+  startDate: DateString;
+  duration: number;
+};
+
+export type DateString = `${string}-${string}-${string}`;
+
+export type ProjectItem = {
+  /**
+   * Title of the iteration that the item is assigned to.
+   */
+  iteration?: string;
+  /**
+   * Story effort/size/estimation.
+   */
+  effort?: number;
+};
