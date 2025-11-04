6722db5f850e97fa0d9b5a9710d928baafca8fef
Christian Bußhoff <christian.busshoff@mail.schwarz>
2025-11-04 16:23:50 +0100

    feat(DemoApp): prepared to add theme banners (#4383)
    Relates to #4373
    - prepared to add theme banners

---
+++ apps/demo-app/app/components/Banner.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { OnyxImage } from "sit-onyx";
+
+const props = defineProps<{
+  src: string;
+}>();
+</script>
+
+<template>
+  <OnyxImage class="banner" alt="Hero banner" :src="props.src" :width="1920" :height="280" />
+</template>
+
+<style lang="scss" scoped>
+.banner {
+  width: 100%;
+  display: block;
+
+  :deep(.onyx-image__source) {
+    display: inherit;
+  }
+}
+</style>
