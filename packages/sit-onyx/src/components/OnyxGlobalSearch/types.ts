92142e3e8529f8eab5324e28ba229ccf8966386a
Lars Rickert <lars.rickert@mail.schwarz>
2025-11-07 13:34:59 +0100

    do not filter internally

--- packages/sit-onyx/src/components/OnyxGlobalSearch/types.ts
+++ packages/sit-onyx/src/components/OnyxGlobalSearch/types.ts
@@ -2,41 +2,16 @@ import type { createComboBox } from "@sit-onyx/headless";
 import type { InjectionKey, Ref } from "vue";
 import type { Nullable } from "../../types/utils.js";
 import type { OnyxBasicDialogProps } from "../OnyxBasicDialog/types.js";
-import type { OnyxGlobalSearchOptionProps } from "../OnyxGlobalSearchOption/types.js";
-import type { OnyxSelectProps } from "../OnyxSelect/types.js";
 
-export type OnyxGlobalSearchProps = Omit<OnyxBasicDialogProps, "modal" | "alignment" | "label"> &
-  Pick<OnyxSelectProps<string, false>, "noFilter"> & {
-    /**
-     * The current search term.
-     */
-    modelValue?: Nullable<string>;
-    /**
-     * Whether the search is currently loading results.
-     */
-    loading?: boolean;
-    /**
-     * Search result groups to display.
-     */
-    groups?: GlobalSearchGroup[];
-  };
-
-export type GlobalSearchGroup = {
+export type OnyxGlobalSearchProps = Omit<OnyxBasicDialogProps, "modal" | "alignment" | "label"> & {
   /**
-   * Label / headline of the group.
+   * The current search term.
    */
-  label: string;
+  modelValue?: Nullable<string>;
   /**
-   * A list of options / search results to display for this group.
+   * Whether the search is currently loading results.
    */
-  options: GlobalSearchOption[];
-};
-
-export type GlobalSearchOption = Pick<OnyxGlobalSearchOptionProps, "label" | "icon" | "link"> & {
-  /**
-   * Unique value / ID of the option.
-   */
-  value: string;
+  loading?: boolean;
 };
 
 export const GLOBAL_SEARCH_INJECTION_KEY = Symbol() as InjectionKey<{
