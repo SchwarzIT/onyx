f5ffdb6191d7b5e647772c544f60895c6204de99
Lars Rickert <lars.rickert@mail.schwarz>
2025-11-09 10:45:35 +0100

    add undo action

---
+++ packages/tiptap-editor/src/components/OnyxEditorToolbarAction/OnyxEditorToolbarAction.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { mergeVueProps, OnyxSystemButton, OnyxTooltip, useForwardProps } from "sit-onyx";
+import type { OnyxEditorToolbarActionProps } from "./types.js";
+
+const props = defineProps<OnyxEditorToolbarActionProps>();
+
+const systemButtonProps = useForwardProps(props, OnyxSystemButton);
+</script>
+
+<template>
+  <OnyxTooltip class="onyx-editor-toolbar-action" :text="props.label">
+    <template #default="{ trigger }">
+      <OnyxSystemButton v-bind="mergeVueProps(systemButtonProps, trigger)" />
+    </template>
+  </OnyxTooltip>
+</template>
+
+<style lang="scss">
+@use "sit-onyx/src/styles/mixins/layers.scss";
+
+.onyx-editor-toolbar-action {
+  @include layers.component() {
+    display: flex;
+  }
+}
+</style>
