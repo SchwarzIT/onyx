ffff4c67c324915657f91483b289022235c273d4
Lars Rickert <lars.rickert@mail.schwarz>
2025-11-10 09:34:47 +0100

    fix test

--- packages/sit-onyx/src/components/OnyxCodeTabs/OnyxCodeTabs.ct.tsx
+++ packages/sit-onyx/src/components/OnyxCodeTabs/OnyxCodeTabs.ct.tsx
@@ -99,7 +99,13 @@ test("should copy code", async ({ mount, page, context, browserName }) => {
   await expect(component).not.toContainText("Copied!");
 });
 
-test("should show error when copy fails", async ({ mount, page, context }) => {
+test("should show error when copy fails", async ({ mount, page, context, browserName }) => {
+  // eslint-disable-next-line playwright/no-skipped-test -- clipboard permission granting is only supported in chromium
+  test.skip(
+    browserName !== "chromium",
+    "clipboard permission granting is only supported in chromium",
+  );
+
   // ARRANGE
   await page.clock.install({ time: new Date(2025, 10, 2) });
   await page.clock.pauseAt(new Date(2025, 10, 3));
