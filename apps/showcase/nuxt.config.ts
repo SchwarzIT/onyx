04ecdfb7f501117057694cac4eded9349fe77e59
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-11-11 12:56:04 +0100

    change

--- apps/showcase/nuxt.config.ts
+++ apps/showcase/nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   extends: ["@sit-onyx/nuxt-docs"],
+  i18n: {
+    defaultLocale: "en",
+    locales: [{ code: "en", language: "en-US", file: "en-US.json", name: "English" }],
+  },
 });
