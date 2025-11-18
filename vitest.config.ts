db73b121e2150a8f4d91f672d85278d78a006fad
Lars Rickert <lars.rickert@mail.schwarz>
2025-11-18 15:03:12 +0100

    revert unintentional change

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: ["{packages,apps}/*"],
-    // coverage: {
-    //   reporter: ["lcov"],
-    // },
-    // include: ["src"],
-    // exclude: ["src/**/*.stories.ts"],
+    coverage: {
+      reporter: ["lcov"],
+    },
+    include: ["src"],
+    exclude: ["src/**/*.stories.ts"],
   },
 });
