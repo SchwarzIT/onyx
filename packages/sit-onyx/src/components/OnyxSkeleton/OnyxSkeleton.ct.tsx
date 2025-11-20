a2e92c871b6a4bb174e3edeaf883780526cc6d22
Jonathan <jonathan_leo.carle@mail.schwarz>
2025-11-20 10:08:51 +0100

    fix(OnyxSkeleton): Fix PointerEvents were able to be triggered even when components are in "skeleton" mode (#4472)
    Relates to #4405 
    
    Applied suggested change: Using `pointer-events: none;` to suppress any
    PointerEvents.

--- packages/sit-onyx/src/components/OnyxSkeleton/OnyxSkeleton.ct.tsx
+++ packages/sit-onyx/src/components/OnyxSkeleton/OnyxSkeleton.ct.tsx
@@ -1,9 +1,13 @@
+import { createEmitSpy, expectEmit } from "@sit-onyx/playwright-utils";
 import { expect, test } from "../../playwright/a11y.js";
 import OnyxSkeleton from "./OnyxSkeleton.vue";
 
 test("should render", async ({ mount, makeAxeBuilder }) => {
   // ARRANGE
-  const component = await mount(<OnyxSkeleton style={{ width: "6rem", height: "2rem" }} />);
+  const onClick = createEmitSpy();
+  const component = await mount(
+    <OnyxSkeleton style={{ width: "6rem", height: "2rem" }} onClick={onClick} />,
+  );
 
   // ASSERT
   await expect(component).toHaveScreenshot("default.png");
@@ -13,4 +17,11 @@ test("should render", async ({ mount, makeAxeBuilder }) => {
 
   // ASSERT
   expect(accessibilityScanResults.violations).toEqual([]);
+
+  // ACT
+  // eslint-disable-next-line playwright/no-force-option -- We want to test and enforce that clicks on skeletons are suppressed.
+  await component.click({ force: true });
+
+  // ASSERT
+  expectEmit(onClick, 0);
 });
