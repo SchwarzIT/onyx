b9db9bdd2ab663f6fb571c422c8749a93178a508
Lars Rickert <lars.rickert@mail.schwarz>
2025-12-16 13:03:19 +0100

    docs(showcase): enable nuxt studio

--- packages/nuxt-docs/playground/content.config.ts
+++ packages/nuxt-docs/playground/content.config.ts
@@ -5,7 +5,7 @@ export default defineContentConfig({
     // content_en is registered by default in @sit-onyx/nuxt-docs so there is no need to register it manually here
     content_de: defineCollection({
       type: "page",
-      source: { include: "de/**", prefix: "" },
+      source: { include: "de/**", prefix: "/" },
     }),
   },
 });
