e8e166e68bf231956d311db0dcf1a32eca4842b4
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-12-18 18:16:05 +0100

    added File structure for showcase

--- apps/showcase/app/components/NavBar.vue
+++ apps/showcase/app/components/NavBar.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup>
 import NavBar from "#layers/onyx/app/components/NavBar.vue";
+const localePath = useLocalePath();
 </script>
 
 <template>
-  <NavBar logo-url="/onyx-logo.svg" />
+  <NavBar logo-url="/onyx-logo.svg">
+    <OnyxNavItem :label="$t('home')" :link="localePath('/')" />
+    <OnyxNavItem :label="$t('introduction')" :link="localePath('/introduction')" />
+    <OnyxNavItem :label="$t('resources')" :link="localePath('/resources')" />
+    <OnyxNavItem :label="$t('support')" :link="localePath('/support')" />
+  </NavBar>
 </template>
