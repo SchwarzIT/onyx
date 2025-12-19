1f134da96299f46bb86f158da65525643cd7a4bd
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-12-19 08:19:46 +0100

    implement useLastSettled composables

---
+++ packages/headless/src/composables/helpers/useLastSettled.spec.ts
@@ -0,0 +1,80 @@
+import { expect, test, vi } from "vitest";
+import { withResolvers } from "../../utils/promise.js";
+import { useLastSettled } from "./useLastSettled.js";
+
+test("should be defined", () => {
+  expect(useLastSettled).toBeDefined();
+});
+
+test("should execute cb after resolved promise", async () => {
+  // ARRANGE
+  vi.useFakeTimers();
+  const spy = vi.fn();
+  const { queue, active } = useLastSettled(spy);
+  expect(active.value).toBe(false);
+
+  // ACT
+  const resolved = Promise.resolve("result");
+  queue(resolved);
+
+  // ASSERT
+  expect(active.value).toBe(true);
+  await vi.runAllTimersAsync();
+  expect(spy).toHaveBeenCalledExactlyOnceWith(true, "result");
+  expect(active.value).toBe(false);
+});
+
+test("should execute cb after rejected promise", async () => {
+  // ARRANGE
+  vi.useFakeTimers();
+  const spy = vi.fn();
+  const { queue, active } = useLastSettled(spy);
+
+  // ACT
+  const rejected = Promise.reject("rejected");
+  queue(rejected);
+
+  // ASSERT
+  expect(active.value).toBe(true);
+  await vi.runAllTimersAsync();
+  expect(spy).toHaveBeenCalledExactlyOnceWith(false);
+  expect(active.value).toBe(false);
+});
+
+test("should execute cb only after last queued promise", async () => {
+  // ARRANGE
+  vi.useFakeTimers();
+  const spy = vi.fn();
+  const { queue, active } = useLastSettled(spy);
+
+  const first = withResolvers<string>();
+  queue(first.promise);
+  const second = withResolvers<string>();
+  queue(second.promise);
+  const last = withResolvers<string>();
+  queue(last.promise);
+
+  // ACT
+  first.resolve("first");
+
+  // ASSERT
+  await vi.runAllTimersAsync();
+  expect(active.value).toBe(true);
+  expect(spy).not.toHaveBeenCalled();
+
+  // ACT
+  last.resolve("last");
+
+  // ASSERT
+  await vi.runAllTimersAsync();
+  expect(active.value).toBe(false);
+  expect(spy).toHaveBeenCalledExactlyOnceWith(true, "last");
+
+  // ACT
+  second.resolve("second");
+
+  // ASSERT
+  await vi.runAllTimersAsync();
+  expect(active.value).toBe(false);
+  expect(spy).toHaveBeenCalledExactlyOnceWith(true, "last");
+});
