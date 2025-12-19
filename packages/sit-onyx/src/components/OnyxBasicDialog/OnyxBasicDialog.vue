03eadd35c61c69fb1c0dc6f62ae02c3a256f10b7
ChristianBusshoff <christian.busshoff@mail.schwarz>
2025-12-19 12:01:56 +0100

    change

--- packages/sit-onyx/src/components/OnyxBasicDialog/OnyxBasicDialog.vue
+++ packages/sit-onyx/src/components/OnyxBasicDialog/OnyxBasicDialog.vue
@@ -10,6 +10,7 @@ const props = withDefaults(defineProps<OnyxBasicDialogProps>(), {
   alert: false,
   alignment: "center",
   nonDismissible: false,
+  backdrop: "light",
 });
 
 const emit = defineEmits<{
@@ -99,6 +100,7 @@ defineExpose({
       densityClass,
       'onyx-truncation-multiline',
       'onyx-basic-dialog',
+      `onyx-basic-dialog__backdrop--${props.backdrop}`,
       { [`onyx-basic-dialog--${props.alignment}`]: props.alignment !== 'center' },
     ]"
     :aria-modal="props.modal"
@@ -144,7 +146,10 @@ defineExpose({
     height: fit-content;
 
     &::backdrop {
-      background-color: var(--onyx-color-component-opacity-backdrop);
+      background-color: var(--onyx-color-component-opacity-backdrop-soft);
+    }
+    &__backdrop--intense::backdrop {
+      background-color: var(onyx-color-component-opacity-backdrop-medium);
     }
 
     &:modal {
