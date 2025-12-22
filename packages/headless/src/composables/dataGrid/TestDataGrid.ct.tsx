e20ea7072666484c4e0241483aaf1d4cd161f686
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2025-12-22 13:14:37 +0100

    add retries to focus expectation

--- packages/headless/src/composables/dataGrid/TestDataGrid.ct.tsx
+++ packages/headless/src/composables/dataGrid/TestDataGrid.ct.tsx
@@ -1,9 +1,19 @@
 import { test } from "@playwright/experimental-ct-vue";
-import TestDataGrid from "./TestDataGrid.vue";
 import { dataGridTesting } from "./createDataGrid.testing.js";
+import TestDataGrid from "./TestDataGrid.vue";
+import TestLazyDataGrid from "./TestLazyDataGrid.vue";
 
 test("data-grid", async ({ mount, page }) => {
   await mount(<TestDataGrid />);
+
+  const grid = page.getByRole("grid");
+  const rows = page.getByRole("row");
+
+  await dataGridTesting(page, grid, rows);
+});
+
+test("data-grid lazy", async ({ mount, page }) => {
+  await mount(<TestLazyDataGrid />);
 
   const grid = page.getByRole("grid");
   const rows = page.getByRole("row");
