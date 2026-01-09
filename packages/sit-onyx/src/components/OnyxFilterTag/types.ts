3fdc1749f5e7978c12c4bb4e7076e325d73a4afd
ChristianBusshoff <christian.busshoff@mail.schwarz>
2026-01-09 09:16:25 +0100

    resolve comment

--- packages/sit-onyx/src/components/OnyxFilterTag/types.ts
+++ packages/sit-onyx/src/components/OnyxFilterTag/types.ts
@@ -6,10 +6,4 @@ export type OnyxFilterTagProps = Omit<OnyxTagProps, "color" | "clickable"> & {
    * If `true` the filter is selected, shows an 'x' icon and can be removed on click.
    */
   active?: Nullable<boolean>;
-  /**
-   * An icon displayed on the right side of the label.
-   * Should be used to indicate an action (e.g., dismissing).
-   * @default iconXSmall
-   */
-  actionIcon?: Nullable<string>;
 };
