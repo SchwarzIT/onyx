11be26d35a97ad4d75ca7a55b1370eb3941ffe3c
ChristianBusshoff <christian.busshoff@mail.schwarz>
2026-01-09 09:16:25 +0100

    resolve comment

--- packages/sit-onyx/src/components/OnyxFilterTag/OnyxFilterTag.stories.ts
+++ packages/sit-onyx/src/components/OnyxFilterTag/OnyxFilterTag.stories.ts
@@ -12,7 +12,6 @@ const meta: Meta<typeof OnyxFilterTag> = {
   argTypes: {
     ...withNativeEventLogging(["onClick"]),
     icon: defineIconSelectArgType(),
-    actionIcon: defineIconSelectArgType(),
   },
 };
 
