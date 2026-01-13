9a745f176e26bbf9a218e0bbcc26d2f2c327b8f2
Lars Rickert <lars.rickert@mail.schwarz>
2026-01-13 13:32:57 +0100

    feat(nuxt-docs): add ProseBr component for accessible line breaks

---
+++ packages/nuxt-docs/app/components/content/ProseBr.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <br aria-hidden="true" class="br" />
+</template>
+
+<style lang="scss" scoped>
+br {
+  display: block;
+  height: 1lh;
+}
+</style>
