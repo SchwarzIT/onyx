f65d447cde020c1e92060dae83de06090ffe7a27
Lars Rickert <lars.rickert@mail.schwarz>
2026-01-30 08:47:06 +0100

    fix(demo-app): correctly apply density to whole app

--- apps/demo-app/app/app.vue
+++ apps/demo-app/app/app.vue
@@ -2,8 +2,16 @@
 import App from "#layers/blueprint/app/app.vue";
 
 const settingsStore = useSettingsStore();
+
+// we need to apply the CSS class to the `<html>` element so the density is correctly defined for all components
+// otherwise (if we would e.g. just bind it to the app below), global styles (e.g. the vertical grid margin etc. would not consider the corresponding density)
+useHead({
+  htmlAttrs: {
+    class: computed(() => `onyx-density-${settingsStore.settings.density}`),
+  },
+});
 </script>
 
 <template>
-  <App :class="`onyx-density-${settingsStore.settings.density}`" />
+  <App />
 </template>
