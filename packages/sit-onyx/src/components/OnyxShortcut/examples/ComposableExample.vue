62399f4f3778aaac1e8d211aa04c928a16d80d78
Lars Rickert <lars.rickert@mail.schwarz>
2026-02-10 16:37:42 +0100

    add composable example

---
+++ packages/sit-onyx/src/components/OnyxShortcut/examples/ComposableExample.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { _unstableUseShortcut } from "../../../index.js";
+
+_unstableUseShortcut({
+  sequence: [{ all: ["Control", "C"] }],
+  onComplete: () => {
+    alert("Shortcut successfully pressed!");
+  },
+});
+</script>
+
+<template>
+  <p class="description">
+    This example uses the `_unstableUseShortcut()` composable. Press "Control + C" to trigger the
+    shortcut.
+  </p>
+</template>
+
+<style lang="scss" scoped>
+.description {
+  font-family: var(--onyx-font-family-paragraph);
+  color: var(--onyx-color-text-icons-info-intense);
+}
+</style>
