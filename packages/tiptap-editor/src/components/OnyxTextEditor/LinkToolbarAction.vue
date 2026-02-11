c14278517aae8eb2fe7c1d0be61c34b8b190112e
Lars Rickert <lars.rickert@mail.schwarz>
2026-02-11 18:25:43 +0100

    update link dialog

--- packages/tiptap-editor/src/components/OnyxTextEditor/LinkToolbarAction.vue
+++ packages/tiptap-editor/src/components/OnyxTextEditor/LinkToolbarAction.vue
@@ -1,7 +1,15 @@
 <script lang="ts" setup>
 import { iconCheckSmall, iconLink } from "@sit-onyx/icons";
 import type { Editor } from "@tiptap/vue-3";
-import { injectI18n, OnyxBottomBar, OnyxButton, OnyxDialog, OnyxForm, OnyxInput } from "sit-onyx";
+import {
+  injectI18n,
+  OnyxBottomBar,
+  OnyxButton,
+  OnyxDialog,
+  OnyxForm,
+  OnyxIcon,
+  OnyxInput,
+} from "sit-onyx";
 import { ref, useId, watch } from "vue";
 import OnyxEditorToolbarAction from "../OnyxEditorToolbarAction/OnyxEditorToolbarAction.vue";
 
@@ -13,15 +21,16 @@ const { t } = injectI18n();
 const id = useId();
 const open = ref(false);
 
-const state = ref({ label: "", href: "" });
+const initialHref = "https://";
+
+const state = ref({ href: initialHref, text: "" });
 
 watch(
   open,
   (isOpen) => {
     if (!isOpen) {
       // reset state if dialog closes
-      state.value = { label: "", href: "" };
-      // TODO: reset form validation
+      state.value = { href: initialHref, text: "" };
       return;
     }
 
@@ -29,38 +38,65 @@ watch(
     const previousHref = href && typeof href === "string" ? href : undefined;
     if (previousHref) state.value.href = previousHref;
 
-    // TODO: get select text and store it in the state
+    if (props.editor) {
+      const { from, to, empty } = props.editor.state.selection;
+      const text = empty ? "" : props.editor.state.doc.textBetween(from, to);
+      state.value.text = text;
+    }
   },
   { immediate: true },
 );
 
 const handleSubmit = () => {
   const href = state.value.href.trim();
+  const text = state.value.text.trim();
   if (!href) return removeLink();
-  // TODO: add target
-  props.editor?.chain().focus().extendMarkRange("link").setLink({ href }).run();
+
+  props.editor
+    ?.chain()
+    .focus()
+    .extendMarkRange("link")
+    .insertContent([
+      {
+        type: "text",
+        text,
+        marks: [{ type: "link", attrs: { href } }],
+      },
+    ])
+    .run();
+
+  // configure editor to not include newly written text in the same link
+  props.editor?.chain().setTextSelection(props.editor.state.selection.to).unsetMark("link").run();
+  open.value = false;
 };
 
 const removeLink = () => {
   props.editor?.chain().focus().extendMarkRange("link").unsetLink().run();
+  open.value = false;
 };
 </script>
 
 <template>
-  <OnyxDialog v-model:open="open" class="dialog" label="Edit link">
+  <OnyxDialog v-model:open="open" class="dialog" :label="t('editor.link.edit')" density="compact">
     <template #trigger="{ trigger }">
       <OnyxEditorToolbarAction
         v-bind="trigger"
-        label="Edit link"
+        :label="t('editor.link.edit')"
         :icon="iconLink"
         :active="props.editor?.isActive('link')"
         :disabled="!props.editor?.can().chain().toggleLink().run()"
       />
     </template>
 
-    <OnyxForm :id class="dialog__content" @submit.prevent="handleSubmit">
-      <OnyxInput v-model="state.label" label="Displayed text" required />
-      <OnyxInput v-model="state.href" label="Linked URL" type="url" required />
+    <!-- using v-if here so the form validation is reset when the dialog closes -->
+    <OnyxForm v-if="open" :id class="dialog__content" @submit.prevent="handleSubmit">
+      <OnyxInput v-model="state.text" :label="t('editor.link.text')" required />
+
+      <OnyxInput v-model="state.href" :label="t('editor.link.link')" type="url" autofocus>
+        <template #leading>
+          <OnyxIcon :icon="iconLink" />
+        </template>
+      </OnyxInput>
     </OnyxForm>
 
     <template #footer>
