d694597b2e2414c280323a4f22c590c545ced779
Lars Rickert <lars.rickert@mail.schwarz>
2026-02-17 12:41:03 +0100

    fix typo

--- packages/nuxt/src/runtime/locales/hr-HR.js
+++ packages/nuxt/src/runtime/locales/hr-HR.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineI18nLocale } from "#i18n";
-import hrHr from "sit-onyx/locales/hr-HR.json";
+import hrHR from "sit-onyx/locales/hr-HR.json";
 
 export default defineI18nLocale(() => {
-  return { onyx: hrHr };
+  return { onyx: hrHR };
 });
