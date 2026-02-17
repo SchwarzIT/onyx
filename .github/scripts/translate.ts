fe593954dcd43554f7316fead35f19a9e0acb8fb
Lars Rickert <lars.rickert@mail.schwarz>
2026-02-17 07:41:33 +0100

    update translation script

--- .github/scripts/translate.ts
+++ .github/scripts/translate.ts
@@ -1,21 +1,36 @@
+import { readdir, readFile, writeFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
 const config = {
   api: {
     url: "https://live.api.schwarz/sit/assortment-generative-ai/assortment-generative-ai-api/v1/api/external/generate",
+    templateID: 262,
   },
   myApi: {
     tokenUrl: "https://live.api.schwarz/oauth/accesstoken/v3",
-    clientId: process.env.MY_API_CLIENT_ID ?? "",
-    clientSecret: process.env.MY_API_CLIENT_SECRET ?? "",
+    clientId: process.env.MY_API_CLIENT_ID || "",
+    clientSecret: process.env.MY_API_CLIENT_SECRET || "",
   },
+  branch: process.env.GITHUB_REF_NAME || "main",
 };
 
-const translations = await translate();
-console.log(translations);
+console.log("Determining existing onyx locales...");
+
+const locales = await getOnyxLocales();
+
+console.log(`Translating locales: ${locales.join(", ")}...`);
+const translations = await translate(locales);
+console.log("Generated translations:", translations);
+
+console.log("Writing new translations...");
+await writeTranslations(translations);
+
+console.log("Done.");
 
 //
 // UTILITY FUNCTIONS
 //
-async function translate() {
+async function translate(locales: string[]) {
   const accessToken = await getMyAPIAccessToken();
 
   const response = await fetch(config.api.url, {
@@ -25,15 +40,11 @@ async function translate() {
       Authorization: `Bearer ${accessToken}`,
     },
     body: JSON.stringify({
-      templateId: 258,
+      templateId: config.api.templateID,
       variables: [
-        {
-          variableName: "outputLanguages",
-          // TODO: replace with actual supported languages
-          value: "Spanish",
-        },
+        { variableName: "locales", value: locales.join(", ") },
+        { variableName: "branch", value: config.branch },
       ],
-      linksToAttachments: [],
     }),
   });
 
@@ -41,10 +52,30 @@ async function translate() {
     throw new Error(`Failed to translate: ${await response.text()}`);
   }
 
-  const data: Record<string, object> = await response.json();
-  return data;
+  const data: { result: string } = await response.json();
+  return JSON.parse(data.result) as Record<string, TranslationJson>;
 }
 
+async function writeTranslations(translations: Record<string, TranslationJson>) {
+  await Promise.allSettled(
+    Object.entries(translations).map(async ([locale, translation]) => {
+      try {
+        const localePath = getFilePath(`../../packages/sit-onyx/src/i18n/locales/${locale}.json`);
+        const existingTranslations: TranslationJson = JSON.parse(
+          await readFile(localePath, "utf-8"),
+        );
+        const newTranslations = mergeTranslations(existingTranslations, translation);
+        await writeFile(localePath, JSON.stringify(newTranslations, null, 2));
+      } catch (e) {
+        console.error(`File write failed for ${locale}`, e);
+      }
+    }),
+  );
+}
+
+/**
+ * Gets a new MyAPI access token.
+ */
 async function getMyAPIAccessToken() {
   const response = await fetch(config.myApi.tokenUrl, {
     method: "POST",
@@ -65,5 +96,62 @@ async function getMyAPIAccessToken() {
   const data: { access_token: string } = await response.json();
   return data.access_token;
 }
+
+/**
+ * Gets a list of all available onyx locales.
+ *
+ * @example ["de-DE", "en-US"]
+ */
+async function getOnyxLocales() {
+  const localePath = getFilePath("../../packages/sit-onyx/src/i18n/locales");
+  const files = await readdir(localePath, { encoding: "utf-8" });
+  return files
+    .map((file) => file.replace(".json", ""))
+    .filter((locale) => !["en-US", "de-DE"].includes(locale));
+}
+
+/**
+ * Merges the new translations from "updates" into "base".
+ */
+function mergeTranslations(base: TranslationJson, update: TranslationJson) {
+  // create a copy of the base to avoid mutation
+  const output = structuredClone(base);
+
+  // iterate over keys in the update object
+  Object.keys(update).forEach((key) => {
+    const baseValue = output[key];
+    const updateValue = update[key];
+
+    // check if both values are objects; if so, recurse
+    if (isObject(baseValue) && isObject(updateValue)) {
+      output[key] = mergeTranslations(baseValue, updateValue);
+    } else {
+      // otherwise, overwrite the value (handles string updates or type changes)
+      output[key] = updateValue;
+    }
+  });
+
+  return output;
+}
+
+/**
+ * Checks whether the given data is an object.
+ */
+function isObject(item: unknown): item is Record<string, unknown> {
+  return !!item && typeof item === "object" && !Array.isArray(item);
+}
+
+/**
+ * Gets the given path while ensuring cross-platform and correct decoding.
+ */
+function getFilePath(path: string) {
+  return fileURLToPath(new URL(path, import.meta.url));
+}
+
+type TranslationValue = string | TranslationJson;
+
+type TranslationJson = {
+  [key: string]: TranslationValue;
+};
 
 export {};
