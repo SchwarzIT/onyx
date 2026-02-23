b7253881f5fd0a6f94da886cb8aeaab8d71c19c4
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2026-02-23 13:45:53 +0100

    apply review findings and change API for OnyxSplitButton

--- .changeset/fuzzy-facts-occur.md
+++ .changeset/fuzzy-facts-occur.md
@@ -2,4 +2,6 @@
 "sit-onyx": minor
 ---
 
-feat: implemented OnyxSplitButton component
+feat: implemented new `OnyxUnstableSplitButton` component
+
+For now, the component is marked as experimental/unstable which means that it is still under active development and the API might change in patch or minor releases. Keep an eye on the **[**changelog**]**(**https://onyx.schwarz/development/packages/changelogs/sit-onyx.html**) when using it.
--- packages/sit-onyx/src/components/OnyxSplitButton/types.ts
+++ packages/sit-onyx/src/components/OnyxSplitButton/types.ts
@@ -1,17 +1,5 @@
 import type { OnyxButtonProps } from "../OnyxButton/types.js";
-import type { FormInjected } from "../OnyxForm/OnyxForm.core.js";
+import type { OnyxFlyoutMenuProps } from "../OnyxNavBar/modules/OnyxFlyoutMenu/types.js";
 
-export type OnyxSplitButtonProps = Omit<
-  OnyxButtonProps,
-  "label" | "icon" | "iconPosition" | "link" | "type"
-> & {
-  splitButtonOptions: splitButtonOption[];
-  disabled?: FormInjected<boolean>;
-};
-type splitButtonOption = {
-  label: string;
-  icon?: string;
-  onClickFunction: () => void;
-} & Pick<OnyxButtonProps, "link" | "type">;
-//* Die erste Option ([0]) dient als permanenter Hauptbutton.
-//* Alle weiteren Optionen werden ausschließlich als Klick-Aktionen im Dropdown gelistet.
+export type OnyxSplitButtonProps = Omit<OnyxButtonProps, "iconPosition" | "type"> &
+  Pick<OnyxFlyoutMenuProps, "trigger" | "open">;
