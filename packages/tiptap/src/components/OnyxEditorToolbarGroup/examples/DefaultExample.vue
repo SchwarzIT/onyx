66ae8f7c2a54143bc27308aba9cadcdce6a812fe
Lars Rickert <lars.rickert@mail.schwarz>
2026-02-24 14:38:45 +0100

    update documentation

---
+++ packages/tiptap/src/components/OnyxEditorToolbarGroup/examples/DefaultExample.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { iconPlaceholder } from "@sit-onyx/icons";
+import { OnyxEditorToolbarAction, OnyxEditorToolbarGroup } from "../../../index.js";
+</script>
+
+<template>
+  <div class="actions">
+    <OnyxEditorToolbarGroup>
+      <OnyxEditorToolbarAction label="Action 1" :icon="iconPlaceholder" />
+      <OnyxEditorToolbarAction label="Action 2" :icon="iconPlaceholder" />
+    </OnyxEditorToolbarGroup>
+
+    <OnyxEditorToolbarGroup>
+      <OnyxEditorToolbarAction label="Action 3" :icon="iconPlaceholder" />
+      <OnyxEditorToolbarAction label="Action 4" :icon="iconPlaceholder" />
+    </OnyxEditorToolbarGroup>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.actions {
+  display: flex;
+  align-items: center;
+  gap: var(--onyx-density-xs);
+}
+</style>
