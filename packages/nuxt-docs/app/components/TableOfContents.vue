9a0c7fd0a6a21fe22744b5041c074ba5fcb30a3c
Lars Rickert <lars.rickert@mail.schwarz>
2026-02-26 11:07:47 +0100

    feat(nuxt-docs): implement TOC for content pages

---
+++ packages/nuxt-docs/app/components/TableOfContents.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import type { TocLink } from "@nuxtjs/mdc";
+
+const props = defineProps<{
+  links: TocLink[];
+}>();
+</script>
+
+<template>
+  <OnyxUnstableTableOfContents>
+    <OnyxUnstableTableOfContentsItem
+      v-for="link in props.links"
+      :key="link.id"
+      :link="`#${link.id}`"
+    >
+      {{ link.text }}
+
+      <template v-if="link.children?.length" #children>
+        <OnyxUnstableTableOfContentsItem
+          v-for="child in link.children"
+          :key="child.id"
+          :link="`#${child.id}`"
+        >
+          {{ child.text }}
+        </OnyxUnstableTableOfContentsItem>
+      </template>
+    </OnyxUnstableTableOfContentsItem>
+  </OnyxUnstableTableOfContents>
+</template>
--- packages/nuxt-docs/app/pages/[...slug].vue
+++ packages/nuxt-docs/app/pages/[...slug].vue
@@ -42,5 +42,41 @@ useSeoMeta({
 
 <!-- eslint-disable-next-line vue/no-root-v-if  -- the v-if here is theoretically not needed because we throw above it ifs undefined so the user will be redirected to the error page. However, there is still a console warning so we include the v-if here to prevent it. -->
 <template>
-  <ContentRenderer v-if="data" :value="data" />
+  <div class="content">
+    <ContentRenderer v-if="data" :value="data" />
+
+    <TableOfContents
+      v-if="data?.body.toc?.links.length"
+      class="content__toc"
+      :links="data.body.toc.links"
+    />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@use "sit-onyx/breakpoints.scss";
+
+.content {
+  --onyx-content-toc-gap: calc(2 * var(--onyx-grid-gutter) + (100 / var(--onyx-grid-columns)) * 1%);
+  display: grid;
+  gap: var(--onyx-content-toc-gap);
+
+  // see: https://storybook.onyx.schwarz/?path=/docs/navigation-tableofcontents--docs
+  grid-template-columns: 1fr minmax(8rem, 15rem);
+
+  &__toc {
+    position: sticky;
+    top: var(--onyx-grid-margin-vertical);
+    height: calc(100vh - 3 * var(--onyx-grid-margin-vertical));
+  }
+
+  // hide TOC on smaller screens
+  @include breakpoints.container(max, md) {
+    grid-template-columns: 1fr;
+
+    .content__toc {
+      display: none;
+    }
+  }
+}
+</style>
