a2e8f8264c8b2e419ef3c631e1bad54f025a90f6
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-02 10:36:38 +0100

    docs(i18n): remove translated percentage

--- apps/docs/src/development/languages.data.ts
+++ apps/docs/src/development/languages.data.ts
@@ -11,8 +11,6 @@ export type Language = {
   name: string;
   /** JSON content of the translations / messages */
   fileContent: object;
-  /** Percentage of translated keys */
-  percentage: number;
 };
 
 declare const data: Data;
@@ -25,34 +23,12 @@ export { data };
 export default defineLoader({
   watch: ["../../../../packages/sit-onyx/src/i18n/locales/*.json"],
   async load(watchedFiles): Promise<Data> {
-    const enUS = (await import("sit-onyx/locales/en-US.json", { with: { type: "json" } })).default;
-    const enKeys = getNestedKeys(enUS);
-
     const languages = watchedFiles.map((path) => {
       const fileContent = JSON.parse(readFileSync(path, "utf-8"));
       const name = path.split("/").at(-1)!.replace(".json", "");
-      const keys = getNestedKeys(fileContent);
-      const percentage = Math.floor((keys.length / enKeys.length) * 100);
-      return { name, fileContent, percentage };
+      return { name, fileContent };
     });
 
     return { languages };
   },
 });
-
-/**
- * Extracts all keys of the given object as array.
- */
-const getNestedKeys = (obj: object, prefix = ""): string[] => {
-  return Object.keys(obj).reduce((res, el) => {
-    const value = obj[el as keyof typeof obj];
-    const path = prefix ? `${prefix}.${el}` : el;
-
-    if (typeof value === "object" && value && !Array.isArray(value)) {
-      // Add the current path and then recurse
-      return [...res, path, ...getNestedKeys(value, path)];
-    }
-
-    return [...res, path];
-  }, [] as string[]);
-};
