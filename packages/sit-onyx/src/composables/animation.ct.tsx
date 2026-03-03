700ee7fb7dd908b50ce847c8426396a1ae8135e2
Jonathan Carle <jonathan_leo.carle@mail.schwarz>
2026-03-03 13:08:28 +0100

    fix skeleton test flakiness

--- packages/sit-onyx/src/composables/animation.ct.tsx
+++ packages/sit-onyx/src/composables/animation.ct.tsx
@@ -1,17 +1,27 @@
+import type { Locator } from "@playwright/test";
 import { expect, test } from "../playwright/a11y.js";
 import TestCase from "./AnimationTestCase.vue";
+
+const getAnimationStart = (locator: Locator) =>
+  locator.evaluate((e) => Number(e.getAnimations().at(0)?.startTime));
 
 test("should sync animation start time", async ({ mount, page }) => {
   // ARRANGE
   await mount(<TestCase />);
   await page.getByTestId("delayed").waitFor({ state: "visible" });
 
-  const [immediate, delayed] = await page.evaluate(() =>
-    document.getAnimations().map((a) => Number(a.startTime)),
-  );
+  const immediateSkeleton = page.getByTestId("immediate");
+  const delayedSkeleton = page.getByTestId("delayed");
 
-  expect(immediate).toBeDefined();
-  expect(delayed).toBeDefined();
-  expect(immediate).toEqual(delayed);
-  expect(delayed).toBe(0);
+  await expect(
+    async () => {
+      const immediate = await getAnimationStart(immediateSkeleton);
+      const delayed = await getAnimationStart(delayedSkeleton);
+      expect(immediate).toBeDefined();
+      expect(delayed).toBeDefined();
+      expect(immediate).toEqual(delayed);
+      expect(delayed).toBe(0);
+    },
+    { message: "animation should synced eventually (uses requestIdleCallback)" },
+  ).toPass();
 });
