6a8d0ae9a9bc7990dc7760aa8a0ec4b538013fd4
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-13 11:29:53 +0100

    update unstable deprecation warnings

--- packages/nuxt-docs/app/components/TableOfContents.vue
+++ packages/nuxt-docs/app/components/TableOfContents.vue
@@ -1,3 +1,11 @@
+<script lang="ts">
+/**
+ * @experimental
+ * @deprecated This component is still under active development and its API might change in patch releases.
+ */
+export default {};
+</script>
+
 <script lang="ts" setup>
 import type { TocLink } from "@nuxtjs/mdc";
 
