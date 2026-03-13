dd554c59a730721e0c45b2315f51765d5a40322d
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-13 11:01:36 +0100

    fix tests

--- packages/sit-onyx/src/components/OnyxFormElement/OnyxFormElement.ct-utils.ts
+++ packages/sit-onyx/src/components/OnyxFormElement/OnyxFormElement.ct-utils.ts
@@ -7,22 +7,33 @@ type FormElementTooltip = "label" | "message";
 
 export const createFormElementUtils = function (page: PageOrLocator) {
   const context = {
-    getRoot: (prev: PageOrLocator = page) => prev.locator(".onyx-form-element"),
-    getTooltipTrigger: (type: FormElementTooltip, prev: PageOrLocator = page) =>
-      context
-        .getRoot(prev)
-        .locator(
+    getRoot: (prev: PageOrLocator = page) =>
+      prev.locator(".onyx-form-element,.onyx-form-element-v2"),
+    getTooltipTrigger: (type: FormElementTooltip, prev: PageOrLocator = page) => {
+      // support both OnyxFormElement and OnyxFormElementV2
+      const locators = {
+        v1:
           type === "label"
             ? `.onyx-form-element__${type}-tooltip figure`
             : `.onyx-form-${type}__tooltip`,
-        ),
-    getTooltipPopover: (type: FormElementTooltip, prev: PageOrLocator = page) =>
-      context
-        .getRoot(prev)
-        .locator(
+        v2: `.onyx-form-element-v2__${type} .onyx-form-element-v2__tooltip figure`,
+      };
+
+      return context.getRoot(prev).locator(`${locators.v1},${locators.v2}`);
+    },
+    getTooltipPopover: (type: FormElementTooltip, prev: PageOrLocator = page) => {
+      // support both OnyxFormElement and OnyxFormElementV2
+      const locators = {
+        v1:
           type === "label" ? `.onyx-form-element__${type}-tooltip` : `.onyx-form-${type}__tooltip`,
-        )
-        .getByRole("tooltip", { includeHidden: true }),
+        v2: `.onyx-form-element-v2__${type} .onyx-form-element-v2__tooltip`,
+      };
+
+      return context
+        .getRoot(prev)
+        .locator(`${locators.v1},${locators.v2}`)
+        .getByRole("tooltip", { includeHidden: true });
+    },
     triggerTooltipVisible: async (type: FormElementTooltip, prev: PageOrLocator = page) => {
       await context.getTooltipTrigger(type, prev).first().hover();
       await expect(context.getTooltipPopover(type, prev).first()).toBeVisible();
@@ -30,3 +41,5 @@ export const createFormElementUtils = function (page: PageOrLocator) {
   };
   return context;
 };
+
+// .onyx-form-element-v2__${type} .onyx-form-element-v2__tooltip figure
