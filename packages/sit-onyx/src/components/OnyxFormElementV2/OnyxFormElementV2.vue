c872b829ee4f94efa3e2f37c4ee2ed6a2e713776
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-16 11:55:35 +0100

    ensure skeleton gap is only applied when skeleton is active

--- packages/sit-onyx/src/components/OnyxFormElementV2/OnyxFormElementV2.vue
+++ packages/sit-onyx/src/components/OnyxFormElementV2/OnyxFormElementV2.vue
@@ -230,10 +230,12 @@ const popoverLayoutProps = useForwardProps(props, MaybePopoverLayout);
     }
   }
 
+  // the skeleton gap would be 0 in compact density so we shrink the label size a bit and increase the gap so it does not look off
   @include density.compact {
-    // the skeleton gap would be 0 in compact density so we shrink the label size a bit and increase the gap so it does not look off
-    --onyx-form-element-v2-gap: var(--onyx-spacing-5xs);
-    --onyx-form-element-v2-label-skeleton-height: calc(1lh - var(--onyx-form-element-v2-gap));
+    &:has(.onyx-form-element-v2__content-skeleton) {
+      --onyx-form-element-v2-gap: var(--onyx-spacing-5xs);
+      --onyx-form-element-v2-label-skeleton-height: calc(1lh - var(--onyx-form-element-v2-gap));
+    }
   }
 }
 
