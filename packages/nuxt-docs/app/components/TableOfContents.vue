f124803244b179e1dbf3364ba73051ff876608a3
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-18 09:02:53 +0100

    remove unintentional deprecation

--- packages/nuxt-docs/app/components/TableOfContents.vue
+++ packages/nuxt-docs/app/components/TableOfContents.vue
@@ -1,11 +1,3 @@
-<script lang="ts">
-/**
- * @experimental
- * @deprecated This component is still under active development and its API might change in patch releases.
- */
-export default {};
-</script>
-
 <script lang="ts" setup>
 import type { TocLink } from "@nuxtjs/mdc";
 
