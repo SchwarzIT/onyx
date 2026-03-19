b1912d236b5fd7c1acbfc44a651180e539e3534d
Lars Rickert <lars.rickert@mail.schwarz>
2026-03-19 13:12:31 +0100

    fix(OnyxAlertModal): correctly hide close button when `nonDismissible` is set (#5094)
    closes #5092
    
    Hide close button for `OnyxAlertModal` when `nonDismissible` property is
    set. Also add tests for OnyxAlertDialog, OnyxModal and OnyxDialog to
    ensure this behaviour.
    
    ## Checklist
    
    - [x] A changeset is added with `npx changeset add` if your changes
    should be released as npm package (because they affect the library
    usage)
    - [x] I have performed a self review of my code ("Files changed" tab in
    the pull request)

--- packages/sit-onyx/src/components/OnyxDialog/TestWrapper.ct.vue
+++ packages/sit-onyx/src/components/OnyxDialog/TestWrapper.ct.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup>
 import { ref } from "vue";
+import { useForwardProps } from "../../utils/props.js";
 import OnyxButton from "../OnyxButton/OnyxButton.vue";
 import OnyxDialog from "./OnyxDialog.vue";
+import type { OnyxDialogProps } from "./types.js";
+
+const props = defineProps<Pick<OnyxDialogProps, "nonDismissible">>();
 
 defineSlots<{
   default?(): unknown;
@@ -12,11 +16,15 @@ defineSlots<{
 const emit = defineEmits<{
   "update:open": [open: boolean];
 }>();
+
 const isOpen = ref(true);
+
+const dialogProps = useForwardProps(props, OnyxDialog);
 </script>
 
 <template>
   <OnyxDialog
+    v-bind="dialogProps"
     v-model:open="isOpen"
     label="Example Dialog"
     @update:open="emit('update:open', $event)"
