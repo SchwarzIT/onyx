d2d6d87d66297ce4a9313c1c1526ca39babfbad1
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-20 10:41:13 +0100

    refactor(OnyxSelect): use `OnyxFormElementV2` internally

--- packages/sit-onyx/src/index.ts
+++ packages/sit-onyx/src/index.ts
@@ -203,6 +203,7 @@ export * from "./components/OnyxItemsPerPage/types.js";
 export { default as OnyxSelectDialog } from "./components/OnyxSelectDialog/OnyxSelectDialog.vue";
 export * from "./components/OnyxSelectDialog/types.js";
 
+// TODO: deprecate or delete
 export { default as OnyxSelectInput } from "./components/OnyxSelectInput/OnyxSelectInput.vue";
 export * from "./components/OnyxSelectInput/types.js";
 
