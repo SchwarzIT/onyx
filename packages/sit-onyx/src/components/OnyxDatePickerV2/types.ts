aa9da6b8b53f006ed7f486b8373920033caab314
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-26 12:59:48 +0100

    fix(OnyxDatePickerV2): remove non-existing properties `hideLabel` and `labelTooltip`

--- packages/sit-onyx/src/components/OnyxDatePickerV2/types.ts
+++ packages/sit-onyx/src/components/OnyxDatePickerV2/types.ts
@@ -2,37 +2,21 @@ import type { AutofocusProp } from "../../types/components.js";
 import type { OnyxCalendarProps, OnyxCalendarSelectionMode } from "../OnyxCalendar/types.js";
 import type { SharedFormElementProps } from "../OnyxFormElement/types.js";
 import type { OnyxFormElementV2Props } from "../OnyxFormElementV2/types.js";
-import type { OnyxInputProps } from "../OnyxInput/types.js";
 
-export type OnyxDatePickerV2Props<TSelection extends OnyxCalendarSelectionMode = "single"> = Omit<
-  SharedFormElementProps,
-  "label"
-> &
-  Pick<OnyxFormElementV2Props, "label" | "open" | "popoverOptions"> &
-  Pick<OnyxInputProps, "name" | "placeholder" | "readonly" | "disabled"> &
-  Pick<
-    OnyxCalendarProps<TSelection>,
-    | "min"
-    | "max"
-    | "weekStartDay"
-    | "showCalendarWeeks"
-    | "selectionMode"
-    | "modelValue"
-    | "viewMonth"
-  > &
-  AutofocusProp & {
-    /**
-     * Whether to show two calendars in range mode.
-     */
-    multiView?: TSelection extends "range" ? boolean : never;
-    /**
-     * Disable specific dates to select individually.
-     *
-     * @example
-     * ```ts
-     * // Disables only weekends (Saturday and Sunday)
-     * { disabled: (date: Date) => date.getDay() === 0 || date.getDay() === 6 }
-     * ```
-     */
-    disabledDays?: (date: Date) => boolean;
-  };
+export type OnyxDatePickerV2Props<TSelection extends OnyxCalendarSelectionMode = "single"> =
+  OnyxFormElementV2Props &
+    Pick<SharedFormElementProps, "name" | "placeholder" | "readonly" | "disabled"> &
+    AutofocusProp &
+    Omit<OnyxCalendarProps<TSelection>, "size" | "disabled"> & {
+      /**
+       * Whether to show two calendars in range mode.
+       */
+      multiView?: TSelection extends "range" ? boolean : never;
+      /**
+       * Disable specific dates to select individually.
+       *
+       * @example
+       * `(date: Date) => date.getDay() === 0 || date.getDay() === 6`
+       */
+      disabledDays?: (date: Date) => boolean;
+    };
