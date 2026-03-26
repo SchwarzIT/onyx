f45d5896129fcf1e53c6cd2c62f266a4cee91c58
Lars Rickert <lars.rickert@digits.schwarz>
2026-03-26 14:17:01 +0100

    feat(OnyxDatePickerV2): update implementation

--- packages/sit-onyx/src/components/OnyxDatePickerV2/types.ts
+++ packages/sit-onyx/src/components/OnyxDatePickerV2/types.ts
@@ -2,12 +2,14 @@ import type { AutofocusProp } from "../../types/components.js";
 import type { OnyxCalendarProps, OnyxCalendarSelectionMode } from "../OnyxCalendar/types.js";
 import type { SharedFormElementProps } from "../OnyxFormElement/types.js";
 import type { OnyxFormElementV2Props } from "../OnyxFormElementV2/types.js";
+import type { OnyxInputProps } from "../OnyxInput/types.js";
 
 export type OnyxDatePickerV2Props<TSelection extends OnyxCalendarSelectionMode = "single"> =
   OnyxFormElementV2Props &
     Pick<SharedFormElementProps, "name" | "placeholder" | "readonly" | "disabled"> &
     AutofocusProp &
-    Omit<OnyxCalendarProps<TSelection>, "size" | "disabled"> & {
+    Omit<OnyxCalendarProps<TSelection>, "size" | "disabled"> &
+    Pick<OnyxInputProps, "hideClearIcon"> & {
       /**
        * Whether to show two calendars in range mode.
        */
