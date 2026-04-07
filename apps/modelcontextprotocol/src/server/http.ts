612024d40a8f5f6a2c0e9c6b85e0758f9084ca83
Jonathan Carle <jonathan_leo.carle@digits.schwarz>
2026-04-07 15:01:33 +0200

    implement cli support for stdio and http mcp server

---
+++ apps/modelcontextprotocol/src/server/http.ts
@@ -0,0 +1,21 @@
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { randomUUID } from "node:crypto";
+import { createServer } from "node:http";
+import { server } from "./server.js";
+
+export const run = async () => {
+  const transport = new StreamableHTTPServerTransport({
+    sessionIdGenerator: () => randomUUID(),
+  });
+
+  await server.connect(transport);
+
+  const httpServer = createServer(transport.handleRequest);
+
+  const PORT = Number.parseInt(process.env["PORT"] ?? "3000");
+  const HOST = process.env["HOST"] ?? "0.0.0.0";
+
+  httpServer.listen(PORT, HOST);
+
+  console.error(`MCP http server listening on ${HOST}:${PORT}.`);
+};
---
+++ apps/modelcontextprotocol/src/server/server.ts
@@ -0,0 +1,18 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import packageJson from "../../package.json" with { type: "json" };
+import { getComponentApi } from "../resources/get-component-api.js";
+import { listComponents } from "../resources/list-components.js";
+
+const { name, version, description } = packageJson;
+
+const server = new McpServer({
+  name,
+  version,
+  description,
+  websiteUrl: "https://onyx.schwarz",
+});
+
+server.registerResource(...listComponents);
+server.registerResource(...getComponentApi);
+
+export { server };
