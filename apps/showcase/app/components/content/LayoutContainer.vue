8aaf95c32c011b44e9ef8c64812b4082ba617eaa
Lars Rickert <lars.rickert@digits.schwarz>
2026-04-20 12:44:37 +0200

    docs(showcase): update `density` page

---
+++ apps/showcase/app/components/content/LayoutContainer.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+defineSlots<{
+  /**
+   * Elements to place inside the layout.
+   */
+  default(): unknown;
+}>();
+</script>
+
+<template>
+  <div class="layout">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "sit-onyx/breakpoints.scss";
+
+.layout {
+  display: grid;
+  gap: var(--onyx-density-lg);
+  grid-template-columns: 1fr 1fr;
+  margin-block: var(--onyx-density-lg);
+
+  @include breakpoints.container(max, lg) {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
