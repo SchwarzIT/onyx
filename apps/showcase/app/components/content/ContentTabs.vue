8fc822c20833f2fe177b33bbd6ef633029b3497f
Lars Rickert <lars.rickert@digits.schwarz>
2026-04-21 16:33:08 +0200

    apply PR feedback

--- apps/showcase/app/components/content/ContentTabs.vue
+++ apps/showcase/app/components/content/ContentTabs.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { createTabs } from "@sit-onyx/headless";
 import { iconCode, iconToolColorFill } from "@sit-onyx/icons";
 import type { SelectOption } from "sit-onyx";
 import nuxtIcon from "~/assets/images/nuxt.svg?raw";
@@ -13,13 +14,7 @@ const slots = defineSlots<{
 const { t } = useI18n();
 
 const availableTabs = computed(() => Object.keys(slots) as ContentTabValue[]);
-
-const selectedOption = ref<ContentTabValue>(availableTabs.value[0] ?? "dev");
-
-const handleChange = (event: Event) => {
-  const target = event.target as HTMLInputElement;
-  selectedOption.value = target.value as typeof selectedOption.value;
-};
+const selectedTab = ref<ContentTabValue>(availableTabs.value[0] ?? "dev");
 
 const options = computed(() => {
   const tabs = [
@@ -33,36 +28,31 @@ const options = computed(() => {
     .filter((option) => availableTabs.value.includes(option.value))
     .sort((a, b) => availableTabs.value.indexOf(a.value) - availableTabs.value.indexOf(b.value));
 });
+
+const { elements } = createTabs({
+  label: computed(() => ""),
+  selectedTab,
+  onSelect: (newValue) => (selectedTab.value = newValue),
+});
 </script>
 
-<!-- eslint-disable vue/require-explicit-slots -- false positive, slots are defined by mapped type -->
 <template>
   <div>
-    <fieldset class="switch" @change="handleChange">
-      <label v-for="option in options" :key="option.value" class="switch__option">
-        <OnyxCard class="switch__card">
-          <OnyxIcon v-if="option.icon" :icon="option.icon" class="switch__icon" />
-
-          <div>
-            <OnyxVisuallyHidden is="div">
-              <input
-                type="radio"
-                name="role"
-                :value="option.value"
-                :checked="selectedOption === option.value"
-                :aria-label="option.label"
-                required
-              />
-            </OnyxVisuallyHidden>
-
-            <span class="switch__label"> {{ option.label }} </span>
-          </div>
-        </OnyxCard>
-      </label>
-    </fieldset>
+    <div class="tablist" v-bind="elements.tablist.value">
+      <OnyxCard
+        v-for="option in options"
+        :key="option.value"
+        clickable
+        class="tab"
+        v-bind="elements.tab.value({ value: option.value })"
+      >
+        <OnyxIcon v-if="option.icon" :icon="option.icon" class="tab__icon" />
+        {{ option.label }}
+      </OnyxCard>
+    </div>
 
     <template v-for="(slot, slotName) in slots" :key="slotName">
-      <div v-show="selectedOption === slotName">
+      <div v-show="selectedTab === slotName" v-bind="elements.tabpanel.value({ value: slotName })">
         <component :is="slot" />
       </div>
     </template>
@@ -70,50 +60,47 @@ const options = computed(() => {
 </template>
 
 <style lang="scss" scoped>
-.switch {
-  --switch-background: var(--onyx-color-base-background-blank);
-  --switch-background-hover: var(--onyx-color-base-neutral-200);
-  --switch-border-color: var(--onyx-color-component-border-neutral);
-  --switch-color: var(--onyx-color-text-icons-neutral-intense);
-  --switch-icon-color: var(--onyx-color-text-icons-primary-intense);
-  list-style: none;
-  padding: 0;
-  border: none;
-
+.tablist {
   display: flex;
   align-items: center;
   gap: var(--onyx-density-sm);
   flex-wrap: wrap;
   margin-block: var(--onyx-markdown-renderer-margin-block);
+}
 
-  &__option {
-    display: contents;
+.tab {
+  --tab-background: var(--onyx-color-base-background-blank);
+  --tab-background-hover: var(--onyx-color-base-neutral-200);
+  --tab-border-color: var(--onyx-color-component-border-neutral);
+  --tab-color: var(--onyx-color-text-icons-neutral-intense);
+  --tab-icon-color: var(--onyx-color-text-icons-primary-intense);
+
+  flex-grow: 1;
+  flex-direction: row;
+  justify-content: center;
+  cursor: pointer;
+  background-color: var(--tab-background);
+  border-color: var(--tab-border-color);
+  color: var(--tab-color);
+
+  &:hover {
+    background-color: var(--tab-background-hover);
+  }
+
+  &:focus-visible {
+    outline: var(--onyx-outline-width) solid var(--onyx-color-component-focus-primary);
+  }
+
+  &[aria-selected="true"] {
+    --tab-background: var(--onyx-color-base-primary-100);
+    --tab-background-hover: var(--onyx-color-base-primary-200);
+    --tab-border-color: var(--onyx-color-component-border-primary);
+    --tab-color: var(--onyx-color-text-icons-primary-bold);
+    --tab-icon-color: var(--tab-color);
   }
 
   &__icon {
-    color: var(--switch-icon-color);
-  }
-
-  &__card {
-    flex-grow: 1;
-    flex-direction: row;
-    justify-content: center;
-    cursor: pointer;
-    background-color: var(--switch-background);
-    border-color: var(--switch-border-color);
-    color: var(--switch-color);
-
-    &:hover {
-      background-color: var(--switch-background-hover);
-    }
-
-    &:has(input:checked) {
-      --switch-background: var(--onyx-color-base-primary-100);
-      --switch-background-hover: var(--onyx-color-base-primary-200);
-      --switch-border-color: var(--onyx-color-component-border-primary);
-      --switch-color: var(--onyx-color-text-icons-primary-bold);
-      --switch-icon-color: var(--switch-color);
-    }
+    color: var(--tab-icon-color);
   }
 }
 </style>
