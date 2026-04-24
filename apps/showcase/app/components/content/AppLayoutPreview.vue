53000d70887cd8ebfbac4420fe83d5a8d12470a1
Lars Rickert <lars.rickert@digits.schwarz>
2026-04-24 10:54:11 +0200

    docs(showcase): update "Layout" page

---
+++ apps/showcase/app/components/content/AppLayoutPreview.vue
@@ -0,0 +1,67 @@
+<script lang="ts" setup></script>
+
+<template>
+  <OnyxAppLayout>
+    <template #navBar>
+      <OnyxNavBar :app-area="{ link: '#' }" :mobile="false" logo-url="/onyx-logo.svg">
+        <div class="placeholder"></div>
+
+        <template #contextArea>
+          <div class="placeholder"></div>
+        </template>
+      </OnyxNavBar>
+    </template>
+
+    <OnyxPageLayout footer-alignment="page">
+      <div class="placeholder"></div>
+
+      <template #sidebar>
+        <OnyxSidebar class="sidebar" label="Example sidebar" :resizable="false">
+          <div class="sidebar__content">
+            <div class="placeholder"></div>
+          </div>
+        </OnyxSidebar>
+      </template>
+
+      <template #footer>
+        <OnyxBottomBar>
+          <div class="placeholder"></div>
+          <div class="placeholder"></div>
+        </OnyxBottomBar>
+      </template>
+    </OnyxPageLayout>
+  </OnyxAppLayout>
+</template>
+
+<style lang="scss" scoped>
+.onyx-app {
+  width: 100%;
+  height: 100%;
+  border: var(--onyx-1px-in-rem) solid var(--onyx-color-component-border-neutral);
+  border-radius: var(--onyx-radius-md);
+
+  :deep(> *),
+  :deep(.onyx-nav-bar) {
+    border-radius: inherit;
+  }
+}
+
+.sidebar {
+  --onyx-sidebar-width: 10rem;
+
+  &__content {
+    padding: var(--onyx-sidebar-padding);
+    display: flex;
+    flex-direction: column;
+    gap: var(--onyx-density-2xs);
+  }
+}
+
+.placeholder {
+  background-color: var(--onyx-color-base-neutral-200);
+  width: 4rem;
+  max-width: 100%;
+  height: 1lh;
+  border-radius: var(--onyx-radius-component-skeleton);
+}
+</style>
---
+++ apps/showcase/app/components/content/ComponentLayoutPreview.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  align?: "left" | "center";
+}>();
+</script>
+
+<template>
+  <ul :class="['list', { 'list--center': props.align === 'center' }]">
+    <li v-for="i in 5" :key="i" class="list__item">List item</li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.list {
+  list-style: none;
+  padding: var(--onyx-density-xs) 0;
+  border-radius: var(--onyx-radius-md);
+  border: var(--onyx-1px-in-rem) solid var(--onyx-color-base-info-500);
+  max-width: 100%;
+
+  &--center {
+    text-align: center;
+  }
+
+  &__item {
+    padding: var(--onyx-density-xs) var(--onyx-density-md);
+
+    &:first-of-type {
+      background-color: var(--onyx-color-base-info-200);
+      color: var(--onyx-color-text-icons-info-bold);
+    }
+  }
+}
+</style>
