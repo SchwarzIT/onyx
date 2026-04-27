fa833abe31cbae975b52359d921545f63672f0ea
Lars Rickert <lars.rickert@digits.schwarz>
2026-04-27 15:31:00 +0200

    add links

---
+++ apps/showcase/app/components/content/LinkCard.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { iconChevronRightSmall } from "@sit-onyx/icons";
+import type { OnyxCardProps } from "sit-onyx";
+
+const props = defineProps<
+  Required<Pick<OnyxCardProps, "link">> & {
+    headline: string;
+  }
+>();
+</script>
+
+<template>
+  <OnyxCard class="card" :link="props.link">
+    <OnyxHeadline is="h3">{{ props.headline }} </OnyxHeadline>
+    <OnyxIcon :icon="iconChevronRightSmall" />
+  </OnyxCard>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  flex-direction: row;
+  justify-content: space-between;
+
+  &:hover {
+    background-color: var(--onyx-color-base-neutral-200);
+  }
+}
+</style>
