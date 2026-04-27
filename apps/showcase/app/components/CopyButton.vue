09be98895d6c437826326d45e5becd3f9ba6c29d
Lars Rickert <lars.rickert@mail.schwarz>
2026-04-27 09:43:21 +0000

    docs(showcase): update "Animations" page (#5378)
    Relates to #5246
    
    Update the "Animations" page of our showcase.
    Can be reviewed locally on:
    http://localhost:3000/introduction/foundation/animations
    
    ## Checklist
    
    - [x] The added / edited code has been documented with
    [JSDoc](https://jsdoc.app/about-getting-started)
    - [x] I have performed a self review of my code ("Files changed" tab in
    the pull request)
    - [x] All relevant changes are documented in the documentation app
    (folder `apps/docs`) if needed

---
+++ apps/showcase/app/components/CopyButton.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { iconCheckSmall, iconCopy } from "@sit-onyx/icons";
+import { useClipboard } from "@vueuse/core";
+
+const props = defineProps<{
+  /**
+   * Value to copy.
+   */
+  value: string;
+}>();
+
+const { copy, copied } = useClipboard({ source: toRef(props, "value") });
+</script>
+
+<template>
+  <OnyxIcon v-if="copied" :icon="iconCheckSmall" color="success" />
+  <OnyxSystemButton v-else :label="$t('copyToClipboard')" :icon="iconCopy" @click="copy()" />
+</template>
