80ab75fee33c3018680beba014efb0fc95c793fd
Jonathan Carle <jonathan_leo.carle@digits.schwarz>
2026-04-29 11:25:39 +0200

    docs(changeset): feat(useMatrixScreenshotTest): add fastNoIsolation flag for useMatrixScreenshotTest

--- packages/playwright-utils/src/index.ts
+++ packages/playwright-utils/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./emits/index.js";
 export * from "./emits/types.js";
 export * from "./screenshots/index.js";
-export * from "./screenshots/ScreenshotMatrix.jsx";
 export * from "./screenshots/types.js";
