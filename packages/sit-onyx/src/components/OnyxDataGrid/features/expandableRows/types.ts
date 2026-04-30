9495557c674d5d0e81475b1182f5fd94e7104ad7
Lars Rickert <lars.rickert@digits.schwarz>
2026-04-30 10:54:56 +0200

    feat: support `expandedState` option

--- packages/sit-onyx/src/components/OnyxDataGrid/features/expandableRows/types.ts
+++ packages/sit-onyx/src/components/OnyxDataGrid/features/expandableRows/types.ts
@@ -1,11 +1,16 @@
-import type { Component } from "vue";
+import type { Component, Ref } from "vue";
+import type { DataGridEntry } from "../../types.js";
 
 /**
  * The configuration options for the expandable rows feature in the OnyxDataGrid component.
  */
-export type UseExpandableRowsOptions<TEntry> = {
+export type UseExpandableRowsOptions<TEntry extends DataGridEntry> = {
   /**
    * Component to render when a specific row is expanded.
    */
   component: (row: TEntry) => Component;
+  /**
+   * A set of currently expanded rows IDs.
+   */
+  expandedState?: Ref<Set<TEntry["id"]>>;
 };
