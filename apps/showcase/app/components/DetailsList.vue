c119c7fee5c318c51dfb157fff8d95d9a69516b6
Lars Rickert <lars.rickert@digits.schwarz>
2026-05-04 11:26:20 +0200

    docs(showcase): update "Typography" page

---
+++ apps/showcase/app/components/DetailsList.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+export type DetailsItem = {
+  label: string;
+  value: string;
+  copy?: boolean;
+};
+
+const props = defineProps<{
+  items?: DetailsItem[];
+}>();
+</script>
+
+<template>
+  <ul class="list">
+    <li v-for="item in props.items" :key="item.label" class="item">
+      <span class="item__label"> {{ item.label }} </span>
+      <span class="item__value">
+        {{ item.value }}
+        <CopyButton v-if="item.copy" :value="item.value" />
+      </span>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.list {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: var(--onyx-density-xs);
+  list-style: none;
+  padding: 0;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 0 var(--onyx-density-sm);
+  flex-wrap: wrap;
+
+  &__label {
+    color: var(--onyx-color-text-icons-neutral-medium);
+  }
+
+  &__value {
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--onyx-density-2xs);
+  }
+}
+</style>
---
+++ apps/showcase/app/components/content/TypographyCard.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import type { DetailsItem } from "../DetailsList.vue";
+
+const props = defineProps<{
+  items?: DetailsItem[];
+}>();
+
+defineSlots<{
+  /**
+   * Preview content of the given typography.
+   */
+  default(): unknown;
+}>();
+</script>
+
+<template>
+  <OnyxCard class="card onyx-grid-span-4">
+    <div class="card__preview">
+      <slot></slot>
+    </div>
+
+    <DetailsList v-if="props.items?.length" class="card__content" :items="props.items" />
+  </OnyxCard>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  --onyx-card-gap: 0;
+  padding: 0;
+
+  &__preview {
+    padding: var(--onyx-card-padding);
+    background-color: var(--onyx-color-base-background-tinted);
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    min-height: var(--onyx-font-line-height-xl);
+    box-sizing: content-box;
+
+    > * {
+      margin-block: 0;
+    }
+  }
+
+  &__content {
+    padding: var(--onyx-card-padding);
+  }
+}
+</style>
