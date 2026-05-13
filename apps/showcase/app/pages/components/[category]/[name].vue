864fea90d62b63560871780423c1c7b0e2ea9623
Lars Rickert <lars.rickert@digits.schwarz>
2026-05-13 11:24:59 +0200

    show TOC only for overview tab

--- apps/showcase/app/pages/components/[category]/[name].vue
+++ apps/showcase/app/pages/components/[category]/[name].vue
@@ -11,17 +11,17 @@ const activeTab = useRouteQuery("tab", "overview");
 </script>
 
 <template>
-  <TableOfContentsLayout :toc="data.body.toc?.links ?? []">
-    <ProseH1>{{ data.title }}</ProseH1>
+  <ProseH1>{{ data.title }}</ProseH1>
 
-    <OnyxTabs v-model="activeTab" :label="$t('components.details')">
-      <OnyxTab :label="$t('components.overview')" value="overview">
+  <OnyxTabs v-model="activeTab" :label="$t('components.details')">
+    <OnyxTab :label="$t('components.overview')" value="overview">
+      <TableOfContentsLayout :toc="data.body.toc?.links ?? []">
         <ContentRenderer :value="data" />
-      </OnyxTab>
+      </TableOfContentsLayout>
+    </OnyxTab>
 
-      <OnyxTab :label="$t('components.property', 2)" value="properties">
-        <ComponentMeta :component="data.componentName" />
-      </OnyxTab>
-    </OnyxTabs>
-  </TableOfContentsLayout>
+    <OnyxTab :label="$t('components.property', 2)" value="properties">
+      <ComponentMeta :component="data.componentName" />
+    </OnyxTab>
+  </OnyxTabs>
 </template>
