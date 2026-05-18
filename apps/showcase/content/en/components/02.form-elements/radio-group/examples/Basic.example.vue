1b58fc51c1e4e5075339a6c18d3908310df2053d
Lars Rickert <lars.rickert@digits.schwarz>
2026-05-18 08:22:35 +0200

    docs(showcase): add "Radio group" examples

---
+++ apps/showcase/content/en/components/02.form-elements/radio-group/examples/Basic.example.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { OnyxRadioGroup, RadioButtonOption } from "sit-onyx";
+import { ref } from "vue";
+
+const selectedOption = ref<number>();
+
+const options = [
+  { label: "Default", value: 1 },
+  { label: "Initially checked", value: 2 },
+  { label: "Required", value: 3, required: true },
+  { label: "Disabled", value: 4, disabled: true },
+  { label: "Loading", value: 5, loading: true },
+] satisfies RadioButtonOption[];
+</script>
+
+<template>
+  <OnyxRadioGroup v-model="selectedOption" label="Example label" :options />
+</template>
