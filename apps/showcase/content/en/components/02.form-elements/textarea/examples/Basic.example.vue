2da617080f41647628cef720a728baa5bc5f8346
Lars Rickert <lars.rickert@digits.schwarz>
2026-05-22 13:18:11 +0200

    add textarea examples

---
+++ apps/showcase/content/en/components/02.form-elements/textarea/examples/Basic.example.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { OnyxTextarea } from "sit-onyx";
+import { ref } from "vue";
+
+const value = ref("");
+</script>
+
+<template>
+  <OnyxTextarea v-model="value" label="Example label" placeholder="Placeholder..." required />
+</template>
---
+++ apps/showcase/content/en/components/02.form-elements/textarea/examples/Readonly.example.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { OnyxTextarea } from "sit-onyx";
+import { ref } from "vue";
+
+const value = ref("Example value");
+</script>
+
+<template>
+  <OnyxTextarea v-model="value" label="Readonly" readonly />
+  <OnyxTextarea v-model="value" label="Disabled" disabled />
+</template>
