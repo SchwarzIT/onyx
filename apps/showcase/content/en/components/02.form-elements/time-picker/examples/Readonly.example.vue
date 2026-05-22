cc34fddd260c578ece70124af9fed8e1d08ced11
Lars Rickert <lars.rickert@digits.schwarz>
2026-05-22 13:53:55 +0200

    add time picker examples

---
+++ apps/showcase/content/en/components/02.form-elements/time-picker/examples/Readonly.example.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { OnyxUnstableTimePicker } from "sit-onyx";
+import { ref } from "vue";
+
+const time = ref<string>("09:00");
+</script>
+
+<template>
+  <OnyxUnstableTimePicker v-model="time" label="Readonly" readonly />
+  <OnyxUnstableTimePicker v-model="time" label="Disabled" disabled />
+</template>
