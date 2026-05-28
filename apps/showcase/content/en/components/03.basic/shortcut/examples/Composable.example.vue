dfdcec5fe5f00e60e87eafb57792100a850cc01f
Lars Rickert <lars.rickert@digits.schwarz>
2026-05-28 10:01:50 +0200

    update example

--- apps/showcase/content/en/components/03.basic/shortcut/examples/Composable.example.vue
+++ apps/showcase/content/en/components/03.basic/shortcut/examples/Composable.example.vue
@@ -6,6 +6,7 @@ const toast = useToast();
 _unstableUseShortcut({
   sequence: [{ all: ["Shift", "7"] }],
   onComplete: () => {
+    // your logic here when the full shortcut is pressed...
     toast.show({
       headline: "Successfully pressed shortcut",
       description: "This shortcut was triggered using the shortcut composable",
