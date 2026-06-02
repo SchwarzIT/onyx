108732c81041e74bd08440beb406346bf856ee1b
ChristianBusshoff <christian.busshoff@digits.schwarz>
2026-06-02 10:16:08 +0200

    change

--- packages/sit-onyx/src/index.ts
+++ packages/sit-onyx/src/index.ts
@@ -329,7 +329,6 @@ export { createOnyx } from "./utils/plugin.js";
 
 export * from "./components/illustrations/index.js";
 export * from "./composables/themeTransition.js";
-export { useMoreList, useMoreListChild } from "./composables/useMoreList.js";
 export { useResizeObserver } from "./composables/useResizeObserver.js";
 export * from "./utils/attrs.js";
 export * from "./utils/props.js";
