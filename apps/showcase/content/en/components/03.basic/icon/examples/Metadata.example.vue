fe51fa6de4f92e95ccce1821e86ee003c586616d
Lars Rickert <lars.rickert@digits.schwarz>
2026-06-02 08:59:31 +0200

    update icon and flag metadata

--- apps/showcase/content/en/components/03.basic/icon/examples/Metadata.example.vue
+++ apps/showcase/content/en/components/03.basic/icon/examples/Metadata.example.vue
@@ -1,15 +1,22 @@
 <script lang="ts" setup>
-import { ICON_METADATA } from "@sit-onyx/icons/utils";
-import { OnyxCodeTab, OnyxCodeTabs } from "sit-onyx";
+import * as ALL_ICONS from "@sit-onyx/icons";
+import { ICON_METADATA, getIconImportName } from "@sit-onyx/icons/utils";
+import { OnyxSelect, SelectOption } from "sit-onyx";
+import { ref } from "vue";
+
+const value = ref<string>("birthday-cake");
+
+const options = Object.entries(ICON_METADATA).map<SelectOption<string>>(([iconName, metadata]) => {
+  const icon = ALL_ICONS[getIconImportName(iconName) as keyof typeof ALL_ICONS];
+  return {
+    label: iconName,
+    group: metadata.category,
+    value: iconName,
+    icon,
+  };
+});
 </script>
 
 <template>
-  <OnyxCodeTabs model-value="code">
-    <OnyxCodeTab
-      value="code"
-      label='Metadata for "bread" icon'
-      :code="JSON.stringify(ICON_METADATA.bread, null, 2)"
-      language="json"
-    />
-  </OnyxCodeTabs>
+  <OnyxSelect v-model="value" label="Icon" list-label="Available icons" :options with-search />
 </template>
