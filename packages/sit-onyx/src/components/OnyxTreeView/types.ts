a3c4a78e4e789a7a2fd662b2f366ac3dfe9c575d
ChristianBusshoff <christian.busshoff@digits.schwarz>
2026-06-09 10:34:10 +0200

    resolve comments

--- packages/sit-onyx/src/components/OnyxTreeView/types.ts
+++ packages/sit-onyx/src/components/OnyxTreeView/types.ts
@@ -1,3 +1,10 @@
 import type { DensityProp } from "../../composables/density.js";
 
-export type OnyxTreeViewProps = DensityProp;
+export const TREE_DEPTH_INJECTION_KEY = Symbol();
+
+export type OnyxTreeViewProps = DensityProp & {
+  /**
+   * Aria label for the TreeView.
+   */
+  label: string;
+};
