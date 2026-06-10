1ab9a31031fe85c33f4ecc784ee5f2f3aa603c7e
Lars Rickert <lars.rickert@digits.schwarz>
2026-06-10 14:05:44 +0200

    optimize search render performance

--- apps/showcase/app/components/IconAssetOverview.vue
+++ apps/showcase/app/components/IconAssetOverview.vue
@@ -2,14 +2,37 @@
 import { normalizedIncludes } from "sit-onyx";
 
 export type IconAssetGroup = {
+  /**
+   * Group / category name.
+   */
   category: string;
+  /**
+   * List of all icons in the group.
+   */
   icons: IconAsset[];
+  /**
+   * Whether the group is currently hidden due to search.
+   */
+  hidden?: boolean;
 };
 
 export type IconAsset = {
+  /**
+   * Asset name.
+   */
   name: string;
+  /**
+   * Icon SVG content.
+   */
   content: string;
+  /**
+   * Optional alias names to include in the search.
+   */
   aliases?: string[];
+  /**
+   * Whether the asset is currently hidden due to search.
+   */
+  hidden?: boolean;
 };
 
 const props = defineProps<{
@@ -36,19 +59,24 @@ const filteredGroups = computed(() => {
   const searchTerm = search.value.trim();
   if (!searchTerm) return props.groups;
 
-  return props.groups
-    .map((group) => {
+  // to optimize performance, we do not actually filter out icons that do not match the search
+  // but instead just visually hide them in the HTML so we do not re-render hundredths of icons when changing the search
+  return props.groups.map((group) => {
+    const icons = group.icons.map((icon) => {
       return {
-        ...group,
-        icons: group.icons.filter((icon) => {
-          return (
-            normalizedIncludes(icon.name, searchTerm) ||
-            icon.aliases?.some((alias) => normalizedIncludes(alias, searchTerm))
-          );
-        }),
+        ...icon,
+        hidden:
+          !normalizedIncludes(icon.name, searchTerm) &&
+          !icon.aliases?.some((alias) => normalizedIncludes(alias, searchTerm)),
       };
-    })
-    .filter((group) => group.icons.length > 0);
+    });
+
+    return {
+      ...group,
+      hidden: icons.every((icon) => icon.hidden),
+      icons,
+    };
+  });
 });
 </script>
 
@@ -58,29 +86,34 @@ const filteredGroups = computed(() => {
       <OnyxUnstableSearch v-model="search" class="onyx-grid-span-8" with-shortcut />
     </div>
 
-    <OnyxEmpty v-if="!filteredGroups.length">
+    <OnyxEmpty v-if="filteredGroups.every((group) => group.hidden)">
       {{ $t("icons.empty") }}
     </OnyxEmpty>
 
-    <div v-for="group in filteredGroups" :key="group.category" class="overview__group">
-      <OnyxHeadline is="h3" class="headline">
-        {{ group.category }}
-        <span class="headline__count">({{ group.icons.length }})</span>
-      </OnyxHeadline>
+    <template v-for="group in filteredGroups" :key="group.category">
+      <div v-show="!group.hidden" class="overview__group">
+        <OnyxHeadline is="h3" class="headline">
+          {{ group.category }}
+          <span class="headline__count">
+            ({{ group.icons.filter((icon) => !icon.hidden).length }})
+          </span>
+        </OnyxHeadline>
 
-      <div class="onyx-grid">
-        <IconPreviewCard
-          v-for="icon in group.icons"
-          :key="icon.name"
-          class="onyx-grid-span-2"
-          :icon="icon.content"
-          :name="icon.name"
-          :get-code-import="props.getCodeImport"
-        >
-          <slot :icon></slot>
-        </IconPreviewCard>
+        <div class="onyx-grid">
+          <template v-for="icon in group.icons" :key="icon.name">
+            <IconPreviewCard
+              v-show="!icon.hidden"
+              class="onyx-grid-span-2"
+              :icon="icon.content"
+              :name="icon.name"
+              :get-code-import="props.getCodeImport"
+            >
+              <slot :icon></slot>
+            </IconPreviewCard>
+          </template>
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
