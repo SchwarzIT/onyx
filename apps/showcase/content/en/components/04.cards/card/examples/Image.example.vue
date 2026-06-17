c9e5b04cc94dd1fb2e4fe1db8c013aaa82360e8d
Lars Rickert <lars.rickert@digits.schwarz>
2026-06-17 15:31:21 +0200

    docs: add "Card" examples

---
+++ apps/showcase/content/en/components/04.cards/card/examples/Image.example.vue
@@ -0,0 +1,87 @@
+<script lang="ts" setup>
+import {
+  OnyxButton,
+  OnyxCard,
+  OnyxFlyoutMenu,
+  OnyxHeadline,
+  OnyxImage,
+  OnyxMenuItem,
+  OnyxTag,
+} from "sit-onyx";
+</script>
+
+<template>
+  <OnyxCard class="card">
+    <OnyxImage
+      class="card__image"
+      alt="Example image"
+      src="https://picsum.photos/320/128"
+      :height="128"
+      :width="320"
+      shape="rounded"
+    />
+
+    <div class="card__header">
+      <div>
+        <div class="card__category onyx-text--small">Category name</div>
+        <OnyxHeadline is="h2">Example headline</OnyxHeadline>
+      </div>
+
+      <OnyxFlyoutMenu label="Actions" trigger="click">
+        <template #options>
+          <OnyxMenuItem>Action 1</OnyxMenuItem>
+          <OnyxMenuItem>Action 2</OnyxMenuItem>
+          <OnyxMenuItem>Action 3</OnyxMenuItem>
+        </template>
+      </OnyxFlyoutMenu>
+    </div>
+
+    <p class="card__description">
+      Lorem ipsum dolor sit amet consectetur. Id neque viverra faucibus ullamcorper dui volutpat.
+      Vel nec aliquet lorem turpis eu dui. At pellentesque senectus sed volutpat vitae nulla. Nisl
+      cursus dignissim sed eget neque tristique interdum pretium elit.
+    </p>
+
+    <div class="card__tags">
+      <OnyxTag v-for="i in 4" :key="i" label="Tag" color="neutral" />
+    </div>
+
+    <OnyxButton class="card__button" label="Button" />
+  </OnyxCard>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  --onyx-card-gap: var(--onyx-density-md);
+
+  &__image {
+    width: 100%;
+  }
+
+  &__category {
+    color: var(--onyx-color-text-icons-neutral-medium);
+  }
+
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: var(--onyx-card-gap);
+  }
+
+  &__description {
+    color: var(--onyx-color-text-icons-neutral-medium);
+  }
+
+  &__tags {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: var(--onyx-density-xs);
+  }
+
+  &__button {
+    width: 100%;
+  }
+}
+</style>
