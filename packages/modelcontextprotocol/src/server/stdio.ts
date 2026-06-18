c2540bdd51827a42842d5c31341944468b0bfb6e
Jonathan Carle <jonathan_leo.carle@digits.schwarz>
2026-06-18 17:11:04 +0200

    docs(changeset): fix: http server only accepting first connection

--- packages/modelcontextprotocol/src/server/stdio.ts
+++ packages/modelcontextprotocol/src/server/stdio.ts
@@ -6,8 +6,8 @@ import { error } from "node:console";
  * MCP server running via stdio.
  * All logging has to use stderr, otherwise the logging to stdio will break the transport.
  */
-export const run = async (server: McpServer) => {
+export const run = async (getServer: () => McpServer) => {
   const transport = new StdioServerTransport();
-  await server.connect(transport);
+  await getServer().connect(transport);
   error("MCP Server running on stdio.");
 };
