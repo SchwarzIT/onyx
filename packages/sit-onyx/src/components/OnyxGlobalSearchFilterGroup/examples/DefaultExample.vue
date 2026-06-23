440a990242b8007990ca066e4f38fbcea59b6946
ChristianBusshoff <christian.busshoff@digits.schwarz>
2026-06-23 08:46:50 +0200

    implement component

---
+++ packages/sit-onyx/src/components/OnyxGlobalSearchFilterGroup/examples/DefaultExample.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { OnyxUnstableGlobalSearchFilterGroup } from "../../../index.js";
+import OnyxFilterBadge from "../../OnyxFilterBadge/OnyxFilterBadge.vue";
+</script>
+
+<template>
+  <OnyxUnstableGlobalSearchFilterGroup label="Filters">
+    <OnyxFilterBadge label="Filter 1" />
+    <OnyxFilterBadge label="Filter 2" />
+    <OnyxFilterBadge label="Filter 3" />
+  </OnyxUnstableGlobalSearchFilterGroup>
+</template>
