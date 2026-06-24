ef2322b7a8e833506e44c7cde2330e7260b4feff
Lars Rickert <lars.rickert@digits.schwarz>
2026-06-24 16:22:47 +0200

    add table examples

---
+++ apps/showcase/content/en/components/05.data/table/examples/ColumnGroups.example.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { OnyxTable, TableColumnGroup } from "sit-onyx";
+
+const data = [
+  { fruit: "Strawberry", price: 4.5, weight: 200, quantity: 100, rating: 5 },
+  { fruit: "Apple", price: 1.99, weight: 3_000, quantity: 200, rating: 3 },
+  { fruit: "Banana", price: 3.75, weight: 18_000, quantity: 300, rating: 4 },
+];
+
+const columnGroups: TableColumnGroup[] = [
+  { key: "general", header: "General", span: 2 },
+  { key: "inventory", header: "Inventory", span: 2 },
+  { key: "rest", span: 1 },
+];
+</script>
+
+<template>
+  <OnyxTable :column-groups with-vertical-borders>
+    <template #head>
+      <tr>
+        <th>Fruit</th>
+        <th>Price (€/kg)</th>
+        <th>Inventory (kg)</th>
+        <th>Inventory (pieces)</th>
+        <th>Rating</th>
+      </tr>
+    </template>
+
+    <tr v-for="row in data" :key="row.fruit">
+      <td>{{ row.fruit }}</td>
+      <td>{{ row.price }}</td>
+      <td>{{ row.weight }}</td>
+      <td>{{ row.quantity }}</td>
+      <td>{{ row.rating }}</td>
+    </tr>
+  </OnyxTable>
+</template>
