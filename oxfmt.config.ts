a0da34ae0ae795ff1797925e4731e1a4d5db313f
Lars Rickert <lars.rickert@digits.schwarz>
2026-06-24 07:24:48 +0200

    update config

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "oxfmt";
 
 export default defineConfig({
-  // sortImports: true,
-  // jsdoc: true,
   ignorePatterns: [
     // prettier can not handle the MDC syntax with YAML properties for components
     // see: https://github.com/nuxt/content/issues/1709
