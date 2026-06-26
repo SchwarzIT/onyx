b7cfa316e80facf6af13e7f21aa2ed88627400cd
Jonathan Carle <jonathan_leo.carle@digits.schwarz>
2026-06-26 13:57:12 +0200

    add skills and support writing skill files

--- packages/modelcontextprotocol/src/index.ts
+++ packages/modelcontextprotocol/src/index.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import { log } from "node:console";
+import { stat } from "node:fs/promises";
 import { parseArgs, type ParseArgsOptionsConfig } from "node:util";
 import packageJson from "../package.json" with { type: "json" };
+import { writeSkillFiles } from "./resources/skills.js";
 import { run as http } from "./server/http.js";
 import { createServer } from "./server/server.js";
 import { run as stdio } from "./server/stdio.js";
@@ -25,6 +27,10 @@ if (import.meta.main) {
       short: "r",
       default: false,
     },
+    writeSkills: {
+      type: "string",
+      short: "w",
+    },
     help: {
       type: "boolean",
       short: "h",
@@ -38,7 +44,7 @@ if (import.meta.main) {
   } satisfies ParseArgsOptionsConfig;
 
   const {
-    values: { transport, version, help, resourcesAsTools },
+    values: { transport, version, help, resourcesAsTools, writeSkills },
   } = parseArgs({
     args: process.argv.slice(2),
     options,
@@ -54,15 +60,23 @@ Usage:
     onyx-mcp [options]
 
 Options:
- -t, --transport <stdio|http> Which kind of MCP server should be started (default: stdio). 
-                              The "http" transport considers PORT and HOST environment variables, when starting the server.
- -r, --resourcesAsTools       Some LLM Coding Assistants (e.g. Gemini) are not able to to use MCP resources (yet). 
-                              This setting makes resources also available as tools to support these Coding Assistants.
- -h, --help                   Show this help text and quit.
- -v, --version                Show version number and quit.`);
+ -t, --transport <"stdio"|"http"> Which kind of MCP server should be started (default: stdio). 
+                                  The "http" transport considers PORT and HOST environment variables, when starting the server.
+ -r, --resourcesAsTools           Some LLM Coding Assistants (e.g. Gemini) are not able to to use MCP resources (yet). 
+                                  This setting makes resources also available as tools to support these Coding Assistants.
+ -w, --write-skills <directory>     Write resources to SKILL.md files in the specified directory.
+ -h, --help                       Show this help text and quit.
+ -v, --version                    Show version number and quit.`);
     process.exit(0);
   } else if (version) {
     log(packageJson.version);
+    process.exit(0);
+  } else if (writeSkills) {
+    const stats = await stat(writeSkills);
+    if (!stats.isDirectory()) {
+      throw new Error(`${writeSkills} is not a directory`);
+    }
+    await writeSkillFiles(writeSkills);
     process.exit(0);
   } else {
     if (!Object.keys(SUPPORTED_TRANSPORTS).includes(transport)) {
--- packages/modelcontextprotocol/src/resources/skills.ts
+++ packages/modelcontextprotocol/src/resources/skills.ts
@@ -1,30 +1,39 @@
-import { ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { log } from "node:console";
+import { mkdir, writeFile } from "node:fs/promises";
+import { resolve } from "node:path";
 import type { SkillMarkdown } from "../../env.js";
 import type { RegisterableResource } from "../types.js";
 
-const skills = import.meta.glob<SkillMarkdown>("./skills/*.md", { eager: true });
+const skills = import.meta.glob<SkillMarkdown>("./skills/**/SKILL.md", { eager: true });
 
-export const allSkills = Object.values(skills).map<RegisterableResource>(
-  ({ default: text, frontmatter: { description, name } }) => {
-    return [
-      name,
-      new ResourceTemplate(`skills://${name}/{version}`, {
-        list: undefined,
-      }),
-      {
-        title: name,
-        description,
-        mimeType: "text/markdown",
-      },
-      async (uri, { version: _version }) => ({
-        contents: [
-          {
-            uri: uri.href,
-            text,
-            mimeType: "text/markdown",
-          },
-        ],
-      }),
-    ];
-  },
+export const allSkills = Object.values(skills).map<RegisterableResource<false>>(
+  ({ default: text, frontmatter: { description, name } }) => [
+    name,
+    `sit-onyx://skill/${name}`,
+    {
+      title: name,
+      description,
+      mimeType: "text/markdown",
+    },
+    (uri: URL) => ({
+      contents: [
+        {
+          uri: uri.href,
+          text,
+          mimeType: "text/markdown",
+        },
+      ],
+    }),
+  ],
 );
+
+export const writeSkillFiles = async (skillsDirectory: string) => {
+  const promises = Object.values(skills).map(async (skill) => {
+    const directory = resolve(skillsDirectory, skill.frontmatter.name);
+    await mkdir(directory, { recursive: true });
+    const file = resolve(directory, "SKILL.md");
+    await writeFile(file, skill.raw);
+    log(`Skill ${skill.frontmatter.name} written to ${directory}`);
+  });
+  return Promise.all(promises);
+};
