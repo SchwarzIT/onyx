25d7425f3ccd0a7f174793a6b398d95100fe5886
ChristianBusshoff <christian.busshoff@digits.schwarz>
2026-07-07 07:10:42 +0200

    change

--- packages/sit-onyx/src/components/OnyxSelectDialog/OnyxSelectDialog.vue
+++ packages/sit-onyx/src/components/OnyxSelectDialog/OnyxSelectDialog.vue
@@ -155,10 +155,10 @@ const handleApply = () => {
         &:hover {
           border-color: var(--onyx-color-component-border-primary);
         }
-
+        .onyx-text--small,
         .onyx-select-dialog__label,
         .onyx-select-dialog__icon {
-          color: var(--onyx-color-text-icons-primary-intense);
+          color: var(--onyx-color-text-icons-neutral-intense);
         }
       }
     }
