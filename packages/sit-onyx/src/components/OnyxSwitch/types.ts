decf131ffe5cfdf5fab685d6dcd484a337e72f61
ChristianBusshoff <christian.busshoff@digits.schwarz>
2026-07-20 23:34:39 +0200

    implement formElementV2 inside switch

--- packages/sit-onyx/src/components/OnyxSwitch/types.ts
+++ packages/sit-onyx/src/components/OnyxSwitch/types.ts
@@ -1,4 +1,5 @@
 import type { BaseSelectOption, Nullable, SelectOptionValue } from "../../types/index.js";
+import type { FormElementV2LabelOptions } from "../OnyxFormElementV2/types.js";
 
 export type OnyxSwitchProps<TValue extends SelectOptionValue = SelectOptionValue> = Omit<
   BaseSelectOption<TValue>,
@@ -8,4 +9,8 @@ export type OnyxSwitchProps<TValue extends SelectOptionValue = SelectOptionValue
    * Whether the switch should be checked or not.
    */
   modelValue?: Nullable<boolean>;
+  /**
+   * Optional top-level form label for the switch container.
+   */
+  descriptionLabel?: string | FormElementV2LabelOptions;
 };
