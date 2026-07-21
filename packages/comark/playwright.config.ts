404874e774999306ab6baba07bca8b4fb2e5f3ca
Jonathan Carle <jonathan_leo.carle@digits.schwarz>
2026-07-21 16:32:51 +0200

    docs(changeset): feat: Initial implementation of @sit-onyx/comark

---
+++ packages/comark/playwright.config.ts
@@ -0,0 +1,6 @@
+import { defineOnyxPlaywrightConfigCT } from "@sit-onyx/shared/playwright.config.base";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineOnyxPlaywrightConfigCT();
---
+++ packages/comark/src/components/prose/ProseUl.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineSlots<{
+  default(): unknown;
+}>();
+</script>
+
+<template>
+  <ul>
+    <slot></slot>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+ul {
+  margin-block: var(--onyx-density-xs);
+}
+</style>
