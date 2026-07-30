18888f0b3fcf02da7402c7630e1d5ee7e2b6e803
Jonathan Carle <jonathan_leo.carle@digits.schwarz>
2026-07-30 10:22:42 +0200

    docs(changeset): fix(useAutofocus): autofocus is not performed when the target element is not visible or any other input element still has focus

--- packages/sit-onyx/src/composables/useAutoFocus.ts
+++ packages/sit-onyx/src/composables/useAutoFocus.ts
@@ -1,5 +1,25 @@
 import { onMounted, watch, type Ref } from "vue";
+import type { Nullable } from "../types/utils.js";
+import { userConsole } from "../utils/console.js";
 
+const anyInputFocused = () => {
+  if (!document.activeElement) {
+    return false;
+  }
+  const { isContentEditable, tagName } = document.activeElement as HTMLElement;
+  const isInputOrTextarea = ["INPUT", "TEXTAREA"].includes(tagName);
+  return isInputOrTextarea || isContentEditable;
+};
+
+/**
+ * Performs manual autofocus on the target, when the component is mounted and not loading anymore.
+ * Autofocus will not be performed if any other input element is already focused or the target
+ * element is not visible.
+ *
+ * @param ref The ref of the target element.
+ * @param props The reactive props of the component.
+ * @returns
+ */
 export const useAutofocus = (
   ref: Ref<HTMLElement | HTMLElement[] | null>,
   props: { autofocus: boolean; loading?: boolean },
@@ -9,8 +29,32 @@ export const useAutofocus = (
   }
 
   const performAutoFocus = () => {
-    const input = Array.isArray(ref) ? ref[0] : ref;
-    input.value?.focus();
+    const elem: Ref<Nullable<HTMLElement>> = Array.isArray(ref) ? ref[0] : ref;
+    if (elem.value === document.activeElement) {
+      return;
+    }
+    const isVisible = elem.value?.checkVisibility({
+      checkOpacity: true,
+      checkVisibilityCSS: true,
+      contentVisibilityAuto: true,
+    });
+    if (!isVisible) {
+      return userConsole?.warn(
+        "Did not perform autofocus on Element ",
+        elem.value,
+        ". The element is not visible!",
+      );
+    }
+    if (anyInputFocused()) {
+      return userConsole?.warn(
+        "Did not perform autofocus on Element ",
+        elem.value,
+        ". Because the ",
+        document.activeElement,
+        " is already focused!",
+      );
+    }
+    elem.value?.focus();
   };
 
   onMounted(() => {
