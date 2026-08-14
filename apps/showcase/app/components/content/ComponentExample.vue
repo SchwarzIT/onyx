4dc58cc11a46d520709e6b38e0ada67b52ad1bb0
Lars Rickert <lars.rickert@digits.schwarz>
2026-08-14 11:00:21 +0000

    docs: update notification center example (#6027)
    Relates to #5499
    
    Implement UX feedback for notification center example:
    - show notifications only once
    - increase example height
    - update button label + icon

--- apps/showcase/app/components/content/ComponentExample.vue
+++ apps/showcase/app/components/content/ComponentExample.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { iconUndo } from "@sit-onyx/icons";
+import { createNotificationsProvider, NOTIFICATIONS_PROVIDER_INJECTION_KEY } from "sit-onyx";
 import type { Component } from "vue";
 import type { ComponentExampleOptions } from "../ComponentExampleOptions.vue";
 
@@ -20,6 +21,11 @@ const props = withDefaults(
      * The orientation of the example if multiple components are used.
      */
     orientation?: "horizontal" | "vertical";
+    /**
+     * Whether to override specific onyx provide/inject keys.
+     * Useful if e.g. notifications should not be shown globally inside the showcase itself.
+     */
+    provide?: { notifications?: boolean };
   }>(),
   {
     layout: "default",
@@ -88,6 +94,11 @@ const options = ref<ComponentExampleOptions>({});
 
 defineOptions({ inheritAttrs: false });
 const attrs = useAttrs();
+
+if (props.provide?.notifications) {
+  const provider = createNotificationsProvider();
+  provide(NOTIFICATIONS_PROVIDER_INJECTION_KEY, provider);
+}
 </script>
 
 <template>
@@ -185,6 +196,7 @@ const attrs = useAttrs();
     .onyx-app {
       width: 100%;
       height: 100%;
+      min-height: 32rem;
       background-color: var(--onyx-color-base-background-tinted);
       border: var(--onyx-1px-in-rem) solid var(--onyx-color-component-border-neutral);
       border-radius: var(--onyx-radius-md);
