5dc7101619ec1a22e72ddd0def66beaf6683aaf6
Lars Rickert <lars.rickert@digits.schwarz>
2026-08-18 14:46:19 +0200

    implement headline examples

---
+++ apps/showcase/content/en/components/08.navigation/headline/examples/Types.example.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { OnyxHeadline } from "sit-onyx";
+</script>
+
+<template>
+  <OnyxHeadline is="h1">Headline 1</OnyxHeadline>
+  <OnyxHeadline is="h2">Headline 2</OnyxHeadline>
+  <OnyxHeadline is="h3">Headline 3</OnyxHeadline>
+  <OnyxHeadline is="h4">Headline 4</OnyxHeadline>
+  <OnyxHeadline is="h5">Headline 5</OnyxHeadline>
+  <OnyxHeadline is="h6">Headline 6</OnyxHeadline>
+</template>
