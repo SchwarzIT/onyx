4aacf3702af1fac682e02b18fc1a1edf26a6ced4
Lars Rickert <lars.rickert@digits.schwarz>
2026-08-20 14:08:43 +0200

    docs: add examples for nav item, nav button, user menu and timer

---
+++ apps/showcase/content/en/components/08.navigation/nav-item/index.md
@@ -0,0 +1,14 @@
+---
+title: Nav item
+componentName: OnyxNavItem
+---
+
+The nav item is used to build the [navigation bar](/components/navigation/nav-bar) component and not intended to be used standalone.
+
+## Examples
+
+<div class="onyx-grid">
+<link-card class="onyx-grid-span-4" headline="Nav bar documentation" link="/components/navigation/nav-bar">
+Please refer to the nav bar component for examples on how to use nav items.
+</link-card>
+</div>
---
+++ apps/showcase/content/en/components/08.navigation/user-menu/examples/Basic.example.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { flagUS, flagDE } from "@sit-onyx/flags";
+import { iconLogout, iconSettings } from "@sit-onyx/icons";
+import {
+  ColorSchemeValue,
+  OnyxColorSchemeMenuItem,
+  OnyxLanguageMenuItem,
+  OnyxMenuItem,
+  OnyxUserMenu,
+  SelectDialogOption,
+} from "sit-onyx";
+import { ref } from "vue";
+
+const colorScheme = ref<ColorSchemeValue>("auto");
+const locale = ref("en");
+
+const languages: SelectDialogOption[] = [
+  {
+    label: "English",
+    value: "en",
+    icon: flagUS,
+  },
+  {
+    label: "Deutsch",
+    value: "de",
+    icon: flagDE,
+  },
+];
+
+const handleLogout = () => {
+  // your logout logic here...
+};
+</script>
+
+<template>
+  <OnyxUserMenu full-name="Jane Doe" description="Company Name">
+    <OnyxMenuItem label="Settings" :icon="iconSettings" link="/settings" />
+    <OnyxColorSchemeMenuItem v-model="colorScheme" />
+    <OnyxLanguageMenuItem v-model="locale" :options="languages" />
+    <OnyxMenuItem label="Logout" :icon="iconLogout" color="danger" @click="handleLogout" />
+
+    <template #footer>
+      App version
+      <span class="onyx-text--monospace">1.0.0</span>
+    </template>
+  </OnyxUserMenu>
+</template>
